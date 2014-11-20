44674a68368b5c4dbde76f0194c1f394df824708
Gabriel Farrell <g@gsf.io>
2014-11-20 16:36:33 -0500

    Mysql should use less memory

--- Vagrantfile
+++ Vagrantfile
@@ -3,9 +3,6 @@ Vagrant.configure("2") do |config|
   config.vm.provider "lxc" do |lxc, override|
     override.vm.box = "fgrehm/trusty64-lxc"
   end
-  config.vm.provider "virtualbox" do |v|
-    v.memory = 1024
-  end
   config.vm.network "forwarded_port", guest: 80, host: 8080
   config.vm.network "forwarded_port", guest: 8000, host: 8000
   config.vm.provision "shell", path: "bootstrap.sh"
--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,11 @@
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
 # Don't apt-get upgrade http://stackoverflow.com/a/15093460/589391
+
+# Need to lower mysql connections before install to limit memory use
+mkdir -p /etc/mysql/conf.d
+echo -e '[mysqld]\nmax_connections = 20' > /etc/mysql/conf.d/low_connect.cnf
+
 apt-get install -y mysql-server-5.6 nginx php5-cli php5-fpm
 
 echo 'Configuring mysql...'
