87e204ada50173325e53348fc8d99db2db6b1b71
Gabriel Farrell <g@gsf.io>
2014-11-25 11:35:00 -0500

    Slim config comments

--- wp-config.php
+++ wp-config.php
@@ -2,21 +2,9 @@
 /**
  * The base configurations of the WordPress.
  *
- * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
- *
- * This file is used by the wp-config.php creation script during the
- * installation. You don't have to use the web site, you can just copy this file
- * to "wp-config.php" and fill in the values.
+ * For all getenv() vars set fastcgi_param values in nginx.
  *
  * @package WordPress
- */
-
-/**
- * For all getenv() vars set fastcgi_param values in nginx
- *
  */
 
 /** For Composer-driven autoload. See http://composer.rarst.net/recipe/site-stack */
