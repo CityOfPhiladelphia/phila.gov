a941cbb30d154bb497901d5c6aee1953a932442a
Gabriel Farrell <g@gsf.io>
2014-12-02 10:32:31 -0500

    Add db dump and import on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,12 +53,15 @@ echo 'Installing composer...'
 curl -sS https://getcomposer.org/download/1.0.0-alpha8/composer.phar > /usr/local/bin/composer
 chmod 755 /usr/local/bin/composer
 
+echo 'Install php components with composer...'
+cd /vagrant
+su vagrant -c 'composer install'
+
 echo 'Installing wp-cli...'
 curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
 chmod 755 /usr/local/bin/wp
 
-echo 'Install php components with composer...'
-cd /vagrant
-su vagrant -c 'composer install'
+echo 'Importing fixture data...'
+su vagrant -c 'wp db import'
 
 echo 'Wordpress should be up and running at http://localhost:19102'
