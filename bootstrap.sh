4d45ebf50ae5dad0adde97a800677371700fb66d
Gabriel Farrell <g@gsf.io>
2014-12-04 13:07:12 -0500

    Better bootstrap order and logging

--- bootstrap.sh
+++ bootstrap.sh
@@ -53,16 +53,16 @@ echo 'Installing composer...'
 curl -sS https://getcomposer.org/download/1.0.0-alpha8/composer.phar > /usr/local/bin/composer
 chmod 755 /usr/local/bin/composer
 
-# Switch to /vagrant and make it the automatic login directory
+echo 'Installing wp-cli...'
+curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
+chmod 755 /usr/local/bin/wp
+
+echo 'Switching to /vagrant and making that automatic for ssh...'
 cd /vagrant
 su vagrant -c 'echo "cd /vagrant" >> /home/vagrant/.bashrc'
 
 echo 'Install php components with composer...'
 su vagrant -c 'composer install'
-
-echo 'Installing wp-cli...'
-curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
-chmod 755 /usr/local/bin/wp
 
 echo 'Importing configuration...'
 su vagrant -c 'wp db import'
