334b92a64c96f690f28bdc621f63577f7be8e7a8
Gabriel Farrell <g@gsf.io>
2015-09-02 20:40:49 +0000

    Exclude .git directory in deploy

--- deploy/main.sh
+++ deploy/main.sh
@@ -3,7 +3,7 @@
 set -e
 
 # $TRAVIS_BRANCH mapped to dest in .ssh/config
-rsync -rz --delete ./ $TRAVIS_BRANCH:phila.gov
+rsync -rz --delete --exclude=".git" ./ $TRAVIS_BRANCH:phila.gov
 
 # Execute root and ubuntu user deploy scripts from the app directory
 ssh $TRAVIS_BRANCH 'cd phila.gov; sudo -E deploy/root.sh; deploy/user.sh'
