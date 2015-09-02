2134a9c3fa48a05d75a72ed1039adb0204c9d661
Gabriel Farrell <g@gsf.io>
2015-09-02 21:54:26 +0000

    Move deploy scripts to .travis and simplify .travis.yml

---
+++ .travis/prep.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# Pull in plugins, etc. with composer
+composer self-update
+composer config -g repositories.private composer $COMPOSER_REPOSITORY_URL
+composer install
+
+# Set up ssh
+openssl aes-256-cbc -K $encrypted_16a3424d8998_key -iv $encrypted_16a3424d8998_iv
+-in .travis/deploy.pem.enc -out .travis/deploy.pem -d
+chmod 400 .travis/deploy.pem
+eval `ssh-agent`
+ssh-add .travis/deploy.pem
+echo -e "Host master\n  User ubuntu\n  HostName $STAG\nHost production\n  User ubuntu\n  HostName $PROD" >> ~/.ssh/config
+ssh-keyscan $STAG >> ~/.ssh/known_hosts 2> /dev/null
+ssh-keyscan $PROD >> ~/.ssh/known_hosts 2> /dev/null
--- deploy/user.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo 'Running WP CLI deploy steps...'
-wp rewrite flush
-wp core update-db
