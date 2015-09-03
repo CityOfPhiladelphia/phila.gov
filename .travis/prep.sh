902e6c8631724be4af40dff91a639c10f94f685e
Gabriel Farrell <g@gsf.io>
2015-09-03 01:43:25 +0000

    Try new ssh config on travis

--- .travis/prep.sh
+++ .travis/prep.sh
@@ -5,9 +5,15 @@ set -e
 # Set up ssh
 openssl aes-256-cbc -K $encrypted_16a3424d8998_key -iv $encrypted_16a3424d8998_iv -in .travis/deploy.pem.enc -out .travis/deploy.pem -d
 chmod 400 .travis/deploy.pem
-eval `ssh-agent`
-ssh-add .travis/deploy.pem
-ssh-add -l
-echo -e "Host master\n  User ubuntu\n  HostName $STAG\nHost production\n  User ubuntu\n  HostName $PROD" | tee -a ~/.ssh/config
-ssh-keyscan $STAG >> ~/.ssh/known_hosts
+cat >> ~/.ssh/config <<EOF
+Host *
+  User ubuntu
+  IdentitiesOnly yes
+  IdentityFile `pwd`/.travis/deploy.pem
+Host master
+  HostName $STAG
+Host production
+  HostName $PROD
+EOF
+ssh-keyscan $STAG >> ~/.ssh/known_hosts 2> /dev/null
 ssh-keyscan $PROD >> ~/.ssh/known_hosts 2> /dev/null
