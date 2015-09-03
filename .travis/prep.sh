b237128e0398c3b099461964de31dfceba1dc49a
Gabriel Farrell <g@gsf.io>
2015-09-03 14:22:49 +0000

    Specify travis key as philagov2

--- .travis/prep.sh
+++ .travis/prep.sh
@@ -3,15 +3,17 @@
 set -e
 
 # Set up ssh
-openssl aes-256-cbc -K $encrypted_16a3424d8998_key -iv $encrypted_16a3424d8998_iv -in .travis/deploy.pem.enc -out ~/.ssh/id_rsa -d
-chmod 400 ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_16a3424d8998_key -iv $encrypted_16a3424d8998_iv -in .travis/philagov2.pem.enc -out ~/.ssh/philagov2.pem -d
+chmod 400 ~/.ssh/philagov2.pem
 cat >> ~/.ssh/config <<EOF
 Host *
   User ubuntu
 Host master
   HostName $STAG
+  IdentityFile ~/.ssh/philagov2.pem
 Host production
   HostName $PROD
+  IdentityFile ~/.ssh/philagov2.pem
 EOF
 ssh-keyscan $STAG >> ~/.ssh/known_hosts 2> /dev/null
 ssh-keyscan $PROD >> ~/.ssh/known_hosts 2> /dev/null
