41e98d68f31368f541136d8e87001b16c5cc479b
Gabriel Farrell <g@gsf.io>
2015-09-03 01:45:51 +0000

    Less noisy deploy script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 # $TRAVIS_BRANCH mapped to dest in .ssh/config
-rsync -rz --delete --exclude=".git" ./ $TRAVIS_BRANCH:phila.gov
+rsync -rz --delete --exclude=.git ./ $TRAVIS_BRANCH:phila.gov
 
 # Execute root and ubuntu user deploy scripts from the app directory
 ssh $TRAVIS_BRANCH 'cd phila.gov && .travis/remote.sh'
