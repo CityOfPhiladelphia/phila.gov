643a71dfd4eb3512b56eceb56178ab64ae276f68
Gabriel Farrell <g@gsf.io>
2015-09-09 20:35:43 +0000

    On deploy purge cache and reload nginx after php

--- .travis/remote.sh
+++ .travis/remote.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+echo 'Modifying php.ini...'
+sudo ed /etc/php5/fpm/php.ini <<'EOF'
+g/post_max_size/s/8/100
+g/upload_max_filesize/s/2/100
+g/expose_php/s/On/Off
+w
+EOF
+
+echo 'Reloading php-fpm...'
+# https://bugs.launchpad.net/ubuntu/+source/php5/+bug/1242376
+sudo kill -USR2 `cat /var/run/php5-fpm.pid`
+
+echo 'Running WP CLI deploy steps...'
+wp rewrite flush
+wp core update-db
+
+
 # Render nginx confs in /etc with env vars
 echo 'Rendering nginx confs...'
 sudo rm -rf /etc/nginx
@@ -20,20 +37,3 @@ sudo nginx -t
 sudo rm -rf /var/run/nginx-cache/*
 
 sudo service nginx reload
-
-echo 'Modifying php.ini...'
-sudo ed /etc/php5/fpm/php.ini <<'EOF'
-g/post_max_size/s/8/100
-g/upload_max_filesize/s/2/100
-g/expose_php/s/On/Off
-w
-EOF
-
-
-echo 'Reloading php-fpm...'
-# https://bugs.launchpad.net/ubuntu/+source/php5/+bug/1242376
-sudo kill -USR2 `cat /var/run/php5-fpm.pid`
-
-echo 'Running WP CLI deploy steps...'
-wp rewrite flush
-wp core update-db
