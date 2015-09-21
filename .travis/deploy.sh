ac07aa17aa96c91fbfaebd08a483eaa0de52fc08
Gabriel Farrell <g@gsf.io>
2015-09-21 18:21:00 +0000

    Verbose rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Deploy to target machine identified in .ssh/config
-rsync -rz --delete --exclude=.git ./ target:phila.gov
+rsync -rzv --delete --exclude=.git ./ target:phila.gov
 
-# Execute root and ubuntu user deploy scripts from the app directory
+# Execute deploy script from the app directory
 ssh target 'cd phila.gov && scripts/deploy.sh'
