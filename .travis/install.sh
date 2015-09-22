3268a1457de2da2f504b01a1b0e6454815c37f9f
Gabriel Farrell <g@gsf.io>
2015-09-22 21:04:12 +0000

    Clean up .travis

--- .travis/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Pull in plugins, etc. with composer
-composer self-update 1.0.0-alpha10
-composer config -g repositories.private composer $COMPOSER_REPOSITORY_URL
-composer install
