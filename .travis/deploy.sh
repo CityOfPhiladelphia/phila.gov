56aacf0ad58820f0155e19c6a8b469aba2176fdf
Gabriel Farrell <g@gsf.io>
2015-10-05 16:53:15 +0000

    No non-empty directory will stand in the way of rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo 'Syncing to target machine'
-rsync -rz --delete --exclude=".*" ./ target:phila.gov
+rsync -rz --delete --exclude=".*" --delete-excluded ./ target:phila.gov
 
 echo 'Executing deploy in app directory on target'
 ssh target 'cd phila.gov && scripts/deploy.sh'
