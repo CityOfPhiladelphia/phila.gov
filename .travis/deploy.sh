0e0f1214f3d2c933b2c5bc888bedd01476d2a3bf
Gabriel Farrell <g@gsf.io>
2015-10-22 14:32:38 +0000

    Preserve permissions on deploy rsync

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo 'Syncing to target machine'
-rsync -rz --delete --exclude=".*" --delete-excluded ./ target:phila.gov
+rsync -rzp --delete --exclude=".*" --delete-excluded ./ target:phila.gov
 
 echo 'Executing deploy in app directory on target'
 ssh target 'cd phila.gov && scripts/deploy.sh'
