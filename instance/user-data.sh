9db9775824f790f9313194fee1f769011552f78c
Gabriel Farrell <g@gsf.io>
2015-11-18 16:19:35 -0500

    Beginnings of instance scripts for easy deploys

---
+++ instance/user-data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql
+
+echo 'Installing composer'
+curl -sS https://getcomposer.org/download/1.0.0-alpha10/composer.phar > /usr/local/bin/composer
+chmod 755 /usr/local/bin/composer
+
+echo 'Installing wp-cli'
+curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
+chmod 755 /usr/local/bin/wp
+
+echo 'Setting env vars for ssh deploys'
+echo 'PermitUserEnvironment yes' >> /etc/ssh/sshd_config
+service ssh restart
