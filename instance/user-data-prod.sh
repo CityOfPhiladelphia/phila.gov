c4a9c9a8f788502855ee3eaaa6016ab29d38ef39
Gabriel Farrell <g@gsf.io>
2015-11-20 16:56:40 -0500

    Instances can be upped with env vars set

--- instance/user-data-prod.sh
+++ instance/user-data-prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
+set -ex
 
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get install -y mysql-server-5.6 nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql
+apt-get install -y nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql
 
 echo 'Installing composer'
 curl -sS https://getcomposer.org/download/1.0.0-alpha10/composer.phar > /usr/local/bin/composer
