6c2d69e0d4096211895dd4bd5a3e8d080cf8059c
Gabriel Farrell <g@gsf.io>
2015-12-07 12:36:28 -0500

    No more composer install

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,10 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y mysql-server-5.6 nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql unzip
 
-echo "Installing composer"
-curl -sS https://getcomposer.org/download/1.0.0-alpha10/composer.phar > /usr/local/bin/composer
-chmod 755 /usr/local/bin/composer
-
 echo "Installing wp-cli"
 curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
 chmod 755 /usr/local/bin/wp
