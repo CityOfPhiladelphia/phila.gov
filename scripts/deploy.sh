a54d35396512c5ef66cccf8c3cb11c969a127862
Gabriel Farrell <g@gsf.io>
2015-12-08 17:10:33 -0500

    Move php.ini modification to install script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,6 @@
 
 set -e
 
-echo 'Modifying php.ini'
-sudo ed -s /etc/php5/fpm/php.ini <<'EOF'
-g/post_max_size/s/8/100
-g/upload_max_filesize/s/2/100
-g/expose_php/s/On/Off
-w
-EOF
-
 echo 'Reloading php-fpm'
 # https://bugs.launchpad.net/ubuntu/+source/php5/+bug/1242376
 sudo kill -USR2 `cat /var/run/php5-fpm.pid`
--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,23 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update
 apt-get install -y mysql-server-5.6 nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql unzip
 
+echo 'Modifying php.ini'
+sudo ed -s /etc/php5/fpm/php.ini <<'EOF'
+g/post_max_size/s/8/100
+g/upload_max_filesize/s/2/100
+g/expose_php/s/On/Off
+w
+EOF
+
 echo "Installing wp-cli"
 curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
 chmod 755 /usr/local/bin/wp
+
+echo "Generating SSL certificate"
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+  -keyout /etc/ssl/private/self-signed.key \
+  -out /etc/ssl/certs/self-signed.crt \
+  -subj "/C=US/ST=Pennsylvania/L=Philadelphia/O=City of Philadelphia/OU=Office of Innovation and Technology/CN=$JOIA_HOSTNAME"
 
 echo 'Installing AWS CLI'
 cd /tmp
@@ -14,12 +28,6 @@ wget https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
 unzip -o awscli-bundle.zip
 ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
 cd -
-
-echo "Generating SSL certificate"
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-  -keyout /etc/ssl/private/self-signed.key \
-  -out /etc/ssl/certs/self-signed.crt \
-  -subj "/C=US/ST=Pennsylvania/L=Philadelphia/O=City of Philadelphia/OU=Office of Innovation and Technology/CN=$JOIA_HOSTNAME"
 
 echo "Configuring AWS CLI to load DB from S3"
 mkdir -p ~/.aws
