65449e370cd204cfc9cf74cc0313a008e6c7ccaf
Gabriel Farrell <g@gsf.io>
2015-12-10 13:38:09 -0500

    No need for sudo in install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,7 +5,7 @@ apt-get update
 apt-get install -y nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql
 
 echo 'Modifying php.ini'
-sudo ed -s /etc/php5/fpm/php.ini <<'EOF'
+ed -s /etc/php5/fpm/php.ini <<'EOF'
 g/post_max_size/s/8/100
 g/upload_max_filesize/s/2/100
 g/expose_php/s/On/Off
