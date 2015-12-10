9436b61a7cb0220b2f43a0e7b6f26991650cbcff
Gabriel Farrell <g@gsf.io>
2015-12-10 12:34:50 -0500

    Switch back to .env for deploy environment

--- scripts/user-env.sh
+++ scripts/user-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo 'Linking .joia/config to .ssh/environment'
+echo 'Linking .env to .ssh/environment'
 cd .ssh
-ln -fs ../phila.gov/.joia/config environment
+ln -fs ../phila.gov/.env environment
 
 echo 'Permitting user env vars for ssh deploys'
 echo 'PermitUserEnvironment yes' >> /etc/ssh/sshd_config
