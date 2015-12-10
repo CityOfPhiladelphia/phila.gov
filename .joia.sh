2f1ded0934c5b7bf07223addf1090ad6c8307b9d
Gabriel Farrell <g@gsf.io>
2015-12-10 16:59:20 -0500

    Better layout in joia_install

--- .joia.sh
+++ .joia.sh
@@ -1,10 +1,20 @@
 # joia config
 
 joia_install () {
+  # Set up .ssh/environment -> phila.gov/.env
   joia_ssh "sudo phila.gov/scripts/user-env.sh"
+
+  # Alias for JOIA_HOSTNAME
   joia_ssh "echo PUBLIC_HOSTNAME=$JOIA_HOSTNAME >> phila.gov/.env"
-  joia_ssh "sudo -E phila.gov/scripts/install.sh"
-  joia_ssh "cd phila.gov; scripts/gen-cert.sh; scripts/local-db.sh; scripts/wp-config.sh"
+
+  # Run install scripts
+  joia_ssh "
+cd phila.gov
+sudo -E scripts/install.sh
+scripts/gen-cert.sh
+scripts/local-db.sh
+scripts/wp-config.sh
+"
 }
 
 joia_deploy () {
