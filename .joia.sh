e7d0509c10c42a17da74ee3bc45417ac7963f96e
Gabriel Farrell <g@gsf.io>
2015-12-10 17:16:41 -0500

    joia now handles cd to app dir on ssh

--- .joia.sh
+++ .joia.sh
@@ -2,14 +2,13 @@
 
 joia_install () {
   # Set up .ssh/environment -> phila.gov/.env
-  joia_ssh "sudo phila.gov/scripts/user-env.sh"
+  joia_ssh "sudo scripts/user-env.sh"
 
   # Alias for JOIA_HOSTNAME
-  joia_ssh "echo PUBLIC_HOSTNAME=$JOIA_HOSTNAME >> phila.gov/.env"
+  joia_ssh "echo PUBLIC_HOSTNAME=$JOIA_HOSTNAME >> .env"
 
   # Run install scripts
   joia_ssh "
-cd phila.gov
 sudo -E scripts/install.sh
 scripts/gen-cert.sh
 scripts/local-db.sh
@@ -18,6 +17,6 @@ scripts/wp-config.sh
 }
 
 joia_deploy () {
-  joia_ssh "cd phila.gov; scripts/deploy.sh"
+  joia_ssh "scripts/deploy.sh"
   echo "Deployed to https://$JOIA_HOSTNAME"
 }
