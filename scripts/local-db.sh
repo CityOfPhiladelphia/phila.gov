0979464c32492a079fad021ec8afd99e2a6d148d
Gabriel Farrell <g@gsf.io>
2015-12-10 13:31:54 -0500

    Use all wp-cli commands in local-db

--- scripts/local-db.sh
+++ scripts/local-db.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-apt-get install -y mysql-server-5.6
+sudo apt-get install -y mysql-server-5.6
+
+echo "Resetting database (creating if not yet)"
+wp db reset --yes
 
 echo "Loading database dump into database"
-mysql -uroot <<EOF
-DROP DATABASE IF EXISTS wp;
-CREATE DATABASE wp;
-EOF
-
 dirn="$(dirname "$0")"
 curl -s "$("$dirn/s3url.sh" "$PHILA_DB_BUCKET" current.sql)" | wp db import -
