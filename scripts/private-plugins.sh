89911bc311fd7a145f005eb18ccf58eef3a70c89
Gabriel Farrell <g@gsf.io>
2015-12-11 11:50:34 -0500

    Make private-plugins script quieter

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -6,5 +6,5 @@ _dir="$(dirname "$0")"
 plugins="meta-box-group-1.0.1.zip wpfront-user-role-editor-personal-pro-2.12.1.zip"
 
 for plugin in $plugins; do
-  wp plugin install --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")"
+  wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")"
 done
