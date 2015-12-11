65e9ca0c31a5ca690a581aa7e200fea57be089c2
Gabriel Farrell <g@gsf.io>
2015-12-11 15:52:06 -0500

    Update google-calendar-events 3.0.8

--- wp/wp-content/plugins/google-calendar-events/includes/events/event-builder.php
+++ wp/wp-content/plugins/google-calendar-events/includes/events/event-builder.php
@@ -233,8 +233,13 @@ class Event_Builder {
 				case 'length' :
 				case 'duration' :
 					if ( false !== $event->end ) {
-						$duration = $event->start - $event->end;
-						$value    = human_time_diff( $event->start, $event->end );
+						$duration = $event->start - $event->end - 1000;
+
+						if ( $event->whole_day ) {
+							$value = human_time_diff( $event->start, ( $event->end - 86500 ) );
+						} else {
+							$value = human_time_diff( $event->start, $event->end );
+						}
 					} else {
 						$duration = '-1';
 						$value    = __( 'No end time', 'google-calendar-events' );
@@ -622,6 +627,8 @@ class Event_Builder {
 
 			if ( $end instanceof Carbon ) {
 
+				$end = ( $event->whole_day ? Carbon::createFromTimestamp( $end->getTimestamp() )->startOfDay()->subSeconds( 1 ) : $end );
+
 				$output .= '-' .
 				           ' <span class="simcal-event-start simcal-event-end-date" ' .
 				           'data-event-start="' . $end->getTimestamp() . '" ' .
@@ -691,7 +698,12 @@ class Event_Builder {
 		if ( 'human' == $format ) {
 			$value = human_time_diff( $event_dt->getTimestamp(), Carbon::now( $event->timezone )->getTimestamp() );
 		} else {
-			$value = date_i18n( $dt_format, $event_dt->getTimestamp() );
+
+			if ( $event->whole_day ) {
+				$value = date_i18n( $dt_format, $event_dt->getTimestamp() - 86400 );
+			} else {
+				$value = date_i18n( $dt_format, $event_dt->getTimestamp() );
+			}
 		}
 
 		return '<span class="simcal-event-' . $bound . ' ' . 'simcal-event-' . $bound . '-' . $format . '"' .
@@ -883,14 +895,14 @@ class Event_Builder {
 			'email' => 'hide',  // show/hide attendee email address
 		), (array) shortcode_parse_atts( $attr ) );
 
-		$photo      = 'hide' != $attr['photo'] ? '<img class="avatar avatar-128 photo" src="' . $organizer['photo'] . '" itemprop="image"  />' : '';
-		$organizer  = $photo . '<span itemprop="name">' . $organizer['name'] . '</span>';
+		$photo           = 'hide' != $attr['photo'] ? '<img class="avatar avatar-128 photo" src="' . $organizer['photo'] . '" itemprop="image"  />' : '';
+		$organizer_html  = $photo . '<span itemprop="name">' . $organizer['name'] . '</span>';
 
 		if ( ! empty( $organizer['email'] ) && ( 'show' == $attr['email'] ) ) {
-			$organizer = sprintf( '<a href="mailto:' . $organizer['email'] . '" itemprop="email">%s</a>', $organizer );
+			$organizer_html = sprintf( '<a href="mailto:' . $organizer['email'] . '" itemprop="email">%s</a>', $organizer_html );
 		}
 
-		return '<div class="simcal-organizer" itemprop="organizer" itemscope itemtype="https://schema.org/Person">' . $organizer . '</div>';
+		return '<div class="simcal-organizer" itemprop="organizer" itemscope itemtype="https://schema.org/Person">' . $organizer_html . '</div>';
 	}
 
 	/**
--- wp/wp-content/plugins/google-calendar-events/vendor/composer/autoload_psr4.php
+++ wp/wp-content/plugins/google-calendar-events/vendor/composer/autoload_psr4.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Component\\Translation\\' => array($vendorDir . '/symfony/translation'),
     'Carbon\\' => array($vendorDir . '/nesbot/carbon/src/Carbon'),
 );
