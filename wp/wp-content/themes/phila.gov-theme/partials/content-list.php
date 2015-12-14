737f0fa4bf3a6372d04058f61d197d08598e8745
Karissa Demi <k@karissademi.com>
2015-12-14 13:17:15 -0500

    Add class for department list for GA, clean up file

--- wp/wp-content/themes/phila.gov-theme/partials/content-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-list.php
@@ -13,7 +13,7 @@
         the_title( sprintf( '<h2 class="h3"><a href="%s" rel="bookmark" class="item">', esc_url( get_permalink() ) ), '</a></h2>' );
     }else //it's the department list so:
         {
-    ?><a href="<?php echo get_permalink(); ?>" class="item"><?php echo the_title(); ?></a>
+    ?><a href="<?php echo get_permalink(); ?>" class="content-department item"><?php echo the_title(); ?></a>
     <?php
         echo '<p class="item-desc">' . the_dept_description() . '</p>';
     }
