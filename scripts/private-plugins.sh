1f2ae955b864719260fb162f5f1f70f797656b65
Gabriel Farrell <g@gsf.io>
2015-12-14 12:14:59 -0500

    meta-box-group 1.0.1 does not need to be private

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -3,7 +3,7 @@
 # Install private plugins
 
 _dir="$(dirname "$0")"
-plugins="meta-box-group-1.0.1.zip wpfront-user-role-editor-personal-pro-2.12.2.zip"
+plugins="wpfront-user-role-editor-personal-pro-2.12.2.zip"
 
 for plugin in $plugins; do
   wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")"
