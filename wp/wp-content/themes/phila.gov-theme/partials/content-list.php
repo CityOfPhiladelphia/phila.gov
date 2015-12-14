a192c4f45f54580f53ffca651bcfb4500461d40a
Karissa Demi <k@karissademi.com>
2015-12-14 13:53:36 -0500

    Clean up

--- wp/wp-content/themes/phila.gov-theme/partials/content-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-list.php
@@ -7,16 +7,13 @@
 ?>
 
 <li>
+  <?php
+  if ( is_tax( 'topics' ) ) :
+    the_title( sprintf( '<h2 class="h3"><a href="%s" rel="bookmark" class="item">', esc_url( get_permalink() ) ), '</a></h2>' );
+  else : //it's the department list
+  ?><a href="<?php echo get_permalink(); ?>" class="content-department item"><?php echo the_title(); ?></a>
 
-    <?php
-    if (is_tax('topics')){
-        the_title( sprintf( '<h2 class="h3"><a href="%s" rel="bookmark" class="item">', esc_url( get_permalink() ) ), '</a></h2>' );
-    }else //it's the department list so:
-        {
-    ?><a href="<?php echo get_permalink(); ?>" class="content-department item"><?php echo the_title(); ?></a>
-    <?php
-        echo '<p class="item-desc">' . the_dept_description() . '</p>';
-    }
+  <p class="item-desc"><?php echo the_dept_description(); ?> </p>
 
-    ?>
+  <?php endif; ?>
 </li>
