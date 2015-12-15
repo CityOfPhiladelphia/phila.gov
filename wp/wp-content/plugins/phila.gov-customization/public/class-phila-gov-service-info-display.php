62f1365623d716b3db297f3f00715c863c9fd460
Karissa Demi <k@karissademi.com>
2015-12-15 11:57:37 -0500

    Markup for service shortcode

--- wp/wp-content/plugins/phila.gov-customization/public/class-phila-gov-service-info-display.php
+++ wp/wp-content/plugins/phila.gov-customization/public/class-phila-gov-service-info-display.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Creates shortcode for use on Department sites, to generate their "Service and Infomration See All" pages
+ * Creates shortcode for use on Department sites, to generate their "Service and Information See All" pages
  *
  * @link https://github.com/CityOfPhiladelphia/phila.gov-customization
  *
@@ -21,6 +21,7 @@
    }
 
   function service_info_shortcode( ){
+
     $categories = get_the_category( );
 
     $cat_slug = $categories[0]->slug;
@@ -35,41 +36,49 @@
       'category_name' => $cat_slug,
       'nopaging' => true,
       'order' => 'ASC',
-      'orderby' =>'name',
+      'orderby' =>'title',
     );
 
     $service_info_pages = new WP_Query( $service_pages );
 
     if ( $service_info_pages->have_posts() ) {
-      echo '<ul>';
+      $current_letter = '';
+      $letters = array();
+      echo '<div class="row small-collapse medium-uncollapse all-services-info-list">';
+        echo '<div class="small-22 small-push-2 medium-push-0 medium-18 columns">';
+          while ( $service_info_pages->have_posts() ) {
 
-      while ( $service_info_pages->have_posts() ) {
-        $service_info_pages->the_post();
-        echo '<li><a href="' . get_the_permalink()  . '">'. get_the_title() . '</a>';
+            $service_info_pages->the_post();
 
-        $current_page_id = get_the_ID();
+            $page_title_letter = strtoupper(substr(get_the_title(),0,1));
 
-        $children = array(
-        	'child_of' => $current_page_id,
-          'sort_column' => 'menu_order',
-        );
-        $pages = get_pages( $children );
+            if ($page_title_letter !== $current_letter) {
+              echo '<h2 id="' . $page_title_letter . '" class="light-stripe">'. $page_title_letter . '</h2>';
 
-        if ( $pages ) {
-          echo '<ul>';
-          foreach( $pages as $page ){
-            echo '<li><a href="' . get_page_link( $page->ID ) . '">' . $page->post_title . '</a></li>';
-          }
-          echo '</ul>';
-        }else{
-          //close open li
-          echo '</li>';
-        }
+              $current_letter = $page_title_letter;
+            }
+
+            echo '<div class="list"><a href="' . get_the_permalink()  . '">'. get_the_title() . '</a></div>';
+
+            array_push($letters, $current_letter);
+
+            }
+            $unique_letters = array_unique($letters);
+          echo '</div>';
+        echo '<div class="small-2 small-pull-22 columns show-for-small-only a-z-list">';
+          echo '<nav>';
+            echo '<ul class="no-bullet">';
+            foreach ($unique_letters as $letter) {
+             echo '<li><a href="#' . $letter . '">' . $letter . '</a></li>';
+            }
+            echo '</ul>';
+          echo '</nav>';
+        echo '</div>';
+      echo '</div>';
+
       }
-      echo '</ul>';
       wp_reset_postdata();
     }
-  }
 
   function register_service_info_shortcode(){
     add_shortcode('all-services-info-list', array( $this, 'service_info_shortcode' ) );
