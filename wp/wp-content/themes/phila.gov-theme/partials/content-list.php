18834c81622bf91eeda5a7da17b9962624b981a2
Karissa Demi <k@karissademi.com>
2015-12-17 15:09:54 -0500

    Add node about content-departments

--- wp/wp-content/themes/phila.gov-theme/partials/content-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-list.php
@@ -11,6 +11,7 @@
   if ( is_tax( 'topics' ) ) :
     the_title( sprintf( '<h2 class="h3"><a href="%s" rel="bookmark" class="item">', esc_url( get_permalink() ) ), '</a></h2>' );
   else : //it's the department list
+    //NOTE: The content-department class is used for Google Analytics and should not be removed. 
   ?><a href="<?php echo get_permalink(); ?>" class="content-department item"><?php echo the_title(); ?></a>
   <p class="item-desc"><?php echo the_dept_description(); ?> </p>
   <?php endif; ?>
