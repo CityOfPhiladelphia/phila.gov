d618be0bf573374294d5ad9910e1550ab636fc3c
Karissa Demi <k@karissademi.com>
2015-12-17 13:51:23 -0500

    Standardize blog/news rendering

--- wp/wp-content/themes/phila.gov-theme/partials/content-single.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-single.php
@@ -8,18 +8,42 @@
   <div class="row">
     <header class="entry-header small-24 columns">
       <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-        <span class="small-text"><i class="fa fa-file-text"></i> <?php _e('Blog Post');?></span>
     </header><!-- .entry-header -->
   </div>
   <div class="row">
     <div data-swiftype-index='true' class="entry-content medium-18 columns">
-      <?php the_content(); ?>
+      <?php
+      if ( has_post_thumbnail() ) : ?>
+        <ul class="clearing-thumbs" data-clearing>
+          <li>
+            <?php
+              $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+              echo '<a href="' . $large_image_url[0] . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+              the_post_thumbnail( 'news-thumb' );
+              echo '</a>'; ?>
+          </li>
+        </ul>
+      <?php
+      endif;
+      $news_desc = rwmb_meta( 'phila_news_desc', $args = array('type' => 'textrea'));
+      $post_desc = rwmb_meta( 'phila_post_desc', $args = array( 'type' => 'textrea' ) );
+
+      if ($post->post_content != ''):
+        the_content();
+      else :
+        if ($news_desc) :
+          echo '<p class="description">' . $news_desc . '</p>';
+        else :
+          echo '<p class="description">' . $post_desc . '</p>';
+        endif;
+      endif;
+      ?>
     </div><!-- .entry-content -->
     <aside id="secondary" class="small-24 medium-6 columns" role="complementary">
       <?php
-      phila_gov_posted_on();
+        phila_gov_posted_on();
 
-        /* A link pointing to the category in which this content lives. We are looking at dpartment pages specifically, so a department link will not appear unless that department is associated with the category in question.  */
+        /* A link pointing to the category in which this content lives. We are looking at department pages specifically, so a department link will not appear unless that department is associated with the category in question.  */
         $current_category = get_the_category();
         if ( !$current_category == '' ) :
           $department_page_args = array(
