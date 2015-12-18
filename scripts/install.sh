549b6bc96e807e3e7678194b232d36b7c35853a4
Gabriel Farrell <g@gsf.io>
2015-12-18 11:28:40 -0500

    Sudo inside install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get update
-apt-get install -y nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql
+sudo apt-get update
+sudo -E apt-get install -y nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql
 
 echo 'Modifying php.ini'
-ed -s /etc/php5/fpm/php.ini <<'EOF'
+sudo ed -s /etc/php5/fpm/php.ini <<'EOF'
 g/post_max_size/s/8/100
 g/upload_max_filesize/s/2/100
 g/expose_php/s/On/Off
@@ -13,5 +13,5 @@ w
 EOF
 
 echo "Installing wp-cli"
-curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > /usr/local/bin/wp
-chmod 755 /usr/local/bin/wp
+curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar | sudo tee /usr/local/bin/wp > /dev/null
+sudo chmod 755 /usr/local/bin/wp
