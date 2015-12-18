5bdba90b19f387721d9123fee3989d3341456909
Gabriel Farrell <g@gsf.io>
2015-12-18 11:24:49 -0500

    Compile ocaml and unison on instance

--- scripts/unison.sh
+++ scripts/unison.sh
@@ -3,13 +3,24 @@
 # Script for installing Unison (http://www.cis.upenn.edu/~bcpierce/unison/)
 
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y exuberant-ctags make ocaml-native-compilers
+sudo apt-get install -y exuberant-ctags make
 
-version=2.48.3
+unison_version="unison-2.48.3"
+ocaml_version="ocaml-4.02.3"
 
 cd /tmp
-wget http://www.seas.upenn.edu/~bcpierce/unison//download/releases/stable/unison-$version.tar.gz
-tar xzf unison-$version.tar.gz
-cd unison-$version
+
+wget http://caml.inria.fr/pub/distrib/ocaml-4.02/$ocaml_version.tar.gz
+tar xzf $ocaml_version.tar.gz
+cd $ocaml_version
+./configure
+make -s world opt
+sudo make -s install
+cd -
+
+wget http://www.seas.upenn.edu/~bcpierce/unison/download/releases/stable/$unison_version.tar.gz
+tar xzf $unison_version.tar.gz
+cd $unison_version
 make -s
 sudo cp -v unison /usr/local/bin/
+cd -
