dd1ddc4287a6bc7d21319dcd299ebd1d001cae1b
Karissa Demi <k@karissademi.com>
2016-01-08 17:38:30 -0500

    Add pages to topics

--- wp/wp-content/themes/phila.gov-theme/templates/topics-parent.php
+++ wp/wp-content/themes/phila.gov-theme/templates/topics-parent.php
@@ -1,30 +1,90 @@
 <?php
 /*
   *
-  * template for displaying 1st level topics page.
+  * Template for displaying L2 topic pages AND Pages tagged with "Show in browse".
   *
   */
   ?>
 <nav class="topics-nav small-24 large-8 columns">
-    <?php get_parent_topics(); ?>
+  <?php get_parent_topics(); ?>
 </nav>
 
 <?php
+  //Get the normal terms.
+  $current_term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+  $term_children = get_term_children( $current_term->term_id, $current_term->taxonomy );
+?>
 
-$current_term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
-$term_children = get_term_children( $current_term->term_id, $current_term->taxonomy );
+<?php
+  //Also get our special tagged pages.
+  $get_L2_pages = new WP_Query(array(
+    'post_type' => 'page',
+    'posts_per_page' => -1,
+    'orderby' => 'title',
+    'order'=> 'asc',
+    'post_parent' => 0,
+    'meta_key'  => 'phila_show_in_browse',
+    'meta_value' => 1,
+    'tax_query' => array(
+      'taxonomy' => 'topics',
+      'terms' => $current_term,
+      'include_children' => false,
+      ),
+    )
+  );
+  $pages_and_topics = array();
 
-echo '<div class="small-24 large-16 columns parent results">';
-echo '<h2>' . $current_term->name . '</h2>';
-echo '<ul class="subtopics">';
+  if ( $get_L2_pages->have_posts() ) : ?>
+    <?php while ( $get_L2_pages->have_posts() ) : $get_L2_pages->the_post(); ?>
 
-  foreach ($term_children as $term_child){
-    $term = get_term_by( 'id', $term_child, $current_term->taxonomy );
-    if (!$term->count == 0) :
-      echo '<li><a class="h4" href="' . get_term_link( $term_child, $current_term->taxonomy ) . '">' . $term->name;
-      echo '<p class="child-description">' . $term->description . '</p></a></li>';
-      echo '<hr>';
-    endif;
-  }
-echo '</ul>';
-echo '</div>';
+      <?php
+        if (function_exists('rwmb_meta')) {
+          $page_desc = rwmb_meta( 'phila_page_desc', $args = array('type' => 'textarea'));
+        }
+        $permalink = get_permalink();
+        $title = get_the_title();
+
+        //add pages to array
+        $pages_and_topics[$title][] = $page_desc;
+        $pages_and_topics[$title][] = $permalink;
+
+      ?>
+    <?php endwhile; ?>
+  <?php endif; ?>
+
+<div class="small-24 large-16 columns parent results">
+  <h1><?php echo $current_term->name ?></h1>
+
+  <ul class="subtopics">
+    <?php
+    foreach ($term_children as $term_child) :
+      $term = get_term_by( 'id', $term_child, $current_term->taxonomy );
+
+      if ( !$term->count == 0 ) :
+        $term_name = $term->name;
+        $term_desc = $term->description;
+
+        $term_link = get_term_link( $term_child, $current_term->taxonomy);
+
+        //add topics to array
+        $pages_and_topics[$term_name][] = $term_desc;
+        $pages_and_topics[$term_name][] = $term_link;
+      endif;
+
+    endforeach;
+      //sort by name
+      ksort($pages_and_topics); ?>
+  <?php foreach ( $pages_and_topics as $display_name => $display_data ) : ?>
+    <li>
+      <?php //display_data[1] is permalink ?>
+      <a href="<?php echo $display_data[1] ?>">
+        <h2 class="h4"><?php echo $display_name ?></h2>
+        <?php //display_data[0] is description ?>
+        <p class="description"><?php echo $display_data[0] ?></p>
+      </a>
+    </li>
+    <hr>
+  <?php endforeach; ?>
+
+  </ul>
+</div>
