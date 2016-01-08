57c4a76a5d478156cd2bc67d4e2ed7e4d31af81b
Karissa Demi <k@karissademi.com>
2016-01-08 17:36:37 -0500

    No more redirects on parent pages w/ content

--- wp/wp-content/themes/phila.gov-theme/page.php
+++ wp/wp-content/themes/phila.gov-theme/page.php
@@ -10,35 +10,37 @@
  * @package phila-gov
  */
 $children = get_pages("child_of=".$post->ID."&sort_column=menu_order");
-	global $post;
-	$content = $post->post_content;
+  global $post;
+  $content = $post->post_content;
 
 if ($children && empty( $content )) {
-		$firstchild = $children[0];
-		wp_redirect(get_permalink($firstchild->ID));
-		exit;
+    $firstchild = $children[0];
+    wp_redirect(get_permalink($firstchild->ID));
+    exit;
 }
 get_header(); ?>
 
-	<div id="primary" class="content-area row">
-		<main id="main" class="site-main small-24 columns" role="main">
+  <div id="primary" class="content-area row">
+    <main id="main" class="site-main small-24 columns" role="main">
 
-			<?php while ( have_posts() ) : the_post();
+      <?php while ( have_posts() ) : the_post();
 
-				$children = get_pages('child_of=' . $post->ID);
-				$this_content = get_the_content();
-				if ( count( $children ) != 0 && ( $this_content == 0 ))  {
-						//this page is a parent, with content
-						get_template_part( 'partials/content', 'page' );
-				}elseif(($post->id = $post->post_parent)) {
-						get_template_part( 'partials/content', 'page-collection' );
-				}else {
-						get_template_part( 'partials/content', 'page' );
-					}	
+        $children = get_pages('child_of=' . $post->ID);
+        $this_content = get_the_content();
+        if ( count( $children ) != 0 && ( $this_content == 0 ))  {
+          //this page is a parent, with content
+          get_template_part( 'partials/content', 'page' );
+        }elseif( ( $post->id = $post->post_parent ) ) {
+          //this is our normal content collection
+          get_template_part( 'partials/content', 'page-collection' );
+        }else {
+          //still show the menu, even if this page has content
+          get_template_part( 'partials/content', 'page-collection' );
+        }
 
-				endwhile; // end of the loop. ?>
+        endwhile; // end of the loop. ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    </main><!-- #main -->
+  </div><!-- #primary -->
 
 <?php get_footer(); ?>
