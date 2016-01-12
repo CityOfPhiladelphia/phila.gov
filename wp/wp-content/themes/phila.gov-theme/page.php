6cef153ed7ab18dab266c8a26d26b9de23e8e44b
Karissa Demi <k@karissademi.com>
2016-01-12 12:04:19 -0500

    Render menu on parent collection pages with content

--- wp/wp-content/themes/phila.gov-theme/page.php
+++ wp/wp-content/themes/phila.gov-theme/page.php
@@ -3,17 +3,15 @@
  * The template for displaying all pages.
  *
  * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site will use a
- * different template.
  *
  * @package phila-gov
  */
 $children = get_pages("child_of=".$post->ID."&sort_column=menu_order");
-  global $post;
-  $content = $post->post_content;
+$content = $post->post_content;
 
-if ($children && empty( $content )) {
+$has_parent = get_post_ancestors( $post );
+
+if ( $children && empty( $content ) ) {
     $firstchild = $children[0];
     wp_redirect(get_permalink($firstchild->ID));
     exit;
@@ -27,13 +25,17 @@ get_header(); ?>
 
         $children = get_pages('child_of=' . $post->ID);
         $this_content = get_the_content();
-        if ( count( $children ) != 0 && ( $this_content == 0 ))  {
-          //this page is a parent, with content
+        if ( ( count( $children ) == 0 ) && ( !$this_content == 0 ) && ( !$has_parent ) )  {
+
+          //single page, no children
           get_template_part( 'partials/content', 'page' );
+
         }elseif( ( $post->id = $post->post_parent ) ) {
+
           //this is our normal content collection
           get_template_part( 'partials/content', 'page-collection' );
         }else {
+
           //still show the menu, even if this page has content
           get_template_part( 'partials/content', 'page-collection' );
         }
