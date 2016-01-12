89c82e81db9c6a0147c69570325bc91c4e5f0d8a
Karissa Demi <k@karissademi.com>
2016-01-12 11:03:20 -0500

    Clean up rendering

--- wp/wp-content/themes/phila.gov-theme/partials/content-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-list.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * The template used for displaying sortable list content
+ * The template used for displaying sortable list content,
+ * primarily used in:
+ * Topic pages
+ * Department list
  *
  * @package phila-gov
  */
@@ -9,9 +12,9 @@
 <li>
   <?php
   if ( is_tax( 'topics' ) ) :
-    the_title( sprintf( '<h2 class="h3"><a href="%s" rel="bookmark" class="item">', esc_url( get_permalink() ) ), '</a></h2>' );
+    the_title( sprintf( '<h2 class="h4"><a href="%s" class="item">', esc_url( get_permalink() ) ), '</a></h2>' );
   else : //it's the department list
-    //NOTE: The content-department class is used for Google Analytics and should not be removed. 
+    //NOTE: The content-department class is used for Google Analytics and should not be removed.
   ?><a href="<?php echo get_permalink(); ?>" class="content-department item"><?php echo the_title(); ?></a>
   <p class="item-desc"><?php echo the_dept_description(); ?> </p>
   <?php endif; ?>
