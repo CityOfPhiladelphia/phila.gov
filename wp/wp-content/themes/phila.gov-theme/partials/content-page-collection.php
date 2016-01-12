68d6d5cb751f5492d9d4fe23a9843442190990d8
Karissa Demi <k@karissademi.com>
2016-01-12 12:04:19 -0500

    Render menu on parent collection pages with content

--- wp/wp-content/themes/phila.gov-theme/partials/content-page-collection.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-page-collection.php
@@ -5,30 +5,42 @@
  * @package phila-gov
  */
 
- $parent_title = get_the_title();
-
- $parent_content = get_the_content();
-?>
-
-<?php if ( $parent_title ) : ?>
-
-  <h1><?php echo $parent_title ?> </h1>
-
-  <p><?php echo $parent_content ?></p>
-
-<?php else :
-
   $walker_menu = new Content_Collection_Walker();
-  if($post->post_parent) {
+
+  if( $post->post_parent ) {
     $children = wp_list_pages(array(
       'sort_column' => 'menu_order',
       'title_li' => '',
       'child_of' => $post->post_parent,
       'echo' => 0,
       'walker' => $walker_menu
-      ));
+      )
+    );
+
     $page_title = get_the_title($post->post_parent);
-  } ?>
+    $parent_title = null;
+    $parent_content = null;
+    $parent_link = get_permalink( $post->post_parent );
+    $current = false;
+  }else{
+
+    $parent_link = get_permalink();
+    $current = true;
+    $parent_title = get_the_title();
+
+    $parent_content = get_the_content();
+
+    $children = wp_list_pages(array(
+      'sort_column' => 'menu_order',
+      'title_li' => '',
+      'child_of' => $post->ID,
+      'echo' => 0,
+      'walker' => $walker_menu
+      )
+    );
+
+  }
+  ?>
   <div class="data-swiftype-index='true'">
     <div class="row">
       <header class="entry-header small-24 columns">
@@ -43,10 +55,10 @@
       <div class="row">
         <div class="small-24 columns">
             <aside>
-              <h1><?php echo $page_title; ?></h1>
-
               <ul class="tabs vertical">
-                <?php  echo $children; ?>
+                <li class="tab-title<?php echo ($current) ? ' active' : ''?>">
+                  <a href="<?php echo $parent_link ?>">Overview</a></li>
+                <?php echo $children; ?>
               </ul>
             </aside>
         <div data-swiftype-name="body" data-swiftype-type="text" class="entry-content tabs-content">
@@ -54,13 +66,15 @@
             <header class="entry-header">
               <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
             </header><!-- .entry-header -->
-            <?php the_content(); ?>
+            <?php if ( $parent_content ) : ?>
+              <?php echo $parent_content ?>
+            <?php else : ?>
+              <?php the_content(); ?>
+            <?php endif; ?>
           </div>
         </div><!-- .entry-content -->
       </div>
     </div>
-  </div>
     <?php get_template_part( 'partials/content', 'modified' ) ?>
   </article><!-- #post-## -->
-
-<?php endif; ?>
+</div>
