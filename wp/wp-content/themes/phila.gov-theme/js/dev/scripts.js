7d6830fe211c20b838462f08f13505162b6d0011
Karissa Demi <k@karissademi.com>
2016-01-13 17:12:33 -0500

    Don't allow 'enter' on filtered lists

--- wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
@@ -10,6 +10,14 @@ new List('filter-list', {
 });
 
 jQuery(document).ready(function($) {
+  //prevent enter from refreshing the page and stopping filter search
+  $("#filter-list input").keypress(function(event){
+    if(event.keyCode == 13) {
+      event.preventDefault();
+      return false;
+    }
+  });
+
   $(".clickable-row").click(function() {
       window.document.location = $(this).data("href");
   });
