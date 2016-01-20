b6f83645871a44675d951c4955a53befb1876b63
Gabriel Farrell <g@gsf.io>
2016-01-20 15:21:41 -0500

    Drop wordfence plugin

--- wp/wp-content/plugins/wordfence/lib/wfAction.php
+++
@@ -1,4 +0,0 @@
-<select class="wfConfigElem" id="<?php echo $throtName; ?>" name="<?php echo $throtName; ?>">
-	<option value="throttle"<?php $w->sel($throtName, 'throttle'); ?>>throttle it</option>
-	<option value="block"<?php $w->sel($throtName, 'block'); ?>>block it</option>
-</select>
