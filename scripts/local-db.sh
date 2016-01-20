f37119735220957cc72d942cbbe3f8e9810e0877
Gabriel Farrell <g@gsf.io>
2016-01-20 12:19:52 -0500

    Low memory mysql install

--- scripts/local-db.sh
+++ scripts/local-db.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Limit mysql memory use for install
+# https://mariadb.com/blog/starting-mysql-low-memory-virtual-machines
+sudo mkdir -p /etc/mysql/conf.d
+cat << EOF | sudo tee /etc/mysql/conf.d/low_mem.cnf > /dev/null
+[mysqld]
+performance_schema = off
+EOF
+
 export DEBIAN_FRONTEND=noninteractive
 sudo -E apt-get install -y mysql-server-5.6
 
