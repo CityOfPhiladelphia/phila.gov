ea1456a333afc2a16515b017190503e5c726da33
Karissa Demi <k@karissademi.com>
2016-01-20 16:03:53 -0500

    WPFront User Role Editor Personal Pro 2.12.4

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -3,7 +3,7 @@
 # Install private plugins
 
 _dir="$(dirname "$0")"
-plugins="wpfront-user-role-editor-personal-pro-2.12.2.zip"
+plugins="wpfront-user-role-editor-personal-pro-2.12.4.zip"
 
 for plugin in $plugins; do
   wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")"
