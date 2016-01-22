e197effa9b4527ddcd2514b2fd1075c50ee5c3a1
Gabriel Farrell <g@gsf.io>
2016-01-22 10:45:32 -0500

    Install php7.0 instead of php5

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
-sudo -E apt-get install -y nginx php5-cli php5-curl php5-fpm php5-gd php5-mysql
+sudo -E apt-get install -y nginx php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-mysql
 
 echo "Installing wp-cli"
 curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar | sudo tee /usr/local/bin/wp > /dev/null
