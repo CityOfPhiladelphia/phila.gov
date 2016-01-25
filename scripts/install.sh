901083ae9eee88f40df86773886a7dc3fca72c25
Gabriel Farrell <g@gsf.io>
2016-01-25 15:35:47 -0500

    Include jq in install

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
-sudo -E apt-get install -y nginx php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-mysql
+sudo -E apt-get install -y jq nginx php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-mysql
 
 echo "Installing wp-cli"
 curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar | sudo tee /usr/local/bin/wp > /dev/null
