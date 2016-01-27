079bb3137e0d12d147a6ffa3f8d09be2f8446531
Gabriel Farrell <g@gsf.io>
2016-01-27 01:36:39 -0500

    Merge user-env into install script

--- .joia.sh
+++ .joia.sh
@@ -1,22 +1,14 @@
 # joia config
 
 joia_install () {
-  # Set up .ssh/environment
-  joia_ssh "scripts/user-env.sh"
+  joia_ssh "scripts/install.sh"
 
-  # Run install scripts
   if [ "$PHILA_TEST" ]; then
     joia_ssh "
-      scripts/install.sh
       scripts/gen-cert.sh
       scripts/wp-config.sh
       scripts/local-db.sh
       scripts/unison.sh
-    "
-  else
-    joia_ssh "
-      scripts/install.sh
-      scripts/wp-config.sh
     "
   fi
 }
