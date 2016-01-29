23c9a3cfe1a3f7b5aab91499330be1bd8103df6e
Karissa Demi <k@karissademi.com>
2016-01-29 13:58:10 -0500

    Swap url and department params

--- wp/wp-content/themes/phila.gov-theme/partials/content-feedback-url.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-feedback-url.php
@@ -1,1 +1,1 @@
-https://alpha.phila.gov/feedback?url=&dept=<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>
+https://alpha.phila.gov/feedback?url=<?php echo $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] ?>&dept=
