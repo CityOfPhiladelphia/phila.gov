f3f1b2c25cd77cb98adfff855db0487735c1474c
Gabriel Farrell <g@gsf.io>
2016-01-29 15:11:06 -0500

    Now instance env is handled by joia

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,10 +8,3 @@ sudo -E apt-get install -y jq nginx php7.0-cli php7.0-curl php7.0-fpm php7.0-gd 
 echo "Installing wp-cli"
 curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar | sudo tee /usr/local/bin/wp > /dev/null
 sudo chmod 755 /usr/local/bin/wp
-
-echo 'Copying .env to ~/.ssh/environment'
-cp .env ~/.ssh/environment
-
-echo 'Permitting user env vars for ssh deploys'
-echo 'PermitUserEnvironment yes' | sudo tee -a /etc/ssh/sshd_config > /dev/null
-sudo service ssh restart
