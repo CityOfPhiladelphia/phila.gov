24a1d6b4a4cc22707c6abd70b06eab87a20084b2
Karissa Demi <k@karissademi.com>
2016-02-04 15:32:12 -0500

    Update menu markup

--- wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
@@ -10,6 +10,9 @@ new List('filter-list', {
 });
 
 jQuery(document).ready(function($) {
+  //force foudation menus to display horizontally on desktop and vertically when 'is-drilldown' is present ( aka, on mobile )
+  $('.is-drilldown').find('ul').addClass('vertical');
+
   //prevent enter from refreshing the page and stopping filter search
   $("#filter-list input").keypress(function(event){
     if(event.keyCode == 13) {
