50f14202c386afb495edc3ea0f682c3121118b87
Michael Kappeler <michael.kappeler@phila.gov>
2016-02-10 14:44:04 -0500

    Add classes to department sidebar widgets

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -197,9 +197,9 @@ function phila_gov_widgets_init() {
       'name'          => __( $name . ' Sidebar', 'phila-gov' ),
       'id'            => 'sidebar-' . $slug .'-' . $cat_id,
       'description'   => '',
-      'before_widget' => '<aside id="%1$s" class="medium-8 columns widget %2$s">',
+      'before_widget' => '<aside id="%1$s" class="medium-8 columns widget %2$s center">',
       'after_widget'  => '</aside>',
-      'before_title'  => '<h1 class="widget-title">',
+      'before_title'  => '<h1 class="h6 widget-title">',
       'after_title'   => '</h1>',
     ) );
   }
