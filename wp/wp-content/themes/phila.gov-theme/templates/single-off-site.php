1dbb5152076276b822b5ecfcc78ae87b563387f5
Karissa Demi <k@karissademi.com>
2016-02-12 11:18:17 -0500

    Correct function name

--- wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
@@ -8,7 +8,7 @@
       <p><?php the_title(); ?> has a <strong>separate website</strong>: <a href="<?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?>"><?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?></a></p>
 
     <a class="button icon" href="<?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?>">You are now leaving
-    <?php util_echo_website_url();?> <i class="fa fa-sign-out"></i></a>
+    <?php phila_util_echo_website_url();?> <i class="fa fa-sign-out"></i></a>
     <?php echo '<p data-swiftype-name="body" data-swiftype-type="text" class="description">' . rwmb_meta( 'phila_dept_desc', $args = array('type' => 'textarea')) . '</p>'; ?>
   </div><!-- .external-site -->
 </div><!--.entry-content -->
