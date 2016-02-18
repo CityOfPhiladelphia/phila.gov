ffe9c34bb85213cb5841c98fd9315c287b7412fc
Michael Kappeler <michael.kappeler@phila.gov>
2016-02-18 13:24:53 -0500

    Adjusts document table margins

--- wp/wp-content/themes/phila.gov-theme/templates/documents.php
+++ wp/wp-content/themes/phila.gov-theme/templates/documents.php
@@ -13,7 +13,7 @@
   $documents = rwmb_meta( 'phila_files', $args = array( 'type' => 'file_advanced' ) );
   ?>
   <p class="description"><?php echo $document_description; ?></p>
-  <table class="no-borders align-left">
+  <table class="no-borders align-left mvm mvl-mu">
     <thead class="light-head">
       <tr>
         <th>
