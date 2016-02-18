4cff476552d0d156454f00589aa29bd8698a3df7
Michael Kappeler <michael.kappeler@phila.gov>
2016-02-18 12:48:48 -0500

    Corrects unclosed div

--- wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
@@ -5,8 +5,8 @@
 ?>
 
 <div data-swiftype-index='true' class="entry-content columns end">
-    <div class="external-site">
-      <?php echo '<p data-swiftype-name="body" data-swiftype-type="text" class="description">' . rwmb_meta( 'phila_dept_desc', $args = array('type' => 'textarea')) . '</p>'; ?>
+  <div class="external-site">
+    <?php echo '<p data-swiftype-name="body" data-swiftype-type="text" class="description">' . rwmb_meta( 'phila_dept_desc', $args = array('type' => 'textarea')) . '</p>'; ?>
       <div class="panel">
         <div class="row">
           <div class="medium-12 column">
@@ -16,6 +16,7 @@
             <a class="button icon" href="<?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?>">You are now leaving
             <?php phila_util_echo_website_url();?> <i class="fa fa-sign-out"></i></a>
           </div>
-    </div>
+        </div>
+      </div>
   </div><!-- .external-site -->
 </div><!--.entry-content -->
