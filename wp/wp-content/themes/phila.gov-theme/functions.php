1784eff739486718a7693633ec029a44fb03cd95
Karissa Demi <k@karissademi.com>
2016-02-18 10:31:56 -0500

    Fix headers, correct padding

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -745,35 +745,14 @@ add_action('wp_head', 'phila_output_header_images', 100);
 function phila_output_header_images(){
   global $post;
 
+  $page_bg_image_url = null;
+
   if ( is_front_page() ) {
     $page_bg_image_url = get_background_image();
 
   }elseif( is_404() ) {
     $page_bg_image_url = null;
 
-  }elseif( !isset($post) ) {
-    $page_bg_image_url = null;
-
-  }elseif ( $post->post_type == 'department_page' ) {
-    $parents = get_post_ancestors( $post->ID );
-
-    $id = ( $parents ) ? $parents[count($parents)-1]: $post->ID;
-    if ( has_post_thumbnail( $id ) ) {
-      $post_id = isset( $_GET['post'] ) ? $_GET['post'] : ( isset( $_POST['post_ID'] ) ? $_POST['post_ID'] : false );
-      $children = get_pages( array( 'child_of' => $post_id ) );
-
-      //this is a parent
-      if( ( count( $children ) != 0 ) && ( $post->post_parent == 0 ) ){
-        $page_bg_image = wp_get_attachment_image_src( get_post_thumbnail_id( $id ), 'full' );
-        $page_bg_image_url = $page_bg_image[0]; // this returns just the URL of the image
-      }else{
-        $page_bg_image_url = null;
-      }
-    }else{
-      $page_bg_image_url = null;
-    }
-  }else {
-    $page_bg_image_url = null;
   }
 
   $output = "<style type='text/css' id='alpha-custom-page-background'>body.custom-background { background-image: url('" . $page_bg_image_url . "') } </style>";
