95dd5f297228e854932b7c03a4ea67ded14241f8
Karissa Demi <k@karissademi.com>
2016-02-19 11:19:20 -0500

    Add button padding and clean up tabs

--- wp/wp-content/themes/phila.gov-theme/single-service_post.php
+++ wp/wp-content/themes/phila.gov-theme/single-service_post.php
@@ -8,9 +8,9 @@
 get_header(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('row service'); ?>>
-	<header class="entry-header small-24 columns">
-		<?php the_title( '<h1 class="entry-title contrast">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+  <header class="entry-header small-24 columns">
+    <?php the_title( '<h1 class="entry-title contrast">', '</h1>' ); ?>
+  </header><!-- .entry-header -->
 
   <?php while ( have_posts() ) : the_post();
    if (function_exists('rwmb_meta')) {
@@ -29,8 +29,8 @@ get_header(); ?>
 
   <div class="columns">
     <?php if ( !$service_before_start == '' ): ?>
-			<div class="intro clearfix equal-height">
-				<div class="small-24 medium-16 intro-item columns before-start-left equal">
+      <div class="intro clearfix equal-height">
+        <div class="small-24 medium-16 intro-item columns before-start-left equal">
           <div class="row">
             <div class="small-2 columns">
               <i class="fa fa-flag fa-2x"></i>
@@ -41,41 +41,41 @@ get_header(); ?>
             </div>
           </div>
         </div>
-				<div class="small-24 medium-8 intro-item columns center equal">
-					<div class="valign mvm mvn-mu">
-						<div class="valign-cell">
-			<?php else: ?>
-				<div class="intro clearfix">
-				<div class="small-24 medium-24 intro-item columns center pam">
-					<div class="valign mvm mvn-mu">
-						<div class="valign-cell">
+        <div class="small-24 medium-8 intro-item columns center equal">
+          <div class="valign mvm mvn-mu">
+            <div class="valign-cell">
+      <?php else: ?>
+        <div class="intro clearfix">
+        <div class="small-24 medium-24 intro-item columns center pam">
+          <div class="valign mvm mvn-mu">
+            <div class="valign-cell">
       <?php endif; ?>
-					<?php if (!$service_url == ''):?>
-	          <a data-swiftype-index="false" class="button" href="<?php echo $service_url;?>">
-	            <?php echo ( ( $service_button_text == '')  ? 'Start Now' :  $service_button_text );?>
-	            <span class="accessible"> External link</span>
-	          </a>
-	        <?php endif;?>
-	        <?php if (!$service_name == ''):?>
-	          <span data-swiftype-index="false" class="small-text">On the <?php echo $service_name;?> website</span>
-	        <?php endif; ?>
-				</div>
+          <?php if (!$service_url == ''):?>
+            <a data-swiftype-index="false" class="button prl pll" href="<?php echo $service_url;?>">
+              <?php echo ( ( $service_button_text == '')  ? 'Start Now' :  $service_button_text );?>
+              <span class="accessible"> External link</span>
+            </a>
+          <?php endif;?>
+          <?php if (!$service_name == ''):?>
+            <span data-swiftype-index="false" class="small-text">On the <?php echo $service_name;?> website</span>
+          <?php endif; ?>
+        </div>
       </div>
     </div>
-	</div>
   </div>
-	<div class="columns mtm mtl-mu">
-		<div data-swiftype-index='true' class="entry-content small-24 medium-17 large-17 columns">
-	    <?php the_content(); ?>
-	    <?php endwhile; // end of the loop. ?>
-	  </div><!-- .entry-content -->
-	  <?php if (!$related_content == ''):?>
-	      <aside id="secondary" class="related widget-area small-24 medium-6 large-6 columns" role="complementary">
-	        <h3 class="alternate">Related Topics</h3>
-	          <?php echo $related_content; ?>
-	      </aside>
-	  <?php endif; ?>
-	</div>
+  </div>
+  <div class="columns mtm mtl-mu">
+    <div data-swiftype-index='true' class="entry-content small-24 medium-17 large-17 columns">
+      <?php the_content(); ?>
+      <?php endwhile; // end of the loop. ?>
+    </div><!-- .entry-content -->
+    <?php if (!$related_content == ''):?>
+        <aside id="secondary" class="related widget-area small-24 medium-6 large-6 columns" role="complementary">
+          <h3 class="alternate">Related Topics</h3>
+            <?php echo $related_content; ?>
+        </aside>
+    <?php endif; ?>
+  </div>
 </article><!-- #post-## -->
 
 <?php get_footer(); ?>
