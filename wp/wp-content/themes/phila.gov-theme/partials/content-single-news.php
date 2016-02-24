4d5e893658c29258c4ab3ee71df07a806a53c7c3
Michael Kappeler <michael.kappeler@phila.gov>
2016-02-24 15:11:51 -0500

    Create news_post template

---
+++ wp/wp-content/themes/phila.gov-theme/partials/content-single-news.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * The content of a single post
+ * @package phila-gov
+ */
+?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <div class="row">
+    <header class="entry-header small-24 columns mbs">
+      <?php the_title( '<h1 class="entry-title contrast ptm">', '</h1>' ); ?>
+      <?php phila_get_posted_on(); ?>
+    </header><!-- .entry-header -->
+  </div>
+  <div class="row">
+    <div data-swiftype-index='true' class="entry-content medium-24 columns">
+      <?php
+      if ( has_post_thumbnail() ) : ?>
+            <?php
+              $large_image_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'full' );
+              the_post_thumbnail( 'news-thumb' , array( 'class' => 'float-left hide-for-small-only' ) );
+      endif;
+      $news_desc = rwmb_meta( 'phila_news_desc', $args = array('type' => 'textrea'));
+      if ($post->post_content != ''):
+        the_content();
+      else :
+        if ($news_desc) :
+          echo '<p class="description">' . $news_desc . '</p>';
+        endif;
+      endif;
+      ?>
+    </div><!-- .entry-content -->
+  </div><!-- .row -->
+</article><!-- #post-## -->
