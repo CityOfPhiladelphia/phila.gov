8fdcccfc115612457b55c7564d0afaf6617fb6e4
Karissa Demi <k@karissademi.com>
2016-02-24 11:41:12 -0500

    Use js to add hover state for no js fallback

--- wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
@@ -11,16 +11,24 @@ new List('filter-list', {
 
 jQuery(document).ready(function($) {
   //prevent enter from refreshing the page and stopping filter search
-  $("#filter-list input").keypress(function(event){
+  $('#filter-list input').keypress(function(event){
     if(event.keyCode == 13) {
       event.preventDefault();
       return false;
     }
   });
 
-  $(".clickable-row").click(function() {
-      window.document.location = $(this).data("href");
+  $('.clickable-row').click(function() {
+    window.document.location = $(this).data('href');
   });
+
+  $('.clickable-row').hover(function() {
+    $(this).addClass('is-hover');
+    },
+    function(){
+      $(this).removeClass('is-hover');
+  });
+
   var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
   if ( w <= 640 ){
     $('.all-services-info-list').addClass('equal-height');
