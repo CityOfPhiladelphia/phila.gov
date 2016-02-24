ee67dcc05ea2b8d44d96b391a24cce7cbd08c257
Michael Kappeler <michael.kappeler@phila.gov>
2016-02-24 16:30:53 -0500

    Corrects unresolved merge conflict

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -223,7 +223,7 @@ add_action( 'wp_enqueue_scripts', 'phila_gov_scripts');
 
 function phila_gov_scripts() {
 
-  wp_enqueue_style( 'pattern_portfolio', '//cityofphiladelphia.github.io/patterns/dist/1.1.0/css/patterns.css' );
+  wp_enqueue_style( 'pattern_portfolio', '//cityofphiladelphia.github.io/patterns/dist/1.1.1/css/patterns.css' );
 
   wp_enqueue_style( 'font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css', array(), '4.4.0' );
 
@@ -244,7 +244,7 @@ function phila_gov_scripts() {
 
   wp_enqueue_script( 'foundation-js', '//cdnjs.cloudflare.com/ajax/libs/foundation/6.1.2/foundation.min.js', array('jquery',  'jquery-migrate'), '2.8.3', true );
 
-  wp_enqueue_script( 'pattern-scripts', '//cityofphiladelphia.github.io/patterns/dist/1.1.0/js/patterns.min.js', array('jquery', 'foundation-js'), true );
+  wp_enqueue_script( 'pattern-scripts', '//cityofphiladelphia.github.io/patterns/dist/1.1.1/js/patterns.min.js', array('jquery', 'foundation-js'), true );
 
   wp_enqueue_script( 'phila-scripts', get_stylesheet_directory_uri().'/js/phila-scripts.min.js', array('jquery', 'text-filtering', 'foundation-js'), 1.0, true );
 }
@@ -544,7 +544,6 @@ function phila_get_department_menu() {
     }
   }
 }
-<<<<<<< Updated upstream
 
 add_filter('nav_menu_css_class', 'phila_add_active_nav_class', 10, 2);
 
@@ -554,8 +553,6 @@ function phila_add_active_nav_class( $classes, $item ){
   }
   return $classes;
 }
-=======
->>>>>>> Stashed changes
 
 function phila_get_dept_contact_blocks() {
   $categories = get_the_category();
