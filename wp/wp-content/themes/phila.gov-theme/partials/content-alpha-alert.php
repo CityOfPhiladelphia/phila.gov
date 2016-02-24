0780cbf832880529f5e8c2933810a124c82e62b0
Karissa Demi <k@karissademi.com>
2016-02-24 17:01:42 -0500

    Simplify alpha alert and footer

--- wp/wp-content/themes/phila.gov-theme/partials/content-alpha-alert.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-alpha-alert.php
@@ -1,22 +1,9 @@
 <div data-swiftype-index='false' id="alpha-alert">
   <div class="row">
     <div class="large-17 columns">
-    <?php
-      global $post;
-
-      $phila_gov_link = sprintf(
-        esc_html_x( '%s', 'site-url', 'phila-gov' ), '<a href="http://www.phila.gov/" target="_new" class="external">phila.gov</a>' );
-
-        $about_link = sprintf( esc_html_x( '%s', 'under construction', 'phila-gov' ), '<a href="/about/">work-in-progress</a>' ); ?>
-
-        <p> <?php _e('This site is a ' . $about_link . ' that will change as we add content. Please ', 'phila-gov') ; ?>
-
-        <a class="feedback" href="<?php echo phila_util_echo_feedback_url(); ?>">
-            <?php printf( __( 'notify us of errors.', 'phila-gov' )); ?>
-          </a>
-         </p>
-
-          <a class="go-back small-text external" href="http://www.phila.gov" target="_blank">Take me back to Phila.gov<span class="accessible"> Opens in new window</span></a>
+      <p>
+        <?php printf( 'We\'re piloting a new, user-friendly website design. To view the existing City website, visit <a class="go-back external" href="http://www.phila.gov" target="_blank">phila.gov</a>.'); ?>
+      </p>
     </div>
     <div class="large-7 columns text-right">
       <i class="fa fa-globe"></i><div id="google_translate_element"></div>
