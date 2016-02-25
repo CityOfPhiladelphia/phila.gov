5e5947044b234f69a9624d2ff60c1b23af338b20
Michael Kappeler <michael.kappeler@phila.gov>
2016-02-25 11:45:56 -0500

    Adjust alpha-header display and break points

--- wp/wp-content/themes/phila.gov-theme/partials/content-alpha-alert.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-alpha-alert.php
@@ -1,11 +1,14 @@
 <div data-swiftype-index='false' id="alpha-alert">
   <div class="row">
-    <div class="large-17 columns">
-      <p>
+    <div class="small-12 medium-18 columns">
+      <p class="hide-for-small-only">
         <?php printf( 'We\'re piloting a new, user-friendly website design. To view the existing City website, visit <a class="go-back external" href="http://www.phila.gov" target="_blank">phila.gov</a>.'); ?>
       </p>
+      <p class="show-for-small-only">
+        Back to <a class="go-back external" href="http://www.phila.gov" target="_blank">phila.gov</a>.
+      </p>
     </div>
-    <div class="large-7 columns text-right">
+    <div class="small-12 medium-6 columns text-right">
       <i class="fa fa-globe"></i><div id="google_translate_element"></div>
         <script type="text/javascript">
           function googleTranslateElementInit() {
