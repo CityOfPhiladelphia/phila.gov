737df0234b69cae885bbedfe73303e737ac0d1d5
Karissa Demi <k@karissademi.com>
2016-02-24 19:43:01 -0500

    Add period

--- wp/wp-content/themes/phila.gov-theme/partials/content-modified.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-modified.php
@@ -13,8 +13,7 @@
     // NOTE: the id is important. Google Tag Manager uses it to attach the
     // last modified date to our web analytics.
     ?>
-    <p class="small-text">This content was last updated on <time id="content-modified-datetime" datetime="<?php the_modified_time('c'); ?>"><?php the_modified_date(); ?></time>
-    <?php
+    <p class="small-text">This content was last updated on <time id="content-modified-datetime" datetime="<?php the_modified_time('c'); ?>"><?php the_modified_date(); ?></time><?php
     /* A link pointing to the category in which this content lives. We are looking at department pages specifically, so a department link will not appear unless that department is associated with the category in question.  */
     $current_category = get_the_category();
     $current_post_type = get_post_type(get_the_ID());
@@ -32,23 +31,24 @@
         'post_parent' => 0,
         'posts_per_page' => 1,
       );
-        $get_department_link = new WP_Query( $department_page_args );
+      $get_department_link = new WP_Query( $department_page_args );
       if ( $get_department_link->have_posts() ) :
-      	while ( $get_department_link->have_posts() ) :
-      		$get_department_link->the_post();
+        while ( $get_department_link->have_posts() ) :
+          $get_department_link->the_post();
           $current_cat_slug = $current_category[0]->slug;
           //we are rendering the department link elsewhere on document pages & posts.
           if ( $current_cat_slug != 'uncategorized' && $current_post_type != 'document' ) :
             // NOTE: the id and data-slug are important. Google Tag Manager
             // uses it to attach the department to our web analytics.
             if ( $current_post_type != 'phila_post' && $current_post_type != 'news_post' && $current_post_type != 'notices' ):
-              echo 'by <a href="' . get_the_permalink() . '" id="content-modified-department"
+              echo ' by <a href="' . get_the_permalink() . '" id="content-modified-department"
                     data-slug="' . $current_cat_slug . '">' . get_the_title() . '</a>';
             endif;
           endif;
         endwhile;
       endif;
     endif;
+    echo '.';
 
     /* Restore original Post Data */
     wp_reset_postdata();
