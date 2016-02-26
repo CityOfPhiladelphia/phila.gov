f3ef03472b8d354c555b0ebd9c72b1cebcf359d6
Michael Kappeler <michael.kappeler@phila.gov>
2016-02-26 14:53:00 -0500

    Add margin to phila_still_migrating_content panel in small view

--- wp/wp-content/themes/phila.gov-theme/footer.php
+++ wp/wp-content/themes/phila.gov-theme/footer.php
@@ -13,7 +13,7 @@
     <?php else : ?>
       <div class="row">
         <div class="small-24 columns">
-          <div class="panel center mbl mtl-mu">
+          <div class="panel center mbl mtm mtl-mu">
             <?php echo phila_still_migrating_content(); ?>
           </div>
         </div>
