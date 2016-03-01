305250d10897b979eccdddd4b22650af5ca2ab22
Gabriel Farrell <g@gsf.io>
2016-03-01 14:48:08 -0500

    Quiet down wp plugin install

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -6,5 +6,5 @@ _dir="$(dirname "$0")"
 plugins="wpfront-user-role-editor-personal-pro-2.12.5.zip"
 
 for plugin in $plugins; do
-  wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")"
+  wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")" > /dev/null
 done
