a200968b52d49c00ed0e6a9b3227d005f5d4ad96
Gabriel Farrell <g@gsf.io>
2016-03-03 10:16:57 -0500

    Include php7.0-mbstring in install

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 sudo add-apt-repository -y ppa:ondrej/php
 sudo apt-get update
-sudo -E apt-get install -y jq nginx php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-mysql php7.0-zlib
+sudo -E apt-get install -y jq nginx php7.0-cli php7.0-curl php7.0-fpm php7.0-gd php7.0-mbstring php7.0-mysql php7.0-zlib
 
 echo "Installing wp-cli"
 curl -sS https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar | sudo tee /usr/local/bin/wp > /dev/null
