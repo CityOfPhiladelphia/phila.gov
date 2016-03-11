00ea1e98e633d5aad891cf102ca841e72d37af27
Karissa Demi <k@karissademi.com>
2016-03-11 14:46:35 -0500

    Add conditional for including department in feedback form

--- wp/wp-content/themes/phila.gov-theme/partials/content-feedback.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-feedback.php
@@ -1,5 +1,28 @@
+<?php
+
+/*
+  Partial for our feedback area.
+*/
+?>
 <div class="row">
-  <div class="panel center">
-      <p>We're still working on this page's design and content. <a href="<?php echo phila_util_echo_feedback_url(); ?>">How can we make it better?</a></p>
+  <div class="small-24 columns">
+    <div class="panel center mbl mtm mtl-mu">
+      <p>We're still working on this page's design and content.
+        <?php
+        $link_text = "How can we make it better?";
+        if ( !is_home() && !is_404() && !is_tax() ) :
+          $current_cat = phila_util_echo_current_cat_name();
+          $dept = "?dept=" . $current_cat;
+          $feedback = '<a href="/feedback/%1$s">%2$s</a>';
+
+          echo sprintf($feedback, $dept, $link_text);
+        else :
+          $feedback = '<a href="/feedback/">%1$s</a>';
+
+          echo sprintf($feedback, $link_text);
+        endif;
+        ?>
+      </p>
+    </div>
   </div>
 </div>
