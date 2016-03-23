234eb1096f2328c16e4e35f7200914581ae3fab2
Karissa Demi <k@karissademi.com>
2016-03-23 16:38:11 -0400

    Consolidate datepicker ui to one location

--- wp/wp-content/plugins/phila.gov-customization/admin/js/admin-documents.js
+++ wp/wp-content/plugins/phila.gov-customization/admin/js/admin-documents.js
@@ -13,10 +13,6 @@ jQuery(document).ready(function($){
         maxlength: 350, required: true
       });
 
-      $('.rwmb-date').datepicker();
-      if($(".rwmb-date").datepicker("getDate") === null) {
-        $('.rwmb-date').datepicker('setDate', new Date());
-      }
       var $eventSelect = $('.rwmb-select-advanced');
     }
   }
