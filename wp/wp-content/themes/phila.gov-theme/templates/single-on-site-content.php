b5b9e53c3a54caf972c92bcc448b2c8f4aaf1bb4
Michael Kappeler <michael.kappeler@phila.gov>
2016-03-29 15:42:25 -0400

    Display hero-header metaboxes on dept pages

--- wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
@@ -15,6 +15,34 @@
 <div class="small-24 columns">
   <div class="row">
     <div data-swiftype-index='true' class="entry-content small-24 columns">
+      <?php if (function_exists('rwmb_meta')): ?>
+        <?php $hero_header_image = rwmb_meta( 'phila_hero_header_image', $args = array('type' => 'file_input'));
+              $hero_header_alt_text = rwmb_meta( 'phila_hero_header_image_alt_text', $args = array('type' => 'text'));
+              $hero_header_title = rwmb_meta( 'phila_hero_header_title', $args = array('type' => 'text'));
+              $hero_header_body_copy = rwmb_meta( 'phila_hero_header_body_copy', $args = array('type' => 'text'));
+              $hero_header_call_to_action_button_url = rwmb_meta( 'phila_hero_header_call_to_action_button_url', $args = array('type' => 'URL'));
+              $hero_header_call_to_action_button_text = rwmb_meta( 'phila_hero_header_call_to_action_button_text', $args = array('type' => 'text'));
+        ?>
+        <?php if (!$hero_header_image == ''): ?>
+            <section class="department-header no-margin">
+              <img id="header-image" class="size-full wp-image-4069" src="<?php echo $hero_header_image; ?>" alt="<?php echo $hero_header_alt_text;?>" width="975" height="431" />
+            <?php if (!$hero_header_title == ''): ?>
+              <div class="intro row">
+                <div class="column">
+                  <h1><?php echo $hero_header_title; ?></h1>
+                  <?php if (!$hero_header_body_copy == ''): ?>
+                    <p><?php echo $hero_header_body_copy; ?></p>
+                  <?php endif; ?>
+                  <?php if (!$hero_header_call_to_action_button_url == ''): ?>
+                    <p><a href="<?php echo $hero_header_call_to_action_button_url; ?>" class="button alternate no-margin"><?php echo $hero_header_call_to_action_button_text; ?></a></p>
+                  <?php endif; ?>
+
+                </div>
+              </div>
+            <?php endif; ?>
+          </section>
+        <?php endif; ?>
+      <?php endif; ?>
        <?php echo the_content();?>
        <!-- If JotForm Embed is present print it -->
        <?php if (function_exists('rwmb_meta')) {
