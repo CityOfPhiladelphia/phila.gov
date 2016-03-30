72d254de10c1defe744010ee199abe11c951ecfb
Michael Kappeler <michael.kappeler@phila.gov>
2016-03-30 15:16:21 -0400

    Points Travis CI deploy at app dir

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -39,7 +39,7 @@ EOF
 ssh-keyscan $IP >> ~/.ssh/known_hosts 2> /dev/null
 
 echo 'Syncing to target machine'
-rsync -rzp --delete --exclude=".git" ./ target:phila.gov
+rsync -rzp --delete --exclude=".git" ./ target:app
 
 echo 'Executing deploy in app directory on target'
-ssh target 'cd phila.gov; scripts/wp-config.sh && scripts/deploy.sh'
+ssh target 'cd app; scripts/wp-config.sh && scripts/deploy.sh'
