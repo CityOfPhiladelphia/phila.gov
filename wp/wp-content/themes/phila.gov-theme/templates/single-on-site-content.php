1d2ada74b5d7221c1c020b796361c99b106f1688
Michael Kappeler <michael.kappeler@phila.gov>
2016-04-14 17:04:01 -0400

    Define vars for first row modules

--- wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
@@ -6,31 +6,40 @@
 *
 */
 ?>
-
 <div class="small-24 columns">
   <?php the_title( '<h2 class="entry-title">', '</h2>' ); ?>
 </div>
-
-
 <div class="small-24 columns">
   <div class="row">
     <div data-swiftype-index='true' class="entry-content small-24 columns">
       <?php if (function_exists('rwmb_meta')): ?>
-        <?php $append_before_wysiwyg = rwmb_meta( 'phila_append_before_wysiwyg', $args = array('type' => 'textarea'));
+        <?php // Set custom markup vars
+              $append_before_wysiwyg = rwmb_meta( 'phila_append_before_wysiwyg', $args = array('type' => 'textarea'));
               $append_after_wysiwyg = rwmb_meta( 'phila_append_after_wysiwyg', $args = array('type' => 'textarea'));
-
+              // Set hero-header vars
               $hero_header_image = rwmb_meta( 'phila_hero_header_image', $args = array('type' => 'file_input'));
               $hero_header_alt_text = rwmb_meta( 'phila_hero_header_image_alt_text', $args = array('type' => 'text'));
               $hero_header_title = rwmb_meta( 'phila_hero_header_title', $args = array('type' => 'text'));
               $hero_header_body_copy = rwmb_meta( 'phila_hero_header_body_copy', $args = array('type' => 'textarea'));
               $hero_header_call_to_action_button_url = rwmb_meta( 'phila_hero_header_call_to_action_button_url', $args = array('type' => 'URL'));
               $hero_header_call_to_action_button_text = rwmb_meta( 'phila_hero_header_call_to_action_button_text', $args = array('type' => 'text'));
-
+              // Set module row vars
               $row_one_col_one_module = rwmb_meta( 'module_row_1_col_1');
+              if (!empty($row_one_col_one_module)){
+                $row_one_col_one_type = $row_one_col_one_module[phila_module_row_1_col_1_type];
+                $row_one_col_one_post_style = $row_one_col_one_module[module_row_1_col_1_options][phila_module_row_1_col_1_post_style];
+                $row_one_col_one_text_title = $row_one_col_one_module[module_row_1_col_1_options][phila_module_row_1_col_1_texttitle];
+                $row_one_col_one_textarea = $row_one_col_one_module[module_row_1_col_1_options][phila_module_row_1_col_1_textarea];
+              }
               $row_one_col_two_module = rwmb_meta( 'module_row_1_col_2');
-
+              if (!empty($row_one_col_two_module)){
+                $row_one_col_two_type = $row_one_col_two_module[phila_module_row_1_col_2_type];
+                $row_one_col_two_post_style = $row_one_col_two_module[module_row_1_col_2_options][phila_module_row_1_col_2_post_style];
+                $row_one_col_two_text_title = $row_one_col_two_module[module_row_1_col_2_options][phila_module_row_1_col_2_texttitle];
+                $row_one_col_two_textarea = $row_one_col_two_module[module_row_1_col_2_options][phila_module_row_1_col_2_textarea];
+              }
         ?>
-        <!-- If Custom Markup append_before_wysiwyg is present print it -->
+        <!-- If custom markup append_before_wysiwyg is present print it -->
         <?php if (!$append_before_wysiwyg == ''):?>
           <?php echo $append_before_wysiwyg; ?>
         <?php endif; ?>
@@ -55,14 +64,15 @@
         <?php endif; ?>
       <?php endif; ?>
       <!-- Begin Row One MetaBox Modules -->
-      <?php if ( ( !$row_one_col_one_module[phila_module_row_1_col_1_type]  == '') && (!$row_one_col_two_module[phila_module_row_1_col_2_type]  == '' ) ): ?>
+      <?php if ( ( !$row_one_col_one_type == '') && (!$row_one_col_two_type  == '' ) ): ?>
         <section>
           <div class="row equal-height">
             <!-- Begin Column One -->
-            <?php if ( $row_one_col_one_module[phila_module_row_1_col_1_type]  == 'phila_module_row_1_col_1_blog_posts' ): ?>
+            <?php if ( $row_one_col_one_type  == 'phila_module_row_1_col_1_blog_posts' ): ?>
               <div class="large-18 columns">
                 <div class="row">
-                  <?php if ($row_one_col_one_module[module_row_1_col_1_options][phila_module_row_1_col_1_post_style] == 'phila_module_row_1_col_1_post_style_list'):?>
+                  <?php if ($row_one_col_one_post_style == 'phila_module_row_1_col_1_post_style_list'):?>
+                    <!-- TURN SHORTCODE STRING INTO VAR -->
                     <?php echo do_shortcode('[recent-posts list posts="3"]'); ?>
                   <?php else: ?>
                     <?php echo do_shortcode('[recent-posts posts="3"]'); ?>
@@ -71,13 +81,15 @@
               </div>
             <?php else: ?>
               <div class="large-18 columns">
-                <h2 class="alternate"><?php echo($row_one_col_one_module[module_row_1_col_1_options][phila_module_row_1_col_1_texttitle]); ?></h2>
-                <?php echo($row_one_col_one_module[module_row_1_col_1_options][phila_module_row_1_col_1_textarea]); ?>
+                <h2 class="alternate"><?php echo($row_one_col_one_text_title); ?></h2>
+                <div>
+                  <?php echo($row_one_col_one_textarea); ?>
+                </div>
               </div>
             <?php endif; ?>
             <!-- End Column One -->
             <!-- Begin Column Two -->
-            <?php if ( $row_one_col_two_module[phila_module_row_1_col_2_type]  == 'phila_module_row_1_col_2_blog_posts' ): ?>
+            <?php if ( $row_one_col_two_type  == 'phila_module_row_1_col_2_blog_posts' ): ?>
               <div class="large-6 columns">
                 <div class="row">
                   <?php echo do_shortcode('[recent-posts posts="1"]'); ?>
@@ -85,10 +97,10 @@
               </div>
             <?php else: ?>
               <div class="large-6 columns">
-                <h2 class="alternate"><?php echo($row_one_col_two_module[module_row_1_col_2_options][phila_module_row_1_col_2_texttitle]); ?></h2>
+                <h2 class="alternate"><?php echo($row_one_col_two_text_title); ?></h2>
                 <div class="panel no-margin">
                   <div>
-                    <?php echo($row_one_col_two_module[module_row_1_col_2_options][phila_module_row_1_col_2_textarea]); ?>
+                    <?php echo($row_one_col_two_textarea); ?>
                   </div>
                 </div>
               </div>
