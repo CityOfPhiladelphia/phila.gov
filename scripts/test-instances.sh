363a6b31cba88f04f56b6ef0eaa1f3a3cb18abbc
Gabriel Farrell <g@gsf.io>
2016-04-15 17:33:08 -0400

    Set debug in wp-config on test instances

--- scripts/test-instances.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-sed -i "/^define('FS_METHOD', 'direct');/a /*Debug true on test instances */\ndefine('WP_DEBUG', true);" /home/ubuntu/app/wp/wp-config.php
-
-echo 'Executing test-instances.sh'
