e75945841b54be4dc30ff1e7630e4cc2040f85ac
Michael Kappeler <michael.kappeler@phila.gov>
2016-04-18 16:21:04 -0400

    Update to Simple Calendar 3.0.16

--- wp/wp-content/plugins/google-calendar-events/google-calendar-events.php
+++ wp/wp-content/plugins/google-calendar-events/google-calendar-events.php
@@ -3,7 +3,7 @@
  * Plugin Name: Simple Calendar
  * Plugin URI:  https://simplecalendar.io
  * Description: Add Google Calendar events to your WordPress site in minutes. Beautiful calendar displays. Fully responsive.
- * Version:     3.0.14
+ * Version:     3.0.16
  * Author:      Moonstone Media
  * Author URI:  https://simplecalendar.io
  * Text Domain: google-calendar-events
@@ -18,18 +18,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-// Composer fallback for PHP < 5.3.0.
-if ( version_compare( PHP_VERSION, '5.3.0' ) === -1 ) {
-	include_once 'vendor/autoload_52.php';
-} else {
-	include_once 'vendor/autoload.php';
-}
-
 // Plugin constants.
 $this_plugin_path      = trailingslashit( dirname( __FILE__ ) );
 $this_plugin_dir       = plugin_dir_url( __FILE__ );
 $this_plugin_constants = array(
-	'SIMPLE_CALENDAR_VERSION'   => '3.0.14',
+	'SIMPLE_CALENDAR_VERSION'   => '3.0.16',
 	'SIMPLE_CALENDAR_MAIN_FILE' => __FILE__,
 	'SIMPLE_CALENDAR_URL'       => $this_plugin_dir,
 	'SIMPLE_CALENDAR_ASSETS'    => $this_plugin_dir . 'assets/',
@@ -49,7 +42,7 @@ include_once 'includes/wp-requirements.php';
 // Check plugin requirements before loading plugin.
 $this_plugin_checks = new SimCal_WP_Requirements( 'Simple Calendar', plugin_basename( __FILE__ ), array(
 		'PHP'        => '5.3.3',
-		'WordPress'  => '4.0.0',
+		'WordPress'  => '4.1',
 		'Extensions' => array(
 			'curl',
 			'iconv',
@@ -62,6 +55,8 @@ if ( $this_plugin_checks->pass() === false ) {
 
 	return;
 }
+
+include_once 'vendor/autoload.php';
 
 // Load plugin.
 include_once 'includes/main.php';
