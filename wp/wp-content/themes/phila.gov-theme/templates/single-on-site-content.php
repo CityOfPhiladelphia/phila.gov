e26505211ce8abdfbebaba190a10693b74282343
Michael Kappeler <michael.kappeler@phila.gov>
2016-04-28 15:54:43 -0400

    Call row two in template

--- wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
@@ -41,6 +41,16 @@
                 $row_one_col_two_text_title = $row_one_col_two_module['module_row_1_col_2_options']['phila_module_row_1_col_2_texttitle'];
                 $row_one_col_two_textarea = $row_one_col_two_module['module_row_1_col_2_options']['phila_module_row_1_col_2_textarea'];
               }
+              $row_two_col_one_module = rwmb_meta( 'module_row_2_col_1');
+              if (!empty($row_two_col_one_module)){
+                $row_two_col_one_type = $row_two_col_one_module['phila_module_row_2_col_1_type'];
+                $row_two_col_one_cal_id = $row_two_col_one_module['module_row_2_col_1_options']['phila_module_row_2_col_1_cal_id'];
+              }
+              $row_two_col_two_module = rwmb_meta( 'module_row_2_col_2');
+              if (!empty($row_two_col_two_module)){
+                $row_two_col_two_type = $row_two_col_two_module['phila_module_row_2_col_2_type'];
+                $row_two_col_two_cal_id = $row_two_col_two_module['module_row_2_col_2_options']['phila_module_row_2_col_2_cal_id'];
+              }
         ?>
         <!-- If Custom Markup append_before_wysiwyg is present print it -->
         <?php if (!$append_before_wysiwyg == ''):?>
@@ -112,8 +122,48 @@
           </div>
         </section>
       <?php endif; ?>
+      <!-- End Row One MetaBox Modules -->
 
        <?php echo the_content();?>
+
+       <!-- Begin Row Two MetaBox Modules -->
+       <?php if ( ( !empty($row_two_col_one_module ) ) && (!empty($row_two_col_two_module ) ) ): ?>
+       <section class="department-module-row-one">
+         <div class="row equal-height">
+           <?php if ( $row_two_col_one_type  == 'phila_module_row_2_col_1_calendar' ): ?>
+             <div class="medium-12 columns">
+               <h2 class="alternate">Calendar</h2>
+               <div class="event-box">
+                 <?php echo do_shortcode('[calendar id="' . $row_two_col_one_cal_id .'"]'); ?>
+               </div>
+               <a class="float-right see-all-right" href="/calendar/mayor/">All Events</a>
+             </div>
+           <?php elseif ( $row_two_col_one_type  == 'phila_module_row_2_col_1_press_release' ): ?>
+               <div class="medium-12 columns">
+                 <div class="row">
+                 <?php echo do_shortcode('[press-releases posts=5]');?>
+                 </div>
+               </div>
+             <?php endif; ?>
+             <?php if ( $row_two_col_two_type  == 'phila_module_row_2_col_2_calendar' ): ?>
+               <div class="medium-12 columns">
+                 <h2 class="alternate">Calendar</h2>
+                 <div class="event-box">
+                   <?php echo do_shortcode('[calendar id="' . $row_two_col_two_cal_id .'"]'); ?>
+                 </div>
+                 <a class="float-right see-all-right" href="/calendar/mayor/">All Events</a>
+              </div>
+             <?php elseif ( $row_two_col_two_type  == 'phila_module_row_2_col_2_press_release' ): ?>
+               <div class="medium-12 columns">
+                 <div class="row">
+                   <?php echo do_shortcode('[press-releases posts=5]');?>
+                 </div>
+               </div>
+             <?php endif; ?>
+         </div>
+       </section>
+       <?php endif; ?>
+       <!-- End Row Two MetaBox Modules -->
 
        <!-- If Custom Markup append_after_wysiwyg is present print it -->
        <?php if (!$append_after_wysiwyg == ''):?>
