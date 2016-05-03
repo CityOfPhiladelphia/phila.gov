23da3ae27d29c8d0130db8a3f763768581351be3
Karissa Demi <k@karissademi.com>
2016-05-03 15:22:52 -0400

    Break header and content into rows

--- wp/wp-content/themes/phila.gov-theme/404.php
+++ wp/wp-content/themes/phila.gov-theme/404.php
@@ -11,11 +11,15 @@ get_header(); ?>
   <main id="main" class="site-main">
     <div class="row">
       <div class="small-24 columns">
-        <section class="error-404 not-found">
+        <section>
           <header>
             <h1 class="contrast"><?php _e( 'Sorry, the page you requested was not found.', 'phila-gov' ); ?></h1>
           </header><!-- .page-header -->
-          <div class="page-content">
+        </div>
+      </div>
+      <div class="row">
+        <div class="small-24 columns">
+          <div class="page-content error-404 not-found">
             <p><?php _e( 'It looks like nothing was found at this location.', 'phila-gov' ); ?></p>
             <div class="find">
               <h2 class="h1 mtn">What can we help you find?</h2>
