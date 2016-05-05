7602214e75ee45fac8601c194adfcb56bb41d20b
Karissa Demi <k@karissademi.com>
2016-05-05 10:17:26 -0400

    Ensure valid markup and tweak styles

--- wp/wp-content/themes/phila.gov-theme/single-service_post.php
+++ wp/wp-content/themes/phila.gov-theme/single-service_post.php
@@ -29,11 +29,11 @@ get_header(); ?>
       <p class="description"><?php echo $service_desc;?></p>
     </div>
   </div>
-  <div class="row">
+  <div class="row equal-height">
     <div class="columns">
-      <?php if ( !$service_before_start == '' ): ?>
-        <div class="intro clearfix equal-height">
-          <div class="small-24 medium-16 intro-item columns before-start-left equal">
+      <?php
+        if ( !$service_before_start == '' ): ?>
+          <div class="intro small-24 medium-16 intro-item columns before-start-left equal">
             <div class="row">
               <div class="medium-2 columns show-for-medium">
                 <i class="fa fa-flag fa-2x"></i>
@@ -44,40 +44,44 @@ get_header(); ?>
               </div>
             </div>
           </div>
-          <div class="small-24 medium-8 intro-item columns center equal">
+          <div class="intro small-24 medium-8 intro-item columns center equal">
             <div class="valign mvm mvn-mu">
               <div class="valign-cell">
-        <?php else: ?>
-          <div class="intro clearfix">
-            <div class="small-24 medium-24 intro-item columns center pam">
+
+        <?php else: //$service_before_start ?>
+            <div class="intro small-24 medium-24 intro-item columns center pam">
               <div class="valign mvm mvn-mu">
                 <div class="valign-cell">
+          <?php endif; //$service_before_start ?>
+
+          <?php if (!$service_url == ''):?>
+            <a data-swiftype-index="false" class="button prxl plxl" href="<?php echo $service_url;?>">
+              <?php echo ( ( $service_button_text == '')  ? 'Start Now' :  $service_button_text );?>
+              <span class="accessible"> External link</span>
+            </a>
+          <?php endif;?>
+
+          <?php if (!$service_name == ''):?>
+            <span data-swiftype-index="false" class="small-text">On the <?php echo $service_name;?> website</span>
           <?php endif; ?>
-              <?php if (!$service_url == ''):?>
-                <a data-swiftype-index="false" class="button prxl plxl" href="<?php echo $service_url;?>">
-                  <?php echo ( ( $service_button_text == '')  ? 'Start Now' :  $service_button_text );?>
-                  <span class="accessible"> External link</span>
-                </a>
-              <?php endif;?>
-              <?php if (!$service_name == ''):?>
-                <span data-swiftype-index="false" class="small-text">On the <?php echo $service_name;?> website</span>
-              <?php endif; ?>
-            </div>
-          </div>
+          
         </div>
       </div>
-    <div class="row mtm mtl-mu">
-      <div data-swiftype-index='true' class="entry-content small-24 medium-17 large-17 columns">
-          <?php the_content(); ?>
-          <?php endwhile; // end of the loop. ?>
-      </div><!-- .entry-content -->
-      <?php if (!$related_content == ''):?>
-        <aside id="secondary" class="related widget-area small-24 medium-6 large-6 columns mtl mtn-mu">
-          <h3 class="alternate mtm mtn-mu">Related Topics</h3>
-          <?php echo $related_content; ?>
-        </aside>
-      <?php endif; ?>
     </div>
+  </div>
+</div>
+  <div class="row mtm mtl-mu">
+    <div data-swiftype-index='true' class="entry-content small-24 medium-17 large-17 columns">
+        <?php the_content(); ?>
+        <?php endwhile; // end of the loop. ?>
+    </div><!-- .entry-content -->
+    <?php if (!$related_content == ''):?>
+      <aside id="secondary" class="related widget-area small-24 medium-6 large-6 columns mtl mtn-mu">
+        <h3 class="alternate mtm mtn-mu">Related Topics</h3>
+        <?php echo $related_content; ?>
+      </aside>
+    <?php endif; ?>
+  </div>
 </article><!-- #post-## -->
 
 <?php get_footer(); ?>
