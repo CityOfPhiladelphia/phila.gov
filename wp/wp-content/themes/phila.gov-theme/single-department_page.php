56de0930fe37cbd2826ffc94aed2e38b4ac7af27
Karissa Demi <k@karissademi.com>
2016-05-06 12:50:23 -0400

    Standarize intro styles and clean up markup

--- wp/wp-content/themes/phila.gov-theme/single-department_page.php
+++ wp/wp-content/themes/phila.gov-theme/single-department_page.php
@@ -24,54 +24,50 @@ if ( $children && count( $ancestors ) == 1 ) {
 }
 get_header(); ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class('row department'); ?>>
-  <div class="small-24 columns">
-    <div class="row">
-      <header class="entry-header small-24 columns">
-        <?php
-         /* Get an array of Ancestors and Parents if they exist */
-          $parents = get_post_ancestors( $post->ID );
-          /* Get the top Level page->ID count base 1, array base 0 so -1 */
-          $id = ($parents) ? $parents[count($parents)-1]: $post->ID;
-          if ($id === $post->ID) {
-
-          }
-          $parent = get_post( $id );
-          ?>
-          <h1 class="entry-title contrast mbn"><?php echo $parent->post_title;?></h1>
-      </header>
-    </div>
+<article id="post-<?php the_ID(); ?>" <?php post_class('department'); ?>>
+  <div class="row">
+    <header class="entry-header small-24 columns">
       <?php
-    //get department homepage alerts
-    call_user_func( array( 'Phila_Gov_Department_Sites', 'department_homepage_alert' ) );
-    ?>
-    <?php
-      /*
-      Our navigation menu. We use categories to drive functionality.
-      This checks to make sure a category exists for the given page,
-      if it does, we render our menu w/ markup.
-      */
-        phila_get_department_menu();
-    ?>
-    <div class="row mtm">
-      <?php
-      if (function_exists('rwmb_meta')) {
-        $external_site = rwmb_meta( 'phila_dept_url', $args = array('type' => 'url'));
-        if (!$external_site == ''){
+       /* Get an array of Ancestors and Parents if they exist */
+        $parents = get_post_ancestors( $post->ID );
+        /* Get the top Level page->ID count base 1, array base 0 so -1 */
+        $id = ($parents) ? $parents[count($parents)-1]: $post->ID;
+        if ($id === $post->ID) {
 
-          get_template_part( 'templates/single', 'off-site' );
-
-         } else {
-           //loop for our regularly scheduled content
-           while ( have_posts() ) : the_post();
-
-            get_template_part( 'templates/single', 'on-site-content' );
-
-            endwhile;
-          }
-      }
-      ?>
-    </div>
+        }
+        $parent = get_post( $id );
+        ?>
+        <h1 class="entry-title contrast mbn"><?php echo $parent->post_title;?></h1>
+    </header>
   </div>
+    <?php
+  //get department homepage alerts
+  call_user_func( array( 'Phila_Gov_Department_Sites', 'department_homepage_alert' ) );
+  ?>
+  <?php
+    /*
+    Our navigation menu. We use categories to drive functionality.
+    This checks to make sure a category exists for the given page,
+    if it does, we render our menu w/ markup.
+    */
+      phila_get_department_menu();
+  ?>
+  <?php
+  if (function_exists('rwmb_meta')) {
+    $external_site = rwmb_meta( 'phila_dept_url', $args = array('type' => 'url'));
+    if (!$external_site == ''){
+
+      get_template_part( 'templates/single', 'off-site' );
+
+     } else {
+       //loop for our regularly scheduled content
+       while ( have_posts() ) : the_post();
+
+        get_template_part( 'templates/single', 'on-site-content' );
+
+        endwhile;
+      }
+  }
+  ?>
 </article><!-- #post-## -->
 <?php get_footer(); ?>
--- wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
@@ -4,21 +4,22 @@
 */
 ?>
 
-<div data-swiftype-index='true' class="entry-content columns end">
-  <?php echo '<p data-swiftype-name="body" data-swiftype-type="text" class="description">' . rwmb_meta( 'phila_dept_desc', $args = array('type' => 'textarea')) . '</p>'; ?>
-  <div class="external-site">
-    <div class="row columns pvm mvm mvl-mu">
-      <div class="small-24 medium-12 column">
-        <div class="pll-mu external-site-link-left">
-          <p><strong><?php the_title(); ?> has a separate website</strong>: <a href="<?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?>"><?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?></a></p>
-        </div>
-      </div>
-      <div class="small-24 medium-12 column">
-        <div class="prl-mu external-site-link-right">
-          <a class="button icon" href="<?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?>"><span class="show-for-large-only">You are now </span>leaving
-          <?php phila_util_echo_website_url();?> <i class="fa fa-sign-out"></i></a>
-        </div>
-      </div>
-    </div>
-  </div><!-- .external-site -->
-</div><!--.entry-content -->
+<?php
+  $department_external_url = rwmb_meta( 'phila_dept_url', $args = array('type' => 'url'));
+?>
+
+<div class="row ptm">
+  <div data-swiftype-index='true' class="entry-content columns end">
+    <?php echo '<p data-swiftype-name="body" data-swiftype-type="text" class="description">' . rwmb_meta( 'phila_dept_desc', $args = array('type' => 'textarea')) . '</p>'; ?>
+  </div>
+</div>
+
+<div class="row columns mvm equal-height">
+  <div class="small-24 medium-12 column intro equal pal external-site-link-left">
+    <p class="mbn"><strong><?php the_title(); ?> has a separate website</strong>: <a href="<?php echo $department_external_url ?>"><?php echo $department_external_url ?></a></p>
+  </div>
+  <div class="small-24 medium-12 column intro equal pal external-site-link-right">
+    <a class="button man icon" href="<?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?>"><span class="show-for-large-only">You are now </span>leaving
+    <?php phila_util_echo_website_url();?> <i class="fa fa-sign-out bg-sidewalk"></i></a>
+  </div>
+</div>
