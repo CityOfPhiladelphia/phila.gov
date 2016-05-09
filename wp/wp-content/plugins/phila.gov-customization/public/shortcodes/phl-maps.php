df6062771fa3dafec146686658671c8c5a4ca35b
Michael Kappeler <michael.kappeler@phila.gov>
2016-05-09 09:20:41 -0400

    Centers map on City Hall by default

--- wp/wp-content/plugins/phila.gov-customization/public/shortcodes/phl-maps.php
+++ wp/wp-content/plugins/phila.gov-customization/public/shortcodes/phl-maps.php
@@ -18,7 +18,7 @@ function phl_maps_shortcode($atts) {
   ), $atts );
 
   if ( $a['map_id'] != '' ){
-    $output = '<div class="embed-container"><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Construction" src="//phl.maps.arcgis.com/apps/Embed/index.html?webmap=' . $a['map_id'] . '&amp;zoom=true&amp;extent=-75.2279,39.9466,-75.149,39.9812&amp;scale=true&amp;disable_scroll=true&amp;theme=light" class="phl-maps-' . $a['width'] . '-width"></iframe></div>';
+    $output = '<div class="embed-container"><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Construction" src="//phl.maps.arcgis.com/apps/Embed/index.html?webmap=' . $a['map_id'] . '&amp;center=-75.1634622,39.9526239&amp;zoom=true&amp;extent=-75.2279,39.9466,-75.149,39.9812&amp;scale=true&amp;disable_scroll=true&amp;theme=light" class="phl-maps-' . $a['width'] . '-width"></iframe></div>';
     return $output;
   }
 }
