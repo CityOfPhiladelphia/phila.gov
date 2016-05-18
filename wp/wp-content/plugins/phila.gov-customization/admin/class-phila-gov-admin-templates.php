025a1e58e12499c6ec579b82a23b056c0fa65d7e
Karissa Demi <k@karissademi.com>
2016-05-18 13:44:45 -0400

    Create template selctor

---
+++ wp/wp-content/plugins/phila.gov-customization/admin/class-phila-gov-admin-templates.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Register templates for use on the front-end
+ *
+ * @link https://github.com/CityOfPhiladelphia/phila.gov-customization
+ *
+ * @package phila-gov_customization
+ * @since 0.17.6
+ */
+
+if ( class_exists( "Phila_Gov_Admin_Templates" ) ){
+  $admin_menu_labels = new Phila_Gov_Admin_Templates();
+}
+
+class Phila_Gov_Admin_Templates {
+
+  public function __construct(){
+
+    add_filter( 'rwmb_meta_boxes', array( $this, 'phila_register_template_selection_metabox'), 10, 1 );
+
+ }
+
+ function phila_register_template_selection_metabox( $meta_boxes ){
+   $prefix = 'phila_';
+
+  $meta_boxes[] = array(
+    'id'       => 'template_selection',
+    'title'    => 'Choose Template',
+    'pages'    => array( 'department_page' ),
+    'context'  => 'advanced',
+    'priority' => 'high',
+
+    'fields' => array(
+
+      array(
+        'desc'  => '',
+        'id'    => $prefix . 'template_select',
+        'type'  => 'radio',
+        'class' => 'template-select',
+        'clone' => false,
+        'options' => array(
+          'homepage' => 'Homepage',
+          'subpage' => 'Subpage',
+          'resource_list' => 'Grouped List of On-Site Links'
+        ),
+       ),
+    ),
+  );
+
+   return $meta_boxes;
+  }
+
+}
