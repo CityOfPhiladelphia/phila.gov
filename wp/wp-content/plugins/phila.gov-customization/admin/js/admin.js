8897b23c56d29e261d6978ddc8000a489ae59485
Michael Kappeler <michael.kappeler@phila.gov>
2016-05-18 16:32:12 -0400

    Sets limit on all titles except Notices and Alerts

--- wp/wp-content/plugins/phila.gov-customization/admin/js/admin.js
+++ wp/wp-content/plugins/phila.gov-customization/admin/js/admin.js
@@ -1,171 +1,180 @@
 /* For all admins */
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
-  //Rename Pages to "Information Pages"
-  //This is here because of permissions weirdness as well as timing problems
-  $('#adminmenuwrap .wp-submenu a').each(function(i) {
-    if($(this).attr("href") == "edit.php?post_type=page") {
-        $(this).text("Information Page");
-      }else if($(this).attr("href") == "post-new.php?post_type=page") {
-        $(this).text("Add Information Page");
+    //Rename Pages to "Information Pages"
+    //This is here because of permissions weirdness as well as timing problems
+    $('#adminmenuwrap .wp-submenu a').each(function(i) {
+        if ($(this).attr("href") == "edit.php?post_type=page") {
+            $(this).text("Information Page");
+        } else if ($(this).attr("href") == "post-new.php?post_type=page") {
+            $(this).text("Add Information Page");
+        }
+    });
+
+    //no one can clone rn
+    $('#department-content-blocks .add-clone').css('visibility', 'hidden');
+
+    if ( $('.misc-pub-attachment input[value*=".pdf"]').val() ) {
+        $('.post-type-attachment #categorydiv input').prop('disabled', true);
+        $('.post-type-attachment #publication_typediv input').prop('disabled', true);
     }
-  });
+    if ( typeof typenow === 'undefined' ) {
+        return;
 
-  //no one can clone rn
-  $('#department-content-blocks .add-clone').css('visibility', 'hidden');
+    } else {
+        if ( ( typenow == 'department_page' || typenow == 'document' || typenow == 'service_post' || typenow == 'news_post' || typenow == 'phila_post' || typenow == 'press_release' ) && adminpage.indexOf( 'post' ) > -1 ) {
+          $('#post').validate({
+              rules: {
+                  'post_title': 'required'
+              }
+          });
+          $('#title').rules('add', {
+              maxlength: 72
+          });
+        }
+        if ( ( typenow == 'department_page' || typenow == 'document' || typenow == 'service_post' ) && adminpage.indexOf( 'post' ) > -1 ) {
+            $("#post").validate({
+                rules: {
+                    'post_title': 'required'
+                }
+            });
+        }
+        if ( typenow == 'news_post' && adminpage.indexOf( 'post' ) > -1 ) {
+            $('#post').validate({
+                rules: {
+                    'post_title': 'required'
+                }
+            });
+            $('#phila_news_desc').rules('add', {
+                required: true
+            });
+        }
 
-  if( $('.misc-pub-attachment input[value*=".pdf"]').val() ) {
-    $('.post-type-attachment #categorydiv input').prop( 'disabled', true );
-    $('.post-type-attachment #publication_typediv input').prop( 'disabled', true );
-  }
-  if ( typeof typenow === 'undefined'){
-      return;
+        if ( typenow == 'phila_post' && adminpage.indexOf( 'post' ) > -1 ) {
+            $('#post').validate({
+                rules: {
+                    'post_title': 'required'
+                }
+            });
+            $('#phila_post_desc').rules('add', {
+                required: true
+            });
+        }
 
-    }else{
+        if ( ( typenow == 'page' || typenow == 'service_post' ) && adminpage.indexOf( 'post' ) > -1 ) {
+            $('#post').validate({
+                rules: {
+                    'post_title': 'required'
+                }
+            });
 
-    if ( (typenow == 'department_page' || typenow == 'document' || typenow == 'service_post') && adminpage.indexOf('post') > -1 ){
-      $("#post").validate({
-          rules: {
-            'post_title' : 'required'
-          }
-        });
-    }
-    if (typenow == 'news_post' && adminpage.indexOf('post') > -1 ){
-      $('#post').validate({
-        rules: {
-           'post_title' : 'required'
-         }
-      });
-      $( '#phila_news_desc' ).rules( 'add', {
-        required: true
-      });
-    }
+            if ( $("#page-display input[name=phila_show_in_browse]").length ) {
 
-    if (typenow == 'phila_post' && adminpage.indexOf('post') > -1 ){
-      $('#post').validate({
-        rules: {
-           'post_title' : 'required'
-         }
-      });
-      $( '#title' ).rules( 'add', {
-        maxlength: 72
-      });
-      $( '#phila_post_desc' ).rules( 'add', {
-        required: true
-      });
-    }
+                $('#page-display input[name=phila_show_in_browse]').click(function() {
 
-    if ( ( typenow == 'page' || typenow == 'service_post' ) && adminpage.indexOf('post') > -1 ){
-      $('#post').validate({
-        rules: {
-           'post_title' : 'required'
-         }
-      });
+                    if ($(this).val() == 'yes') {
 
-      if ( $( "#page-display input[name=phila_show_in_browse]" ).length ) {
+                        $('#page-display .rwmb-textarea-wrapper').show();
 
-        $('#page-display input[name=phila_show_in_browse]').click(function(){
+                    } else if ($(this).val() == 'no') {
 
-          if ( $(this).val() == 'yes' ) {
+                        $('#page-display .rwmb-textarea-wrapper').hide();
+                    }
 
-            $('#page-display .rwmb-textarea-wrapper').show();
+                });
 
-          }else if( $(this).val() == 'no' ){
+                if ( $('#page-display input[name=phila_show_in_browse]:checked').val() == 'yes' ) {
 
-            $('#page-display .rwmb-textarea-wrapper').hide();
-          }
+                    $('#page-display .rwmb-textarea-wrapper').show();
 
-        });
+                } else if ( $('#page-display input[name=phila_show_in_browse]:checked').val() == 'no' ) {
 
-        if ( $('#page-display input[name=phila_show_in_browse]:checked').val() == 'yes' ){
+                    $('#page-display .rwmb-textarea-wrapper').hide();
 
-          $('#page-display .rwmb-textarea-wrapper').show();
+                }
 
-        }else if ($('#page-display input[name=phila_show_in_browse]:checked').val() == 'no'){
+                $("#phila_page_desc").prop('required', true);
 
-          $('#page-display .rwmb-textarea-wrapper').hide();
+            }
 
         }
 
-        $("#phila_page_desc").prop( 'required', true );
+        if ( typenow == 'phila_post' && adminpage.indexOf( 'post' ) > -1 ) {
 
-      }
+            $('a#link-post_tag').click();
 
-    }
+        }
 
-    if (typenow == 'phila_post' && adminpage.indexOf('post') > -1 ){
-
-      $('a#link-post_tag').click();
-
-    }
-
-    if ( ( typenow == 'press_release' || typenow == 'document') && adminpage.indexOf('post') > -1 ){
+        if ( ( typenow == 'press_release' || typenow == 'document' ) && adminpage.indexOf( 'post' ) > -1 ) {
 
 
-      $('.rwmb-date').datepicker();
-      if($(".rwmb-date").datepicker("getDate") === null) {
+            $('.rwmb-date').datepicker();
+            if ($(".rwmb-date").datepicker("getDate") === null) {
 
-        $('.rwmb-date').val($.datepicker.formatDate('MM dd, yy', new Date()));
-      }
-
-    }
-
-    if ( ( typenow == 'department_page') && adminpage.indexOf('post') > -1 ){
-        var colTypes = {};
-
-        function setColInfo(){
-
-          for (var row = 1; row < 3; row++){
-            for(var col = 1; col < 3; col++){
-              var moduleType = $('#phila_module_row_' + row + '_col_' + col + '_type').val();
-              var moduleOptions = '#phila_module_row_' + row + '_col_' + col + '_type';
-              colTypes['row'+row+'Col'+col] = {'row':row, 'col':col, 'loc':moduleOptions,'type':moduleType};
-            };
-          };
-
-        };
-
-        function hideOptions(c){
-
-            for (i in c){
-              if ( c[i]['row']=== 1){
-                $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_texttitle').closest('.rwmb-text-wrapper').hide();
-                $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_textarea').closest('.rwmb-textarea-wrapper').hide();
-                $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_post_style').closest('.rwmb-select-wrapper').hide();
-
-                if (c[i]['type'] === 'phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_blog_posts'){
-                  $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_post_style').closest('.rwmb-select-wrapper').toggle();
-                }
-                else if (c[i]['type'] === 'phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_custom_text'){
-                  $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_texttitle').closest('.rwmb-text-wrapper').toggle();
-                  $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_textarea').closest('.rwmb-textarea-wrapper').toggle();
-                }
-              }
-            else if ( c[i]['row']=== 2){
-              // Hide Calendar ID option
-              $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_id').closest('.rwmb-text-wrapper').hide();
-              // Hide Calendar URL option
-              $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_url').closest('.rwmb-url-wrapper').hide();
-
-              if (c[i]['type'] === 'phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_calendar'){
-                // Toggle Calendar ID option
-                $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_id').closest('.rwmb-text-wrapper').toggle();
-                // Toggle Calendar URL option
-                $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_url').closest('.rwmb-url-wrapper').toggle();
-              }
+                $('.rwmb-date').val($.datepicker.formatDate('MM dd, yy', new Date()));
             }
-          }
-        };
 
-        setColInfo();
-        hideOptions(colTypes);
+        }
 
-        $( "#phila_module_row_1_col_1_type, #phila_module_row_1_col_2_type, #phila_module_row_2_col_1_type, #phila_module_row_2_col_2_type" ).change(function() {
-          setColInfo();
-          hideOptions(colTypes);
-        });
+        if ( ( typenow == 'department_page') && adminpage.indexOf( 'post' ) > -1 ) {
+            var colTypes = {};
 
-      }
+            function setColInfo() {
 
-  }
+                for ( var row = 1; row < 3; row++ ) {
+                    for ( var col = 1; col < 3; col++ ) {
+                        var moduleType = $('#phila_module_row_' + row + '_col_' + col + '_type').val();
+                        var moduleOptions = '#phila_module_row_' + row + '_col_' + col + '_type';
+                        colTypes['row' + row + 'Col' + col] = {
+                            'row': row,
+                            'col': col,
+                            'loc': moduleOptions,
+                            'type': moduleType
+                        };
+                    };
+                };
+
+            };
+
+            function hideOptions(c) {
+
+                for ( i in c ) {
+                    if ( c[i]['row'] === 1 ) {
+                        $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_texttitle').closest('.rwmb-text-wrapper').hide();
+                        $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_textarea').closest('.rwmb-textarea-wrapper').hide();
+                        $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_post_style').closest('.rwmb-select-wrapper').hide();
+
+                        if ( c[i]['type'] === 'phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_blog_posts' ) {
+                            $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_post_style').closest('.rwmb-select-wrapper').toggle();
+                        } else if ( c[i]['type'] === 'phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_custom_text' ) {
+                            $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_texttitle').closest('.rwmb-text-wrapper').toggle();
+                            $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_textarea').closest('.rwmb-textarea-wrapper').toggle();
+                        }
+                    } else if ( c[i]['row'] === 2 ) {
+                        // Hide Calendar ID option
+                        $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_id').closest('.rwmb-text-wrapper').hide();
+                        // Hide Calendar URL option
+                        $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_url').closest('.rwmb-url-wrapper').hide();
+
+                        if ( c[i]['type'] === 'phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_calendar' ) {
+                            // Toggle Calendar ID option
+                            $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_id').closest('.rwmb-text-wrapper').toggle();
+                            // Toggle Calendar URL option
+                            $(c[i]['loc']).closest('.rwmb-group-wrapper').find('#phila_module_row_' + c[i]['row'] + '_col_' + c[i]['col'] + '_cal_url').closest('.rwmb-url-wrapper').toggle();
+                        }
+                    }
+                }
+            };
+
+            setColInfo();
+            hideOptions( colTypes );
+
+            $("#phila_module_row_1_col_1_type, #phila_module_row_1_col_2_type, #phila_module_row_2_col_1_type, #phila_module_row_2_col_2_type").change(function() {
+                setColInfo();
+                hideOptions( colTypes );
+            });
+
+        }
+
+    }
 });
