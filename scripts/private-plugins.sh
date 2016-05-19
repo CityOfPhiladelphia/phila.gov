0ceb2c2ae87e185ef0c16fd6a13ffd0ff0c4f777
Karissa Demi <k@karissademi.com>
2016-05-19 17:14:44 -0400

    Swap inclue-exclude with conditional logic

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -3,7 +3,7 @@
 # Install private plugins
 
 _dir="$(dirname "$0")"
-plugins="meta-box-group-1.1.1.zip meta-box-include-exclude-1.0.3.zip meta-box-show-hide-1.0.0.zip wpfront-user-role-editor-personal-pro-2.12.5.zip"
+plugins="meta-box-group-1.1.1.zip meta-box-include-exclude-1.0.3.zip meta-box-conditional-logic-1.3.0.zip wpfront-user-role-editor-personal-pro-2.12.5.zip"
 
 for plugin in $plugins; do
   wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")" > /dev/null
