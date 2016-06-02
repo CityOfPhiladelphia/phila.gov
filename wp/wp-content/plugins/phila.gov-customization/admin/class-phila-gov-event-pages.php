24c642b523cbd2f744d34281f2f1b72142af8331
Michael Kappeler <michael.kappeler@phila.gov>
2016-06-02 09:52:35 -0400

    Move event specific metaboxes from meta-boxes.php

--- wp/wp-content/plugins/phila.gov-customization/admin/class-phila-gov-event-pages.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/class-phila-gov-event-pages.php
@@ -82,6 +82,179 @@ if ( class_exists("Phila_Gov_Event_Pages" ) ){
       )
     );//Event Connect Information
 
+    $meta_boxes[] = array(
+      'title'    => 'Service Updates & Changes',
+      'pages'    => array( 'event_page' ),
+      'context'  => 'normal',
+      'priority' => 'high',
+
+      'fields' => array(
+        array(
+         'id' => 'service_updates',
+         'type' => 'group',
+         'clone'  => true,
+
+         'fields' => array(
+           array(
+              'name' => 'Update Type',
+              'id'   => $prefix . 'update_type',
+              'type' => 'select',
+              'placeholder' => 'Choose type...',
+              'options' => array(
+                'city' => 'City',
+                'roads' => 'Roads',
+                'transit' => 'Transit',
+                'trash' => 'Trash',
+              ),
+           ),
+           array(
+              'name' => 'Urgency Level',
+              'id'   => $prefix . 'update_level',
+              'type' => 'select',
+              'placeholder' => 'Choose type...',
+              'options' => array(
+                'normal' => 'Normal (Green)',
+                'warning' => 'Warning (Yellow)',
+                'critical' => 'Critical (Red)',
+              ),
+           ),
+           array(
+             'name'  => 'Update Message',
+             'id'    => $prefix . 'service_update_message',
+             'type'  => 'textarea',
+             'class' => 'service-update-message',
+             'size'  => '60'
+           ),
+           array(
+             'name'  => 'Update Link Text',
+             'id'    => $prefix . 'update_link_text',
+             'type'  => 'text',
+             'class' => 'update-link-text',
+             'size'  => '60'
+           ),
+           array(
+             'name'  => 'Update Link',
+             'id'    => $prefix . 'update_link',
+             'type'  => 'url',
+             'class' => 'update-link',
+           ),
+           array(
+             'name'  => 'Update Effective Date',
+             'id'    => $prefix . 'update_effective_date',
+             'type'  => 'text',
+             'class' => 'update-effective-date',
+             'size'  => '60'
+           ),
+          //  array(
+          //    'name'  => 'Start Date',
+          //    'id'    => $prefix . 'update_start_date',
+          //    'class' =>  'update-start-date',
+          //    'type'  => 'date',
+          //    'size'  =>  25,
+          //    'js_options' =>  array(
+          //      'dateFormat'=>'MM dd',
+          //    ),
+          //    'timestamp' => false
+          //  ),
+          //  array(
+          //    'name'  => 'End Date',
+          //    'id'    => $prefix . 'update_end_date',
+          //    'class' =>  'update-end-date',
+          //    'type'  => 'date',
+          //    'size'  =>  25,
+          //    'js_options' =>  array(
+          //      'dateFormat'=>'MM dd',
+          //    ),
+          //    'timestamp' => false
+          //  ),
+          )
+        )
+      )
+    );
+
+    $meta_boxes[] = array(
+      'title'    => 'Event Content Blocks',
+      'pages'    => array( 'event_page' ),
+      'context'  => 'normal',
+      'priority' => 'high',
+
+      'fields' => array(
+        array(
+          'name'  => 'Section Heading',
+          'desc'  => 'Custom heading for optional Content Blocks',
+          'id'    => $prefix . 'event_content_blocks_heading',
+          'type'  => 'text',
+          'class' => 'event_content_blocks_heading',
+          'clone' => false,
+        ),
+        array(
+          'name'  => '"More Link" Text',
+          'desc'  => 'Custom text to encourage visitors to "see more"',
+          'id'    => $prefix . 'event_content_blocks_link_text',
+          'type'  => 'text',
+          'class' => 'event_content_blocks_link_text',
+          'clone' => false,
+        ),
+        array(
+          'name'  => '"More Link" URL',
+          'desc'  => 'Location for more Content Block content',
+          'id'    => $prefix . 'event_content_blocks_link',
+          'type'  => 'url',
+          'class' => 'event_content_blocks_link',
+          'clone' => false,
+        ),
+        array(
+          'type' => 'divider'
+        ),
+        array(
+         'id' => 'event_content_blocks',
+         'type' => 'group',
+         'clone'  => true,
+
+         'fields' => array(
+           array(
+             'name'  => 'Title',
+             'id'    => $prefix . 'event_block_content_title',
+             'type'  => 'text',
+             'class' => 'event-block-content-title',
+             'desc'  => '70 character maximum.',
+             'size'  => '60'
+           ),
+            array(
+              'name'  => 'Image',
+              'id'    => $prefix . 'event_block_image',
+              'type'  => 'file_input',
+              'class' => 'event-block-image',
+              'desc'  => 'Image should be no smaller than 274px by 180px.'
+            ),
+            array(
+              'name' => 'Image Credit',
+              'id'   => $prefix . 'event_block_image_credit',
+              'type' => 'text',
+              'class' => 'event-block-image-credit',
+              'desc'  => 'Provide attribution information when necessary.',
+              'size'  => '60'
+            ),
+            array(
+              'name'  => 'Summary',
+              'id'    => $prefix . 'event_block_summary',
+              'type'  => 'textarea',
+              'class' => 'event-block-summary',
+              'desc'  => '225 character maximum.'
+            ),
+            array(
+              'name'  => 'Link to Content',
+              'id'    => $prefix . 'event_block_link',
+              'type'  => 'url',
+              'class' => 'event-block-url',
+              'desc'  => 'Enter a URL. E.g. http://alpha.phila.gov/oem',
+              'size'  => '60',
+            ),
+          )
+        )
+      )
+    ); //Event Content Blocks
+
     return $meta_boxes;
 
   }
