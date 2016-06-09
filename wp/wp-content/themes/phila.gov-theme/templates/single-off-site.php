fe05c971bae14289f07cd69f13a241a2b964cc09
Karissa Demi <k@karissademi.com>
2016-06-09 16:52:48 -0400

    Hide icon from screen readers

--- wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-off-site.php
@@ -20,6 +20,6 @@
   </div>
   <div class="small-24 medium-12 column intro equal pal external-site-link-right bg-ghost-gray">
     <a class="button man icon" href="<?php echo rwmb_meta( 'phila_dept_url', $args = array('type' => 'url')); ?>"><span class="show-for-large-only">You are now </span>leaving
-    <?php phila_util_echo_website_url();?> <i class="fa fa-sign-out bg-sidewalk"></i></a>
+    <?php phila_util_echo_website_url();?> <i class="fa fa-sign-out bg-sidewalk" aria-hidden="true"></i></a>
   </div>
 </div>
