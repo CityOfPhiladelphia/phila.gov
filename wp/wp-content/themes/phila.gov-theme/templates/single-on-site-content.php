59f71275d77cf0f040e85f475700c6b7d8a14a5a
Karissa Demi <k@karissademi.com>
2016-06-16 16:11:47 -0400

    Set vars and render call to action panel

--- wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
+++ wp/wp-content/themes/phila.gov-theme/templates/single-on-site-content.php
@@ -38,13 +38,33 @@
             }
           }
           $row_one_col_two_module = rwmb_meta( 'module_row_1_col_2');
+          $row_one_col_two_action_panel = rwmb_meta('module_row_1_col_2_call_to_action_panel');
+
           if (!empty($row_one_col_two_module)){
             $row_one_col_two_type = isset( $row_one_col_two_module['phila_module_row_1_col_2_type'] ) ? $row_one_col_two_module['phila_module_row_1_col_2_type'] : '';
+
             if ( $row_one_col_two_type == 'phila_module_row_1_col_2_blog_posts' ){
               $row_one_col_two_post_style = 'phila_module_row_1_col_2_post_style_cards';
-            } else {
+            } elseif( $row_one_col_two_type == 'phila_module_row_1_col_2_custom_text' ) {
+
               $row_one_col_two_text_title = $row_one_col_two_module['module_row_1_col_2_options']['phila_module_row_1_col_2_texttitle'];
+
               $row_one_col_two_textarea = $row_one_col_two_module['module_row_1_col_2_options']['phila_module_row_1_col_2_textarea'];
+
+            }else{
+              $row_one_col_two_action_panel_title = isset( $row_one_col_two_action_panel['phila_action_section_title'] ) ? $row_one_col_two_action_panel['phila_action_section_title'] : '' ;
+
+              $row_one_col_two_action_panel_summary = isset( $row_one_col_two_action_panel['phila_action_panel_summary'] ) ? $row_one_col_two_action_panel['phila_action_panel_summary'] : '';
+
+              $row_one_col_two_action_panel_cta_text = isset( $row_one_col_two_action_panel['phila_action_panel_cta_text'] ) ? $row_one_col_two_action_panel['phila_action_panel_cta_text'] : '';
+
+              $row_one_col_two_action_panel_link = isset( $row_one_col_two_action_panel['phila_action_panel_link'] ) ? $row_one_col_two_action_panel['phila_action_panel_link'] : '';
+
+              $row_one_col_two_action_panel_link_loc  = isset(  $row_one_col_two_action_panel['phila_action_panel_link_loc'] ) ? $row_one_col_two_action_panel['phila_action_panel_link_loc'] : '';
+
+              $row_one_col_two_action_panel_fa_circle  = isset( $row_one_col_two_action_panel['phila_action_panel_fa_circle'] ) ? $row_one_col_two_action_panel['phila_action_panel_fa_circle'] : '' ;
+
+              $row_one_col_two_action_panel_fa = isset( $row_one_col_two_action_panel['phila_action_panel_fa'] ) ? $row_one_col_two_action_panel['phila_action_panel_fa'] : '';
             }
           }
 
@@ -168,6 +188,35 @@
               </div>
             </div>
           </div>
+        <?php elseif (!$row_one_col_two_action_panel_summary == ''): ?>
+          <div class="large-6 columns">
+            <h2 class="contrast"><?php echo $row_one_col_two_action_panel_title; ?></h2>
+              <?php if (!$row_one_col_two_action_panel_link == ''): ?>
+                <a href="<?php echo $action_panel_link; ?>"  class="action-panel">
+                  <div class="panel">
+                    <header>
+                      <?php if ($row_one_col_two_action_panel_fa_circle): ?>
+                        <div>
+                          <span class="fa-stack fa-4x center" aria-hidden="true">
+                          <i class="fa fa-circle fa-stack-2x"></i>
+                          <i class="fa <?php echo $row_one_col_two_action_panel_fa; ?> fa-stack-1x fa-inverse"></i>
+                        </span>
+                      </div>
+                      <?php else: ?>
+                        <div>
+                          <span><i class="fa <?php echo $row_one_col_two_action_panel_fa; ?> fa-7x" aria-hidden="true"></i></span>
+                        </div>
+                      <?php endif; ?>
+                        <?php if (!$row_one_col_two_action_panel_cta_text == ''): ?>
+                          <span class="center <?php if ($row_one_col_two_action_panel_link_loc) echo 'external';?>"><?php echo $row_one_col_two_action_panel_cta_text; ?></span>
+                        <?php endif; ?>
+                    </header>
+                    <hr class="mll mrl">
+                      <span class="details"><?php echo $row_one_col_two_action_panel_summary; ?></span>
+                  </div>
+                </a>
+              <?php endif; ?>
+            </div>
           <!-- End Column Two -->
         <?php endif; ?>
       </div>
