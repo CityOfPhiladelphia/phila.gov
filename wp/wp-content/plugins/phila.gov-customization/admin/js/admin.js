2af7d235688e618f3db66d81ba10b4c1f277a1f0
Karissa Demi <k@karissademi.com>
2016-06-23 15:10:26 -0400

    Use readOnly on text fields, disable all but the correct dropdown selection

--- wp/wp-content/plugins/phila.gov-customization/admin/js/admin.js
+++ wp/wp-content/plugins/phila.gov-customization/admin/js/admin.js
@@ -157,15 +157,24 @@ jQuery(document).ready(function($) {
     }
   }
   function setOnePageInputVals(){
-    $('#phila_module_row_1_col_1_type').val('phila_module_row_1_col_1_custom_text').prop( 'disabled', true );
+    $("#phila_module_row_1_col_1_type option").each(function(){
+      if ( $(this).val() != 'phila_module_row_1_col_1_custom_text' ){
+        $(this).prop('disabled', true);
+      }
+    });
 
-    $('#phila_module_row_1_col_1_texttitle').val('What We Do').prop( 'disabled', true );
+    $("#phila_module_row_1_col_2_type option").each(function(){
+      if ( $(this).val() != 'phila_module_row_1_col_2_connect_panel' ){
+        $(this).prop('disabled', true);
+      }
+    });
 
-    $('#phila_module_row_1_col_2_type').val('phila_module_row_1_col_2_connect_panel').prop( 'disabled', true );
+    $('#phila_module_row_1_col_1_texttitle').val('What We Do').prop( 'readOnly', true );
+
   }
 
   if ( ( typenow == 'department_page' ) )  {
-    var templateSelect =   $('#phila_template_select');
+    var templateSelect = $('#phila_template_select');
 
     if ( templateSelect.val() == 'one_page_department' ){
       setOnePageInputVals();
@@ -180,6 +189,7 @@ jQuery(document).ready(function($) {
         //enable the disabled elements
         $('#phila_module_row_1').each(function(){
           var inputs = $( this ).find( ':input' );
+          inputs.prop( 'readOnly', false );
           inputs.prop( 'disabled', false );
         });
       }
