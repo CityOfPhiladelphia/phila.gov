61034c792e145fef6ac96efd0c77f166391cf0fe
Michael Kappeler <michael.kappeler@phila.gov>
2016-06-26 17:31:57 -0400

    Adjust for multiple Leadership entries. Set max char count for summary

--- wp/wp-content/themes/phila.gov-theme/partials/departments/content-staff-directory.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/content-staff-directory.php
@@ -34,7 +34,7 @@ if (has_category()):
       if ( $staff_leadership ):
         $staff_options = rwmb_meta('phila_leadership_options');
         $staff_summary = $staff_options['phila_summary'];
-
+        $staff_leadership_output .= '<div class="row">';
         // Leadership Thumbnail
         if ( has_post_thumbnail() ):
           $staff_photo = get_the_post_thumbnail( $post->ID, 'thumbnail', 'class= small-thumb mrm' );
@@ -64,6 +64,7 @@ if (has_category()):
           $staff_leadership_output .= '</div>';
           $staff_leadership_output .= '<div class="medium-16 columns">' . $staff_summary . '</div>';
         endif;
+        $staff_leadership_output .= '</div>';
 
       else:
 
@@ -83,9 +84,7 @@ if (has_category()):
         <div class="row">
           <div class="large-24 columns">
             <h2 class="contrast">Leadership</h2>
-              <div class="row">
                 <?php echo $staff_leadership_output; ?>
-              </div>
             </div>
         </div>
       </section>
