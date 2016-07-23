edf076686d0171de98cd525b4ea74e85a5ae455a
Michael Kappeler <michael.kappeler@phila.gov>
2016-07-23 14:41:26 -0400

    Adjust formatting

--- wp/wp-content/themes/phila.gov-theme/partials/departments/content-programs-initiatives.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/content-programs-initiatives.php
@@ -8,14 +8,16 @@
 <?php
   // MetaBox variables
   $p_i_page_rows = rwmb_meta('phila_row');
-  // print_r($dept_homepage_rows);
 ?>
 <div>
 <?php
     foreach ($p_i_page_rows as $key => $value):
       $current_row = $p_i_page_rows[$key];?>
+
       <!-- Grid Row -->
       <?php if ( $current_row['phila_grid_options'] == 'phila_grid_options_full'):
+
+          // Begin full width row
           $current_row_option = $current_row['phila_full_options']['phila_full_options_select'];
 
           if ( $current_row_option == 'phila_blog_posts'): ?>
@@ -26,12 +28,13 @@
               </div>
             </div>
 
-          <?php elseif ( $current_row_option == 'phila_full_width_calendar'):
-            //TODO: verify that these vars exist before setting... offload to function?
-            $cal_id = $current_row['phila_full_options']['phila_full_width_calendar']['phila_full_width_calendar_id'];
-            $cal_url = $current_row['phila_full_options']['phila_full_width_calendar']['phila_full_width_calendar_url'];?>
-
+          <?php elseif ( $current_row_option == 'phila_full_width_calendar'): ?>
             <!-- Full Width Calendar -->
+            <?php
+              //TODO: verify that these vars exist before setting... offload to function?
+              $cal_id = $current_row['phila_full_options']['phila_full_width_calendar']['phila_full_width_calendar_id'];
+              $cal_url = $current_row['phila_full_options']['phila_full_width_calendar']['phila_full_width_calendar_url'];
+            ?>
             <div class="row">
               <div class="columns">
                 <h2>Events</h2>
@@ -42,15 +45,19 @@
                 <?php echo do_shortcode('[calendar id="' . $cal_id . '"]'); ?>
               </div>
             </div>
-          <?php elseif ( $current_row_option == 'phila_callout'):
-            $callout_type = $current_row['phila_full_options']['phila_callout']['phila_callout_type'];
-            $callout_text = $current_row['phila_full_options']['phila_callout']['phila_callout_text'];
-          ?>
+
+          <?php elseif ( $current_row_option == 'phila_callout'): ?>
+            <!-- Display Callout -->
+            <?php
+              $callout_type = $current_row['phila_full_options']['phila_callout']['phila_callout_type'];
+              $callout_text = $current_row['phila_full_options']['phila_callout']['phila_callout_text'];
+            ?>
             <div class="row">
               <div class="large-24 column">
                   <?php echo do_shortcode('[callout summary="' . $callout_text . '" type="' . $callout_type . '"]'); ?>
               </div>
             </div>
+
           <?php elseif ($current_row_option == 'phila_get_involved'): ?>
             <!-- Display Multi Call to Action as Get Involved -->
             <?php
@@ -74,7 +81,7 @@
             ?>
 
           <?php elseif ( $current_row_option == 'phila_custom_text'): ?>
-
+            <!-- Display Custom Text -->
             <?php $custom_text = $current_row['phila_full_options']['phila_custom_text']; ?>
               <div class="row">
                 <div class="large-24 columns">
@@ -92,6 +99,8 @@
         <?php endif; ?>
 
       <?php elseif ($current_row['phila_grid_options'] == 'phila_grid_options_half'):
+
+        // Begin 1/2 x 1/2 row
         $current_row_option_one = $current_row['phila_half_options'] ['phila_half_col_1'];
         $current_row_option_two = $current_row['phila_half_options'] ['phila_half_col_2']; ?>
         <section class="mvl">
@@ -110,6 +119,7 @@
                     </div>
                   <?php endif; ?>
                 </div>
+
             <?php elseif ( $current_row_option_one['phila_half_col_1_option'] == 'phila_pullquote'):?>
               <div class="large-12 columns">
                 <?php echo do_shortcode('[pullquote quote="To meaningfully change the outcomes of our schools, the single most important investment we can make is in pre-K." attribution="Mayor Jim Kenney"]'); ?>
@@ -143,46 +153,48 @@
           </div>
         </section>
       <?php elseif ($current_row['phila_grid_options'] == 'phila_grid_options_thirds'):
+
+        // Begin 2/3 x 1/3 row
         $current_row_option_one = $current_row['phila_two_thirds_options'] ['phila_two_thirds_col'];
         $current_row_option_two = $current_row['phila_two_thirds_options'] ['phila_one_third_col']; ?>
 
         <section class="mvl">
           <div class="row">
-        <?php if ( $current_row_option_one['phila_two_thirds_col_option'] == 'phila_two_thirds_column_blog_posts'): ?>
-          <!-- Blog Content -->
-          <div class="large-18 columns">
-            <div class="row">
-              <?php echo do_shortcode('[recent-posts posts="3"]'); ?>
-            </div>
-          </div>
-        <?php elseif ( $current_row_option_one['phila_two_thirds_col_option'] == 'phila_custom_text'):?>
-
-          <?php $custom_text = $current_row_option_one['phila_custom_text']; ?>
-
-            <div class="large-18 columns">
-              <h2 class="contrast"><?php echo($custom_text['phila_custom_text_title']); ?></h2>
-              <div>
-                <?php echo($custom_text['phila_custom_text_content']); ?>
-              </div>
-              <?php if ( $custom_text == '' ) :?>
-                <div class="placeholder">
-                  Please enter content.
+            <?php if ( $current_row_option_one['phila_two_thirds_col_option'] == 'phila_two_thirds_column_blog_posts'): ?>
+              <!-- Blog Content -->
+              <div class="large-18 columns">
+                <div class="row">
+                  <?php echo do_shortcode('[recent-posts posts="3"]'); ?>
                 </div>
-              <?php endif; ?>
-            </div>
+              </div>
+            <?php elseif ( $current_row_option_one['phila_two_thirds_col_option'] == 'phila_custom_text'):?>
+              <!-- Custom Text -->
+              <?php $custom_text = $current_row_option_one['phila_custom_text']; ?>
 
-          <?php endif;?>
+                <div class="large-18 columns">
+                  <h2 class="contrast"><?php echo($custom_text['phila_custom_text_title']); ?></h2>
+                  <div>
+                    <?php echo($custom_text['phila_custom_text_content']); ?>
+                  </div>
+                  <?php if ( $custom_text == '' ) :?>
+                    <div class="placeholder">
+                      Please enter content.
+                    </div>
+                  <?php endif; ?>
+                </div>
 
-          <?php if ( $current_row_option_two['phila_one_third_col_option'] == 'phila_one_third_column_connect'):
-            $connect_panel = $current_row_option_two['phila_connect_panel'];
+              <?php endif;?>
 
-            // Set Connect Panel vars
-            $connect_vars = phila_connect_panel($connect_panel);
-            // TODO: get_template_part vs. include: can't pass arbitrary vars to templates
-            // get_template_part( 'partials/departments/content', 'connect' );
-            include(locate_template('partials/departments/content-connect.php'));
+              <?php if ( $current_row_option_two['phila_one_third_col_option'] == 'phila_one_third_column_connect'):
+                $connect_panel = $current_row_option_two['phila_connect_panel'];
 
-          endif; ?>
+                // Set Connect Panel vars
+                $connect_vars = phila_connect_panel($connect_panel);
+                // TODO: get_template_part vs. include: can't pass arbitrary vars to templates
+                // get_template_part( 'partials/departments/content', 'connect' );
+                include(locate_template('partials/departments/content-connect.php'));
+
+              endif; ?>
 
           </div>
         </div>
