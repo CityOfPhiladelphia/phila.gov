942f366cb46508d36c3fd3b1d4d0bfa14abc764a
Michael Kappeler <michael.kappeler@phila.gov>
2016-07-27 17:41:59 -0400

    Update programs and initiatives header markup and styles

--- wp/wp-content/themes/phila.gov-theme/partials/departments/content-programs-initiatives-header.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/content-programs-initiatives-header.php
@@ -13,11 +13,11 @@
 ?>
 <div class="row programs-header" >
   <div class="small-24 column">
-    <div class="header-wrap">
+    <div class="header-wrap valign">
     <?php if ( isset( $header_images['phila_p_i_header'] ) && !$header_images['phila_p_i_header'] == ''):?>
       <img alt="" class="" src="<?php echo $header_images['phila_p_i_header'];?>">
     <?php endif; ?>
-    <?php the_title( '<div class="text-wrap"><h2 class="sub-page-title">', '</h2></div>' ); ?>
+    <?php the_title( '<div class="valign-cell"><hr><h2 class="sub-page-title">', '</h2><hr></div>' ); ?>
     </div>
   </div>
 </div>
