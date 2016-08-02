2a065b9505edf18cf8d9b612d2812dfcd357b101
Michael Kappeler <michael.kappeler@phila.gov>
2016-08-02 17:22:03 -0400

    Add space between id and class attributes

--- wp/wp-content/themes/phila.gov-theme/partials/resource-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/resource-list.php
@@ -28,7 +28,7 @@
         $resource_list_slug = sanitize_title_with_dashes($resource_list_title);
 
         $list_item_output .= '<div class="medium-8 small-24 column resource-list end">';
-        $list_item_output .= '<header><h2 id="' . $resource_list_slug . '"class="h4 phm pvs">' . $resource_list_title . '</h2></header>';
+        $list_item_output .= '<header><h2 id="' . $resource_list_slug . '" class="h4 phm pvs">' . $resource_list_title . '</h2></header>';
         $list_item_output .= '<ul>';
 
         //items
