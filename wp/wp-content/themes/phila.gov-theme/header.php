0a30337c2ac58e84fd41f6b55e0df0a3305eed51
Karissa Demi <k@karissademi.com>
2016-08-15 11:29:41 -0400

    Update function name

--- wp/wp-content/themes/phila.gov-theme/header.php
+++ wp/wp-content/themes/phila.gov-theme/header.php
@@ -11,12 +11,12 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
 
-  <meta name="description" content="<?php echo_item_meta_desc(); ?>">
+  <meta name="description" content="<?php echo phila_get_item_meta_desc(); ?>">
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Swiftype -->
-  <meta class="swiftype" name="title" data-type="string" content="<?php echo str_replace(' | ' . get_bloginfo('name'), '', phila_filter_title( $title ) ); ?>" />
+  <meta class="swiftype" name="title" data-type="string" content="<?php echo str_replace(' | ' . get_bloginfo('name'), '', phila_filter_title( $title ) ); ?>">
 
   <?php if (is_single()) : ?>
     <meta class="swiftype" name="published_at" data-type="date" content="<?php echo get_the_time('c', $post->ID); ?>" />
