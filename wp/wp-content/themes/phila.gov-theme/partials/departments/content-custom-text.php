3b3a20144967b76d101791a34f1d0dd4739d83d2
Michael Kappeler <michael.kappeler@phila.gov>
2016-08-16 13:01:32 -0400

    Create custom-text and featured-page partials

---
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/content-custom-text.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ *
+ * Partial for rendering Custom Textareas
+ *
+ */
+?>
+<?php if ( !empty( $custom_text )):
+  $custom_text_title = $custom_text['phila_custom_text_title'];
+  $custom_text_content = $custom_text['phila_custom_text_content'];
+?>
+  <?php if ( !empty( $custom_text_title ) ) :?>
+    <h2 class="contrast"><?php echo( $custom_text_title ); ?></h2>
+  <?php endif; ?>
+  <?php if ( !empty( $custom_text_content ) ) :?>
+    <div>
+      <?php echo( $custom_text_content ); ?>
+    </div>
+  <?php else :?>
+    <div class="placeholder">
+      Please enter content.
+    </div>
+  <?php endif; ?>
+<?php endif; ?>
