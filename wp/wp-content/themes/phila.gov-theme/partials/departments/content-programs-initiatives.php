7e49168417630db884e6393312aaae79534f6dc0
Michael Kappeler <michael.kappeler@phila.gov>
2016-08-16 12:01:25 -0400

    Create content-custom-text-multi.php partial

--- wp/wp-content/themes/phila.gov-theme/partials/departments/content-programs-initiatives.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/content-programs-initiatives.php
@@ -223,37 +223,12 @@
 
             <?php elseif ( $current_row_option_one['phila_two_thirds_col_option'] == 'phila_custom_text_multi'):?>
               <?php
-              // TODO: Move this block to partials/content-custom-text-multi.php
-                $custom_text = $current_row_option_one['phila_custom_text_multi'];
-                $custom_text_title = $custom_text['phila_custom_row_title'];
-                $custom_text_group = $custom_text['phila_custom_text_group'];
+                $custom_text = isset ( $current_row_option_one['phila_custom_text_multi'] ) ? $current_row_option_one['phila_custom_text_multi'] : '';
+                if ( !empty($custom_text) ):
+                  include(locate_template('partials/departments/content-custom-text-multi.php'));
+                endif;
               ?>
-              <div class="large-18 columns custom-text-multi">
-                <h2 class="contrast"><?php echo($custom_text['phila_custom_row_title']); ?></h2>
-              <?php if ( is_array( $custom_text_group ) ):?>
-                <?php $item_count = count($custom_text_group); ?>
-                <?php $columns = phila_grid_column_counter( $item_count ); ?>
-                <div class="row <?php if( $item_count > 1 ) echo 'equal-height';?> ">
-                  <?php foreach ($custom_text_group as $key => $value):?>
-                    <div class="medium-<?php echo $columns ?> columns <?php if( $item_count > 1 ) echo 'equal';?>">
 
-                      <?php if ( isset( $custom_text_group[$key]['phila_custom_text_title'] ) && $custom_text_group[$key]['phila_custom_text_title'] != '') : ?>
-                        <h3><?php echo $custom_text_group[$key]['phila_custom_text_title']; ?></h3>
-                      <?php endif;?>
-
-                      <?php if ( isset( $custom_text_group[$key]['phila_custom_text_content'] ) && $custom_text_group[$key]['phila_custom_text_content'] != '') : ?>
-                        <p><?php echo $custom_text_group[$key]['phila_custom_text_content']; ?></p>
-                      <?php else :?>
-                        <div class="placeholder">
-                          Please enter content.
-                        </div>
-                      <?php endif;?>
-
-                    </div>
-                  <?php endforeach; ?>
-                </div>
-              <?php endif; ?>
-            </div>
             <?php endif;?>
 
               <?php
