33e89b171dc3a06ff53ed97466ecf2d15c80c99e
Karissa Demi <k@karissademi.com>
2016-09-01 09:44:20 -0400

    Enclosing div for proper padding

--- wp/wp-content/themes/phila.gov-theme/partials/content-service-default.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-service-default.php
@@ -30,34 +30,35 @@
       <?php if ( isset( $content['phila_wysiwyg_heading'] ) ): ?>
         <h3 class="black bg-ghost-gray h2 phm-mu mtl mbm"><?php echo $content['phila_wysiwyg_heading']; ?></h3>
       <?php endif; ?>
+      <div class="phm-mu">
+        <?php $wysiwyg_content = isset( $content['phila_unique_wysiwyg_content'] ) ? $content['phila_unique_wysiwyg_content'] : ''; ?>
+        <?php $is_address = isset( $content['phila_address_select'] ) ? $content['phila_address_select'] : ''; ?>
 
-      <?php $wysiwyg_content = isset( $content['phila_unique_wysiwyg_content'] ) ? $content['phila_unique_wysiwyg_content'] : ''; ?>
-      <?php $is_address = isset( $content['phila_address_select'] ) ? $content['phila_address_select'] : ''; ?>
+        <?php if ( (!empty($wysiwyg_content) || (!empty($is_address) ) ) ) : ?>
+          <?php echo apply_filters( 'the_content', $wysiwyg_content ) ;?>
+            <?php
+            $address_1 = isset( $content['phila_std_address']['address_group']['phila_std_address_st_1'] ) ? $content['phila_std_address']['address_group']['phila_std_address_st_1'] : '';
 
-      <?php if ( (!empty($wysiwyg_content) || (!empty($is_address) ) ) ) : ?>
-        <?php echo apply_filters( 'the_content', $wysiwyg_content ) ;?>
-          <?php
-          $address_1 = isset( $content['phila_std_address']['address_group']['phila_std_address_st_1'] ) ? $content['phila_std_address']['address_group']['phila_std_address_st_1'] : '';
+            $address_2 = isset( $content['phila_std_address']['address_group']['phila_std_address_st_2'] ) ? $content['phila_std_address']['address_group']['phila_std_address_st_2'] : '';
 
-          $address_2 = isset( $content['phila_std_address']['address_group']['phila_std_address_st_2'] ) ? $content['phila_std_address']['address_group']['phila_std_address_st_2'] : '';
+            $city = isset( $content['phila_std_address']['address_group']['phila_std_address_city'] ) ? $content['phila_std_address']['address_group']['phila_std_address_city'] : '';
 
-          $city = isset( $content['phila_std_address']['address_group']['phila_std_address_city'] ) ? $content['phila_std_address']['address_group']['phila_std_address_city'] : '';
+            $state = isset( $content['phila_std_address']['address_group']['phila_std_address_state'] ) ? $content['phila_std_address']['address_group']['phila_std_address_state'] : '';
 
-          $state = isset( $content['phila_std_address']['address_group']['phila_std_address_state'] ) ? $content['phila_std_address']['address_group']['phila_std_address_state'] : '';
+            $zip = isset( $content['phila_std_address']['address_group']['phila_std_address_zip'] ) ? $content['phila_std_address']['address_group']['phila_std_address_zip'] : '';
+            ?>
 
-          $zip = isset( $content['phila_std_address']['address_group']['phila_std_address_zip'] ) ? $content['phila_std_address']['address_group']['phila_std_address_zip'] : '';
-          ?>
-
-          <?php if ( $is_address == 1 ) : ?>
-          <div class="vcard">
-            <span class="street-address"><?php echo $address_1; ?></span><br>
-            <?php if ( !empty($address_2) ) : ?>
-              <span class="street-address"><?php echo $address_2; ?></span><br>
-            <?php endif; ?>
-            <span class="locality"><?php echo $city; ?></span>, <span class="region" title="Pennsylvania"><?php echo $state; ?></span>
-            <span class="postal-code"><?php echo $zip; ?></span>
+            <?php if ( $is_address == 1 ) : ?>
+            <div class="vcard">
+              <span class="street-address"><?php echo $address_1; ?></span><br>
+              <?php if ( !empty($address_2) ) : ?>
+                <span class="street-address"><?php echo $address_2; ?></span><br>
+              <?php endif; ?>
+              <span class="locality"><?php echo $city; ?></span>, <span class="region" title="Pennsylvania"><?php echo $state; ?></span>
+              <span class="postal-code"><?php echo $zip; ?></span>
+            </div>
+            <?php endif;?>
           </div>
-          <?php endif;?>
         <?php endif;?>
         </section>
       </div>
