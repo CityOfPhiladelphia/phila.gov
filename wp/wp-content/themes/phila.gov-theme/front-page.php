1d54ab7557a4c10c5f101a041626592286c68031
Karissa Demi <k@karissademi.com>
2016-09-02 09:54:49 -0400

    Revert "Merge branch 'service-nav' into service-updates"
    This reverts commit eeef837fb8fa69ddef997e7e8b594de3f6c01edc, reversing
    changes made to a7749249d980c1ac3c1c911f73ff5bd4678812ec.
    
    Merged into wrong branch. This commit undoes the error.

--- wp/wp-content/themes/phila.gov-theme/front-page.php
+++ wp/wp-content/themes/phila.gov-theme/front-page.php
@@ -56,6 +56,20 @@ get_header(); ?>
             </div>
           </div><!--#popular -->
           <div class="row expanded topic-main-nav">
+          <?php
+             $args = array(
+              'orderby' => 'name',
+              'fields'=> 'all',
+              'parent' => 0,
+              'hide_empty'=> true
+             );
+            $terms = get_terms( 'topics', $args );
+              if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+                foreach ( $terms as $term ) {
+                  echo '<a href="/browse/' . $term->slug .  '">' . $term->name . '</a>';
+                 }
+              }
+            ?>
           </div>
         </section>
       </div>
