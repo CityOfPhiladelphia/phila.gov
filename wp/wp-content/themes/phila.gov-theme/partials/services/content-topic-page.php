bffd83dd8b6f26653eb7b7f047390c7de7cc0889
Karissa Demi <k@karissademi.com>
2016-09-02 13:34:02 -0400

    Add row counter to topic display

--- wp/wp-content/themes/phila.gov-theme/partials/services/content-topic-page.php
+++ wp/wp-content/themes/phila.gov-theme/partials/services/content-topic-page.php
@@ -6,24 +6,28 @@
  */
  ?>
 <section class="row">
-<div class="medium-24">
-  <?php the_content(); ?>
-</div>
+  <div class="medium-24">
+    <?php the_content(); ?>
+  </div>
 </section>
 <?php $topic_args = array(
   'post_type'      => 'service_page',
   'posts_per_page' => -1,
   'post_parent'    => $post->ID,
   'order'          => 'ASC',
-  'orderby'        => 'menu_order'
+  'orderby'        => 'title'
 ); ?>
+<?php $counter = 0; ?>
 
 <?php $topic_children = new WP_Query( $topic_args ); ?>
 <?php if ( $topic_children->have_posts() ): ?>
-  <?php // TODO: count to three and close the row ?>
   <?php while ( $topic_children->have_posts() ) : ?>
     <?php $topic_children->the_post(); ?>
-      <div class="small-24 medium-8 column">
+    <?php $counter++; ?>
+    <?php if($counter % 3 == 1) :?>
+      <div class="row equal-height">
+    <?php endif;?>
+      <div class="small-24 medium-8 column end">
         <a href="<?php the_permalink(); ?>" class="card sub-topic equal">
           <div class="content-block">
             <h3><?php the_title(); ?></h3>
@@ -31,6 +35,9 @@
           </div>
         </a>
       </div>
+    <?php if($counter % 3 == 0) :?>
+      </div>
+    <?php endif;?>
   <?php endwhile;?>
 <?php endif; ?>
 <?php wp_reset_query(); ?>
