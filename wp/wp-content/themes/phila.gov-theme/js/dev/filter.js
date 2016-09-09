70b857867b3fcf5f0d56a05198fd241f5c55851c
Karissa Demi <k@karissademi.com>
2016-09-09 17:06:49 -0400

    Create filter js

---
+++ wp/wp-content/themes/phila.gov-theme/js/dev/filter.js
@@ -0,0 +1,20 @@
+jQuery(document).ready(function($) {
+  $('#service_filter input:checkbox').click(function(e) {
+    var activeEl = e.target.id;
+    console.log(activeEl);
+    if (activeEl != 'all-services'){
+      $('#all-services').prop('checked', false);
+      $('.result').each(function() {
+
+        if (activeEl != $(this).data('service')){
+          $(this).hide();
+        }
+
+      });
+    }else{
+      //when all services, uncheck everything else
+      $('.result').show();
+      $('#all-services').prop('checked', true);
+    }
+  });
+});
