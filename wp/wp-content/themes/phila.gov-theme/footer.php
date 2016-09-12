a34c580370b36cea79cfb0502959ef258d9025f5
Michael Kappeler <michael.kappeler@phila.gov>
2016-09-12 13:37:32 -0400

    Add 311 markup and styles

--- wp/wp-content/themes/phila.gov-theme/footer.php
+++ wp/wp-content/themes/phila.gov-theme/footer.php
@@ -15,11 +15,11 @@
     <div class="row">
       <div class="columns">
         <h2>Philly311</h2>
-        <p><a href="/" class="external">311</a> provides direct access to City government information, services, and real-time service updates. Multiple languages are available. Call 3-1-1 or tweet @philly311 for a quick response.</p>
+        <span><a href="/" class="external">311</a> provides direct access to City government information, services, and real-time service updates. Multiple languages are available. Call 3-1-1 or tweet @philly311 for a quick response.</span>
       </div>
     </div>
-    <div class="row pal">
-      <div class="small-24 medium-8 columns plm prxl">
+    <div class="row pvn pvl-mu">
+      <div class="small-24 medium-8 columns phl prxl-mu pvm pvn-mu bdr-sidewalk vr-right-mu">
         <h3>Interact with 311 online</h3>
 
         <a href="#/" class="button icon clearfix">
@@ -44,9 +44,10 @@
         </a>
 
       </div>
-      <div class="medium-16 columns trending-requests plxl">
+      <div class="medium-16 columns trending-requests phl plxl-mu pvm pvn-mu">
         <h3>Trending Requests</h3>
-          <ul class="columns-2">
+        <!-- TODO: Begin pulling these in from 311 -->
+          <ul class="columns-2-mu">
             <li>
               <a href="#" >Sample textarea</a>
             </li>
@@ -79,42 +80,87 @@
     <?php echo phila_get_dept_contact_blocks(); ?>
     <?php get_template_part( 'partials/content', 'modified' ) ?>
   </div>
-  <section class="fat">
+  <section class="fat pvs">
     <div class="row">
-      <div class="large-8 columns">
-        <h1>Government</h1>
-        <nav class="government">
+      <div class="medium-8 columns pll-mu pvm pvn-mu bdr-sidewalk bdr-bottom-sm vr-right-mu">
+        <h1 class="mtn mbm">Take action in your community</h1>
+        <nav class="take-action">
           <ul>
-            <li><a href="http://alpha.phila.gov"><?php phila_util_echo_website_url() ;?></a></li>
-            <li><a href="/departments"><?php printf( __('City Government Directory', 'phila-gov') ); ?></a></li>
-            <li><a href="/mayor"><?php printf( __('Mayor\'s Office', 'phila-gov') ); ?></a></li>
-            <li><a href="http://iframe.publicstuff.com/#?client_id=242"><?php printf( __('Report an Issue / 311', 'phila-gov') ); ?></a></li>
-            <li><a href="/news"><?php printf( __('News', 'phila-gov') ); ?></a></li>
+            <li class="pvxs"><a href="" class="external">PhillyRising Collaborative</a></li>
+            <li class="pvxs"><a href="" class="external">Citizens Planning Institute</a></li>
+            <li class="pvxs"><a href="" class="external">Neighborhood Liaison program</a></li>
+            <li class="pvxs"><a href="" class="external">Citizen action toolkit</a></li>
+            <li class="pvxs"><a href="" class="external">Clean up your block</a></li>
+            <li class="pvxs"><a href="" class="external">Apply for an event permit</a></li>
+            <li class="pvxs"><a href="" class="external">City, District, Council, &amp; Ward maps</a></li>
           </ul>
         </nav>
       </div>
-      <div class="large-16 columns">
-        <h1>Browse alpha.phila.gov</h1>
-        <nav class="browse-alpha">
-            <?php
-            /* temp top-level topics list w/ descriptions */
-               $args = array(
-                  'orderby' => 'name',
-                  'fields'=> 'all',
-                  'parent' => 0,
-                  'hide_empty'=> true
-                 );
-              $terms = get_terms( 'topics', $args );
-                if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
-                     echo '<ul class="columns-2">';
-                     foreach ( $terms as $term ) {
-                         echo '<li><a href="/browse/' . $term->slug .  '">' . $term->name . '</a>';
-                     }
-                     echo '</ul>';
-                    }
-                    ?>
-              </nav>
-        </div>
+      <div class="medium-8 columns pll-mu pvm pvn-mu bdr-sidewalk bdr-bottom-sm vr-right-mu">
+        <h1 class="mtn mbm"> Know your City government</h1>
+        <nav class="take-action">
+          <ul>
+            <li class="pvxs"><a href="" class="external">Mayor’s Office</a></li>
+            <li class="pvxs"><a href="" class="external">Citizens Planning Institute</a></li>
+            <li class="pvxs"><a href="" class="external">Neighborhood Liaison program</a></li>
+            <li class="pvxs"><a href="" class="external">Citizen action toolkit</a></li>
+            <li class="pvxs"><a href="" class="external">Clean up your block</a></li>
+            <li class="pvxs"><a href="" class="external">Apply for an event permit</a></li>
+            <li class="pvxs"><a href="" class="external">City, District, Council, &amp; Ward maps</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="medium-8 columns pll-mu pvm pvn-mu">
+        <h1 class="mtn mbm">Connect with City government</h1>
+        <nav class="city-social">
+          <ul class="inline-list">
+            <li class="pvxs">
+              <a href="" target="_blank" class="phm">
+                <i class="fa fa-tv fa-2x" title="TV" aria-hidden="true"></i>
+                <span class="show-for-sr">TV</span>
+              </a>
+            </li>
+            <li class="pvxs">
+              <a href="" target="_blank" class="phm">
+                <i class="fa fa-facebook fa-2x" title="Facebook" aria-hidden="true"></i>
+                <span class="show-for-sr">Facebook</span>
+              </a>
+            </li>
+            <li class="pvxs">
+              <a href="" target="_blank" class="phm">
+                <i class="fa fa-twitter fa-2x" title="Twitter" aria-hidden="true"></i>
+                <span class="show-for-sr">Twitter</span>
+              </a>
+            </li>
+            <br/>
+            <li class="pln pvxs">
+              <a href="" target="_blank" class="phm">
+                <i class="fa fa-instagram fa-2x" title="Instagram" aria-hidden="true"></i>
+                <span class="show-for-sr">Instagram</span>
+              </a>
+            </li>
+            <li class="pvxs">
+              <a href="" target="_blank" class="phm">
+                <i class="fa fa-youtube fa-2x" title="Youtube" aria-hidden="true"></i>
+                <span class="show-for-sr">Youtube</span>
+              </a>
+            </li>
+            <li class="pvxs">
+              <a href="" target="_blank" class="phm">
+                <i class="fa fa-flickr fa-2x" title="Flickr" aria-hidden="true"></i>
+                <span class="show-for-sr">Flickr</span>
+              </a>
+            </li>
+            <br/>
+            <li class="pvxs">
+              <a href="" target="_blank" class="phm">
+                <i class="fa fa-github fa-2x" title="GitHub" aria-hidden="true"></i>
+                <span class="show-for-sr">GitHub</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div><!-- row -->
   </section><!-- fat -->
   <div class="row classic">
