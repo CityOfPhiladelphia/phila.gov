bdc00d3021196235510e3ba0d9aeb8054676a9ec
Karissa Demi <k@karissademi.com>
2016-09-12 16:22:40 -0400

    Filter on mutiple service types

--- wp/wp-content/themes/phila.gov-theme/js/dev/filter.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/filter.js
@@ -1,20 +1,39 @@
 jQuery(document).ready(function($) {
+
+  $('#all').click(function() {
+    $('#service_filter input:checkbox').not(this).attr('checked', false);
+    $('.result').each(function() {
+      $(this).show();
+    });
+  });
+
+  var serviceType = {};
+
   $('#service_filter input:checkbox').click(function(e) {
-    var activeEl = e.target.id;
-    console.log(activeEl);
-    if (activeEl != 'all-services'){
-      $('#all-services').prop('checked', false);
-      $('.result').each(function() {
-
-        if (activeEl != $(this).data('service')){
-          $(this).hide();
-        }
-
-      });
-    }else{
-      $('.result').show();
-
-      $('#all-services').prop('checked', true);
+    var check = $(this);
+    if (check.prop('checked')) {
+      serviceType[check.attr('name')] = check.val();
+    } else {
+      delete serviceType[check.attr('name')];
     }
+
+    $.each(serviceType, function (index, value){
+      if (value != 'all'){
+        $('#all').prop('checked', false);
+
+        $('.result').filter(function( ){
+          if ( $(this).data('service') == value ){
+            $(this).hide();
+          }else{
+            $(this).show();
+          }
+        });
+
+      }else{
+        $('.result').show();
+        $('#all').prop('checked', true);
+      }
+
+    });
   });
 });
