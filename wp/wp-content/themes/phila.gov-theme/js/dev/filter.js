7ef5f428e74314bf5839b3c94461bf8bf8c3d78a
Karissa Demi <k@karissademi.com>
2016-09-13 09:59:55 -0400

    When nothing is selcted, default to all

--- wp/wp-content/themes/phila.gov-theme/js/dev/filter.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/filter.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function($) {
 
+  //TODO: Abstract for use on other data-types
   var parents = $('.a-z-group');
   var hiddenLetter = {};
 
+  //Hide/show sets of letter groups
   $('.a-z-group .result').bind('update', function() {
 
     parents.each(function(index, item) {
@@ -21,23 +23,20 @@ jQuery(document).ready(function($) {
 
       }
     });
-
-      $('.a-z-list nav li a').each( function ( i, value ) {
-        var el = $(value);
-        console.log(el);
-          if ( el.data('alphabet') in hiddenLetter ) {
-            $(this).attr('disabled', 'disabled');
-            $(this).attr('aria-disabled', true);
-          }else{
-            $(this).removeAttr('disabled');
-            $(this).attr('aria-disabled', false);
-          }
-        });
-
-
+    //Disable anchor links when no results present for letter group
+    $('.a-z-list nav li a').each( function ( i, value ) {
+      var el = $(value);
+      if ( el.data('alphabet') in hiddenLetter ) {
+        $(this).attr('disabled', 'disabled');
+        $(this).attr('aria-disabled', true);
+      }else{
+        $(this).removeAttr('disabled');
+        $(this).attr('aria-disabled', false);
+      }
+    });
   });
 
-
+  //Watch checkboxes
   $("#service_filter :checkbox").click(function() {
     $('.result').hide().addClass('is-hidden');
 
@@ -48,31 +47,33 @@ jQuery(document).ready(function($) {
       });
 
       parents.show();
+
       $(this).prop('checked', true);
       $('.result').show().removeClass('is-hidden');
 
-
     }else{
       $('#all').prop('checked', false);
-
     }
 
-  $('#service_filter :checkbox:checked').each(function(e) {
-    var serviceType = $(this).val();
+    if( $('#service_filter :checkbox:checked').length > 0 ){
 
-      $('.result').each(function( index, value ){
+      //Match array values with checked items
+      $('#service_filter :checkbox:checked').each(function(e) {
+        var serviceType = $(this).val();
 
-        $('.result').filter(function() {
-          var arr = $(this).data('service').toString().split(/,\s+/);
-            return $.inArray( serviceType, arr ) != -1;
-          }).show().removeClass('is-hidden');
+          $('.result').each(function( index, value ){
 
+            $('.result').filter(function() {
+              var arr = $(this).data('service').toString().split(/,\s+/);
+                return $.inArray( serviceType, arr ) != -1;
+              }).show().removeClass('is-hidden');
+          });
 
+        $('.a-z-group .result').trigger('update');
       });
-
-    });
-    $('.a-z-group .result').trigger('update');
-
+    }else{
+      $('#all').prop('checked', true);
+    }
   });
 
 });
