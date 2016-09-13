19a781d46d14952cbed5c2d4a84f8fe4d874462d
Karissa Demi <k@karissademi.com>
2016-09-12 22:46:10 -0400

    Use filter() method to determine which items are visible

--- wp/wp-content/themes/phila.gov-theme/js/dev/filter.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/filter.js
@@ -1,60 +1,61 @@
 jQuery(document).ready(function($) {
 
+  var parents = $('.a-z-group');
+
   $('.a-z-group .result').bind('update', function() {
 
-    var parents = $('.a-z-group');
-
-    // Loop through each parent element, finding only it's childeren
     parents.each(function(index, item) {
 
       var $item = $(item);
-      var childElements = $item.find('.result.hidden');
+      var childElements = $item.find('.result.is-hidden');
 
       var total = $item.find('.result');
 
       if (childElements.length === total.length) {
           $item.hide();
+      }else{
+        $item.show();
       }
     });
   });
 
 
-  $('#all').click(function() {
-    $('#service_filter input:checkbox').not(this).attr('checked', false);
-    $('.result').each(function() {
-      $(this).show();
-    });
-  });
+//  var serviceType = {};
 
-  var serviceType = {};
+  $("#service_filter :checkbox").click(function() {
+    $('.result').hide().addClass('is-hidden');
 
-  $('#service_filter input:checkbox').click(function(e) {
-    var check = $(this);
-    if (check.prop('checked')) {
-      serviceType[check.attr('name')] = check.val();
-    } else {
-      delete serviceType[check.attr('name')];
+    if ( $(this).val() == 'all' ){
+
+      $('#service_filter :checkbox').each(function(){
+        $(this).prop('checked', false);
+      });
+      parents.show();
+      $(this).prop('checked', true);
+      $('.result').show().removeClass('is-hidden');
+
+
+    }else{
+      $('#all').prop('checked', false);
+
     }
 
-    $.each(serviceType, function (index, value){
-      if (value != 'all'){
-        $('#all').prop('checked', false);
+  $('#service_filter :checkbox:checked').each(function(e) {
+    var serviceType = $(this).val();
 
-        $('.result').filter(function( ){
-          if ( $(this).data('service') == value ){
-            $(this).hide().addClass('hidden');
-          }else{
-            $(this).show();
-          }
-        });
+      $('.result').each(function( index, value ){
 
-      }else{
-        $('.result').show();
-        $('#all').prop('checked', true);
-      }
+        $('.result').filter(function() {
+          var arr = $(this).data('service').toString().split(/,\s+/);
+            return $.inArray( serviceType, arr ) != -1;
+          }).show().removeClass('is-hidden');
+
+
+      });
 
     });
-
     $('.a-z-group .result').trigger('update');
+
   });
+
 });
