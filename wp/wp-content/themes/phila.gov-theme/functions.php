a5039fb18106cba41c79e8a91be1fa4af69c32e0
Michael Kappeler <michael.kappeler@phila.gov>
2016-09-20 16:44:49 -0400

    Updates homepage news markup

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -973,8 +973,8 @@ function phila_get_home_news(){
 
     echo '<div class="content-block">';
 
-    the_title('<h3>', '</h3>');
-    
+    the_title('<h3 class="pvm">', '</h3>');
+
     the_date('' ,'<span class="small-text">','</span>');
 
     if ($contributor === ''){
