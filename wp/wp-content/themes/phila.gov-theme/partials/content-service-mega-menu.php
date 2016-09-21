f6040aa0583df00dc0be8a3216d5c08a3cd3820b
Karissa Demi <k@karissademi.com>
2016-09-21 09:52:56 -0400

    Swap var name & ensure count is closed

--- wp/wp-content/themes/phila.gov-theme/partials/content-service-mega-menu.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-service-mega-menu.php
@@ -17,34 +17,32 @@
   );
 ?>
 <?php $top_service_pages = new WP_Query( $service_args ); ?>
+<?php $i = 0; ?>
 <?php if ( $top_service_pages->have_posts() ) : ?>
   <div class="global-nav">
     <div class="dropdown-pane" id="services-mega-menu" data-dropdown data-options="closeOnClick:true; hover: true; hoverPane: true">
-      <?php $counter = 0 ?>
 
       <?php while ( $top_service_pages->have_posts() ) : $top_service_pages->the_post(); ?>
-        <?php $counter++;?>
+        <?php $i++;?>
 
-        <?php if ($counter % 3 == 0) :?>
-
-          <div class="row expanded mbxs collapse">
-
+        <?php if ( $i % 3 == 1 ) :?>
+          <div class="row expanded mbxs">
         <?php endif; ?>
-        <div class="medium-8 columns">
-          <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-        </div>
-
-        <?php if ($counter % 3 == 0) :?>
+          <div class="medium-8 columns end">
+            <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a>
+          </div>
+        <?php if ( $i % 3 == 0 ) :?>
           </div>
         <?php endif;?>
     <?php endwhile; ?>
-<div class="row expanded collapse bg-ghost-gray mega-menu-footer">
-  <div class="medium-8 float-right white bg-ben-franklin-blue left-arrow-indent">
-    <div class="valign">
-      <a href="" class="phl valign-cell service-directory">Services Directory</a>
     </div>
-  </div>
-  </div>
+    <div class="row expanded collapse bg-ghost-gray mega-menu-footer">
+      <div class="medium-8 float-right white bg-ben-franklin-blue left-arrow-indent">
+        <div class="valign">
+          <a href="" class="phl valign-cell service-directory">Services Directory</a>
+        </div>
+      </div>
+    </div>
   </div><!-- end service lvl 1 -->
 </div><!-- end global nav-->
 <?php wp_reset_postdata();?>
