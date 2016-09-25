3adb3b67ae3159fda3a73b0865969e04f4805423
Karissa Demi <k@karissademi.com>
2016-09-25 17:02:06 -0400

    Update svg

--- wp/wp-content/themes/phila.gov-theme/header.php
+++ wp/wp-content/themes/phila.gov-theme/header.php
@@ -126,9 +126,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <!-- Primary Navigation -->
             <div class="top-bar-right">
               <nav data-swiftype-index="false">
-                <ul class="vertical medium-horizontal dropdown menu pan" data-responsive-menu="drilldown medium-dropdown">
+                <ul class="vertical medium-horizontal dropdown menu pan" data-responsive-menu="drilldown medium-dropdown equal-height">
                   <li class="menu-item menu-item-type-custom menu-item-object-custom show-for-small-only"><a href="/"><i class="fa fa-home fa-lg" aria-hidden="true"></i> Home</a></li>
-                  <li class="service-menu-link is-dropdown-submenu-parent"  data-toggle="services-mega-menu" data-hover="false">
+                  <li class="service-menu-link is-dropdown-submenu-parent equal"  data-toggle="services-mega-menu" data-hover="false">
                     <a href="#" class="services-link"><i class="fa fa-list show-for-small-only" aria-hidden="true"></i> Services</a>
                     <ul class="menu vertical">
                       <?php
@@ -145,13 +145,13 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                       ?>
                     </ul>
                   </li>
-                  <li>
+                  <li class="equal">
                     <a href="#programs-and-initiatives"><i class="fa fa-info-circle show-for-small-only" aria-hidden="true"></i> Programs &amp; Initiatives</a>
                   </li>
-                  <li>
+                  <li class="equal">
                     <a href="#news-and-events"><i class="fa fa-microphone show-for-small-only" aria-hidden="true"></i> News &amp; Events</a>
                   </li>
-                  <li>
+                  <li class="equal">
                     <a href="#publications-and-forms"><i class="fa fa-file-text show-for-small-only" aria-hidden="true"></i> Publications &amp; Forms</a>
                   </li>
                   <li class="menu-item menu-item-type-custom menu-item-object-custom show-for-small-only bg-sidewalk">
