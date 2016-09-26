1b432c5751b50a464c8e9aad174c8f95254bc573
Karissa Demi <k@karissademi.com>
2016-09-26 17:20:19 -0400

    Add search markup

--- wp/wp-content/themes/phila.gov-theme/searchform.php
+++ wp/wp-content/themes/phila.gov-theme/searchform.php
@@ -5,11 +5,20 @@
  * @package phila-gov
  */
 ?>
-
-
-<form role="search" method="get" class="search" id="search-form" action="<?php echo home_url( '/search' ); ?>">
-  <label for="search-field"><span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span></label>
-    <input type="text" class="search-field" placeholder="<?php echo esc_attr_x( 'Search alpha.phila.gov', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" id="search-field"/>
-    <i class="fa fa-search search-icon show-for-small-only" aria-hidden="true"></i>
-    <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>">
-</form>
+<div class="search-pane">
+  <div class="dropdown-pane mega-menu-dropdown" data-dropdown data-v-offset="0" data-hover-pane="true" data-trap-focus="true" data-close-on-click="true" id="search-dropdown">
+    <div class="row columns expanded bg-ghost-gray">
+      <div class="row">
+        <div class="medium-12 columns small-centered">
+          <div class="paxl">
+            <form role="search" method="get" class="search" action="<?php echo home_url( '/search' ); ?>">
+              <label for="st-search-input"><span class="screen-reader-text"><?php echo _x( 'Search:', 'label' ) ?></span></label>
+              <input type="text" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" id="st-search-input"/>
+              <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
