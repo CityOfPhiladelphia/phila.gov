6099beccc342cc4d51037aa6950dffd8f7b7811c
Karissa Demi <k@karissademi.com>
2016-09-30 11:54:24 -0400

    Pick a style and stick with it, Karissa

--- wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
@@ -17,21 +17,21 @@ function noLink(e){
 //provide logo fallback for old browsers. Thanks https://css-tricks.com/a-complete-guide-to-svg-fallbacks/
 function svgasimg() {
   return document.implementation.hasFeature(
-    "http://www.w3.org/TR/SVG11/feature#Image", "1.1");
+    'http://www.w3.org/TR/SVG11/feature#Image', '1.1');
 }
 
 if (!svgasimg()){
-  var e = document.getElementsByTagName("img");
+  var e = document.getElementsByTagName('img');
   if (!e.length){
-    e = document.getElementsByTagName("IMG");
+    e = document.getElementsByTagName('IMG');
   }
   for (var i=0, n=e.length; i<n; i++){
     var img = e[i],
-        src = img.getAttribute("src");
+        src = img.getAttribute('src');
     if (src.match(/svgz?$/)) {
       /* URL ends in svg or svgz */
-      img.setAttribute("src",
-             img.getAttribute("data-fallback"));
+      img.setAttribute('src',
+             img.getAttribute('data-fallback'));
     }
   }
 }
@@ -261,7 +261,7 @@ jQuery(document).ready(function($) {
   /* Hijack mailchimp signup forms to use ajax handler */
 
   /*NOTE: this method requires that the form action URL from mailchimp uses subscribe/post-json */
-  ajaxMailChimpForm($("#mc-embedded-subscribe-form"), $("#mc_embed_signup"));
+  ajaxMailChimpForm($('#mc-embedded-subscribe-form'), $('#mc_embed_signup'));
   // Turn the given MailChimp form into an ajax version of it.
   // If resultElement is given, the subscribe result is set as html to
   // that element.
@@ -270,22 +270,22 @@ jQuery(document).ready(function($) {
     $form.submit(function(e) {
       e.preventDefault();
       if (!isValidEmail($form)) {
-        var error =  "A valid email address must be provided.";
+        var error =  'A valid email address must be provided.';
         $resultElement.append(error);
-        $resultElement.css("color", "#f99300");
+        $resultElement.css('color', '#f99300');
       } else {
-        $resultElement.css("color", "black");
-        $resultElement.append("Subscribing...");
+        $resultElement.css('color', 'black');
+        $resultElement.append('Subscribing...');
         submitSubscribeForm($form, $resultElement);
       }
     });
   }
   // Validate the email address in the form
   function isValidEmail($form) {
-    var email = $form.find("input[type='email']").val();
+    var email = $form.find('input[type="email"]').val();
     if (!email || !email.length) {
         return false;
-    } else if (email.indexOf("@") == -1) {
+    } else if (email.indexOf('@') == -1) {
         return false;
     }
     return true;
@@ -294,31 +294,31 @@ jQuery(document).ready(function($) {
   // Based on http://stackoverflow.com/a/15120409/215821
   function submitSubscribeForm($form, $resultElement) {
     $.ajax({
-      type: "GET",
-      url: $form.attr("action"),
+      type: 'GET',
+      url: $form.attr('action'),
       data: $form.serialize(),
       cache: false,
-      dataType: "jsonp",
-      jsonp: "c", // trigger MailChimp to return a JSONP response
-      contentType: "application/json; charset=utf-8",
+      dataType: 'jsonp',
+      jsonp: 'c', // trigger MailChimp to return a JSONP response
+      contentType: 'application/json; charset=utf-8',
       error: function(error){
           // According to jquery docs, this is never called for cross-domain JSONP requests
       },
       success: function(data){
-        if (data.result != "success") {
-          var message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
-          $resultElement.css("color", "#f99300");
-          if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
-            message = "You're already subscribed. Thank you.";
-            $resultElement.css("color", "black");
-          }else if (data.msg && data.msg.indexOf("zip code") >= 0) {
-            message = "Please enter a valid zip code.";
-            $resultElement.css("color", "f99300");
+        if (data.result != 'success') {
+          var message = data.msg || 'Sorry. Unable to subscribe. Please try again later.';
+          $resultElement.css('color', '#f99300');
+          if (data.msg && data.msg.indexOf('already subscribed') >= 0) {
+            message = 'You\'re already subscribed. Thank you.';
+            $resultElement.css('color', 'black');
+          }else if (data.msg && data.msg.indexOf('zip code') >= 0) {
+            message = 'Please enter a valid zip code.';
+            $resultElement.css('color', 'f99300');
           }
           $resultElement.append(message);
         } else {
-          $resultElement.css("color", "#58c04d");
-          $resultElement.html("Thank you!<br>You must confirm the subscription in your inbox.");
+          $resultElement.css('color', '#58c04d');
+          $resultElement.html('Thank you!<br>You must confirm the subscription in your inbox.');
         }
       }
     });
