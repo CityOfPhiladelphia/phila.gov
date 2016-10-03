dd576871a585380551abb2f65a14cdeec91df2c7
john-day1solutions <john@day1solutions.com>
2016-10-03 10:05:26 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,7 +8,7 @@ _dir="$(dirname "$0")"
 source "$_dir/lib/mo"
 
 echo 'Running grunt tasks'
-cd wp/wp-content/themes/phila.gov-theme
+cd /home/ubuntu/app/wp/wp-content/themes/phila.gov-theme
 npm install
 grunt
 cd -
