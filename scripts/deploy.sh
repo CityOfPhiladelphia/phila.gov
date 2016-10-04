b9c736803a653ebef7cff5c86e38e8a035393414
john-day1solutions <john@day1solutions.com>
2016-10-04 16:52:55 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,9 @@ _dir="$(dirname "$0")"
 
 source "$_dir/lib/mo"
 
+echo 'Running wp-config.sh'
+"$_dir/wp-config.sh"
+
 echo 'Running grunt tasks'
 cd /home/ubuntu/app/wp/wp-content/themes/phila.gov-theme
 npm install
