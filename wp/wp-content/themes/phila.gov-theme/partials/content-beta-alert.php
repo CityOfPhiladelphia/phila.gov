78e7658053c1d0c43b02764f296ee1bd86387a47
Michael Kappeler <michael.kappeler@phila.gov>
2016-10-05 11:07:43 -0400

    Update styles and markup for beta-alert

--- wp/wp-content/themes/phila.gov-theme/partials/content-beta-alert.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-beta-alert.php
@@ -3,11 +3,11 @@
  * Partial for the "beta alert," i.e. the bar says this site is a work in progress
 */
 
-//TODO: Move away from using id based styles / remove reference to #alpha-alert 
+//TODO: Move away from using id based styles / remove reference to #alpha-alert
 ?>
-<div data-swiftype-index='false' id="alpha-alert">
+<div data-swiftype-index='false' class="alert alert-persistent">
   <div class="row">
-    <div class="small-12 medium-18 columns">
+    <div class="small-24 columns">
       <p class="hide-for-small-only">
         <?php printf( 'We\'re piloting a new, user-friendly website design. To view the existing City website, visit <a class="go-back external" href="http://www.phila.gov" data-action="beta-opt-out">phila.gov</a>.'); ?>
       </p>
