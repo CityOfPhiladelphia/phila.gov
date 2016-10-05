e29b902a07767016e5584b215549868ed6f7deb7
john-day1solutions <john@day1solutions.com>
2016-10-05 15:58:32 -0400

    Update private-plugins.sh

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -3,7 +3,7 @@
 # Install private plugins
 
 _dir="$(dirname "$0")"
-plugins="meta-box-group-1.1.1.zip meta-box-include-exclude-1.0.3.zip meta-box-conditional-logic-1.3.0.zip wpfront-user-role-editor-personal-pro-2.12.5.zip"
+plugins="meta-box-group-1.1.1.zip meta-box-include-exclude-1.0.3.zip meta-box-conditional-logic-1.3.0.zip wpfront-user-role-editor-personal-pro-2.12.5.zip c3-cloudfront-clear-cache.zip"
 
 cd /home/ubuntu/app/wp
 for plugin in $plugins; do
