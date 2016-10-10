6c86a9726091ae3b5792ba0075d749cccb2fdde3
Karissa Demi <k@karissademi.com>
2016-10-10 14:07:01 -0400

    Add global h1

--- wp/wp-content/themes/phila.gov-theme/header.php
+++ wp/wp-content/themes/phila.gov-theme/header.php
@@ -68,6 +68,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 <!-- End Google Tag Manager -->
 
   <header class="global-nav no-js pbm pbn-mu mbn-mu">
+    <h1 class="accessible">City of Philadelphia</h1>
     <!-- Beta opt-out -->
     <?php get_template_part( 'partials/content', 'beta-alert' ); ?>
     <!-- Secondary Navigation -->
