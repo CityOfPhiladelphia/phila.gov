03bfca03bc03246d197361426e4c8acfe4851c8d
Tim Wisniewski <tim@timwis.com>
2016-10-11 13:02:28 -0400

    Add Code Deploy configuration (#354)
    * Create appspec.yml
    
    * Create postdeploy
    
    * Update postdeploy
    
    * Update postdeploy
    
    * Update postdeploy
    
    * Update appspec.yml
    
    * Update postdeploy
    
    * Update postdeploy
    
    * Create predeploy
    
    * Update appspec.yml
    
    * Update predeploy
    
    * Update predeploy
    
    * Delete fastcgi_cache.conf
    
    * Update nginx.conf
    
    * Update appspec.yml
    
    * Update predeploy
    
    * Update predeploy
    
    * Update appspec.yml
    
    * Update appspec.yml
    
    * Update deploy.sh
    
    * Update private-plugins.sh
    
    * Update deploy.sh
    
    * Update appspec.yml
    
    * Delete fastcgi_params
    
    * Update php.conf
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Rename postdeploy to pstdply
    
    * Update appspec.yml
    
    * Update deploy.sh
    
    * Update appspec.yml
    
    * Rename deploy.sh to deploy.sh.bak
    
    * Create deploy.sh
    
    * Update deploy.sh
    
    * Update appspec.yml
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update php.conf
    
    * Create fastcgi_cache.conf
    
    * Create fastcgi_params
    
    * Update appspec.yml
    
    * Update wp-config.sh
    
    * Update wp-config.sh
    
    * Delete deploy.sh.bak
    
    * Delete pstdply
    
    * Update deploy.sh
    
    * Update install.sh
    
    * Update appspec.yml
    
    * Update php.conf
    
    * Update php.conf
    
    * Update deploy.sh
    
    * Update wp-config.sh
    
    * Update wp-config.sh
    
    * Update private-plugins.sh
    
    * Update private-plugins.sh
    
    * Update private-plugins.sh
    
    * Update s3url.sh
    
    * Update private-plugins.sh
    
    * Update rate_limit.conf
    
    * Create W3TC.conf
    
    * Update W3TC.conf
    
    * Update root.conf
    
    * Update deploy.sh
    
    * Update wp-config.sh
    
    * Update wp-config.sh
    
    * Update deploy.sh
    
    * Delete W3TC.conf
    
    * Update deploy.sh
    
    * Update wp-config.sh
    
    * Update private-plugins.sh
    
    * Update wp-config.sh
    
    * Update deploy.sh
    
    * Update deploy.sh
    
    * Update wp-config.sh
    
    * Update private-plugins.sh
    
    * Update private-plugins.sh
    
    * Update cache_status.conf
    
    * Update cache_status.conf
    
    * Update nginx.conf
    
    * Update nginx.conf
    
    * make deploy.sh executable
    
    * private-plugins should be executed within app dir
    
    * Rename predeploy to predeploy.sh
    
    * Update appspec.yml
    
    * Update deploy.sh
    
    * remove core deps from install.sh

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -2,9 +2,14 @@
 #
 # Install private plugins
 
+source /home/ubuntu/.ssh/environment
+
 _dir="$(dirname "$0")"
 plugins="meta-box-group-1.1.1.zip meta-box-include-exclude-1.0.3.zip meta-box-conditional-logic-1.3.0.zip wpfront-user-role-editor-personal-pro-2.12.5.zip"
 
+cd /home/ubuntu/app
 for plugin in $plugins; do
-  wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")" > /dev/null
+  #wp plugin install --quiet --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")" > /dev/null
+  echo "--- Installing $plugin ---"
+  wp plugin install --force --activate "$("$_dir/s3url.sh" "$PHILA_PLUGIN_BUCKET" "$plugin")"
 done
