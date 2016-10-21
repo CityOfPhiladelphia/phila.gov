133689f563fd8e66df02cc17b095cbb57001b87d
Karissa Demi <k@karissademi.com>
2016-10-21 15:38:23 -0400

    Dump  var

--- wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-standard-metaboxes.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-standard-metaboxes.php
@@ -6,16 +6,18 @@ if ( class_exists('Phila_Gov_Standard_Metaboxes' ) ){
 
  class Phila_Gov_Standard_Metaboxes {
 
+
   public function __construct(){
+
     add_action('init', array( $this, 'phila_metabox_document_page_selector'), 1 );
 
   }
 
+
   public static function phila_metabox_document_page_selector(){
-    $prefix = 'phila_';
 
     return $meta_var_document_page_selector = array(
-      'id'  => $prefix . 'document_page_picker',
+      'id'  => 'phila_document_page_picker',
       'type'  => 'post',
       'post_type' => 'document',
       'field_type'  => 'select_advanced',
