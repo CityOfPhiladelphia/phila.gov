5ccdbf993008463842c3ae64d9d4fa94646ef6f6
Karissa Demi <k@karissademi.com>
2016-10-25 13:42:04 -0400

    Begin adding more contact fields

--- wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/page-template-contact-us.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/page-template-contact-us.php
@@ -5,38 +5,57 @@ add_filter( 'rwmb_meta_boxes', 'phila_register_department_contact_us' );
 function phila_register_department_contact_us( $meta_boxes ){
 
   $meta_boxes[] = array(
-    'title' => 'Contact row',
+    'title' => 'Contact us',
     'pages' => array('department_page'),
-    'visible' => array( 'phila_template_select', 'contact_us_v2' ),
 
     'fields' => array(
       array(
         'id' => 'phila_contact_us',
-        'title'    => 'Contact us',
         'context'  => 'normal',
         'priority' => 'high',
         'type'  => 'group',
         'clone' => true,
+        'visible' => array( 'phila_template_select', 'contact_us_v2' ),
 
         'fields' => array(
-          Phila_Gov_Standard_Metaboxes::phila_v2_icon_selection(),
-          //icon
-          //Heading
-          //Address
-          //phone
-          //email
-          //Fax
-          //hours
+          array(
+            'type' => 'heading',
+            'name' => 'Contact row title',
+          ),
+          array(
+            'id'  => 'phila_contact_row_title',
+            'type' => 'text',
+          ),
+          array(
+            'id'     => 'phila_contact_group',
+            'type'   => 'group',
+            'clone'  => true,
+            'max_clone' => 3,
 
+            'fields' => array(
+              array(
+                'type' => 'heading',
+                'name'  => 'Column title'
+              ),
+              array(
+                'id' => 'phila_contact_column_title',
+                'type'  => 'text',
+                'desc'  => 'Enter the title for this column of contact junk'
+              ),
+              Phila_Gov_Standard_Metaboxes::phila_v2_icon_selection(),
+              Phila_Gov_Standard_Metaboxes::phila_metabox_v2_address_fields(),
+              Phila_Gov_Standard_Metaboxes::phila_v2_phone(),
+              Phila_Gov_Standard_Metaboxes::phila_v2_fax(),
+              Phila_Gov_Standard_Metaboxes::phila_v2_email(),
+              Phila_Gov_Standard_Metaboxes::phila_v2_hours(),
+
+            ),
+          ),
         ),
       ),
     )
   );
 
-
-  $meta_boxes[] = array(
-
-  );
   return $meta_boxes;
 
 }
