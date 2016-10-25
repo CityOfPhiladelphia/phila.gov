bd4d68f35bc3daa3c4549674dd333880c61a3534
Michael Kappeler <michael.kappeler@phila.gov>
2016-10-25 16:36:29 -0400

    Initial v2 header called in single-department_page.php

--- wp/wp-content/themes/phila.gov-theme/single-department_page.php
+++ wp/wp-content/themes/phila.gov-theme/single-department_page.php
@@ -37,21 +37,67 @@ get_header(); ?>
         }
         $parent = get_post( $id );
         ?>
-        <h1 class="entry-title contrast mbn"><?php echo $parent->post_title;?></h1>
-    </header>
-  </div>
+
+        <?php $user_selected_template = phila_get_selected_template();?>
+
+        <?php
+          // TODO: check if this is one of an array of v2 templates... currently only checks homepage
+          if ($user_selected_template == 'department_homepage_v2') :
+        ?>
+
+          <div class="hero-content">
+            <?php // TODO: Get the actual hero image and call it in the img src ?>
+            <img class="show-for-small-only" src="https://ec2-54-165-91-192.compute-1.amazonaws.com/wp-content/themes/phila.gov-theme/img/beta_homepage_coverphoto_full.jpg" alt="">
+            <div class="hero-wrap">
+            <?php // TODO: Determine whether or not we need photo credits ?>
+              <!-- <div class="photo-credit small-text">
+                <span><i class="fa fa-camera" aria-hidden="true"></i> Photo by M. Edlow for Visit Philadelphia</span>
+              </div> -->
+              <?php // TODO: Get the actual hero image and apply as background via inline css ?>
+              <div class="row expanded ptl pbs pvxl-mu" style="">
+                <div class="medium-14 small-centered columns beta-message">
+
+                  <?php
+                  // TODO: Call title with $parent->post_title, regex match on 'Department of', 'Office of' to add line break. This can be refactored and should become a util if we intend to reuse
+
+                  $target_phrases = array("Department of","Office of");
+                  $break_after_phrases = array('<span class="h4 break-after" style="line-height:1;">Department of</span>','<span class="h4 break-after" style="line-height:1;">Office of</span>');
+                  $new_title = str_replace($target_phrases,$break_after_phrases, $parent->post_title );
+                  echo  '<h1 style="line-height:1">' . $new_title . '</h1>';
+                  ?>
+                  <?php //TODO: Use shortcode in below <p></p> ?>
+                  <p class="mvs mbn-mu"><em>Collecting taxes due to the City and the School District of Philadelphia.</em></p>
+                </div>
+              </div>
+              <?php
+                /*
+                Our navigation menu. We use categories to drive functionality.
+                This checks to make sure a category exists for the given page,
+                if it does, we render our menu w/ markup.
+                */
+                  phila_get_department_menu();
+              ?>
+            </div>
+          </div>
+        </header>
+      </div>
+        <?php else: ?>
+          <h1 class="entry-title contrast mbn"><?php echo $parent->post_title;?></h1>
+      </header>
+    </div>
     <?php
-  //get department homepage alerts
-  call_user_func( array( 'Phila_Gov_Department_Sites', 'department_homepage_alert' ) );
-  ?>
-  <?php
+    //get department homepage alerts
+    call_user_func( array( 'Phila_Gov_Department_Sites', 'department_homepage_alert' ) );
+
     /*
     Our navigation menu. We use categories to drive functionality.
     This checks to make sure a category exists for the given page,
     if it does, we render our menu w/ markup.
     */
       phila_get_department_menu();
-  ?>
+    ?>
+  <?php endif; ?>
+
   <?php
   if (function_exists('rwmb_meta')) {
     $external_site = rwmb_meta( 'phila_dept_url', $args = array('type' => 'url'));
