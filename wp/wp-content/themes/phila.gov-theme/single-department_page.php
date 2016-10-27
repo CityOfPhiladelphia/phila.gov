d949e44152fe981a8df32bfc339fdd884845c70a
Karissa Demi <k@karissademi.com>
2016-10-27 11:23:06 -0400

    Clean up intending

--- wp/wp-content/themes/phila.gov-theme/single-department_page.php
+++ wp/wp-content/themes/phila.gov-theme/single-department_page.php
@@ -30,60 +30,58 @@ get_header(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('department clearfix'); ?>>
   <header class="entry-header">
-      <?php
-       /* Get an array of Ancestors and Parents if they exist */
-        $parents = get_post_ancestors( $post->ID );
-        /* Get the top Level page->ID count base 1, array base 0 so -1 */
-        $id = ($parents) ? $parents[count($parents)-1]: $post->ID;
-        if ($id === $post->ID) {
+  <?php
+    /* Get an array of Ancestors and Parents if they exist */
+    $parents = get_post_ancestors( $post->ID );
+    /* Get the top Level page->ID count base 1, array base 0 so -1 */
+    $id = ($parents) ? $parents[count($parents)-1]: $post->ID;
 
-        }
-        $parent = get_post( $id );
+    $parent = get_post( $id );
 
-        if ( phila_util_is_v2_template() ) : ?>
+    if ( phila_util_is_v2_template() ) : ?>
 
-          <div class="hero-content">
-            <?php // TODO: Get the actual hero image and call it in the img src ?>
-            <img class="show-for-small-only" src="https://ec2-54-165-91-192.compute-1.amazonaws.com/wp-content/themes/phila.gov-theme/img/beta_homepage_coverphoto_full.jpg" alt="">
-            <div class="hero-wrap">
-            <?php // TODO: Determine whether or not we need photo credits ?>
-              <!-- <div class="photo-credit small-text">
-                <span><i class="fa fa-camera" aria-hidden="true"></i> Photo by M. Edlow for Visit Philadelphia</span>
-              </div> -->
-              <?php // TODO: Get the actual hero image and apply as background via inline css ?>
-              <div class="row expanded ptl pbs pvxl-mu">
-                <div class="medium-14 small-centered columns beta-message">
-
-                  <?php
-                  // TODO: Call title with $parent->post_title, regex match on 'Department of', 'Office of' to add line break. This can be refactored and should become a util if we intend to reuse
-
-                  $target_phrases = array("Department of","Office of");
-                  $break_after_phrases = array('<span class="h4 break-after" style="line-height:1;">Department of</span>','<span class="h4 break-after" style="line-height:1;">Office of</span>');
-                  $new_title = str_replace($target_phrases,$break_after_phrases, $parent->post_title );
-                  echo  '<h1 style="line-height:1">' . $new_title . '</h1>';
-                  ?>
-                  <p class="mvs mbn-mu"><em><?php echo phila_get_item_meta_desc(); ?></em></p>
-                </div>
-              </div>
+      <div class="hero-content">
+        <?php // TODO: Get the actual hero image and call it in the img src ?>
+        <img class="show-for-small-only" src="https://ec2-54-165-91-192.compute-1.amazonaws.com/wp-content/themes/phila.gov-theme/img/beta_homepage_coverphoto_full.jpg" alt="">
+        <div class="hero-wrap">
+        <?php // TODO: Determine whether or not we need photo credits ?>
+          <!-- <div class="photo-credit small-text">
+            <span><i class="fa fa-camera" aria-hidden="true"></i> Photo by M. Edlow for Visit Philadelphia</span>
+          </div> -->
+          <?php // TODO: Get the actual hero image and apply as background via inline css ?>
+          <div class="row expanded ptl pbs pvxl-mu">
+            <div class="medium-14 small-centered columns">
               <?php
-                /*
-                Our navigation menu. We use categories to drive functionality.
-                This checks to make sure a category exists for the given page,
-                if it does, we render our menu w/ markup.
-                */
-                  phila_get_department_menu();
+              // TODO: Call title with $parent->post_title, regex match on 'Department of', 'Office of' to add line break. This can be refactored and should become a util if we intend to reuse
+
+              $target_phrases = array("Department of","Office of");
+              $break_after_phrases = array('<span class="h3 break-after" style="line-height:1;">Department of</span>','<span class="h3 break-after" style="line-height:1;">Office of</span>');
+              $new_title = str_replace($target_phrases,$break_after_phrases, $parent->post_title );
+              echo  '<h1 style="line-height:1">' . $new_title . '</h1>';
               ?>
+              <?php if ($user_selected_template == 'homepage_v2'): ?>
+                <p class="mvs mbn-mu"><em><?php echo phila_get_item_meta_desc(); ?></em></p>
+              <?php endif;?>
             </div>
           </div>
-        </header>
+          <?php
+            /*
+            Our navigation menu. We use categories to drive functionality.
+            This checks to make sure a category exists for the given page,
+            if it does, we render our menu w/ markup.
+            */
+              phila_get_department_menu();
+          ?>
+        </div>
       </div>
-        <?php else: ?>
-          <div class="row">
-            <div class="columns">
-              <h1 class="entry-title contrast mbn"><?php echo $parent->post_title;?></h1>
-            </div>
-          </div>
-      </header>
+    </header>
+    <?php else: ?>
+      <div class="row">
+        <div class="columns">
+          <h1 class="entry-title contrast mbn"><?php echo $parent->post_title;?></h1>
+        </div>
+      </div>
+  </header>
     <?php
     //get department homepage alerts
     call_user_func( array( 'Phila_Gov_Department_Sites', 'department_homepage_alert' ) );
