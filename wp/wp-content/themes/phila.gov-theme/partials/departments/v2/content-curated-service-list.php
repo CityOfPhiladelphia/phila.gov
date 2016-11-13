7ff8734a4d9898066b2f25322b955e8883110bfd
Karissa Demi <k@karissademi.com>
2016-11-13 16:10:51 -0500

    Render services list

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-curated-service-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-curated-service-list.php
@@ -1,36 +1,34 @@
 <?php
 /**
  * Display curated service lists on department homepages
- *  
+ *
  * @package phila-gov
  */
 ?>
 
 <?php
-  $service_list = rwmb_meta( 'phila_v2_services_list' );
-  $services = phila_get_curated_service_list_v2( $service_list );
 
-  $icons = rwmb_meta( 'phila_v2_homepage_services' );
-  $icon = phila_loop_clonable_metabox( $icons );
+  $services_list = rwmb_meta( 'phila_v2_homepage_services' );
+  $services = phila_loop_clonable_metabox( $services_list );
 
-var_dump($icons);
 ?>
-
 <div class="row">
   <div class="columns content-list inverse">
+    <h2>Services</h2>
+    <div class="row collapse" data-equalizer>
       <?php foreach ( $services as $service ) : ?>
-        <?php var_dump($service); ?>
-        <div class="content-list-item valign pvm phm phl-l">
-          <a href="<?php echo get_permalink( $service['phila_v2_service_page'] ) ?>" class=" valign-cell">
-            <?php foreach ( $icon as $con ): ?>
+        <div class="content-list-items medium-8 columns">
 
-               <div><i class="fa <?php echo $con ?> fa-lg" aria-hidden="true"></i></div>
-             <?php endforeach; ?>
+          <div class="content-list-item valign pvm phm phl-l" data-equalizer-watch>
+            <a href="<?php echo get_permalink( $service['phila_v2_service_page'] ) ?>" class="valign-cell">
 
-           <div> <?php echo get_the_title( $service['phila_v2_service_page'] ) ?> </div>
-         </a>
+              <div><i class="fa <?php echo $service['phila_v2_icon'] ?> fa-2x" aria-hidden="true"></i></div>
+
+             <div> <?php echo get_the_title( $service['phila_v2_service_page'] ) ?> </div>
+           </a>
+         </div>
        </div>
-   <?php endforeach; ?>
-
- </div>
+      <?php endforeach; ?>
+   </div>
+  </div>
 </div>
