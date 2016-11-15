f196b76b2ceb8157202e85119a3ad79ad1a474ba
Michael Kappeler <michael.kappeler@phila.gov>
2016-11-15 16:24:21 -0500

    Minor cleanup

--- wp/wp-content/themes/phila.gov-theme/partials/departments/content-forms-documents-v2.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/content-forms-documents-v2.php
@@ -12,10 +12,9 @@
 <?php if ( !empty($forms_documents) ) : ?>
   <section>
     <div class="forms-documents row">
-  <?php foreach ($forms_documents as $key => $value): ?>
+    <?php foreach ( $forms_documents as $key => $value ): ?>
       <div class="large-8 columns">
-        <a href="<?php echo $forms_documents[$key]['phila_action_panel_link_multi']; ?>
-" class="card action-panel mbn">
+        <a href="<?php echo $forms_documents[$key]['phila_action_panel_link_multi']; ?>" class="card action-panel mbn">
           <div class="panel equal" data-equalizer-watch="" style="height: 237px;">
             <header class="">
               <div class="">
@@ -29,13 +28,12 @@
             <hr class="mll mrl"> <span class="details"><?php echo $forms_documents[$key]['phila_action_panel_summary_multi']; ?></span>
           </div>
         </a>
-        <?php if ( !empty( $forms_documents[$key]['phila_featured_documents'] ) ) :?>
-          <?php $featured_documents_array = $forms_documents[$key]['phila_featured_documents'];?>
+        <?php if ( !empty( $forms_documents[$key]['phila_featured_documents'] ) ) : ?>
+          <?php $featured_documents_array = $forms_documents[$key]['phila_featured_documents']; ?>
           <div class="resource-list">
             <ul>
               <?php foreach ( $featured_documents_array as $i => $v):?>
-                <?php $featured_document = get_post( $featured_documents_array[$i] );
-                ?>
+                <?php $featured_document = get_post( $featured_documents_array[$i] ); ?>
                 <li class="phm pvs clickable-row" data-href="http://">
                   <a href="<?php echo get_permalink($featured_document); ?>">
                     <div><i class="fa fa-file-text fa-lg" aria-hidden="true"></i></div>
@@ -46,16 +44,16 @@
             </ul>
           </div>
         <?php endif; ?>
-        <a class="see-all-right see-all-arrow float-right" href="<?php echo $forms_documents[$key]['phila_action_panel_link_multi']; ?>" aria-label="See all <?php echo strtolower( $forms_documents[$key]['phila_action_panel_cta_text_multi'] ); ?>">
-          <div class="valign equal-height">
-            <div class="see-all-label phm prxs valign-cell equal">See all</div>
-            <div class="valign-cell equal">
-              <img style="height:28px" src="<?php echo get_stylesheet_directory_uri(); ?>/img/see-all-arrow.svg" alt="">
+          <a class="see-all-right see-all-arrow float-right" href="<?php echo $forms_documents[$key]['phila_action_panel_link_multi']; ?>" aria-label="See all <?php echo strtolower( $forms_documents[$key]['phila_action_panel_cta_text_multi'] ); ?>">
+            <div class="valign equal-height">
+              <div class="see-all-label phm prxs valign-cell equal">See all</div>
+              <div class="valign-cell equal">
+                <img style="height:28px" src="<?php echo get_stylesheet_directory_uri(); ?>/img/see-all-arrow.svg" alt="">
+              </div>
             </div>
-          </div>
-        </a>
-      </div>
-  <?php endforeach; ?>
-</div>
-</section>
+          </a>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </section>
 <?php endif; ?>
