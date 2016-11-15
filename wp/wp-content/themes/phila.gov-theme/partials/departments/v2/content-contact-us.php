a5356f44d4168095585c69ff60816ceaf7b6b2a6
Karissa Demi <k@karissademi.com>
2016-11-14 20:48:45 -0500

    Begin rendering contact us

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-contact-us.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-contact-us.php
@@ -9,9 +9,20 @@
 $contact_us_vars = rwmb_meta('phila_contact_us');
 $contact_us = phila_loop_clonable_metabox($contact_us_vars);
 ?>
-
-<div class="row">
-  <div class="columns small-24 medium-8">
-    <?php var_dump($contact_us); ?>
-  </div>
-</div>
+<?php foreach ($contact_us as $row) : ?>
+    <div class="row">
+      <div class="columns">
+        <?php
+        $row_title = isset( $row['phila_contact_row_title']) ? $row['phila_contact_row_title'] : '';
+        ?>
+        <?php if ($row_title) :?>
+          <h2 class="contrast"> <?php echo $row_title; ?> </h2>
+        <?php endif; ?>
+        <?php foreach($row['phila_contact_group'] as $column ): ?>
+          <?php $column_title = isset( $column['phila_contact_column_title']) ? $column['phila_contact_column_title'] : '';?>
+          <h3><?php echo $column_title ?> </h3>
+          
+        <?php endforeach; ?>
+      </div>
+    </div>
+<?php endforeach; ?>
