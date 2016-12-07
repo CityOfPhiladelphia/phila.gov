8645e30c0c1535fe50d32f313a23d32bead1bf9c
Karissa Demi <k@karissademi.com>
2016-12-07 15:21:15 -0500

    Add noninteractive incase user input is required

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 /usr/local/bin/aws s3 cp s3://phila-prod-environment/environment /home/ubuntu/.ssh/environment
+
+export DEBIAN_FRONTEND=noninteractive
+
 sudo apt-get update
 sudo apt-get upgrade -y
