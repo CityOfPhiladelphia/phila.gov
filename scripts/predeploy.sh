fd70a2ba4b03cd4d4311179361a83462dbd55b1d
Karissa Demi <k@karissademi.com>
2016-12-07 15:39:10 -0500

    REALLY don't stop for user input

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -5,4 +5,4 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-sudo apt-get upgrade -y
+sudo apt-get Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
