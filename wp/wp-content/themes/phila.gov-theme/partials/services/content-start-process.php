391c2d59b091ed4af950b3a61479734d6135883f
Karissa Demi <k@karissademi.com>
2017-01-11 15:02:29 -0500

    Add error handling

--- wp/wp-content/themes/phila.gov-theme/partials/services/content-start-process.php
+++ wp/wp-content/themes/phila.gov-theme/partials/services/content-start-process.php
@@ -11,32 +11,35 @@
 ?>
 <p><?php echo phila_get_item_meta_desc() ?></p>
 
-<div class="row columns mvm">
-  <div class="panel info small-24 intro-item columns pal">
-    <div class="row">
-      <div class="medium-2 columns show-for-medium">
-        <i class="fa fa-flag fa-2x"></i>
+<?php if ( !empty( $start_process['content'] ) ) : ?>
+  <div class="row columns mvm">
+    <div class="panel info small-24 intro-item columns pal">
+      <div class="row">
+        <div class="medium-2 columns show-for-medium">
+          <i class="fa fa-flag fa-2x"></i>
+        </div>
+        <div class="medium-22 small-24 columns">
+          <h2 class="h4 mtn">Before you start</h2>
+          <?php echo $start_process['content'] ?>
+        </div>
       </div>
-      <div class="medium-22 small-24 columns">
-        <h2 class="h4 mtn">Before you start</h2>
-        <?php echo $start_process['content'] ?>
-      </div>
-    </div>
-    <div class="row">
-      <div class="ptl center columns">
-        <a data-swiftype-index="false" href="<?php echo $start_process['button_url']; ?>" class="button clearfix">
-          <div class="valign">
-            <div class="button-label valign-cell"><?php echo $start_process['button_text'] ?>
-              <?php if ( $start_process['button_external'] == 1 ) :?>
-                <i class="fa fa-external-link" aria-hidden="true"></i> <span class="accessible"> External link</span>
-              <?php endif;?></div>
+      <?php if ( !empty( $start_process['button_url'] ) && !empty( $start_process['button_text'] ) ): ?>
+        <div class="row">
+          <div class="ptl center columns">
+            <a data-swiftype-index="false" href="<?php echo $start_process['button_url']; ?>" class="button clearfix">
+              <div class="valign">
+                <div class="button-label valign-cell"><?php echo $start_process['button_text'] ?>
+                  <?php if ( $start_process['button_external'] == 1 ) :?>
+                    <i class="fa fa-external-link" aria-hidden="true"></i> <span class="accessible"> External link</span>
+                  <?php endif;?></div>
+              </div>
+            </a>
           </div>
-        </a>
-      </div>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
-</div>
-
+<?php endif;?>
 <?php get_template_part( 'partials/content', 'custom-markup-before-wysiwyg' ); ?>
 <div class="row">
   <div class="columns">
