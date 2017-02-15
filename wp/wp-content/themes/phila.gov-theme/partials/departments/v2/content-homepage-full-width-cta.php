4c3e4508e16ffc27eac7686cdd76710e1e4139ee
Karissa Demi <k@karissademi.com>
2017-02-15 14:49:35 -0500

    Rename survey to cta_full and include survey check

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-homepage-full-width-cta.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-homepage-full-width-cta.php
@@ -7,31 +7,31 @@
 
 ?>
 <?php
-  $survey_link = rwmb_meta( 'phila_v2_survey' );
-  $survey = phila_survey_display( $survey_link );
+  $cta_link = rwmb_meta( 'phila_v2_cta_full' );
+  $link = phila_cta_full_display( $cta_link );
 ?>
-<?php if ( !empty( $survey ) ) :?>
+<?php if ( !empty( $link ) ) :?>
   <div class="row mvxl">
-    <div class="columns panel survey">
+    <div class="columns panel <?php echo isset($cta_survey ) ? 'survey' : '' ?>">
       <div class="row equal-height">
         <div class="small-24 medium-18 columns valign equal">
           <div class="valign-cell">
-            <?php if ( !empty( $survey['title'] ) ) : ?>
-              <h3><?php echo $survey['title'] ?></h3>
+            <?php if ( !empty( $link['title'] ) ) : ?>
+              <h3><?php echo $link['title'] ?></h3>
             <?php endif; ?>
-            <?php if ( !empty( $survey['description'] ) ) : ?>
-              <p><?php echo $survey['description'] ?></p>
+            <?php if ( !empty( $link['description'] ) ) : ?>
+              <p><?php echo $link['description'] ?></p>
             <?php endif; ?>
           </div>
         </div>
-        <?php if ( !empty( $survey['url'] ) ) : ?>
+        <?php if ( !empty( $link['url'] ) ) : ?>
         <div class="small-24 medium-6 columns valign equal center">
           <div class="valign-cell">
-            <a href="<?php echo $survey['url'] ?>" class="button clearfix">
-              <?php if ( !empty( $survey['link_text'] ) ) :?>
+            <a href="<?php echo $link['url'] ?>" class="button clearfix">
+              <?php if ( !empty( $link['link_text'] ) ) :?>
                 <div class="valign">
-                  <div class="button-label valign-cell center"><?php echo $survey['link_text']?>
-                    <?php if ( $survey['external'] == 1 ) :?>
+                  <div class="button-label valign-cell center"><?php echo $link['link_text']?>
+                    <?php if ( $link['external'] == 1 ) :?>
                       <i class="fa fa-external-link" aria-hidden="true"></i> <span class="accessible"> External link</span>
                     <?php endif;?>
                   </div>
