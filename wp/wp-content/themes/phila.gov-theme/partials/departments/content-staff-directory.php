79174988067ebd48b4a462dd91c11449c98b9e7f
Karissa Demi <k@karissademi.com>
2017-04-19 14:46:19 -0400

    Add leadership label to v2 homepage renderings

--- wp/wp-content/themes/phila.gov-theme/partials/departments/content-staff-directory.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/content-staff-directory.php
@@ -152,7 +152,7 @@ if ( has_category() ):
     if (!empty($staff_leadership_array)):?>
       <div class="row staff-leadership <?php if ( $user_selected_template == 'staff_directory') echo 'mbl'; ?>">
           <div class="large-24 columns">
-            <?php if ($user_selected_template == 'one_page_department') : ?>
+            <?php if ($user_selected_template == 'one_page_department' || $user_selected_template == 'homepage_v2') : ?>
               <h2 class="contrast">Leadership</h2>
             <?php endif; ?>
             <?php
