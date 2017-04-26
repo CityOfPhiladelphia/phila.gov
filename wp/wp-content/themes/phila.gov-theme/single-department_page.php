9bac5be686058356bac8411d99ebf19779109789
Karissa Demi <k@karissademi.com>
2017-04-26 14:35:11 -0400

    Wrap tagline

--- wp/wp-content/themes/phila.gov-theme/single-department_page.php
+++ wp/wp-content/themes/phila.gov-theme/single-department_page.php
@@ -54,7 +54,11 @@ get_header(); ?>
             <div class="medium-18 small-centered columns text-overlay">
               <?php echo phila_get_department_homepage_typography( $parent ); ?>
               <?php if ($user_selected_template === 'homepage_v2'): ?>
-                <p class="sub-title mvs mbn-mu"><strong><?php echo phila_get_item_meta_desc( ); ?></strong></p>
+                <div class="row">
+                  <div class="medium-16 small-centered columns text-overlay">
+                    <p class="sub-title mbn-mu"><strong><?php echo phila_get_item_meta_desc( ); ?></strong></p>
+                  </div>
+                </div>
               <?php endif;?>
             </div>
           </div>
