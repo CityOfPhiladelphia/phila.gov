b8f8865d7cbc6f4afe3d57fa1a7561cc36c4bc60
Karissa Demi <k@karissademi.com>
2017-05-17 16:12:42 -0400

    No need for printf

--- wp/wp-content/themes/phila.gov-theme/partials/content-wip.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-wip.php
@@ -7,7 +7,7 @@
   <div class="row">
     <div class="small-24 columns">
       <p class="hide-for-small-only">
-        <?php printf( 'We\'re piloting a new, user-friendly website design. To view the existing City website, visit <a class="go-back external" aria-label="back to phila.gov" href="http://www.phila.gov?opt-out">phila.gov</a>.'); ?>
+        We're piloting a new, user-friendly website design. To view the existing City website, visit <a class="go-back external" aria-label="back to phila.gov" href="http://www.phila.gov?opt-out">phila.gov</a>.
       </p>
       <p class="show-for-small-only">
         Back to <a class="go-back external" aria-label="back to phila.gov" href="http://www.phila.gov?opt-out">phila.gov</a>.
