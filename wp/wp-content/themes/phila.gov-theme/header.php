42b60c21ede366737303a65321cbbd42950158fa
Karissa Demi <k@karissademi.com>
2017-05-24 13:03:05 -0400

    Clean up formatting

--- wp/wp-content/themes/phila.gov-theme/header.php
+++ wp/wp-content/themes/phila.gov-theme/header.php
@@ -10,10 +10,10 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="description" content="<?php echo phila_get_item_meta_desc( ); ?>">
-
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="theme-color" content="#2176d2">
 
   <!-- Swiftype -->
   <meta class="swiftype" name="title" data-type="string" content="<?php echo str_replace(' | ' . get_bloginfo('name'), '', phila_filter_title( $title ) ); ?>">
@@ -43,7 +43,7 @@
 
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> lang="en">
 
 <?php if ( !is_archive() && !is_tax() && !is_home() ) : ?>
   <!-- Google Tag Manager DataLayer -->
@@ -95,29 +95,29 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             <div class="row equal-height sticky-header-width">
               <div class="small-16 medium-6 columns equal valign small-push-4 medium-push-0">
                 <div class="valign-cell">
-              <a href="<?php echo get_home_url(); ?>" class="logo" tabindex="1" aria-label="City of Philadelphia">
-                <img src="<?php echo get_stylesheet_directory_uri() . "/img/city-of-philadelphia-logo.svg" ?>" data-fallback="//cityofphiladelphia.github.io/patterns/images/city-of-philadelphia.png" alt="City of Philadelphia">
-              </a>
-            </div>
-          </div>
-          <a href="#page" aria-hidden="false" class="accessible">Skip to main content</a>
-          <div class="medium-17 columns show-for-medium equal desktop-nav">
-            <div class="top-bar-right valign-mu">
-              <nav data-swiftype-index="false" class="valign-mu">
-                <ul class="horizontal menu pan valign-mu">
-                  <li class="services-menu-link" data-toggle="services-mega-menu">
-                    <a href="" class="no-link valign-cell" data-link="/services/" onclick="noLink(event)">Services</a>
-                    </li>
-                  <li>
-                    <a href="/programs-initiatives/" class="valign-cell">Programs &amp; initiatives</a>
-                  </li>
-                  <li>
-                    <a href="/news/" class="valign-cell"> News &amp; events</a>
-                  </li>
-                  <li>
-                    <a href="/documents/" class="valign-cell"> Publications &amp; forms</a>
-                  </li>
-                </ul>
+                  <a href="<?php echo get_home_url(); ?>" class="logo" tabindex="1" aria-label="City of Philadelphia">
+                    <img src="<?php echo get_stylesheet_directory_uri() . "/img/city-of-philadelphia-logo.svg" ?>" data-fallback="//cityofphiladelphia.github.io/patterns/images/city-of-philadelphia.png" alt="City of Philadelphia">
+                  </a>
+                </div>
+              </div>
+              <a href="#page" aria-hidden="false" class="accessible">Skip to main content</a>
+              <div class="medium-17 columns show-for-medium equal desktop-nav">
+                <div class="top-bar-right valign-mu">
+                  <nav data-swiftype-index="false" class="valign-mu">
+                    <ul class="horizontal menu pan valign-mu">
+                      <li class="services-menu-link" data-toggle="services-mega-menu">
+                        <a href="" class="no-link valign-cell" data-link="/services/" onclick="noLink(event)">Services</a>
+                      </li>
+                      <li>
+                        <a href="/programs-initiatives/" class="valign-cell">Programs &amp; initiatives</a>
+                      </li>
+                      <li>
+                        <a href="/news/" class="valign-cell"> News &amp; events</a>
+                      </li>
+                      <li>
+                        <a href="/documents/" class="valign-cell"> Publications &amp; forms</a>
+                      </li>
+                    </ul>
                 </nav>
               </div>
             </div>
@@ -129,20 +129,20 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 </button>
               </div>
             </div>
-          </div>
-        </div>
+          </div> <!-- close row -->
+        </div><!-- close columns -->
       </div>
     </div>
-  <!--Begin mobile nav -->
-  <div class="top-bar">
-    <div class="title-bar small-5 columns" data-responsive-toggle="mobile-nav" data-swiftype-index="false" data-hide-for="medium">
-      <button class="menu-icon" type="button" data-toggle>
-        <i class="fa fa-bars fa-3x" aria-hidden="true"></i>
-        <span class="title-bar-title">Menu</span>
-      </button>
-    </div>
-    <div class="primary-menu medium-15 medium-push-2 small-24 columns equal valign-mu" id="mobile-nav">
-      <div class="top-bar-right valign-mu show-for-small-only">
+    <!--Begin mobile nav -->
+    <div class="top-bar">
+      <div class="title-bar small-5 columns" data-responsive-toggle="mobile-nav" data-swiftype-index="false" data-hide-for="medium">
+        <button class="menu-icon" type="button" data-toggle>
+          <i class="fa fa-bars fa-3x" aria-hidden="true"></i>
+          <span class="title-bar-title">Menu</span>
+        </button>
+      </div>
+      <div class="primary-menu medium-15 medium-push-2 small-24 columns equal valign-mu" id="mobile-nav">
+        <div class="top-bar-right valign-mu show-for-small-only">
         <nav data-swiftype-index="false" class="valign-mu">
           <ul id="mobile-nav-drilldown" class="vertical menu pan valign-mu">
             <li><a href="/"><i class="fa fa-home fa-lg"></i> Home</a></li>
@@ -163,31 +163,30 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                   wp_list_pages($args);
                 ?>
               </ul>
-            </li>
-            <li tabindex="0">
-              <a href="/programs-initiatives/" class="valign-cell"><i class="fa fa-info-circle"></i> Programs &amp; initiatives</a>
-            </li>
-            <li tabindex="0">
-              <a href="/news/" class="valign-cell"><i class="fa fa-microphone"></i> News &amp; events</a>
-            </li>
-            <li tabindex="0">
-              <a href="/documents/" class="valign-cell"><i class="fa fa-file-text"></i> Publications &amp; forms</a>
-            </li>
-            <li class="bg-sidewalk" tabindex="0">
-              <a href="/mayor/"><i class="fa fa-university"></i> Mayor's Office</a>
-            </li>
-            <li class="bg-sidewalk" tabindex="0"><a href="/departments/"><i class="fa fa-sitemap"></i> City government directory</a>
-            </li>
-          </ul>
+              </li>
+              <li tabindex="0">
+                <a href="/programs-initiatives/" class="valign-cell"><i class="fa fa-info-circle"></i> Programs &amp; initiatives</a>
+              </li>
+              <li tabindex="0">
+                <a href="/news/" class="valign-cell"><i class="fa fa-microphone"></i> News &amp; events</a>
+              </li>
+              <li tabindex="0">
+                <a href="/documents/" class="valign-cell"><i class="fa fa-file-text"></i> Publications &amp; forms</a>
+              </li>
+              <li class="bg-sidewalk" tabindex="0">
+                <a href="/mayor/"><i class="fa fa-university"></i> Mayor's Office</a>
+              </li>
+              <li class="bg-sidewalk" tabindex="0"><a href="/departments/"><i class="fa fa-sitemap"></i> City government directory</a>
+              </li>
+            </ul>
           </nav>
         </div>
       </div>
     </div>
     <!--End mobile nav -->
-
   </div>
 </header>
-<div id="page" class="hfeed site">
+<div id="page">
   <?php
     //create alerts when appropriate
     call_user_func(array('Phila_Gov_Site_Wide_Alert_Rendering', 'create_site_wide_alerts')); ?>
@@ -202,4 +201,4 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       </div>
     <?php endif; ?>
 
-<div id="content" class="site-content">
+  <div id="content">
