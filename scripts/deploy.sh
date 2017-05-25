2e38bd7f5e7a73c7de7fd3ef006a85b7848db2da
Karissa Demi <k@karissademi.com>
2017-05-25 11:22:57 -0400

    Use npm, yarn is breaking the builds horribly

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,9 +12,9 @@ echo 'Running wp-config.sh'
 
 echo 'Running build tasks'
 cd /home/ubuntu/app/wp/wp-content/themes/phila.gov-theme
-yarn install
+npm install
 npm rebuild node-sass
-yarn build:js && yarn build:css
+npm run build:js && npm run build:css
 cd /home/ubuntu/app
 
 echo 'Modifying php configs'
