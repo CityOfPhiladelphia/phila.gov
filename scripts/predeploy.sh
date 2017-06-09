da24fe8882185ee9bb2700ded01185d61f9cf950
Karissa Demi <k@karissademi.com>
2017-06-09 13:52:18 -0400

    Use export, add sudo

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -2,7 +2,7 @@
 
 /usr/local/bin/aws s3 cp s3://phila-prod-environment/environment /home/ubuntu/.ssh/environment
 
-sudo DEBIAN_FRONTEND=noninteractive
+export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-apt-get -yq upgrade
+sudo apt-get -yq upgrade
