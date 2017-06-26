100e5abcdfce9040eade611806e6b185fdfed897
Karissa Demi <k@karissademi.com>
2017-06-26 16:01:42 -0400

    Use wp api

--- wp/wp-content/themes/phila.gov-theme/partials/content-service-mega-menu.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-service-mega-menu.php
@@ -7,43 +7,11 @@
 **/
 
 ?>
-<?php
-  $service_args = array(
-    'post_type' => 'service_page',
-    'post_parent' => 0,
-    'posts_per_page'  => -1,
-    'order' => 'ASC',
-    'orderby' => 'title'
-  );
-?>
-<?php $top_service_pages = new WP_Query( $service_args ); ?>
-<?php $i = 0; ?>
-<?php if ( $top_service_pages->have_posts() ) : ?>
-  <div id="services-list" class="global-nav show-for-medium">
-    <div class="dropdown-pane mega-menu-dropdown" data-dropdown data-v-offset="0" data-hover-pane="true" data-trap-focus="true" data-hover="true" data-close-on-click="true" id="services-mega-menu">
-      <div class="inner-wrapper">
-
-        <?php while ( $top_service_pages->have_posts() ) : $top_service_pages->the_post(); ?>
-          <?php $i++;?>
-          <?php $icon = rwmb_meta( 'phila_page_icon' ); ?>
-
-          <?php if ( $i % 3 == 1 ) :?>
-            <div class="row expanded mbxs" data-equalizer data-equalize-by-row="true">
-          <?php endif; ?>
-            <div class="medium-8 columns end">
-              <div class="valign">
-                <div class="valign-cell">
-                  <a href="<?php echo get_the_permalink(); ?>" data-equalizer-watch><span><i class="fa <?php echo $icon ?> fa-2x phm"></i> <?php echo get_the_title(); ?></span></a>
-                </div>
-              </div>
-            </div>
-          <?php if ( $i % 3 == 0 ) :?>
-            </div>
-          <?php endif;?>
-      <?php endwhile; ?>
-      <?php if ( $i % 3 == 2 || $i % 3 == 1 ) :?>
-        </div>
-      <?php endif;?>
+<div id="services-list" class="global-nav show-for-medium">
+  <div class="dropdown-pane mega-menu-dropdown" data-dropdown data-v-offset="0" data-hover-pane="true" data-trap-focus="true" data-hover="true" data-close-on-click="true" id="services-mega-menu">
+    <div class="inner-wrapper">
+      <div class="row expanded mbxs" data-equalizer data-equalize-by-row="true" data-services-menu>
+      </div>
       <div class="row expanded collapse bg-ghost-gray mega-menu-footer">
         <div class="medium-8 float-right white bg-dark-ben-franklin left-arrow-indent">
           <div class="valign">
@@ -54,6 +22,3 @@
     </div>
   </div><!-- end service lvl 1 -->
 </div><!-- end global nav-->
-<?php wp_reset_postdata();?>
-
-<?php endif; ?>
