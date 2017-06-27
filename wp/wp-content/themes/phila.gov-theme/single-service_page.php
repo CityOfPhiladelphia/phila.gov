82cc5eb154135909b72bdbc23b954128506a8dcf
Karissa Demi <k@karissademi.com>
2017-06-27 09:51:02 -0400

    Article tag should surround body content

--- wp/wp-content/themes/phila.gov-theme/single-service_page.php
+++ wp/wp-content/themes/phila.gov-theme/single-service_page.php
@@ -20,7 +20,7 @@
       }
       $icon = phila_get_page_icon( $parent_id );
       ?>
-    <article id="post-<?php the_ID(); ?>">
+    <div id="post-<?php the_ID(); ?>">
       <div class="row">
         <header class="small-24 columns">
           <h1 class="contrast">
@@ -51,58 +51,60 @@
             </nav>
         </div>
         <div class="medium-16 columns equal pbxl">
-          <header class="entry-header">
-            <h2><?php echo ( $parent_title != get_the_title() ) ?  get_the_title() : '' ?></h2>
-          </header>
-          <div data-swiftype-index='true' data-swiftype-name="body" data-swiftype-type="text" class="entry-content">
-          <?php if ($user_selected_template == 'tax_detail') : ?>
-            <?php get_template_part('partials/services/content', 'tax-detail');?>
-          <?php elseif ($user_selected_template == 'start_process') : ?>
-              <?php get_template_part('partials/services/content', 'start-process');?>
-              <!-- Service Stub  -->
-          <?php elseif ($user_selected_template == 'service_stub') : ?>
-            <?php if ( null !== rwmb_meta( 'phila_stub_source' ) ) : ?>
-              <?php $stub_source = rwmb_meta( 'phila_stub_source' );?>
-              <?php $post_id = intval( $stub_source );?>
+          <article>
+            <header class="entry-header">
+              <h2><?php echo ( $parent_title != get_the_title() ) ?  get_the_title() : '' ?></h2>
+            </header>
+            <div data-swiftype-index='true' data-swiftype-name="body" data-swiftype-type="text" class="entry-content">
+            <?php if ($user_selected_template == 'tax_detail') : ?>
+              <?php get_template_part('partials/services/content', 'tax-detail');?>
+            <?php elseif ($user_selected_template == 'start_process') : ?>
+                <?php get_template_part('partials/services/content', 'start-process');?>
+                <!-- Service Stub  -->
+            <?php elseif ($user_selected_template == 'service_stub') : ?>
+              <?php if ( null !== rwmb_meta( 'phila_stub_source' ) ) : ?>
+                <?php $stub_source = rwmb_meta( 'phila_stub_source' );?>
+                <?php $post_id = intval( $stub_source );?>
 
-                <?php $stub_args = array(
-                  'p' => $post_id,
-                  'post_type' => 'service_page'
-                ); ?>
-                <?php $stub_post = new WP_Query($stub_args); ?>
-                <?php if ( $stub_post->have_posts() ): ?>
-                  <?php while ( $stub_post->have_posts() ) : ?>
-                    <?php $stub_post->the_post(); ?>
-                    <?php $source_template =  rwmb_meta( 'phila_template_select'); ?>
-                      <?php if ($source_template == 'default') :?>
-                        <?php get_template_part('partials/content', 'default'); ?>
+                  <?php $stub_args = array(
+                    'p' => $post_id,
+                    'post_type' => 'service_page'
+                  ); ?>
+                  <?php $stub_post = new WP_Query($stub_args); ?>
+                  <?php if ( $stub_post->have_posts() ): ?>
+                    <?php while ( $stub_post->have_posts() ) : ?>
+                      <?php $stub_post->the_post(); ?>
+                      <?php $source_template =  rwmb_meta( 'phila_template_select'); ?>
+                        <?php if ($source_template == 'default') :?>
+                          <?php get_template_part('partials/content', 'default'); ?>
 
-                      <?php elseif ($source_template == 'tax_detail') : ?>
-                        <?php get_template_part('partials/services/content', 'tax-detail'); ?>
+                        <?php elseif ($source_template == 'tax_detail') : ?>
+                          <?php get_template_part('partials/services/content', 'tax-detail'); ?>
 
-                      <?php elseif ($source_template == 'topic_page') : ?>
-                        <?php get_template_part('partials/services/content', 'topic-page'); ?>
+                        <?php elseif ($source_template == 'topic_page') : ?>
+                          <?php get_template_part('partials/services/content', 'topic-page'); ?>
 
-                      <?php elseif ($source_template == 'start_process') : ?>
-                        <?php get_template_part('partials/services/content', 'start-process'); ?>
+                        <?php elseif ($source_template == 'start_process') : ?>
+                          <?php get_template_part('partials/services/content', 'start-process'); ?>
 
-                      <?php endif; ?>
-                    <?php endwhile; ?>
+                        <?php endif; ?>
+                      <?php endwhile; ?>
+                    <?php endif; ?>
+                    <?php wp_reset_query(); ?>
                   <?php endif; ?>
-                  <?php wp_reset_query(); ?>
-                <?php endif; ?>
-                <!-- END Service Stub -->
-              <?php elseif ($user_selected_template == 'topic_page'):?>
-                <?php get_template_part('partials/services/content', 'topic-page'); ?>
+                  <!-- END Service Stub -->
+                <?php elseif ($user_selected_template == 'topic_page'):?>
+                  <?php get_template_part('partials/services/content', 'topic-page'); ?>
 
-              <?php else : ?>
-                <?php get_template_part('partials/content', 'default'); ?>
+                <?php else : ?>
+                  <?php get_template_part('partials/content', 'default'); ?>
 
-            <?php endif; ?>
-            </div>
+              <?php endif; ?>
+              </div>
+            </article>
           </div>
         </div>
-    </article><!-- #post-## -->
+    </div><!-- #post-## -->
 
 
   <?php  endwhile; // end of the loop. ?>
