48b08f0f12d0c431c623e4329c2b3dbc8c7eb191
Karissa Demi <k@karissademi.com>
2017-06-27 14:39:46 -0400

    Add mailchimp

--- wp/wp-content/themes/phila.gov-theme/js/dev/main.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/main.js
@@ -6,5 +6,6 @@ require('./city-directory-list');
 require('./feedback-forms');
 require('./filter');
 require('./global-is-active');
+require('./mailchimp');
 require('./search');
 require('./skip-link-focus-fix');
