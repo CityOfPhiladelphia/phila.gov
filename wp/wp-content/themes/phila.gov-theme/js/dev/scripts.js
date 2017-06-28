6065a603b1469ebd9bbf99ca7dec23337830dc3d
Karissa Demi <k@karissademi.com>
2017-06-28 11:33:57 -0400

    Remove duplicate feedbackify

--- wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/scripts.js
@@ -41,34 +41,8 @@ jQuery(document).ready(function($) {
     }
   });
 
-  /* Async loaded feedback forms */
-  $.fn.feedbackify = function( src ) {
-    postscribe('#form-container', '<script  type="text/javascript" src="' + src + '"><\/script>');
-    $('html,body').animate({
-        scrollTop: $('.feedback').position().top - $('header .is-stuck').height()
-      }, 700 );
-    return false;
-  };
-
-  $("footer .feedback").on('click', function(){
-    $(this).feedbackify('https://form.jotform.com/jsform/62765090493967');
-  });
-
   $(".neighborhood-resources .feedback").on('click', function(){
     $(this).feedbackify('https://form.jotform.com/jsform/62516788470970');
   });
-
-  function hideEmptyCols(table) {
-    var rows = $("tr", table).length-1;
-    var numCols = $("th", table).length;
-    for ( var i=1; i<=numCols; i++ ) {
-        if ( $("span:empty", $("td:nth-child(" + i + ")", table)).length == rows ) {
-            $("td:nth-child(" + i + ")", table).hide(); //hide <td>'s
-            $("th:nth-child(" + i + ")", table).hide(); //hide header <th>
-        }
-    }
-  }
-
-  hideEmptyCols('.staff');
 
 });
