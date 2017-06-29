f10c04286b24115dd70629b6a053f6f53c074dc0
Karissa Demi <k@karissademi.com>
2017-06-29 14:22:08 -0400

    Clean up spacing

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -299,7 +299,6 @@ function phila_gov_scripts() {
 
   wp_enqueue_script( 'phila-scripts', get_stylesheet_directory_uri().'/js/phila-scripts.min.js', array(), '0.7.0', true );
 
-
   wp_enqueue_script( 'html5shiv', '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js', array(), '3.7.3', false);
 
   wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
