ee8de58fc219e2d667a5e3f3fcf91de04c696833
Karissa Demi <k@karissademi.com>
2017-06-29 14:17:09 -0400

    Use minified files

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -291,17 +291,17 @@ add_action( 'wp_enqueue_scripts', 'phila_gov_scripts');
 function phila_gov_scripts() {
   wp_deregister_script( 'jquery' );
 
-  wp_enqueue_style( 'standards', get_stylesheet_directory_uri() . '/css/styles.css' );
+  wp_enqueue_style( 'standards', get_stylesheet_directory_uri() . '/css/styles.min.css' );
 
   wp_enqueue_style( 'ie-only', get_stylesheet_directory_uri() . '/css/lt-ie-9.css', array( 'theme-styles' )  );
 
   wp_style_add_data( 'ie-only', 'conditional', 'lt IE 9' );
 
-  //TODO set flag to use minified js in prod
-  wp_enqueue_script( 'phila-scripts', get_stylesheet_directory_uri().'/js/phila-scripts.js', 1.0, true );
+  wp_enqueue_script( 'phila-scripts', get_stylesheet_directory_uri().'/js/phila-scripts.min.js', array(), '0.7.0', true );
 
 
   wp_enqueue_script( 'html5shiv', '//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js', array(), '3.7.3', false);
+
   wp_script_add_data( 'html5shiv', 'conditional', 'lt IE 9' );
 
 }
