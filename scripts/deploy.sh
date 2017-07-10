01877d7baaa5628cfe55758eacc9c36d112be689
Karissa Demi <k@karissademi.com>
2017-07-10 12:42:35 -0400

    Run postbuild script & ensure postcss-cli & autoprefixer are installed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,10 +13,11 @@ echo 'Running wp-config.sh'
 echo 'Running build tasks'
 cd /home/ubuntu/app/wp/wp-content/themes/phila.gov-theme
 npm install
-sudo npm i -g browserify uglify-js
+sudo npm i -g browserify uglify-js postcss-cli autoprefixer
 npm update
 npm rebuild node-sass
 npm run build:js && npm run build:css
+npm run postbuild
 cd /home/ubuntu/app
 
 echo 'Modifying php configs'
