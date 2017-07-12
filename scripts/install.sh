1bdb7e84264d8b344931f724c40379078daca9e9
Karissa Demi <k@karissademi.com>
2017-07-12 16:33:19 -0400

    Move npm install to install steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,6 +9,13 @@ sudo apt-get update
 echo "Allowing hq access"
 echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDde/ohFVNJyZgI3KXXt9uwipFNpu0oVFxZzeht/WAu9PRuiqOh9PUCHT+Ca/R1y70ltrO8OqGjOkmbmxBt99nvncxF8LPjQUG1rUePDOqOGNWs/d56rhLAsVM7cWXZG7xdLkJt8c5VrtXHsbRzaJ28RRwqT6C/x+ZPtx7POl/x1t8gNGeagAbbS3hq5O77ymHe4lukgcz4K5TuU8y36fH0Qp1Doe3exCDaN2DdBIz/OXYQru6vO5yRWvxMUTkxjh7GO1qsn0efWEfknrZekINNck/wP1hbvbP9xjwntyhOjxLAQrwYF6nH1iNY6J+hW/0qYrFdZqpC7dL+cmm+XVUH hq' >> ~/.ssh/authorized_keys
 
+echo "Install npm & command line utilities"
+sudo npm i -g browserify uglify-js postcss-cli autoprefixer
+cd /home/ubuntu/app/wp/wp-content/themes/phila.gov-theme
+npm install
+npm update
+cd /home/ubuntu/app
+
 if [ "$PHILA_TEST" ]; then
   $_dir/unison.sh
   $_dir/gen-cert.sh
