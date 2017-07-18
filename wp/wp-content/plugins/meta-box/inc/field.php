c91add52cc4a12ca16f2177feaf4ba71e2227cf9
Karissa Demi <k@karissademi.com>
2017-07-18 16:51:41 -0400

    Metabox 4.12.2

--- wp/wp-content/plugins/meta-box/inc/field.php
+++ wp/wp-content/plugins/meta-box/inc/field.php
@@ -184,8 +184,13 @@ abstract class RWMB_Field {
 			return '';
 		}
 
-		$object_type = ! empty( $args['object_type'] ) ? $args['object_type'] : 'post';
-		$storage = rwmb_get_storage( $object_type );
+		if ( isset( $args['object_type'] ) ) {
+			$storage = rwmb_get_storage( $args['object_type'] );
+		} elseif ( isset( $field['storage'] ) ) {
+			$storage = $field['storage'];
+		} else {
+			$storage = rwmb_get_storage( 'post' );
+		}
 
 		if ( ! isset( $args['single'] ) ) {
 			$args['single'] = $field['clone'] || ! $field['multiple'];
@@ -282,10 +287,11 @@ abstract class RWMB_Field {
 	 */
 	public static function save( $new, $old, $post_id, $field ) {
 		$name = $field['id'];
+		$storage = $field['storage'];
 
 		// Remove post meta if it's empty.
 		if ( '' === $new || array() === $new ) {
-			delete_post_meta( $post_id, $name );
+			$storage->delete( $post_id, $name );
 			return;
 		}
 
@@ -300,7 +306,7 @@ abstract class RWMB_Field {
 			}
 			// Reset indexes.
 			$new = array_values( $new );
-			update_post_meta( $post_id, $name, $new );
+			$storage->update( $post_id, $name, $new );
 			return;
 		}
 
@@ -310,17 +316,17 @@ abstract class RWMB_Field {
 			$new = (array) $new;
 			$new_values = array_diff( $new, $old );
 			foreach ( $new_values as $new_value ) {
-				add_post_meta( $post_id, $name, $new_value, false );
+				$storage->add( $post_id, $name, $new_value, false );
 			}
 			$old_values = array_diff( $old, $new );
 			foreach ( $old_values as $old_value ) {
-				delete_post_meta( $post_id, $name, $old_value );
+				$storage->delete( $post_id, $name, $old_value );
 			}
 			return;
 		}
 
 		// Default: just update post meta.
-		update_post_meta( $post_id, $name, $new );
+		$storage->update( $post_id, $name, $new );
 	}
 
 	/**
@@ -352,6 +358,7 @@ abstract class RWMB_Field {
 			'class'      => '',
 			'disabled'   => false,
 			'required'   => false,
+			'autofocus'  => false,
 			'attributes' => array(),
 		) );
 
@@ -368,11 +375,12 @@ abstract class RWMB_Field {
 	 */
 	public static function get_attributes( $field, $value = null ) {
 		$attributes = wp_parse_args( $field['attributes'], array(
-			'disabled' => $field['disabled'],
-			'required' => $field['required'],
-			'id'       => $field['id'],
-			'class'    => '',
-			'name'     => $field['field_name'],
+			'disabled'  => $field['disabled'],
+			'autofocus' => $field['autofocus'],
+			'required'  => $field['required'],
+			'id'        => $field['id'],
+			'class'     => '',
+			'name'      => $field['field_name'],
 		) );
 
 		$attributes['class'] = implode( ' ', array_merge( array( "rwmb-{$field['type']}" ), (array) $attributes['class'] ) );
