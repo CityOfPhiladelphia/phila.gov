f5f7e4683a3f4e4e4877dcdcc2e3eb5b1c2d5b29
Karissa Demi <k@karissademi.com>
2017-07-28 15:44:19 -0400

    Turn off autocomplete

--- wp/wp-content/themes/phila.gov-theme/page-search.php
+++ wp/wp-content/themes/phila.gov-theme/page-search.php
@@ -18,7 +18,7 @@ get_header(); ?>
         <div class="medium-16 columns">
           <form role="search" method="get" class="search" action="<?php echo home_url( '/search' ); ?>">
             <label for="st-search-input"><span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span></label>
-            <input type="text" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" id="st-search-input"/>
+            <input type="text" class="search-field" placeholder="<?php echo esc_attr_x( 'Search', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" id="st-search-input"/ autocomplete="off">
             <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>">
           </form>
         </div>
