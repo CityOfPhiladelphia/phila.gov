ba8c8df994589629915026c7a32a7d50def32d07
Karissa Demi <k@karissademi.com>
2017-08-28 15:18:12 -0400

    Clean up press contact

--- wp/wp-content/themes/phila.gov-theme/partials/press-release-meta.php
+++ wp/wp-content/themes/phila.gov-theme/partials/press-release-meta.php
@@ -7,26 +7,29 @@
   if ( function_exists('rwmb_meta') ) :
     $category = phila_get_current_department_name( get_the_category() );
 
-    $press_contacts = rwmb_meta( 'press_release_contact' );
+    $press_contact_vars = rwmb_meta( 'press_release_contact' );
+    $press_contacts = phila_loop_clonable_metabox($press_contact_vars);
 
     $press_date = rwmb_meta( 'phila_press_release_date', $args = array('type' => 'date'));
     printf( _e( '<span class="date-released"><strong>For immediate release: </strong>' . $press_date . '</span>', 'phila-gov' ) );
 
     printf ( _e('<span><strong>Published by:</strong> ' . $category . '</span>', 'phila-gov' ) ) ;
 
-    foreach( $press_contacts as $contact ) :
-      $press_name = isset( $contact['phila_press_release_contact_name'] ) ? $contact['phila_press_release_contact_name'] : '';
+    if (isset($press_contacts)) : ?>
+    <span><strong>Contact: </strong>
+      <?php foreach( $press_contacts as $contact ) :
 
-      $press_phone = isset( $contact['phila_press_release_contact_phone'] ) ? $contact['phila_press_release_contact_phone'] : '';
+      $press_name = isset( $contact['phila_press_release_contact_name'] ) ? trim($contact['phila_press_release_contact_name'] ) : '';
 
-      $press_email = isset( $contact['phila_press_release_contact_email'] ) ? $contact['phila_press_release_contact_email'] : '';
+      $press_phone = isset( $contact['phila_press_release_contact_phone'] ) ? trim($contact['phila_press_release_contact_phone']) : '';
 
-      $phone_exists = ( $press_phone == '' ) ? '' : $press_phone . ', ';
+      $press_email = isset( $contact['phila_press_release_contact_email'] ) ? trim($contact['phila_press_release_contact_email']) : '';
+      ?>
+      <?php echo (!empty($press_name) ) ? ($press_name) : ''; ?>
+      <?php echo (!empty($press_email) ) ? ('<a href="mailto:' . $press_email . '">' . $press_email . '</a>') : ''; ?>
+      <?php echo (!empty($press_phone) ) ? ($press_phone) : ''; ?>
+    <?php endforeach;?>
+  </span>
 
-      $all_contacts = trim($press_name) . ', '. trim($phone_exists) . ' <a href="mailto:' . $press_email . '">' . $press_email . '</a> ' ;
-    endforeach;
-
-    printf( _e( '<span><strong>Contact: </strong>' . $all_contacts . '</span>', 'phila-gov' ) );
-
-  endif;
-?>
+  <?php endif;?>
+<?php endif; ?>
