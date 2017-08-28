d454b7ecba61005539bf7adfe423aa57b7caa0e9
Karissa Demi <k@karissademi.com>
2017-08-28 11:44:31 -0400

    Add FB share

---
+++ wp/wp-content/themes/phila.gov-theme/js/dev/social-media.js
@@ -0,0 +1,14 @@
+var fbElement = document.getElementById('fb-share');
+
+if (fbElement) {
+  fbElement.onclick = function() {
+    FB.ui({
+      method: 'share',
+      display: 'popup',
+      mobile_iframe: true,
+      href: window.location.href,
+    }, function(response){
+       console.log(response);
+    });
+  }
+}
