c2232d7fbbfc2739d82ae71f811f2377158ebd80
Karissa Demi <k@karissademi.com>
2017-09-01 11:39:46 -0400

    Add slugifier function

--- wp/wp-content/themes/phila.gov-theme/inc/utilities.php
+++ wp/wp-content/themes/phila.gov-theme/inc/utilities.php
@@ -105,3 +105,15 @@ function phila_util_return_parsed_email( $email_address ){
   return $staff_email_parsed;
 
 }
+//thanks https://stackoverflow.com/questions/5305879/automatic-clean-and-seo-friendly-url-slugs/9535967#9535967
+function phila_format_uri( $string, $separator = '-' )
+{
+    $accents_regex = '~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i';
+    $special_cases = array( '&' => 'and', "'" => '');
+    $string = mb_strtolower( trim( $string ), 'UTF-8' );
+    $string = str_replace( array_keys($special_cases), array_values( $special_cases), $string );
+    $string = preg_replace( $accents_regex, '$1', htmlentities( $string, ENT_QUOTES, 'UTF-8' ) );
+    $string = preg_replace("/[^a-z0-9]/u", "$separator", $string);
+    $string = preg_replace("/[$separator]+/u", "$separator", $string);
+    return $string;
+}
