83458dea9e255dc3e1ad172b55797356de0bddc0
Karissa Demi <k@karissademi.com>
2017-09-06 10:31:10 -0400

    Add error handling

--- wp/wp-content/themes/phila.gov-theme/js/dev/posts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/posts.js
@@ -1,26 +1,29 @@
 var $thumb = $('.js-thumbnail-image').detach();
 $('.post-content > div').prepend($thumb);
 
-var postRestBase = phila_js_vars.postRestBase;
-var postID = phila_js_vars.postID;
+if (typeof phila_js_vars !== 'undefined') {
 
-$(document).on('open.zf.reveal', '#phila-lightbox', function (e) {
-  var $modal = $(this);
-  var $content = $('.lightbox-content');
-  var ajaxURL = 'https://ec2-54-210-141-119.compute-1.amazonaws.com/wp-json/wp/v2/' + postRestBase + '/' + postID + '/' + '?_embed=true' ;
+  var postRestBase = phila_js_vars.postRestBase;
+  var postID = phila_js_vars.postID;
 
-  if (ajaxURL) {
-    $content.html('Loading...');
+  $(document).on('open.zf.reveal', '#phila-lightbox', function (e) {
+    var $modal = $(this);
+    var $content = $('.lightbox-content');
+    var ajaxURL = 'https://ec2-54-210-141-119.compute-1.amazonaws.com/wp-json/wp/v2/' + postRestBase + '/' + postID + '/' + '?_embed=true' ;
 
-    $.ajax(ajaxURL).done(function (response) {
-      var fullSizeImg = response._embedded["wp:featuredmedia"]["0"].media_details.sizes.large.source_url;
-      var featuredCaption = response._embedded["wp:featuredmedia"]["0"].caption.rendered;
+    if (ajaxURL) {
+      $content.html('Loading...');
 
-      $content.html('<img src="' + fullSizeImg + '" alt="">');
-      $content.append(featuredCaption);
-    })
-    .fail(function() {
-      $content.html('Image cannot be loaded.');
-    });
-  }
-});
+      $.ajax(ajaxURL).done(function (response) {
+        var fullSizeImg = response._embedded["wp:featuredmedia"]["0"].media_details.sizes.large.source_url;
+        var featuredCaption = response._embedded["wp:featuredmedia"]["0"].caption.rendered;
+
+        $content.html('<img src="' + fullSizeImg + '" alt="">');
+        $content.append(featuredCaption);
+      })
+      .fail(function() {
+        $content.html('Image cannot be loaded.');
+      });
+    }
+  });
+}
