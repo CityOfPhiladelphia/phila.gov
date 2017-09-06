459b5b5483d6ba97cdbf4b5c344dc5e370e9f6b7
Karissa Demi <k@karissademi.com>
2017-09-06 14:49:22 -0400

    See-alls should always have margin

--- wp/wp-content/themes/phila.gov-theme/partials/content-see-all.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-see-all.php
@@ -1,4 +1,4 @@
-<div class="row">
+<div class="row mtm">
   <div class="columns">
     <a class="button button--inverse float-right content-type-<?php echo $see_all_content_type?>" href="<?php echo $see_all_URL ?>" aria-label="See all <?php echo $see_all_content_type ?>">
       See all <?php echo $see_all_content_type ?> <i class="fa fa-angle-right fa-lg" aria-hidden="true"></i>
