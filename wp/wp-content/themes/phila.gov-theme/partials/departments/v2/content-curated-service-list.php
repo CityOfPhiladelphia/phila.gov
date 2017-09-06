f37f7fd79f3b0b81572f16f0f7864f513aac271f
Karissa Demi <k@karissademi.com>
2017-09-06 14:49:49 -0400

    Correctly wrap service section

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-curated-service-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-curated-service-list.php
@@ -14,31 +14,35 @@
 <div class="row mtl">
   <div class="columns">
     <h2>Services</h2>
-    <div class="row inside-border-group" data-equalizer>
-      <?php $item_count = count($services); ?>
-      <?php $columns = phila_grid_column_counter( $item_count ); ?>
-      <?php
-      if ( $item_count % 3 == 0) :
-        $columns = "8";
-      elseif( $item_count == 4) :
-        $columns = "12";
-      endif;
-      ?>
-      <?php foreach ( $services as $service ) : ?>
-        <?php $alt_title = isset( $service['alt_title'] ) ? $service['alt_title'] : ''; ?>
-        <div class="inside-border-group-item medium-<?php echo $columns ?> small-12 columns end">
-          <a href="<?php echo get_permalink( $service['phila_v2_service_page'] ) ?>" class="valign">
-            <div class="valign-cell pal phl-l" data-equalizer-watch>
-              <div><i class="fa <?php echo $service['phila_v2_icon'] ?> fa-2x" aria-hidden="true"></i></div>
-              <?php if( $alt_title == '' ) : ?>
-                <div><?php echo get_the_title( $service['phila_v2_service_page'] ) ?> </div>
-              <?php else: ?>
-                <div><?php echo $service['alt_title'] ?> </div>
-              <?php endif; ?>
-            </div>
-          </a>
-        </div>
-      <?php endforeach; ?>
+    <div class="row" data-equalizer>
+      <div class="columns small-collapse">
+        <?php $item_count = count($services); ?>
+        <?php $columns = phila_grid_column_counter( $item_count ); ?>
+        <?php
+        if ( $item_count % 3 == 0) :
+          $columns = "8";
+        elseif( $item_count == 4) :
+          $columns = "12";
+        endif;
+        ?>
+        <div class="row inside-border-group">
+        <?php foreach ( $services as $service ) : ?>
+          <?php $alt_title = isset( $service['alt_title'] ) ? $service['alt_title'] : ''; ?>
+          <div class="inside-border-group-item medium-<?php echo $columns ?> small-12 columns end">
+            <a href="<?php echo get_permalink( $service['phila_v2_service_page'] ) ?>" class="valign">
+              <div class="valign-cell pal phl-l" data-equalizer-watch>
+                <div><i class="fa <?php echo $service['phila_v2_icon'] ?> fa-2x" aria-hidden="true"></i></div>
+                <?php if( $alt_title == '' ) : ?>
+                  <div><?php echo get_the_title( $service['phila_v2_service_page'] ) ?> </div>
+                <?php else: ?>
+                  <div><?php echo $service['alt_title'] ?> </div>
+                <?php endif; ?>
+              </div>
+            </a>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    </div>
     </div>
   </div>
 </div>
