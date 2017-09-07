8fbbaffeb3e7dab870e06208a0b768f9ab4a7034
Karissa Demi <k@karissademi.com>
2017-09-07 15:34:18 -0400

    Pass full-width button var

--- wp/wp-content/themes/phila.gov-theme/partials/content-see-all.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-see-all.php
@@ -1,6 +1,6 @@
 <div class="row mtm">
   <div class="columns">
-    <a class="button float-right <?php echo isset($see_all_content_type) ? 'content-type-' . $see_all_content_type : ''?>" href="<?php echo $see_all_URL ?>" aria-label="See all <?php echo $see_all_content_type ?>">
+    <a class="button float-right <?php echo isset($see_all_content_type) ? 'content-type-' . $see_all_content_type : ''?> <?php echo isset($is_full) ? 'full' : ''?>" href="<?php echo $see_all_URL ?>" aria-label="See all <?php echo $see_all_content_type ?>">
       See all <?php echo $see_all_content_type ?> <i class="fa fa-angle-right fa-lg" aria-hidden="true"></i>
     </a>
   </div>
