780224fb832348138e1cc6413df820f36cc68210
Karissa Demi <k@karissademi.com>
2017-09-07 14:58:38 -0400

    Don't always include inverse button class

--- wp/wp-content/themes/phila.gov-theme/partials/content-see-all.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-see-all.php
@@ -1,6 +1,6 @@
 <div class="row mtm">
   <div class="columns">
-    <a class="button button--inverse float-right content-type-<?php echo $see_all_content_type?>" href="<?php echo $see_all_URL ?>" aria-label="See all <?php echo $see_all_content_type ?>">
+    <a class="button float-right <?php echo isset($see_all_content_type) ? 'content-type-' . $see_all_content_type : ''?>" href="<?php echo $see_all_URL ?>" aria-label="See all <?php echo $see_all_content_type ?>">
       See all <?php echo $see_all_content_type ?> <i class="fa fa-angle-right fa-lg" aria-hidden="true"></i>
     </a>
   </div>
