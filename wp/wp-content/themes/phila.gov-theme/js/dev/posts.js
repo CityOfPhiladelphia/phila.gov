9f61f37279c5eb94e87f4bf931bef91bb98c7808
Karissa Demi <k@karissademi.com>
2017-09-08 15:00:41 -0400

    Check if click came from light-boxed image. If so, render the large version of that image

--- wp/wp-content/themes/phila.gov-theme/js/dev/posts.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/posts.js
@@ -6,27 +6,39 @@ if (typeof phila_js_vars !== 'undefined') {
   var postRestBase = phila_js_vars.postRestBase;
   var postID = phila_js_vars.postID;
 
-  $(document).on('open.zf.reveal', '#phila-lightbox', function (e) {
-    var $modal = $(this);
-    var $content = $('.lightbox-content');
-    var ajaxURL = 'https://ec2-54-210-141-119.compute-1.amazonaws.com/wp-json/wp/v2/' + postRestBase + '/' + postID + '/' + '?_embed=true' ;
 
-    if (ajaxURL) {
-      $content.html('Loading...');
+  $.each($('.lightbox-link'), function(){
+    $(this).click(function() {
+      var url = $(this).data('img-url');
 
-      $.ajax(ajaxURL).done(function (response) {
-        var fullSizeImg = response._embedded["wp:featuredmedia"]["0"].media_details.sizes.full.source_url;
-        var featuredCaption = response._embedded["wp:featuredmedia"]["0"].caption.rendered;
+      $(document).on('open.zf.reveal', '#phila-lightbox', function (e) {
+        var $modal = $(this);
+        var $content = $('.lightbox-content');
+        var ajaxURL = 'https://ec2-54-210-141-119.compute-1.amazonaws.com/wp-json/wp/v2/' + postRestBase + '/' + postID + '/' + '?_embed=true' ;
+        $content.html('Loading...');
 
-        $content.html('<img src="' + fullSizeImg + '" alt="">');
-        $content.append(featuredCaption);
-      })
-      .fail(function() {
-        $content.html('Image cannot be loaded.');
+        if ( url != null ) {
+          $content.html('<img src="' + url + '" alt="">');
+
+        }else if( ajaxURL ){
+
+          $.ajax(ajaxURL).done(function (response) {
+            var fullSizeImg = response._embedded["wp:featuredmedia"]["0"].media_details.sizes.full.source_url;
+            var featuredCaption = response._embedded["wp:featuredmedia"]["0"].caption.rendered;
+
+            $content.html('<img src="' + fullSizeImg + '" alt="">');
+            $content.append(featuredCaption);
+          })
+          .fail(function() {
+            $content.html('Image cannot be loaded.');
+          });
+        }
       });
-    }
+
+    });
   });
 }
+
 //disable lightboxes on mobile
 $(function(){
   if (Foundation.MediaQuery.current === 'small') {
