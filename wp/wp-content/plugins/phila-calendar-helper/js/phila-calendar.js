3b5b146c5a6a352fbfce5887a0ef5bf34569c51d
Karissa Demi <k@karissademi.com>
2017-09-08 15:29:02 -0400

    Only look for an @ sign when validating

--- wp/wp-content/plugins/phila-calendar-helper/js/phila-calendar.js
+++ wp/wp-content/plugins/phila-calendar-helper/js/phila-calendar.js
@@ -1,7 +1,7 @@
 jQuery( document ).ready( function() {
     jQuery( '#publish' ).click( function() {
         if( ! jQuery( this ).data( "valid" ) ) {
-            if( jQuery('#post input[name="phila_calendar_google_calendar_id"]' ).val().indexOf( 'calendar.google.com' ) <= -1 ){
+            if( jQuery('#post input[name="phila_calendar_google_calendar_id"]' ).val().indexOf( '@' ) <= -1 ){
                 alert( "Error: Oops!, it looks like you didn't entered a valid Google Calendar ID" );
                 jQuery( "#post" ).data( "valid", false );
 
