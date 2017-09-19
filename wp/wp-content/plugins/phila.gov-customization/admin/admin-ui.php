f0f26609800a5ba6e60c11feaf22eaff1719b6e2
Karissa Demi <k@karissademi.com>
2017-09-19 14:26:44 -0400

    Allow users to see featured image and parent post div

--- wp/wp-content/plugins/phila.gov-customization/admin/admin-ui.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/admin-ui.php
@@ -129,5 +129,5 @@ function phila_remove_thumbnails_from_pages() {
 add_filter( 'default_hidden_meta_boxes', 'phila_hide_meta_boxes', 10, 2 );
 
 function phila_hide_meta_boxes( $hidden, $screen ) {
-  return array( 'tagsdiv-post_tag', 'tagsdiv', 'postimagediv', 'formatdiv', 'pageparentdiv');
+  return array( 'tagsdiv-post_tag', 'tagsdiv', 'formatdiv');
 }
