3d1422a9cd9f842253ad4d1a6192a648ee8a4c78
Karissa Demi <k@karissademi.com>
2017-09-23 14:41:10 -0400

    Add necessary markup

--- wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
@@ -14,30 +14,30 @@
       <header class="small-24 columns">
         <?php the_title( '<h1 class="contrast">', '</h1>' ); ?>
         <div class="data-sticky-container">
-          <nav class="sticky sticky--in-page centered" data-sticky data-top-anchor="global-sticky-nav:bottom">
-            <ul>
-              <li class="featured">
+          <nav class="sticky sticky--in-page center" data-sticky data-top-anchor="global-sticky-nav:bottom">
+            <ul class="inline-list grid-x grid-margin-x">
+              <li class="featured auto cell">
                 <a href="#featured">
-                  <i class="fa fa-newspaper-o" aria-hidden="true"></i>
-                  <span>Featured</span>
+                  <i class="fa fa-3x fa-newspaper-o" aria-hidden="true"></i>
+                  <div>Featured</div>
                 </a>
               </li>
-              <li class="posts">
+              <li class="posts auto cell">
                 <a href="#posts">
-                  <i class="fa fa-bullhorn" aria-hidden="true"></i>
-                  <span>Posts</span>
-              </a>
+                  <i class="fa fa-3x fa-bullhorn" aria-hidden="true"></i>
+                  <div>Posts</div>
+                </a>
               </li>
-              <li class="press-releases">
+              <li class="press-releases auto cell">
                 <a href="#press-releases">
-                  <i class="fa fa-file-text-o" aria-hidden="true"></i>
-                <span>Press releases</span>
+                  <i class="fa fa-3x fa-file-text-o" aria-hidden="true"></i>
+                <div>Press releases</div>
               </a>
               </li>
-              <li class="events">
+              <li class="events auto cell">
                 <a href="#events">
-                  <i class="fa fa-calendar-check-o" aria-hidden="true"></i>
-                  <span>Events</span>
+                  <i class="fa fa-3x fa-calendar-check-o" aria-hidden="true"></i>
+                  <div>Events</div>
                 </a>
               </li>
             </ul>
