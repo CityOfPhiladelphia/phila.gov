908cb8a9c2db73579b972b47425cb9cae210c035
Karissa Demi <k@karissademi.com>
2017-09-23 12:38:57 -0400

    Break card & releated content into seperate templates

---
+++ wp/wp-content/themes/phila.gov-theme/partials/content-card.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The template used for displaying a card, containing a title and link
+ *
+ * @package phila-gov
+ */
+?>
+<?php $categories = get_the_category($post->ID); ?>
+<?php $desc = phila_get_item_meta_desc( ); ?>
+<?php if( isset( $is_press_release ) ): ?>
+  <div class="medium-12 cell">
+<?php endif;?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <a href="<?php echo the_permalink(); ?>" class="card card--list card--<?php echo isset( $is_press_release ) ? 'press-release' : ''; ?> grid-x">
+    <div class="content-block cell medium-24 grid-x">
+      <div class="cell align-self-top">
+        <div class="post-label post-label--<?php echo isset( $is_press_release ) ? 'press-release' : ''; ?>">
+          <i class="fa fa-file-text-o" aria-hidden="true"></i>
+          <span><?php echo strtolower($post_obj->labels->name); ?></span>
+        </div>
+        <header>
+          <h1 class="h4"><?php echo get_the_title(); ?></h1>
+        </header>
+        <?php if( !isset( $is_press_release ) ): ?>
+          <p><?php echo $desc ?></p>
+        <?php endif;?>
+        </div>
+      </div>
+      <div class="cell align-self-bottom">
+        <div class="post-meta">
+          <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
+          <span class="departments"><?php echo phila_get_current_department_name( $categories, $byline = false, $break_tags = false, $name_list = true ); ?></span>
+        </div>
+      </div>
+    </div>
+  </a>
+</article>
+<?php if( isset( $is_press_release ) ): ?>
+  </div>
+<?php endif;?>
---
+++ wp/wp-content/themes/phila.gov-theme/partials/content-related.php
@@ -0,0 +1,33 @@
+<?php
+/* Related content
+*  at the bottom of posts, etc.
+* $related_content_args should be passed by the file this is included in.
+*/
+?>
+
+<?php $related_posts = new WP_Query( $related_content_args ); ?>
+<?php $count = -1;?>
+<?php if ( $related_posts->have_posts() ) : ?>
+  <div class="grid-container">
+    <h2><?php _e( 'Related' ); ?>
+    <?php echo strtolower($post_obj->labels->name); ?></h2>
+  </div>
+  <div class="grid-container">
+    <div class="grid-x grid-margin-x">
+
+  <?php while ( $related_posts->have_posts() ) : $related_posts->the_post(); ?>
+    <?php $count++; ?>
+    <?php if ( isset( $is_press_release ) ):  ?>
+        <?php include( locate_template( 'partials/content-card.php' ) ); ?>
+      <?php else :  ?>
+      <div class="grid-container">
+        <?php get_template_part( 'partials/content', 'list-featured-image' ); ?>
+      </div>
+
+     <?php endif; ?>
+  <?php endwhile; ?>
+</div>
+
+</div>
+  <?php wp_reset_postdata(); ?>
+<?php endif; ?>
