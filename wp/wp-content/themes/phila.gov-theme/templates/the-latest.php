b2a29ac78f65ca3e2db2a09cbc02b62dffc04758
Karissa Demi <k@karissademi.com>
2017-09-23 13:51:09 -0400

    Render header

--- wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Template Name: The latest
- * Description: Custom Page template for the latest
+ * Description: Custom Page template for the
  * @package phila-gov
  */
 
@@ -10,6 +10,11 @@
 
 <div id="primary" class="content-area">
   <main id="main" class="site-main">
+    <div class="row">
+      <header class="small-24 columns">
+        <?php the_title( '<h1 class="contrast">', '</h1>' ); ?>
+      </header>
+    </div>
 
   </main><!-- #main -->
 </div><!-- #primary -->
