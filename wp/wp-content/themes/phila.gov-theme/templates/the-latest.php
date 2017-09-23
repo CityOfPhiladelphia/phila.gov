931e16187b2e554d7739f3c40cb76a6b2b2f99f7
Karissa Demi <k@karissademi.com>
2017-09-23 14:10:53 -0400

    Begin menu markup

--- wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
@@ -8,11 +8,41 @@
   get_header();
 ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="the-latest content-area">
   <main id="main" class="site-main">
     <div class="row">
       <header class="small-24 columns">
         <?php the_title( '<h1 class="contrast">', '</h1>' ); ?>
+        <div class="data-sticky-container">
+          <nav class="sticky sticky--in-page centered" data-sticky data-top-anchor="global-sticky-nav:bottom">
+            <ul>
+              <li class="featured">
+                <a href="#featured">
+                  <i class="fa fa-newspaper-o" aria-hidden="true"></i>
+                  <span>Featured</span>
+                </a>
+              </li>
+              <li class="posts">
+                <a href="#posts">
+                  <i class="fa fa-bullhorn" aria-hidden="true"></i>
+                  <span>Posts</span>
+              </a>
+              </li>
+              <li class="press-releases">
+                <a href="#press-releases">
+                  <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                <span>Press releases</span>
+              </a>
+              </li>
+              <li class="events">
+                <a href="#events">
+                  <i class="fa fa-calendar-check-o" aria-hidden="true"></i>
+                  <span>Events</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     </div>
 
