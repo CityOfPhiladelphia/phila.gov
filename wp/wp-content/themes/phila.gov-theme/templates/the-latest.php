80bbd0940a3c07aaf02501aa7678b122be43a08b
Karissa Demi <k@karissademi.com>
2017-09-23 13:38:46 -0400

    Create the latest template

---
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Template Name: The latest
+ * Description: Custom Page template for the latest
+ * @package phila-gov
+ */
+
+  get_header();
+?>
+
+<div id="primary" class="content-area">
+  <main id="main" class="site-main">
+
+  </main><!-- #main -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
