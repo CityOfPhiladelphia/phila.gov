a99d344395f1506e86b451fb5fb72c05f06d57a7
Karissa Demi <k@karissademi.com>
2017-09-25 12:05:59 -0400

    Use switch statement

--- wp/wp-content/themes/phila.gov-theme/partials/content-card.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-card.php
@@ -8,17 +8,27 @@
 <?php $categories = get_the_category($post->ID); ?>
 <?php $desc = phila_get_item_meta_desc( ); ?>
 
+<?php
+if ( isset( $label ) ) :
+  switch( $label ) {
+    case 'press-release':
+      $label_nice = 'Press Release';
+      break;
+    case 'featured':
+      $label_nice = 'Featured';
+      break;
+  }
+  endif;
+?>
+
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a href="<?php echo the_permalink(); ?>" class="card card--list card--<?php echo isset( $is_press_release ) ? 'press-release' : ''; ?><?php echo isset( $is_featured ) ? 'featured' : ''; ?> pam">
-    <div class="post-label post-label--<?php echo isset( $is_press_release ) ? 'press-release' : ''; ?>">
-      <i class="fa fa-file-text-o" aria-hidden="true"></i> <span><?php echo isset($is_featured) ? 'Featured' : strtolower($post_obj->labels->name); ?></span>
+  <a href="<?php echo the_permalink(); ?>" class="card card--<?php echo $label ?> pam">
+    <div class="post-label post-label--<?php echo $label?>">
+      <i class="fa fa-file-text-o" aria-hidden="true"></i> <span><?php echo $label_nice; ?></span>
     </div>
       <header class="mbm">
         <h1><?php echo get_the_title(); ?></h1>
       </header>
-      <?php if( !isset( $is_press_release ) && !isset($is_featured) ): ?>
-        <p><?php echo $desc ?></p>
-      <?php endif;?>
     <div class="align-self-bottom">
       <div class="post-meta">
         <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
