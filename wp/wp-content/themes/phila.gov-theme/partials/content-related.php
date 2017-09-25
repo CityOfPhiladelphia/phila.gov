7bb256c39e3d7dd60b60a75bf6a16ab716315da2
Karissa Demi <k@karissademi.com>
2017-09-25 11:33:11 -0400

    Clean up

--- wp/wp-content/themes/phila.gov-theme/partials/content-related.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-related.php
@@ -18,8 +18,7 @@
         <?php $count++; ?>
         <?php if ( isset( $is_press_release ) ):  ?>
           <div class="cell medium-12">
-
-              <?php include( locate_template( 'partials/content-card.php' ) ); ?>
+            <?php include( locate_template( 'partials/content-card.php' ) ); ?>
             </div>
           <?php else :  ?>
           <div class="grid-container">
