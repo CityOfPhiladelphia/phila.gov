715c782b4a60ab5b50d5f15d9c575430b4893185
Karissa Demi <k@karissademi.com>
2017-10-02 16:28:41 -0400

    Add nested grid for card text alignment

--- wp/wp-content/themes/phila.gov-theme/partials/posts/content-card-image.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/content-card-image.php
@@ -30,14 +30,14 @@ if ( isset( $label ) ) :
     <?php if ( has_post_thumbnail() ) : ?>
         <?php echo phila_get_thumbnails(); ?>
     <?php endif; ?>
-    <div class="pam">
-      <div class="post-label post-label--<?php echo $label?>">
+    <div class="grid-x card--content pam">
+      <div class="cell post-label post-label--<?php echo $label?>">
         <i class="fa fa-<?php echo $icon ?> fa-lg" aria-hidden="true"></i> <span><?php echo $label_nice; ?></span>
       </div>
-        <header class="mbm">
-          <h1><?php echo get_the_title(); ?></h1>
-        </header>
-      <div class="align-self-bottom">
+      <header class="cell mbm">
+        <h1><?php echo get_the_title(); ?></h1>
+      </header>
+      <div class="cell align-self-bottom">
         <div class="post-meta">
           <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
         </div>
