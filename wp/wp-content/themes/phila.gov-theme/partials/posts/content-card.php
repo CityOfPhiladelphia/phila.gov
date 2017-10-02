6025c18ccd3773787f10b1cc87cb6325f4f31962
Karissa Demi <k@karissademi.com>
2017-10-02 16:29:08 -0400

    Add check for is_last

--- wp/wp-content/themes/phila.gov-theme/partials/posts/content-card.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/content-card.php
@@ -22,19 +22,26 @@ if ( isset( $label ) ) :
   }
   endif;
 ?>
+<?php if ($count == 3 && $label == 'featured') : ?>
+  <?php $is_last = true; ?>
+<?php endif; ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a href="<?php echo the_permalink(); ?>" class="card card--<?php echo $label ?> pam">
-    <div class="post-label post-label--<?php echo $label?>">
-      <i class="fa fa-<?php echo $icon ?> fa-lg" aria-hidden="true"></i> <span><?php echo $label_nice; ?></span>
-    </div>
-      <header class="mbm">
+  <a href="<?php echo the_permalink(); ?>" class="card card--<?php echo $label ?> <?php echo isset($is_last) ? 'card--last' : ''; ?> pam">
+    <div class="grid-x card--content">
+      <div class="cell post-label post-label--<?php echo $label?>">
+        <i class="fa fa-<?php echo $icon ?> fa-lg" aria-hidden="true"></i> <span><?php echo $label_nice; ?></span>
+      </div>
+      <header class="cell mbm">
         <h1><?php echo get_the_title(); ?></h1>
       </header>
-    <div class="align-self-bottom">
-      <div class="post-meta">
-        <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
-        <span class="departments"><?php echo phila_get_current_department_name( $categories, $byline = false, $break_tags = false, $name_list = true ); ?></span>
+      <div class="cell align-self-bottom">
+        <div class="post-meta">
+          <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
+          <?php if( $label != 'featured') : ?>
+            <span class="departments"><?php echo phila_get_current_department_name( $categories, $byline = false, $break_tags = false, $name_list = true ); ?></span>
+          <?php endif; ?>
+        </div>
       </div>
     </div>
   </a>
