e7b25deb2718592630ba020bd88fe56ced40bd20
Karissa Demi <k@karissademi.com>
2017-10-04 11:58:52 -0400

    First pass at announcement/post mashup

--- wp/wp-content/themes/phila.gov-theme/partials/posts/post-announcement.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/post-announcement.php
@@ -5,29 +5,63 @@
 */
 ?>
 
-<?php $posts_announcements  = array(
-  'posts_per_page' => 1,
+<?php $announcements_args  = array(
+  'posts_per_page' => 2,
   'post_type' => array( 'announcements' ),
   'order' => 'asc',
   'orderby' => 'post_date',
 
 ); ?>
 
-<?php $label = 'announcement'; ?>
+<?php $posts_args  = array(
+  'posts_per_page' => 2,
+  'post_type' => array( 'post' ),
+  'order' => 'desc',
+  'orderby' => 'post_date',
+  'meta_query'  => array(
+    'relation'  => 'AND',
+    array(
+      'key' => 'phila_template_select',
+      'value' => 'post',
+      'compare' => '=',
+    ),
+    array(
+      'key' => 'phila_is_feature',
+      'value' => '0',
+      'compare' => '=',
+    ),
+  ),
+); ?>
+<?php
+//special handling for old press release CPT
+  $announcements = new WP_Query( $announcements_args );
+  $posts = new WP_Query( $posts_args );
+  $result = new WP_Query();
+  $result->posts = array_merge( $announcements->posts, $posts->posts );
+  $result->post_count = count( $result->posts );
+?>
+
 <?php $count = 0; ?>
 
 <div class="grid-container">
   <div class="grid-x grid-margin-x">
-    <?php $posts = new WP_Query( $posts_announcements ); ?>
 
-    <?php if ( $posts->have_posts() ) : ?>
-      <div class="cell medium-8">
-        <?php while ( $posts->have_posts() ) : $posts->the_post(); ?>
+    <?php if ( $result->have_posts() ) : ?>
+        <?php while ( $result->have_posts() ) : $result->the_post(); ?>
           <?php $post_type = get_post_type(); ?>
           <?php $post_obj = get_post_type_object( $post_type ); ?>
           <?php $count++; ?>
-
-          <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
+          <?php if ($post_type == 'announcements') : ?>
+            <div class="cell medium-8">
+              <?php $label = 'announcement'; ?>
+              <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
+          </div>
+          <?php else: ?>
+            <?php $label = 'post'; ?>
+            <div class="cell medium-16">
+            <?php include( locate_template( 'partials/posts/content-card-image.php' ) ); ?>
+          </div>
+          <?php endif; ?>
 
         <?php endwhile; ?>
       </div>
