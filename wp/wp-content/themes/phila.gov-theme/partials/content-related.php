7e0d8675bbc51aa9e24337ce18613328953b673e
Karissa Demi <k@karissademi.com>
2017-10-06 11:43:05 -0400

    Correct path

--- wp/wp-content/themes/phila.gov-theme/partials/content-related.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-related.php
@@ -8,10 +8,8 @@
 <?php $related_posts = new WP_Query( $related_content_args ); ?>
 <?php $count = -1;?>
 <?php if ( $related_posts->have_posts() ) : ?>
-  <div class="grid-container">
     <h2><?php _e( 'Related' ); ?>
     <?php echo strtolower($post_obj->labels->name); ?></h2>
-  </div>
   <div class="grid-container">
     <div class="grid-x grid-margin-x">
       <?php while ( $related_posts->have_posts() ) : $related_posts->the_post(); ?>
@@ -21,9 +19,7 @@
             <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
             </div>
           <?php else :  ?>
-          <div class="grid-container">
-            <?php get_template_part( 'partials/posts/content', 'list-featured-image' ); ?>
-          </div>
+          <?php get_template_part( 'partials/content', 'list-featured-image' ); ?>
 
          <?php endif; ?>
       <?php endwhile; ?>
