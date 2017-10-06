6529de594ef88442744acd924a6b8834ca380eba
Karissa Demi <k@karissademi.com>
2017-10-06 15:23:38 -0400

    Begin formatting table

--- wp/wp-content/themes/phila.gov-theme/js/dev/archives.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/archives.js
@@ -1,6 +1,14 @@
 var Vue = require('vue')
 var VueResource = require('vue-resource')
+var moment = require('moment')
+
 Vue.use(VueResource)
+
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment( String(value) ).format('MMM. DD, YYYY');
+  };
+})
 
 //TODO: create custom endpoint instead of hitting WP API
 
@@ -18,14 +26,22 @@ var archives = new Vue ({
     <tr><th>Title</th><th>Date</th><th>Department</th></tr>
     </thead>
       <tr v-for="post in posts">
-        <td class="title"><a v-bind:href="post.link">{{ post.meta.phila_template_select }} {{ post.title.rendered }}</a></td>
-        <td class="date">{{ post.date }}</td>
+        <td class="title"><a v-bind:href="post.link">
+        <span v-if="post.meta.phila_template_select === 'post'">
+          <i class="fa fa-pencil"></i>
+        </span>
+        <span v-else>
+          <i class="fa fa-file-text-o"></i>
+        </span> {{ post.title.rendered }}</a>
+        </td>
+        <td class="date">{{ post.date  | formatDate }}</td>
         <td class="categories">{{ post.categories }}</td>
       </tr>
     </table>`,
   data: function() {
     return{
-        posts: []
+      //validate post.meta.phila_template_select in here
+      posts: []
     }
   },
   mounted: function () {
