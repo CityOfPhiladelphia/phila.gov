de0f21854a777ff3163cd3920806b8ab8ae3440c
Karissa Demi <k@karissademi.com>
2017-10-06 11:24:50 -0400

    Use flex grid to apply equal heights to press releases

--- wp/wp-content/themes/phila.gov-theme/partials/posts/press-releases.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/press-releases.php
@@ -44,21 +44,28 @@
 <?php $count = 0; ?>
 
 <div class="grid-container">
-  <div class="grid-x grid-margin-x">
     <?php if ( $result->have_posts() ) : ?>
       <?php while ( $result->have_posts() ) : $result->the_post(); ?>
         <?php $post_type = get_post_type(); ?>
         <?php $post_obj = get_post_type_object( $post_type ); ?>
         <?php $count++; ?>
-        <?php
-        if ($count <= 4) : ?>
-          <div class="cell medium-12">
-            <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
-            <?php if ($count == 4) : ?>
-              <?php $see_all_content_type = $label; ?>
-              <?php $is_full = true; ?>
-              <?php $see_all_URL = 'archive'?>
-              <?php include( locate_template( 'partials/content-see-all.php' ) ); ?>
+        <?php if ($count == 1) :?>
+          <div class="grid-x grid-margin-x grid-full-height">
+        <?php elseif( $count == 3 ) : ?>
+          <div class="grid-x grid-margin-x grid-full-height mtm">
+        <?php endif;?>
+          <?php
+          if ($count <= 4) : ?>
+            <div class="cell medium-12 align-self-stretch">
+              <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
+              <?php if ($count == 4) : ?>
+                <?php $see_all_content_type = $label; ?>
+                <?php $is_full = true; ?>
+                <?php $see_all_URL = 'archive'?>
+                <?php include( locate_template( 'partials/content-see-all.php' ) ); ?>
+              <?php endif;?>
+              <?php if ($count == 2 || $count == 4) :?>
+              </div>
             <?php endif;?>
           </div>
         <?php endif;?>
