c89e755767c85373d047e8d6045d12de53d732f7
Karissa Demi <k@karissademi.com>
2017-10-12 11:53:01 -0400

    Stick after scroll get to bottom of filter div

--- wp/wp-content/themes/phila.gov-theme/js/dev/archives.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/archives.js
@@ -65,7 +65,7 @@ var archives = new Vue ({
     </div>
 
     <table class="stack theme-light"  data-sticky-container>
-      <thead class="sticky center bg-white" data-sticky data-top-anchor="global-sticky-nav:bottom" data-options="marginTop:4.8;">
+      <thead class="sticky center bg-white" data-sticky data-top-anchor="filter-results:bottom" data-options="marginTop:4.8;">
         <tr><th width="500">Title</th><th width="125">Publish date</th><th>Department</th></tr>
       </thead>
       <tbody>
