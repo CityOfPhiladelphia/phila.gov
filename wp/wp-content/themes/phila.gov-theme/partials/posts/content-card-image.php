36da44122e6f8c35bff7f05a8e42f66f9dcae528
Karissa Demi <k@karissademi.com>
2017-10-16 17:30:08 -0400

    Content card img should fill full height with available space

--- wp/wp-content/themes/phila.gov-theme/partials/posts/content-card-image.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/content-card-image.php
@@ -33,21 +33,25 @@ if ( isset( $label ) ) :
   endif;
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-  <a href="<?php echo the_permalink(); ?>" class="card card--<?php echo $label ?>">
-    <?php if ( has_post_thumbnail() ) : ?>
-        <?php echo phila_get_thumbnails(); ?>
-    <?php endif; ?>
-    <div class="grid-x card--content pam">
-      <div class="cell post-label post-label--<?php echo $label?>">
-        <i class="fa fa-<?php echo $icon ?> fa-lg" aria-hidden="true"></i> <span><?php echo $label_nice; ?></span>
+<article id="post-<?php the_ID(); ?>" <?php post_class('full-height'); ?>>
+  <a href="<?php echo the_permalink(); ?>" class="card card--<?php echo $label ?> flex-container flex-dir-row full-height">
+    <div class="grid-x flex-dir-column">
+      <div class="flex-child-shrink">
+        <?php if ( has_post_thumbnail() ) : ?>
+            <?php echo phila_get_thumbnails(); ?>
+        <?php endif; ?>
       </div>
-      <header class="cell mbm">
-        <h1><?php echo get_the_title(); ?></h1>
-      </header>
-      <div class="cell align-self-bottom">
-        <div class="post-meta">
-          <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
+      <div class="card--content pam flex-child-grow">
+        <div class="cell align-self-top post-label post-label--<?php echo $label?>">
+          <i class="fa fa-<?php echo $icon ?> fa-lg" aria-hidden="true"></i> <span><?php echo $label_nice; ?></span>
+          <header class="cell mbm">
+            <h1><?php echo get_the_title(); ?></h1>
+          </header>
+        </div>
+        <div class="cell align-self-bottom">
+          <div class="post-meta">
+            <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
+          </div>
         </div>
       </div>
     </div>
