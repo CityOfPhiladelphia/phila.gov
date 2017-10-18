259ca7908c88443ae304bc233d0176d85b1da198
Karissa Demi <k@karissademi.com>
2017-10-18 13:22:28 -0400

    Change up list layout

--- wp/wp-content/themes/phila.gov-theme/partials/content-list-featured-image.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-list-featured-image.php
@@ -9,18 +9,18 @@
 <?php $desc = phila_get_item_meta_desc( ); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('mbm'); ?>>
-  <a href="<?php echo the_permalink(); ?>" class="grid-x grid-margin-x">
+  <a href="<?php echo the_permalink(); ?>" class="grid-x grid-margin-x hover-fade">
     <?php if ( has_post_thumbnail() ) : ?>
       <div class="cell medium-7">
         <?php echo phila_get_thumbnails(); ?>
       </div>
     <?php endif; ?>
-    <div class="cell medium-17 grid-x card card--<?php echo $label ?>">
-      <div class="cell align-self-top">
-        <header>
+    <div class="cell medium-17 grid-x bg-ghost-gray card card--<?php echo $label ?> pam">
+      <div class="cell align-self-top post-label post-label--<?php echo $label?>">
+        <i class="fa fa-<?php echo $icon ?> fa-lg" aria-hidden="true"></i> <span><?php echo $label_nice; ?></span>
+        <header class="mvm">
           <h1><?php echo get_the_title(); ?></h1>
         </header>
-        <p><?php echo $desc ?></p>
       </div>
       <div class="cell align-self-bottom">
         <div class="post-meta">
