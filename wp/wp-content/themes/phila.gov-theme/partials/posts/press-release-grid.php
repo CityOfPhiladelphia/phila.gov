372542e368ea3a17ec43d9eca1322eb852baa348
Karissa Demi <k@karissademi.com>
2017-10-19 13:50:00 -0400

    Wrapping div

--- wp/wp-content/themes/phila.gov-theme/partials/posts/press-release-grid.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/press-release-grid.php
@@ -42,35 +42,37 @@
 
 <?php $label = 'press_release'; ?>
 <?php $count = 0; ?>
+<div class="press-grid">
 
-<div class="grid-container">
-  <?php if ( $result->have_posts() ) : ?>
-    <?php while ( $result->have_posts() ) : $result->the_post(); ?>
-      <?php $post_type = get_post_type(); ?>
-      <?php $post_obj = get_post_type_object( $post_type ); ?>
-      <?php $count++; ?>
-      <?php if ($count == 1) :?>
-        <div class="grid-x grid-margin-x grid-full-height">
-      <?php elseif( $count == 3 ) : ?>
-        <div class="grid-x grid-margin-x grid-full-height mtm">
-      <?php endif;?>
-        <?php
-        if ($count <= 4) : ?>
-          <div class="cell medium-12 align-self-stretch">
-            <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
-            <?php if ($count == 4) : ?>
-              <?php $see_all_content_type = $label; ?>
-              <?php $is_full = true; ?>
-              <?php $see_all_URL = 'archive?template=press_release'?>
-              <?php include( locate_template( 'partials/content-see-all.php' ) ); ?>
+  <div class="grid-container">
+    <?php if ( $result->have_posts() ) : ?>
+      <?php while ( $result->have_posts() ) : $result->the_post(); ?>
+        <?php $post_type = get_post_type(); ?>
+        <?php $post_obj = get_post_type_object( $post_type ); ?>
+        <?php $count++; ?>
+        <?php if ($count == 1) :?>
+          <div class="grid-x grid-margin-x grid-full-height">
+        <?php elseif( $count == 3 ) : ?>
+          <div class="grid-x grid-margin-x grid-full-height mtm">
+        <?php endif;?>
+          <?php
+          if ($count <= 4) : ?>
+            <div class="cell medium-12 align-self-stretch">
+              <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
+              <?php if ($count == 4) : ?>
+                <?php $see_all_content_type = $label; ?>
+                <?php $is_full = true; ?>
+                <?php $see_all_URL = 'archive?template=press_release'?>
+                <?php include( locate_template( 'partials/content-see-all.php' ) ); ?>
+              <?php endif;?>
+              <?php if ($count == 2 || $count == 4) :?>
+              </div>
             <?php endif;?>
-            <?php if ($count == 2 || $count == 4) :?>
-            </div>
-          <?php endif;?>
-        </div>
-      <?php endif;?>
+          </div>
+        <?php endif;?>
 
-      <?php endwhile; ?>
-    <?php endif; ?>
-  <?php wp_reset_postdata(); ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    <?php wp_reset_postdata(); ?>
+  </div>
 </div>
