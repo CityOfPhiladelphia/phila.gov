0b775733f71e9703ea782f3975f308ea70400f13
Karissa Demi <k@karissademi.com>
2017-10-20 17:22:05 -0400

    Use vueify

---
+++ wp/wp-content/themes/phila.gov-theme/js/vuesrc/main.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './Archives.vue'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+ route,
+  mode: 'history',
+  props: true
+})
+
+const route = [
+  { path: '/archives' },
+]
+
+new Vue({
+  el: '#archive-results',
+  router,
+  render: h => h(App)
+})
