33202f5364a8b940d28d170e06aa0b5679996fd3
Karissa Demi <k@karissademi.com>
2017-10-24 17:16:16 -0400

    Add vue paginate

--- wp/wp-content/themes/phila.gov-theme/js/vuesrc/main.js
+++ wp/wp-content/themes/phila.gov-theme/js/vuesrc/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './Archives.vue'
+import VuePaginate from 'vue-paginate'
 
+Vue.use(VuePaginate)
 Vue.use(VueRouter)
 
 const router = new VueRouter({
