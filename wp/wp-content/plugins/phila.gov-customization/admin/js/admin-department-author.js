1a2c130842bfe882a0f4f5dde960899e9c54601b
Karissa Demi <k@karissademi.com>
2017-11-02 15:47:59 -0400

    If post is the only option, make it the default

--- wp/wp-content/plugins/phila.gov-customization/admin/js/admin-department-author.js
+++ wp/wp-content/plugins/phila.gov-customization/admin/js/admin-department-author.js
@@ -101,12 +101,12 @@ jQuery(document).ready(function($){
   }
   if ( ( typenow == 'post') && adminpage.indexOf('post') > -1 ){
     if( !phila_WP_User.includes('secondary_press_release_editor') && !phila_WP_User.includes('secondary_press_release_contributor') ) {
-
       $('#phila_template_select option').each( function () {
         if( $(this).val() !== '' && $(this).val() !== 'post' ){
           $(this).css('display', 'none');
         }
       });
+      $("#phila_template_select").val('post');
     }
   }
 
