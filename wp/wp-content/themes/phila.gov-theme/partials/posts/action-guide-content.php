723963d711ceed2b9974fbfd375d3aab60d86ca9
Karissa Demi <k@karissademi.com>
2017-11-07 16:14:49 -0500

    Renderings for action guides

---
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/action-guide-content.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * Action guide display
+ */
+
+$get_facts = rwmb_meta( 'phila_action_facts' );
+$facts = phila_loop_clonable_metabox( $get_facts );
+?>
+
+
+<div class="one-quarter-layout bdr-dark-gray">
+  <div class="row mvl">
+    <div class="medium-6 columns">
+      <h3 id="get-informed">Get informed</h3>
+    </div>
+    <div class="medium-18 columns pbxl">
+      <?php echo rwmb_meta('phila_action_get_informed'); ?>
+    </div>
+  </div>
+
+  <div class="row mvl">
+    <div class="medium-6 columns">
+      <h3 id="know-the-facts">Know the facts</h3>
+    </div>
+    <div class="medium-18 columns pbxl">
+      <?php foreach( $facts as $fact ) :?>
+        <div class="panel info mbl">
+          <h4 class="h2"><?php echo $fact['phila_custom_wysiwyg']['phila_wysiwyg_title'] ?></h4>
+          <?php echo $fact['phila_custom_wysiwyg']['phila_wysiwyg_content'] ?>
+        </div>
+      <?php endforeach; ?>
+    </div>
+  </div>
+
+</div>
