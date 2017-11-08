ac2cd542eaae017d1dea240487aadad88156a051
Karissa Demi <k@karissademi.com>
2017-11-08 15:23:04 -0500

    Add sections

--- wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest.php
@@ -59,53 +59,66 @@
         </nav>
       </div>
     </header>
-    <div id="featured" data-magellan-target="featured">
-      <header class="row columns mtl">
-        <h2>Featured</h2>
-      </header>
-      <?php get_template_part('partials/posts/featured', 'grid'); ?>
-    </div>
 
-    <div id="announcements" data-magellan-target="announcements">
-      <header class="row columns mtl">
-        <h2>Announcements</h2>
-      </header>
-      <?php get_template_part('partials/posts/announcements', 'grid'); ?>
-    </div>
+    <section>
+      <div id="featured" data-magellan-target="featured">
+        <header class="row columns mtl">
+          <h1>Featured</h1>
+        </header>
+        <?php get_template_part('partials/posts/featured', 'grid'); ?>
+      </div>
+    </section>
 
-    <div id="posts" data-magellan-target="posts">
-      <header class="row columns mtl">
-        <h2>The latest posts from departments</h2>
-      </header>
-      <?php get_template_part('partials/posts/post', 'grid'); ?>
-    </div>
+    <section>
+      <div id="announcements" data-magellan-target="announcements">
+        <header class="row columns mtl">
+          <h1>Announcements</h1>
+        </header>
+        <?php get_template_part('partials/posts/announcements', 'grid'); ?>
+      </div>
+    </section>
 
-    <div id="action-guides" data-magellan-target="action-guides">
-      <header class="row columns mtl">
-        <h2>Action guides</h2>
-      </header>
-      <?php get_template_part('partials/posts/action-guide', 'grid'); ?>
-    </div>
+    <section>
+      <div id="posts" data-magellan-target="posts">
+        <header class="row columns mtl">
+          <h1>The latest posts from departments</h1>
+        </header>
+        <?php get_template_part('partials/posts/post', 'grid'); ?>
+      </div>
+    </section>
 
-    <div id="events" data-magellan-target="events">
-      <header class="row columns mtl">
-        <h2>Upcoming events</h2>
-      </header>
-      <div class="grid-container">
-        <div class="grid-x">
-          <div class="cell small-24">
-            <?php echo do_shortcode('[calendar id="28661"]')?>
+    <section>
+      <div id="action-guides" data-magellan-target="action-guides">
+        <header class="row columns mtl">
+          <h1>Action guides</h1>
+        </header>
+        <?php get_template_part('partials/posts/action-guide', 'grid'); ?>
+      </div>
+    </section>
+
+    <section>
+      <div id="events" data-magellan-target="events">
+        <header class="row columns mtl">
+          <h2>Upcoming events</h2>
+        </header>
+        <div class="grid-container">
+          <div class="grid-x">
+            <div class="cell small-24">
+              <?php echo do_shortcode('[calendar id="28661"]')?>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
 
-    <div id="press-releases" data-magellan-target="press-releases">
-      <header class="row columns mtl">
-        <h2>Press Releases</h2>
-      </header>
-      <?php get_template_part('partials/posts/press-release', 'grid'); ?>
-    </div>
+    <section>
+      <div id="press-releases" data-magellan-target="press-releases">
+        <header class="row columns mtl">
+          <h2>Press Releases</h2>
+        </header>
+        <?php get_template_part('partials/posts/press-release', 'grid'); ?>
+      </div>
+    </section>
 
   </main><!-- #main -->
 </div><!-- #primary -->
