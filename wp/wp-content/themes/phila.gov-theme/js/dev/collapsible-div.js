9b968d8c4b87c20bf321b64b3dd17c4c92afcfa5
Karissa Demi <k@karissademi.com>
2017-11-10 11:00:39 -0500

    Just use siblings

--- wp/wp-content/themes/phila.gov-theme/js/dev/collapsible-div.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/collapsible-div.js
@@ -2,7 +2,7 @@ module.exports = $(function(){
   // Any div summary expand
   $('[data-toggle="data-expandable"]').click(function(e){
     e.preventDefault();
-    $('this').parent('collapsible').toggleClass('expandable');
+    $(this).siblings().toggleClass('expandable');
     if($(this).html() === ' Expand + '){
       $(this).html(' Collapse - ');
     } else {
