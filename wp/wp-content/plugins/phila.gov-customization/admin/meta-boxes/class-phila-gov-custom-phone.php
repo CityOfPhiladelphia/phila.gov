cf35f74605104d1c3d92bf674c8d9d8bd071cabf
Daniel Faria <daniel.faria@phila.gov>
2017-11-24 10:13:47 -0500

    quick fix to phone field

--- wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-custom-phone.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-custom-phone.php
@@ -35,7 +35,7 @@ if ( class_exists( 'RWMB_Field' ) )
       }
 
 			return sprintf(
-				'(<input type="tel" name="%1$s[area]" id="%2$s_area" value="%3$s" size="3">)<input type="tel" name="%1$s[phone-co-code]" id="%2$s_phone-co-code" value="%4$s" size="3">-<input type="tel" name="%1$s[phone-subscriber-number]" id="%2$s_phone-subscriber-number" value="%5$s" size="4">',
+				'(<input type="tel" name="%1$s[area]" id="%2$s_area" value="%3$s" size="3" class="rwmb-phone-area">)<input type="tel" name="%1$s[phone-co-code]" id="%2$s_phone-co-code" value="%4$s" size="3" class="rwmb-phone-co-code">-<input type="tel" name="%1$s[phone-subscriber-number]" id="%2$s_phone-subscriber-number" value="%5$s" size="4" class="rwmb-phone-subscriber-number">',
 				$field['field_name'],
 				$field['id'],
 				$meta['area'],
