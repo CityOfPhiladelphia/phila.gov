7b0f9ba85f3f878d4236b8946ced54741a075160
Karissa <k@karissademi.com>
2017-12-05 10:26:14 -0500

    Add super/subscript

--- wp/wp-content/plugins/phila.gov-customization/admin/tiny-mce.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/tiny-mce.php
@@ -5,3 +5,15 @@ add_action( 'after_wp_tiny_mce', 'custom_after_wp_tiny_mce' );
 function custom_after_wp_tiny_mce() {
     printf( '<script type="text/javascript" src="%s"></script>',  plugins_url('js/tiny-mce.js', __FILE__) );
 }
+
+/**
+ * Add in a core button that's disabled by default
+ */
+add_filter( 'mce_buttons_2', 'phila_mce_2_addons' );
+
+function phila_mce_2_addons( $buttons ) {
+  $buttons[] = 'superscript';
+  $buttons[] = 'subscript';
+
+  return $buttons;
+}
