e8ef361fd83f5e4dbfd5b26aa544683d747f1a58
Karissa <k@karissademi.com>
2017-12-13 17:36:39 -0500

    Begin rendering table data

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
@@ -8,11 +8,10 @@
     'post_type' => 'attachment'
 );
 
-$attachments = get_posts($attachment_args);
-d($attachments); ?>
+$attachments = get_posts($attachment_args);?>
 <div class="row mtl">
   <div class="large-24 columns">
-    <table>
+    <table class="responsive">
       <thead>
         <tr>
           <th>Title</th>
@@ -22,19 +21,40 @@ d($attachments); ?>
         </tr>
       </thead>
       <?php foreach( $attachments as $attachment ) : ?>
-        <?php d($attachment)?>
-        <tr>
+        <?php
+        $attachment_data = wp_prepare_attachment_for_js( $attachment->ID );
+
+        $file_type = $attachment_data['subtype'];
+
+        $attachment_published = rwmb_meta( 'phila_document_page_release_date', $args = array(), $post_id = $attachment->ID );
+
+        $type = get_the_terms($attachment->ID, 'media_type');
+        $author = get_the_terms($attachment->ID, 'media_author');
+        $types = array();
+        $authors = array();
+        if ( empty($attachment_published ) ){
+          $attachment_published = get_the_date( $d = '', $attachment->ID );
+        }?>
+        <tr class="clickable-row" data-href="<?php echo $attachment->guid; ?>" id="<?php echo phila_format_uri($attachment->title); ?>">
           <td>
-            <?php echo $attachment->post_title ?>
+            <?php echo $attachment->post_title;
+            echo $file_type;
+             ?>
           </td>
           <td>
-            <?php echo $attachment->type ?>
+            <?php foreach ($type as $t) :
+              array_push($types, $t->name);
+            endforeach;
+            echo implode(", ", $types);?>
           </td>
           <td>
-            <?php echo $attachment->author ?>
+            <?php foreach ($author as $a) :
+              array_push($authors, $a->name);
+            endforeach;
+            echo implode(", ", $authors);?>
           </td>
           <td>
-            <?php echo $attachment->post_date ?>
+            <?php echo $attachment_published ?>
           </td>
         </tr>
       <?php endforeach; ?>
