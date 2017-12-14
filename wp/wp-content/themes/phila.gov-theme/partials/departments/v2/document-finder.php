b6ef09c20bbe9c170faa85653e82192783cc5237
Karissa <k@karissademi.com>
2017-12-14 10:34:21 -0500

    Only allow pdfs and word docs

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
@@ -27,59 +27,61 @@ $attachments = get_posts($attachment_args);?>
 
         $file_type = $attachment_data['subtype'];
 
-        $attachment_published = rwmb_meta( 'phila_document_page_release_date', $args = array(), $post_id = $attachment->ID );
+        if ( $file_type == 'pdf' || $file_type ==' docx' ) :
+          $attachment_published = rwmb_meta( 'phila_document_page_release_date', $args = array(), $post_id = $attachment->ID );
 
-        $attachment_url = get_post_meta($attachment->ID, 'amazonS3_info');
+          $attachment_url = get_post_meta($attachment->ID, 'amazonS3_info');
 
-        $url = get_site_url() . '/' . $attachment_url[0]['key'];
+          $url = get_site_url() . '/' . $attachment_url[0]['key'];
 
-        $date_override = rwmb_meta( 'phila_override_release_date' );
+          $date_override = rwmb_meta( 'phila_override_release_date' );
 
-        $type = get_the_terms($attachment->ID, 'media_type');
-        $author = get_the_terms($attachment->ID, 'media_author');
-        $types = array();
-        $authors = array();
+          $type = get_the_terms($attachment->ID, 'media_type');
+          $author = get_the_terms($attachment->ID, 'media_author');
+          $types = array();
+          $authors = array();
 
-        if ( empty( $attachment_published ) ){
-          $attachment_published = get_the_date( $d = '', $attachment->ID );
-        }
-        ?>
-        <tr class="clickable-row" data-href="<?php echo $url; ?>" id="<?php echo phila_format_uri($attachment->title); ?>">
-          <td>
-            <a href="<?php echo $url ?>"><?php echo $attachment->post_title ?> <span class="show-for-sr"><?php phila_format_document_type( $file_type ); ?></span></a>
-          </td>
-          <td>
-            <?php
-            if( !empty( $type ) ) :
-              foreach ( $type as $t ) :
-                array_push( $types, $t->name );
-              endforeach;
-              echo implode( ', ', $types );
-            endif;?>
-          </td>
-          <td>
-            <?php
-            if( !empty( $author ) ) :
-              foreach ( $author as $a ) :
-                array_push( $authors, $a->name );
-              endforeach;
-              echo implode( ', ', $authors );
-            endif;
-            ?>
-          </td>
-          <td>
-            <?php echo $attachment_published ?>
-          </td>
-          <td>
-            <?php if ( $file_type ): ?>
-            <div aria-hidden="true">
-              <span class="file-type prs"><?php phila_format_document_type( $file_type ); ?></span>
-              <a href="<?php echo $url ?>" data-file-name="<?php echo $attachment->title ?>" aria-hidden="true"><i class="fa fa-download fa-2x"></i>
-              </a>
-            </div>
-            <?php endif; ?>
-          </td>
-        </tr>
+          if ( empty( $attachment_published ) ){
+            $attachment_published = get_the_date( $d = '', $attachment->ID );
+          }
+          ?>
+          <tr class="clickable-row" data-href="<?php echo $url; ?>" id="<?php echo phila_format_uri($attachment->title); ?>">
+            <td>
+              <a href="<?php echo $url ?>"><?php echo $attachment->post_title ?> <span class="show-for-sr"><?php phila_format_document_type( $file_type ); ?></span></a>
+            </td>
+            <td>
+              <?php
+              if( !empty( $type ) ) :
+                foreach ( $type as $t ) :
+                  array_push( $types, $t->name );
+                endforeach;
+                echo implode( ', ', $types );
+              endif;?>
+            </td>
+            <td>
+              <?php
+              if( !empty( $author ) ) :
+                foreach ( $author as $a ) :
+                  array_push( $authors, $a->name );
+                endforeach;
+                echo implode( ', ', $authors );
+              endif;
+              ?>
+            </td>
+            <td>
+              <?php echo $attachment_published ?>
+            </td>
+            <td>
+              <?php if ( $file_type ): ?>
+              <div aria-hidden="true">
+                <span class="file-type prs"><?php phila_format_document_type( $file_type ); ?></span>
+                <a href="<?php echo $url ?>" data-file-name="<?php echo $attachment->title ?>" aria-hidden="true"><i class="fa fa-download fa-2x"></i>
+                </a>
+              </div>
+              <?php endif; ?>
+            </td>
+          </tr>
+        <?php endif; ?>
       <?php endforeach; ?>
     </table>
   </div>
