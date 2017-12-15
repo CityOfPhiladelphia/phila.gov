8c913a0c6815ae92dee9a5ce8767c4511e1e4989
Ben Dolly <bromeodolly@gmail.com>
2017-12-15 15:28:50 -0500

    WIP: PPR Featured Activities parrial w/ data ajax loaded from Carto
    data source https://phl.carto.com/api/v2/sql?q=SELECT *, facility->>0 as facility, gender->>0 as gender FROM ppr_programs INNER JOIN ppr_facilities ON ppr_facilities.id = ppr_programs.facility->>0 LIMIT 3'

---
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-featured-activities.php
@@ -0,0 +1,13 @@
+<section class="row ppr-feat-activites">
+    <h2 class="contrast">Featured activities</h2>
+    <div class="grid-container">
+        <div id="ppr-feat-activites__grid" class="grid-x grid-margin-x align-stretch">
+          <?php
+            include(locate_template('partials/departments/ppr/ppr-feat-activity-card.php'));
+            include(locate_template('partials/departments/ppr/ppr-feat-activity-card.php'));
+            include(locate_template('partials/departments/ppr/ppr-feat-activity-card.php'));
+          ?>
+        </div>
+    </div>
+</section>
+
--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-things-to-do.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-things-to-do.php
@@ -1,6 +1,7 @@
 <?php
 
     include(locate_template('partials/departments/v2/photo-callout-block.php'));
+    include(locate_template('partials/departments/ppr/ppr-featured-activities.php'));
 
 
 ?>
