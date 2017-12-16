8936aeefbbd8dc04a7c38f30c219c698abeb18c6
Ben Dolly <bromeodolly@gmail.com>
2017-12-16 14:33:28 -0500

    gulp watch and sync ec2 files

--- _gulp/tasks/watch.js
+++ _gulp/tasks/watch.js
@@ -2,6 +2,8 @@
 
 var config  = require('../config');
 var gulp    = require('gulp');
+var browserSync  = require('browser-sync').get(config.clientName);
+const shell = require('gulp-shell');
 
 
 gulp.task('watch',['browserSync'],function() {
@@ -10,10 +12,20 @@ gulp.task('watch',['browserSync'],function() {
   //JSLINT
   // gulp.watch(config.scripts.src, ['lint']);
   // COMPASS, SASS, AUTOPREFIXER, MINIFIACATION, SOURCEMAPS
-  gulp.watch(config.styles.src,  ['styles']);
+  gulp.watch(config.styles.src,  ['styles','EC2-sync']);
+  gulp.watch([config.dev_theme+'/**/*.php'],['EC2-sync']);
   // COMPRESSION, PNG SVG JPEG (theme images only not uploads)
   // gulp.watch(config.images.src,  ['images']);
   // reload BrowserSync when new fonts are created
   // gulp.watch(config.fonts.src,   ['fonts']);
 
 });
+
+gulp.task('EC2-sync',function(done) {
+
+    gulp.src([config.dev_theme+'/**/*'])
+        .pipe(shell(['joia push']))
+        .pipe(browserSync.stream({ once: true }));
+
+    done();
+})
