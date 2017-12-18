cd7e1adaa337c485b46b4759547153b625d76082
Karissa <k@karissademi.com>
2017-12-18 14:28:57 -0500

    Add breadcrumbs

--- wp/wp-content/themes/phila.gov-theme/single-programs.php
+++ wp/wp-content/themes/phila.gov-theme/single-programs.php
@@ -18,23 +18,9 @@ $user_selected_template = phila_get_selected_template();
 
     $parent = phila_util_get_furthest_ancestor($post);
 
-    /**
-     * Department Homepage V2 Hero
-     */
-    $hero_data = array(
-      'parent' => phila_util_get_furthest_ancestor($post),
-      'bg' => array(
-        'desktop'      => phila_get_hero_header_v2( $parent->ID ),
-        'mobile'       => phila_get_hero_header_v2( $parent->ID, true ),
-        'photo_credit' => rwmb_meta( 'phila_v2_photo_credit', $parent->ID )
-      )
-    );
-
-  //    get_dept_partial('hero', $hero_data);
-
-
       while ( have_posts() ) : the_post();
         include( locate_template( 'partials/programs/header.php' ) );
+        get_template_part( 'partials/breadcrumbs' );
         get_template_part( 'partials/departments/v2/our', 'services' );
         get_template_part( 'partials/departments/content', 'programs-initiatives' );
       endwhile;
