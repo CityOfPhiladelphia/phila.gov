8c60d33490156e702957f1820a022ccfd86f67bd
Karissa <k@karissademi.com>
2017-12-18 13:54:35 -0500

    Add header file for programs

---
+++ wp/wp-content/themes/phila.gov-theme/partials/programs/header.php
@@ -0,0 +1,31 @@
+<?php
+  /*
+   * Programs and initiatives header
+  */
+
+  $hero = rwmb_meta('prog_header_img', array( 'limit' => 1 ) );
+  $hero = reset($hero);
+
+  $sub_hero = rwmb_meta('prog_header_img_sub', array( 'limit' => 1 ));
+  $sub_hero = reset( $sub_hero );
+
+  $credit = rwmb_meta('phila_photo_credit');
+  $description = rwmb_meta('phila_meta_desc');
+
+?>
+<header class="hero-half">
+  <div class="grid-x">
+    <div class="cell large-12 bg-shade bg-ben-franklin-blue white hero-half--container medium-center">
+      <div class="grid-x grid-container align-right">
+        <div class="hero-half--title mvm">
+          <h1><?php echo the_title() ?></h1>
+          <p class="description"><?php echo $description ?></p>
+        </div>
+      </div>
+    </div>
+    <div class="cell large-12 align-self-stretch hero-image">
+      <img src="<?php echo $hero['full_url'] ?>" alt="" class="show-for-large">
+      <?php echo !empty($credit) ? '<div class="photo-credit">' . $credit . '</div>' : '' ?>
+    </div>
+  </div>
+</header>
