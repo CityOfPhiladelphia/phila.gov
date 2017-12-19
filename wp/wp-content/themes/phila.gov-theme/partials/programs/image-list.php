19216bebf02e067206c61fc3fefb357bf52a73ae
Karissa <k@karissademi.com>
2017-12-19 10:18:32 -0500

    Add image list options

---
+++ wp/wp-content/themes/phila.gov-theme/partials/programs/image-list.php
@@ -0,0 +1,30 @@
+<?php
+/*
+ * Displays images at fixed widths. Initially created to display a list of sponsors.
+*/?>
+<?php
+  if ( !isset( $image_list_vars ) ) :
+    $image_list_vars = phila_image_list($image_list);
+  endif;
+  d($image_list_vars);
+?>
+<section>
+  <div class="grid-container">
+    <div class="grid-x">
+      <div class="cell">
+        <header>
+          <h2 class="contrast"><?= $image_list_vars['title']?></h2>
+        </header>
+      </div>
+    </div>
+  </div>
+  <div class="grid-container">
+    <div class="grid-x">
+      <div class="cell image-list">
+        <?php foreach( $image_list_vars['urls'] as $url ) : ?>
+          <img src=<?= $url ?> alt=""/>
+        <?php endforeach; ?>
+      </div>
+    </div>
+  </div>
+</section>
