1133430519a7a02bd89f695fe63ffa7687950fc1
Karissa <k@karissademi.com>
2017-12-19 11:02:40 -0500

    Include single programs

--- wp/wp-content/themes/phila.gov-theme/single-programs.php
+++ wp/wp-content/themes/phila.gov-theme/single-programs.php
@@ -10,17 +10,17 @@ get_header();
 $user_selected_template = phila_get_selected_template(); ?>
 
 <div id="post-<?php the_ID(); ?>" <?php post_class('program clearfix'); ?>>
-  <header>
 
   <?php
-
-    $parent = phila_util_get_furthest_ancestor($post);
-
       while ( have_posts() ) : the_post();
+      if ($user_selected_template == 'phila_one_quarter'):
+        get_template_part( 'partials/departments/v2/content', 'one-quarter' );
+      endif;
         include( locate_template( 'partials/programs/header.php' ) );
         get_template_part( 'partials/breadcrumbs' );
         get_template_part( 'partials/departments/v2/our', 'services' );
         get_template_part( 'partials/departments/content', 'programs-initiatives' );
+
       endwhile;
 
   ?>
