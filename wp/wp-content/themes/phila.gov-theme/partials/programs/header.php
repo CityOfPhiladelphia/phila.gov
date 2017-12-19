bb6033b62486e7961070a31d84871f8ad68ce212
Karissa <k@karissademi.com>
2017-12-19 09:35:42 -0500

    Add subpage header option

--- wp/wp-content/themes/phila.gov-theme/partials/programs/header.php
+++ wp/wp-content/themes/phila.gov-theme/partials/programs/header.php
@@ -2,30 +2,42 @@
   /*
    * Programs and initiatives header
   */
-
+  $parent = phila_util_get_furthest_ancestor($post);
   $hero = rwmb_meta('prog_header_img', array( 'limit' => 1 ) );
   $hero = reset($hero);
 
-  $sub_hero = rwmb_meta('prog_header_img_sub', array( 'limit' => 1 ));
+  $sub_hero = rwmb_meta('prog_header_img_sub', array( 'limit' => 1 ), $parent->ID);
   $sub_hero = reset( $sub_hero );
 
   $credit = rwmb_meta('phila_photo_credit');
   $description = rwmb_meta('phila_meta_desc');
-
 ?>
-<header class="hero-half">
-  <div class="grid-x">
-    <div class="cell large-12 bg-shade bg-ben-franklin-blue white hero-half--container">
-      <div class="grid-x grid-container align-right">
-        <div class="hero-half--title mvm">
-          <h1><?php echo the_title() ?></h1>
-          <p class="description"><?php echo $description ?></p>
+<header>
+  <?php if ( !empty( get_post_ancestors( $post->ID ) ) ) : ?>
+    <div class="hero-subpage" style="background-image:url(<?php echo $sub_hero['full_url']  ?>) ">
+      <div class="grid-container pvl">
+        <div class="grid-x center">
+          <div class="cell">
+            <h1><?= $parent->post_title ?></h1>
+          </div>
         </div>
       </div>
     </div>
-    <div class="cell large-12 align-self-stretch hero-image">
-      <img src="<?php echo $hero['full_url'] ?>" alt="" class="show-for-large">
-      <?php echo !empty($credit) ? '<div class="photo-credit">' . $credit . '</div>' : '' ?>
-    </div>
-  </div>
+  <?php else: ?>
+    <div class="hero-half">
+      <div class="grid-x">
+        <div class="cell large-12 bg-shade bg-ben-franklin-blue white hero-half--container">
+          <div class="grid-x grid-container align-right">
+            <div class="hero-half--title mvm">
+              <h1><?php echo the_title() ?></h1>
+              <p class="description"><?php echo $description ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="cell large-12 align-self-stretch hero-image">
+          <img src="<?php echo $hero['full_url'] ?>" alt="" class="show-for-large">
+          <?php echo !empty($credit) ? '<div class="photo-credit">' . $credit . '</div>' : '' ?>
+        </div>
+      </div>
+  <?php endif; ?>
 </header>
