97b79d03937890f2e6f88960150b1c3bb4a81017
Karissa <k@karissademi.com>
2017-12-27 10:31:15 -0500

    Center seal

--- wp/wp-content/themes/phila.gov-theme/partials/posts/action-guide-title.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/action-guide-title.php
@@ -1,5 +1,5 @@
 <div class="grid-x grid-padding-x align-middle mbm">
-  <div class="cell medium-3">
+  <div class="cell medium-3 center">
     <img src="<?php echo get_template_directory_uri() . '/img/city-seal.gif'?>" alt="Seal of the City of Philadelphia" class="city-seal">
   </div>
   <div class="cell medium-21">
