c2141e5a3104474665e8cbe05475a951afe424f4
Ben Dolly <bromeodolly@gmail.com>
2017-12-29 14:05:16 -0500

    adds PPR Signature Events meta box group

--- wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-signature-event.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-signature-event.php
@@ -1,8 +1,12 @@
+<?php if( $this->link_url): ?>
 <div class="cell large-7 medium-7 small-18 ppf-signature-event__card">
     <div class="columns">
-        <a href="">
-            <img src="http://via.placeholder.com/285x200" alt="">
-            <h3>Foobar Event</h3>
+        <a href="<?= $this->link_url ?>" target="_blank">
+            <?php if($this->photo_url): ?>
+                <img src="<?= $this->photo_url ?>" alt="">
+            <?php endif; ?>
+            <h3><?= $this->title ?></h3>
         </a>
     </div>
 </div>
+<?php endif; ?>
--- wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-signature-events.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-signature-events.php
@@ -5,15 +5,26 @@
     <div class="columns">
         <div class="grid-container">
         <div class="grid-x grid-margin-x align-spaced">
-            <?php
-                include(locate_template('partials/departments/ppr/ppr-signature-event.php'));
-                include(locate_template('partials/departments/ppr/ppr-signature-event.php'));
-                include(locate_template('partials/departments/ppr/ppr-signature-event.php'));
-                include(locate_template('partials/departments/ppr/ppr-signature-event.php'));
-                include(locate_template('partials/departments/ppr/ppr-signature-event.php'));
-                include(locate_template('partials/departments/ppr/ppr-signature-event.php'));
-             ?>
-        </div>
+        <?php
+            $group_values = rwmb_meta( 'phila_v2_ppr_signature_events' );
+            // var_dump($group_values);
+
+            if ( ! empty( $group_values ) ) {
+                foreach ( $group_values as $group_value ) {
+                    $image_id = isset( $group_value['phila_v2_ppr_sig_event__photo'][0]) ? $group_value['phila_v2_ppr_sig_event__photo'][0] : null;
+                    $image    = RWMB_Image_Field::file_info( $image_id, array( 'size' => 'full' ) );
+                    $imageUrl = $image ? $image['full_url'] : '';
+
+                    phila_get_template_part('partials/departments/ppr/ppr-signature-event',
+                        array(
+                            'title'     => isset( $group_value['phila_v2_ppr_sig_event__header'] ) ? $group_value['phila_v2_ppr_sig_event__header'] : '',
+                            'photo_url' => $imageUrl,
+                            'link_url'  => isset( $group_value['phila_v2_ppr_sig_event__link'] ) ? $group_value['phila_v2_ppr_sig_event__link'] : ''
+                        )
+                    );
+                }
+            }
+         ?>
     </div>
     </div>
 
