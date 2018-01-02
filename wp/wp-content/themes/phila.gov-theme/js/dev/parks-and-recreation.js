fd426ca4d6b52df3fbeac58c7df77d335ba407c3
Ben Dolly <bromeodolly@gmail.com>
2018-01-02 14:19:36 -0500

    adds Our Locations template and partials

--- wp/wp-content/themes/phila.gov-theme/js/dev/parks-and-recreation.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/parks-and-recreation.js
@@ -1,6 +1,8 @@
 jQuery(document).ready(function($) {
     var featActivitiesQueryURL = 'https://phl.carto.com/api/v2/sql?q=SELECT *, facility->>0 as facility, gender->>0 as gender FROM ppr_programs INNER JOIN ppr_facilities ON ppr_facilities.id = ppr_programs.facility->>0 AND ppr_programs.program_is_featured AND ppr_programs.program_is_public AND ppr_programs.program_is_approved LIMIT 3'
     var $activityCards = $('.ppr-feat-activity')
+    var featLocationTypesQueryURL = 'https://phl.carto.com/api/v2/sql?q=SELECT count(*) AS count, ppr_location_types.location_type_name, ppr_location_types.location_type_description, ppr_location_types.location_type_photo, ppr_location_types.id , ppr_location_types.location_type_is_published, ppr_location_types.location_type_is_featured FROM ppr_location_types AS ppr_location_types  INNER JOIN ppr_facilities ON (ppr_facilities.location_type->>0 = ppr_location_types.id)  INNER JOIN ppr_website_locatorpoints ON (ppr_facilities.website_locator_points_link_id = ppr_website_locatorpoints.linkid)  WHERE ppr_location_types.location_type_is_published AND ppr_location_types.location_type_is_featured GROUP BY ppr_location_types.location_type_name, ppr_location_types.location_type_description, ppr_location_types.location_type_photo,  ppr_location_types.id, ppr_location_types.location_type_is_published, ppr_location_types.location_type_is_featured LIMIT 6';
+    var $locationTypeCards = $('.ppr-feat-locationType')
 
     var render = function(card, data){
         for(var prop in data) {
@@ -8,31 +10,55 @@ jQuery(document).ready(function($) {
             if(typeof data[prop] == 'object'){
                 render(card, data[prop])
             }
+
             var template = $(card).find('[data-'+prop+']')
             if(template.length){
-                $(card).find('.ppr-feat-activity__loader').remove()
+                $(card).find('.ppr-loader').remove()
                 $(card).addClass('loaded')
                 if(typeof data[prop] != 'object') {
+                    let imageRegex = /(photo)|(image)|(img)/;
+
+                    // if(imageRegex.test(prop)){
+                    //     if(data[prop] == '' || data[prop] == null ){
+                    //         template.remove()
+                    //     }
+                    //     template.attr('src', data[prop])
+                    // }
+                    //
+                    //
                     template.html(data[prop])
                 }
             }
         }
     }
 
-    $.get(featActivitiesQueryURL, function(results){
-        var activityData = results.rows
-        $activityCards.each(function(idx, card){
-            if(activityData[idx]){
-               render(card, activityData[idx])
-            } else {
-                $(card)
-                  .find('.ppr-feat-activity__loader')
-                  .html('<i class="fa fa-exclamation-circle" aria-hidden="true"></i>')
-                $(card).addClass('error')
-            }
 
+
+    function loadCardData (queryURL, $cards) {
+        $.get(queryURL, function(results){
+            var data = results.rows
+            $cards.each(function(idx, card){
+                if(data[idx]){
+                   render(card, data[idx])
+                } else {
+                    $(card)
+                      .find('.ppr-loader')
+                      .html('<i class="fa fa-exclamation-circle" aria-hidden="true"></i>')
+                    $(card).addClass('error')
+                    $(card).animate({opacity: 0}, 250)
+                }
+
+            })
         })
-    })
+    }
+
+    if($activityCards.length) {
+        loadCardData(featActivitiesQueryURL, $activityCards)
+    }
+
+    if($locationTypeCards.length) {
+        loadCardData(featLocationTypesQueryURL, $locationTypeCards)
+    }
 
 
 });
---
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-featured-locationTypes.php
@@ -0,0 +1,22 @@
+<section class="row ppr-feat-locations">
+    <div class="columns">
+        <h2 class="contrast">Our Locations</h2>
+    </div>
+
+    <div class="columns">
+        <div class="grid-container">
+        <div id="ppr-feat-locations__grid" class="grid-x grid-margin-x align-stretch align-center">
+          <?php
+            include(locate_template('partials/departments/ppr/ppr-feat-locationType-card.php'));
+            include(locate_template('partials/departments/ppr/ppr-feat-locationType-card.php'));
+            include(locate_template('partials/departments/ppr/ppr-feat-locationType-card.php'));
+            include(locate_template('partials/departments/ppr/ppr-feat-locationType-card.php'));
+            include(locate_template('partials/departments/ppr/ppr-feat-locationType-card.php'));
+            include(locate_template('partials/departments/ppr/ppr-feat-locationType-card.php'));
+          ?>
+        </div>
+        </div>
+    </div>
+
+</section>
+
