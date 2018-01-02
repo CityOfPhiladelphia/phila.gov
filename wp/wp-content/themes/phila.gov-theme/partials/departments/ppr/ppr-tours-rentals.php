2fbbb01d6f12b91230ce021a0e8f5c073e1148e9
Ben Dolly <bromeodolly@gmail.com>
2018-01-02 11:42:15 -0500

    adds PPR Things To Do template full width CTA .row wrapper

--- wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-tours-rentals.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-tours-rentals.php
@@ -1,6 +1,8 @@
 <?php if(rwmb_meta('phila_v2_ppr_tours_rentals__link-text')): ?>
 <section class="row mvl ppr-tours-rentals">
-    <h2 class="contrast">Tours and rentals</h2>
-    <p><?= rwmb_meta('phila_v2_ppr_tours_rentals__link-text'); ?></p> <a href="<?= rwmb_meta('phila_v2_ppr_tours_rentals__link'); ?>"><?= rwmb_meta('phila_v2_ppr_tours_rentals__link-text'); ?></a>
+    <div class="columns">
+        <h2 class="contrast">Tours and rentals</h2>
+        <p><?= rwmb_meta('phila_v2_ppr_tours_rentals__link-text'); ?></p> <a href="<?= rwmb_meta('phila_v2_ppr_tours_rentals__link'); ?>"><?= rwmb_meta('phila_v2_ppr_tours_rentals__link-text'); ?></a>
+    </div>
 </section>
 <?php endif; ?>
