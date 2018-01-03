1b50fe6e549cb550d28c8a12982780e0f8f13bd4
Ben Dolly <bromeodolly@gmail.com>
2018-01-03 13:24:07 -0500

    loads PPR Our Location cards data from CartoAPI
    loads the published and featured Location Types from Carto and their associated Flickr photos

--- wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-feat-locationType-card.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/ppr/ppr-feat-locationType-card.php
@@ -1,7 +1,7 @@
-<div class="cell ppr-feat-locationType large-8 medium-8 small-20 align-self-center loaded" >
+<div class="cell ppr-feat-locationType large-7 medium-8 small-20 align-self-center loaded" >
   <div class="ppr-feat-locationType__img-container">
     <div class="ppr-feat-locationType__count-badge"><i class="fa fa-map-marker" aria-hidden="true"></i><span data-count></span></div>
-    <img data-location_type_photo class="ppr-feat-locationType__img" src="http://via.placeholder.com/330x175" alt="">
+    <img data-location_type_photo class="ppr-feat-locationType__img" src="http://via.placeholder.com/320x214" alt="">
   </div>
 
   <h4 data-location_type_name="ppr-feat-locationType__name"></h4>
