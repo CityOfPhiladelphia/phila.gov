cfba26fc5d0b5eb3b1d43b1ab76ce3b0e78ce9a2
Ben Dolly <bromeodolly@gmail.com>
2018-01-03 12:01:54 -0500

    adjusts Things To Do partials image sizes and styles
    • updates photo callout block to use medium image size
    • updates linked image grid photo size to 320  214

---
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-our-locations.php
@@ -0,0 +1,9 @@
+<section class="our-locations">
+<?php
+    include(locate_template('partials/departments/v2/photo-callout-block.php'));
+    include(locate_template('partials/departments/ppr/ppr-featured-locationTypes.php'));
+?>
+<div class="row mvxl">
+    <?php include(locate_template('partials/departments/v2/full-width-call-to-action.php')); ?>
+</div>
+</section>
