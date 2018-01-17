d84670eab3db5be7f7fbb675f65909d771b8f0ac
Karissa <k@karissademi.com>
2018-01-17 15:55:07 -0500

    Remove sticky nav and scroll to top instead

--- wp/wp-content/themes/phila.gov-theme/js/vuesrc/Programs.vue
+++ wp/wp-content/themes/phila.gov-theme/js/vuesrc/Programs.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="programs-initiatives-landing">
     <div class="grid-x grid-margin-x">
-      <div class="small-24 medium-8 cell" data-sticky-container>
-        <section class="sticky" data-sticky data-top-anchor="programs-initiatives-landing:top" data-btm-anchor="page:bottom" data-options="marginTop:4.8;">
+      <div class="small-24 medium-8 cell">
+        <section>
           <div class="panel phm">
             <h3>Search within Programs</h3>
             <form v-on:submit.prevent="onSubmit">
@@ -223,6 +223,8 @@ export default {
     },
     filterResults: function (event) {
       this.loading = true
+
+      jQuery('html,body').animate({scrollTop:0},700);
 
       this.$nextTick(function () {
         var params = {
