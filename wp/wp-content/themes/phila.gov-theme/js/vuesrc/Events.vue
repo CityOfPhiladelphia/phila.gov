840487b95b08556df0f8463b0c14511870264cab
Karissa <k@karissademi.com>
2018-02-01 17:39:18 -0500

    Add no results text

--- wp/wp-content/themes/phila.gov-theme/js/vuesrc/Events.vue
+++ wp/wp-content/themes/phila.gov-theme/js/vuesrc/Events.vue
@@ -77,6 +77,9 @@
               </div>
             </div>
           </div>
+        </div>
+      <div v-if="filteredEvents == ''">
+        <p class="h3 mtm center">Sorry, there are no results for that search.</p>
       </div>
     </div>
       <div v-for="(event, index) in events"
