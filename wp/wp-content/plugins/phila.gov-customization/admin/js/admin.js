836df85519344254cf2c3d7a7684335aab144dc6
Karissa <k@karissademi.com>
2018-02-16 14:49:30 -0500

    Add help text + disable field

--- wp/wp-content/plugins/phila.gov-customization/admin/js/admin.js
+++ wp/wp-content/plugins/phila.gov-customization/admin/js/admin.js
@@ -63,6 +63,8 @@ jQuery(document).ready(function($) {
         $('#title').rules('add', {
             maxlength: 72 + 14
           });
+          $( "#title" ).attr('disabled', true);
+          $( "<div style='color:#838383; padding-left:5px;'>This field isn't avilable to edit. To change the title, save as a new item.</div> " ).insertAfter('#title');
       }else{
       $('#title').rules('add', {
           maxlength: 72
