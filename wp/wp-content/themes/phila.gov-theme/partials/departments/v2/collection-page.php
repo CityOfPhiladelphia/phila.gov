4f0a8bad0a77e0725c490434c0566ef0edee02a6
Karissa <k@karissademi.com>
2018-02-22 12:58:17 -0500

    Add markup

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/collection-page.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/collection-page.php
@@ -15,11 +15,27 @@ foreach ($row_content as $key => $value ) :
     ?>
     <div class="row">
       <div class="columns medium-6">
-        <?php echo $headline ?>
+        <h3 id="<?= sanitize_title_with_dashes($headline, null, 'save')?>"><?php echo $headline ?></h3>
       </div>
-      <div class="columns medium-18">
+      <div class="columns medium-18 pbxl">
+        <?php $counter = 0; ?>
+
         <?php foreach( $current_row['service_pages']['phila_v2_service_page'] as $service_page ) : ?>
-          <a href="<?php echo get_the_permalink($service_page);?>"><?php echo get_the_title($service_page); ?></a>
+          <?php $counter++; ?>
+          <?php if($counter % 3 == 1) :?>
+          <div class="row equal-height fat-gutter">
+          <?php endif;?>
+          <div class="small-24 medium-8 column end">
+            <a href="<?php echo get_the_permalink($service_page);?>" class="card sub-topic equal">
+              <div class="content-block">
+                <h4 class="h3"><?php echo get_the_title($service_page); ?></h3>
+                <?php echo rwmb_meta( 'phila_meta_desc', $args = '', $service_page ); ?>
+              </div>
+            </a>
+          </div>
+        <?php if($counter % 3 == 0) :?>
+          </div>
+        <?php endif;?>
       <?php endforeach; ?>
       </div>
     </div>
@@ -30,11 +46,21 @@ foreach ($row_content as $key => $value ) :
     <?php $headline = $current_row['document_pages']['phila_custom_text_title'];?>
     <div class="row">
       <div class="columns medium-6">
-        <?php echo $headline ?>
+        <h3 id="<?= sanitize_title_with_dashes($headline, null, 'save')?>"><?php echo $headline ?></h3>
       </div>
-      <div class="columns medium-18">
-        <?php foreach( $current_row['document_pages']['phila_document_page_picker'] as $document_page ) : ?>
-          <a href="<?php echo get_the_permalink($document_page);?>"><?php echo get_the_title($document_page); ?></a>
+      <div class="columns medium-18 pbxl">
+        <?php foreach($current_row['document_pages']['document_page_group'] as $group): ?>
+          <h4><?php echo $group['phila_custom_wysiwyg']['phila_wysiwyg_title']?></h4>
+          <p><?php echo $group['phila_custom_wysiwyg']['phila_wysiwyg_content']?></p>
+          <div class="resource-list">
+            <ul>
+              <?php foreach($group['phila_document_page_picker'] as $doc): ?>
+              <li class="phm pvs clickable-row" data-href="<?php echo get_the_permalink($doc); ?>">
+                <a href="<?php echo get_the_permalink($doc);?>">                <div><i class="fa fa-file-text" aria-hidden="true"></i></div> <?php echo get_the_title($doc); ?></a></li>
+              <?php endforeach; ?>
+            </ul>
+          </div>
+
       <?php endforeach; ?>
       </div>
     </div>
@@ -43,14 +69,28 @@ foreach ($row_content as $key => $value ) :
   <?php if ($current_row_option === 'program') :  ?>
 
     <?php $headline = $current_row['program_pages']['phila_custom_text_title'];?>
+
     <div class="row">
       <div class="columns medium-6">
-        <?php echo $headline ?>
+        <h3 id="<?php echo sanitize_title_with_dashes($headline, null, 'save')?>"><?php echo $headline ?></h3>
       </div>
-      <div class="columns medium-18">
-        <?php foreach( $current_row['program_pages']['phila_select_programs'] as $program_page ) : ?>
-          <a href="<?php echo get_the_permalink($program_page);?>"><?php echo get_the_title($program_page); ?></a>
-      <?php endforeach; ?>
+      <div class="columns medium-18 pbxl">
+        <div class="row fat-gutter">
+          <?php foreach( $current_row['program_pages']['phila_select_programs'] as $program_page ) : ?>
+            <div class="medium-12 columns end mbl">
+              <a class="card program-card" href="<?php echo get_the_permalink($program_page);?>">
+                <?php
+                $img = rwmb_meta( 'prog_header_img', $args = array( 'size' => 'medium', 'limit' => 1 ), $program_page );
+                $img = reset( $img );?>
+                <img src="<?php echo $img['url'] ?>" alt="<?php echo $img['alt']?>">
+                <div class="content-block">
+                  <h4 class="h4"><?php echo get_the_title($program_page); ?></h3>
+                  <?php echo rwmb_meta( 'phila_meta_desc', $args = '', $program_page ); ?></h4>
+                </div>
+              </a>
+            </div>
+        <?php endforeach; ?>
+      </div>
       </div>
     </div>
   <?php endif; ?>
@@ -58,12 +98,13 @@ foreach ($row_content as $key => $value ) :
   <?php if ($current_row_option === 'free_text') :  ?>
 
     <?php foreach( $current_row['free_text'] as $free_text ) : ?>
+      <?php $headline = $free_text['phila_custom_wysiwyg']['phila_wysiwyg_title']; ?>
 
       <div class="row">
         <div class="columns medium-6">
-          <?php echo $free_text['phila_custom_wysiwyg']['phila_wysiwyg_title'] ?>
+            <h3 id="<?= sanitize_title_with_dashes($headline, null, 'save')?>"><?php echo $headline ?></h3>
         </div>
-        <div class="columns medium-18">
+        <div class="columns medium-18 pbxl">
           <?php echo $free_text['phila_custom_wysiwyg']['phila_wysiwyg_content'] ?>
 
         </div>
