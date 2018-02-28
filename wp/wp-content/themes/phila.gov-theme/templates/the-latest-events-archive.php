83428f66386a0b13501332c9078324f5e40326d4
Karissa <k@karissademi.com>
2018-02-28 14:14:58 -0500

    Prep for passing department owner data

--- wp/wp-content/themes/phila.gov-theme/templates/the-latest-events-archive.php
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest-events-archive.php
@@ -25,25 +25,27 @@ $calendar_q = new WP_Query( $cal_a );
 if ( $calendar_q->have_posts() ) : ?>
 <?php $post_ids = array();?>
 <?php $cal_ids = array(); ?>
+<?php $cal_cats = array();?>
   <?php while ( $calendar_q->have_posts() ) : $calendar_q->the_post(); ?>
-    <?php array_push($post_ids, get_the_id() ); ?>
+    <?php $categories = get_the_category( get_the_id() ); ?>
+    <?php if ($categories != null) : ?>
+      <?php array_push($post_ids, get_the_id() ); ?>
+      <?php array_push($cal_cats, $categories[0]->cat_ID);?>
+    <?php endif;?>
   <?php endwhile; ?>
   <?php wp_reset_postdata(); ?>
 <?php endif; ?>
-
 <?php foreach ($post_ids as $post_id) : ?>
+
   <?php array_push($cal_ids, base64_decode(get_post_meta( $post_id, '_google_calendar_id', true ) ) ); ?>
-<?php endforeach;
-  $final_array = array_filter($cal_ids);
-  $final_array = array_unique($final_array);
-  $final_array = array_values($final_array);
-?>
-
-<?php $json_final = json_encode($final_array);
-
+<?php endforeach; ?>
+  <?php $final_array = array_combine($cal_cats, $cal_ids); ?>
+<?php $calendar_ids = json_encode($final_array);
+  var_dump($calendar_ids);
+  /* g_cal_data - array of all calendar ids, duplicates removed */
   wp_localize_script('vuejs-app',
   'g_cal_data', array(
-    'json' => __($json_final)
+    'json' => __($calendar_ids)
       )
     );
 ?>
