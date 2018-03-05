98de3274b91fdf64cbd50ba8df941d8021e6db10
Karissa <k@karissademi.com>
2018-03-05 10:46:03 -0500

    Pass category data

--- wp/wp-content/themes/phila.gov-theme/templates/the-latest-events-archive.php
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest-events-archive.php
@@ -28,6 +28,7 @@ if ( $calendar_q->have_posts() ) {
   $cal_ids = array();
   $cal_cat_ids = array();
   $cal_nice_name = array();
+  $names = array();
   $links = array();
   while ( $calendar_q->have_posts() ) : $calendar_q->the_post();
     $categories = get_the_category( get_the_id() );
@@ -43,33 +44,42 @@ if ( $calendar_q->have_posts() ) {
   foreach ($post_ids as $post_id) {
     array_push($cal_ids, base64_decode(get_post_meta( $post_id, '_google_calendar_id', true ) ) );
   }
+  $i = 0;
   foreach ($cal_nice_name as $nice){
+    $i++;
     //var_dump($nice[0]);
     $links[$nice[0]->cat_ID] = phila_get_current_department_name($nice);
+    $names[$i]['id'] = $nice[0]->cat_ID;
+    $names[$i]['name'] = $nice[0]->name;
+
   }
   $final_array = array_combine($cal_cat_ids, $cal_ids);
   $final_array = array_filter($final_array);
 
   $links = array_filter($links);
 
-
   $calendar_ids = json_encode($final_array);
 
-  //var_dump($links);
-  //var_dump($final_array);
+  $names = json_encode($names);
 
-  /* g_cal_data - Object: key is category ID, value is ids*/
+  /* g_cal_data - Object: key is category ID, value is calednar ids*/
   wp_localize_script('vuejs-app',
   'g_cal_data', array(
     'json' => __($calendar_ids)
       )
     );
-    /*calendar_owners - object: key is category ID, value is department link */
+    /*calendar_owners - Array: key is category ID, value is department link */
     wp_localize_script('vuejs-app',
     'calendar_owners', array(
       'json' => __($links)
-        )
-      );
+      )
+    );
+    /* calendar_nice_names - Array. key is category ID, value is department nice name */
+    wp_localize_script('vuejs-app',
+    'calendar_nice_names', array(
+      'json' => __($names)
+      )
+    );
 
 ?>
 <section id="events-archive" class="content-area archive">
