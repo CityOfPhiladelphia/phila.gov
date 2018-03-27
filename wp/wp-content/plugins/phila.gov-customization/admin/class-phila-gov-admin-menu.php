5ea951eecc4d0ac2fa160c10cebcf130d6acc187
Karissa <k@karissademi.com>
2018-03-27 09:49:22 -0400

    Add nav menus as option for program pages

--- wp/wp-content/plugins/phila.gov-customization/admin/class-phila-gov-admin-menu.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/class-phila-gov-admin-menu.php
@@ -72,8 +72,10 @@ class Phila_Gov_Admin_Menu {
 
   function change_admin_post_label(){
 
-    // Add Menus as a Department Site submenu
+    // Add Menus as a Department Site submenu and program pages
     add_submenu_page( 'edit.php?post_type=department_page', 'Nav Menu', 'Nav Menu', 'edit_posts', 'nav-menus.php');
+    add_submenu_page( 'edit.php?post_type=programs', 'Nav Menu', 'Nav Menu', 'edit_posts', 'nav-menus.php');
+
 
     remove_menu_page( 'edit.php?post_type=announcement' );
 
