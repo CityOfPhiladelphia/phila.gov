8f32042d868dfedb12ecf182cfa591ae24aa4022
Karissa <k@karissademi.com>
2018-04-12 17:08:29 -0400

    Correct name

--- wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-row-select-options.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-row-select-options.php
@@ -46,7 +46,7 @@ class Phila_Gov_Row_Select_Options {
         'phila_image_list' => 'Image list',
         'phila_list_items' => 'List items (grid)',
         'phila_full_width_press_releases' => 'Press Releases',
-        'phila_enrollment'  => 'Registration available'
+        'phila_registration'  => 'Registration available'
       ),
     );
   }
