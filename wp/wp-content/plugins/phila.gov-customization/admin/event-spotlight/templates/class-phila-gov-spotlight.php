a1e46c179de63666ca5e016ba5b0bf835db457d3
Karissa <k@karissademi.com>
2018-05-07 16:37:59 -0400

    Test map functionality

--- wp/wp-content/plugins/phila.gov-customization/admin/event-spotlight/templates/class-phila-gov-spotlight.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/event-spotlight/templates/class-phila-gov-spotlight.php
@@ -17,7 +17,7 @@ class Phila_Gov_Event_Spotlight_Template {
 
     $meta_boxes[] = array(
       'id'       => 'phila_spotlight_header',
-      'title'    => 'Spotlight images',
+      'title'    => 'Spotlight overview',
       'pages' => array( 'event_spotlight' ),
       'priority' => 'high',
       'include' => array(
@@ -46,7 +46,104 @@ class Phila_Gov_Event_Spotlight_Template {
           'name'  => 'Photo credit',
         ),
         Phila_Gov_Standard_Metaboxes::phila_metabox_title('Name & organization', 'photo_credit', 'E.g.: N. Santos for VISIT PHILADELPHIA™', '60' ),
-      )
+
+        array(
+          'name' => 'Date/Time Format',
+          'id' => 'phila_date_format',
+          'type' => 'select',
+          'placeholder' => 'Choose date format...',
+          'options' => array(
+            'date' => 'Date only',
+            'datetime' => 'Date & Time',
+          ),
+        ),
+        array(
+          'name'  => 'Start Day and Time',
+          'id'    => 'phila_effective_start_datetime',
+          'class' =>  'effective-start-time',
+          'type'  => 'datetime',
+          'size'  =>  25,
+          'columns' => 6,
+          'js_options' =>  array(
+            'timeFormat' =>  'hh:mm tt',
+            'dateFormat'=>'mm-dd-yy',
+            'stepMinute' => 15,
+            'showHour' => 'true',
+            'controlType'=> 'select',
+            'oneLine'=> true,
+            'timeInput' => true,
+          ),
+          'timestamp' => true,
+          'visible' => array('phila_date_format', '=', 'datetime'),
+        ),
+        array(
+          'name'  => 'End Day and Time',
+          'id'    => 'phila_effective_end_datetime',
+          'type'  => 'datetime',
+          'class' =>  'effective-end-time',
+          'size'  =>  25,
+          'columns' => 6,
+          'js_options' =>  array(
+            'timeFormat' => 'hh:mm tt',
+            'dateFormat' => 'mm-dd-yy',
+            'stepMinute' => 15,
+            'showHour' => 'true',
+            'controlType'=> 'select',
+            'oneLine'=> true,
+            'timeInput' => true
+          ),
+          'timestamp' => true,
+          'visible' => array('phila_date_format', '=', 'datetime'),
+        ),
+        array(
+          'name'  => 'Start Date',
+          'id'    => 'phila_effective_start_date',
+          'class' =>  'effective-start-time',
+          'type'  => 'date',
+          'size'  =>  25,
+          'columns' => 6,
+          'js_options' =>  array(
+            'dateFormat'=>'mm-dd-yy',
+            'stepMinute' => 15,
+            'showHour' => 'true',
+            'controlType'=> 'select',
+            'oneLine'=> true,
+            'timeInput' => true,
+          ),
+          'timestamp' => true,
+          'visible' => array('phila_date_format', '=', 'date'),
+        ),
+        array(
+          'name'  => 'End Date',
+          'id'    => 'phila_effective_end_date',
+          'type'  => 'date',
+          'class' =>  'effective-end-time',
+          'size'  =>  25,
+          'columns' => 6,
+          'js_options' =>  array(
+            'dateFormat' => 'mm-dd-yy',
+            'stepMinute' => 15,
+            'showHour' => 'true',
+            'controlType'=> 'select',
+            'oneLine'=> true,
+            'timeInput' => true
+          ),
+          'timestamp' => true,
+          'visible' => array('phila_date_format', '=', 'date'),
+        ),
+        array(
+          'name' => 'Address',
+          'id'  => 'address',
+          'type'  => 'text'
+        ),
+        array(
+          'name'  => 'Location',
+          'id'  => 'location',
+          'type'  => 'map',
+          'api_key' => '',
+          'address_field' => 'address'
+        )
+      ),
     );
 
     $meta_boxes[] = array(
@@ -114,6 +211,7 @@ class Phila_Gov_Event_Spotlight_Template {
                 ),
                 'fields' => Phila_Gov_Standard_Metaboxes::phila_metabox_v2_calendar_full()
             ),
+
           ),
         ),
       )
