bc5788b1620fe794e3aa89bdd7510f3180626b55
Karissa <k@karissademi.com>
2018-05-18 15:38:39 -0400

    Account for nothing but name and title

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/board-commission-members.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/board-commission-members.php
@@ -10,27 +10,30 @@
     <?php if (!empty($section_title)) :?>
       <h2 class="contrast"><?php echo $section_title ?></h2>
     <?php endif; ?>
-    <div class="accordion" data-accordion>
+    <div class="accordion" data-accordion data-multi-expand="true" data-allow-all-closed="true">
       <?php foreach ($members as $member):?>
-        <li class="accordion-item" data-accordion-item data-multi-expand="true">
           <!-- Accordion tab title -->
-          <a href="#" class="accordion-title"><?php echo isset( $member['full_name']) ? $member['full_name'] : ''; ?> <?php echo isset($member['title']) ? '<i>' . $member['title'] . '</i>': '';?></a>
+          <?php if( empty($member['headshot']) && empty($member['bio']) && empty($member['email']) && empty($member['phone']) ) : ?>
+            <div class="disabled accordion-title"><?php echo isset( $member['full_name']) ? $member['full_name'] : ''; ?> <?php echo isset($member['title']) ? '<i>' . $member['title'] . '</i>': '';?></div>
+          <?php else :?>
+            <div class="accordion-item" data-accordion-item>
 
-          <div class="accordion-content" data-tab-content>
-            <?php $image = wp_get_attachment_image_src($member['headshot'][0], $size = 'full'); ?>
-            <?php echo isset($member['headshot']) ? '<img src="' . $image[0] . '" alt="' . $member['full_name'] .'" class="float-left" width="200" height="200">'  : ''; ?>
-
-            <?php echo isset($member['bio']) ? apply_filters('the_content', $member['bio']) . '<br>'  : ''?>
-            <?php echo isset( $member['email'] ) ? '<a href="mailto:' . $member['email'] .'"> ' .  $member['email']. '</a>' : ''
-            ?>
-            <?php if ($member['email'] != '' && $member['phone'] !=
-            '' ) echo ' | ' ?>
-            <?php echo isset($member['phone']) ?
-            '(' . $member['phone']['area'] . ') ' . $member['phone']['phone-co-code'] .'-' . $member['phone']['phone-subscriber-number']  : ''?>
+              <a href="#" class="accordion-title"><?php echo isset( $member['full_name']) ? $member['full_name'] : ''; ?> <?php echo isset($member['title']) ? '<i>' . $member['title'] . '</i>': '';?></a>
+          <?php endif; ?>
+            <div class="accordion-content" data-tab-content>
+              <?php if( isset($member['headshot'])) : ?>
+                <?php $image = wp_get_attachment_image_src($member['headshot'][0], $size = 'full');
+                echo isset($member['headshot']) ? '<img src="' . $image[0] . '" alt="' . $member['full_name'] .'" class="float-left" width="200" height="200">'  : ''; ?>
+              <?php endif?>
+              <?php echo isset($member['bio']) ? apply_filters('the_content', $member['bio']) : ''?>
+              <?php echo isset( $member['email'] ) ? '<a href="mailto:' . $member['email'] .'"> ' .  $member['email']. '</a>' : ''
+              ?>
+              <?php if (isset($member['email']) == true && isset($member['phone']) == true) echo ' | ' ?>
+              <?php echo isset($member['phone']) ?
+              '<a href="tel:' . $member['phone']['area'] . $member['phone']['phone-co-code'] . $member['phone']['phone-subscriber-number']  . '">(' . $member['phone']['area'] . ') ' . $member['phone']['phone-co-code'] .'-' . $member['phone']['phone-subscriber-number'] . '</a>' : ''?>
 
           </div>
-        </li>
-
+        </div>
       <?php endforeach;?>
     </div>
   </section>
