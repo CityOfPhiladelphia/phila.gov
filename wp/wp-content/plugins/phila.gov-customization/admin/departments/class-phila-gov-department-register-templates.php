435cceff3399c72776673750af72dd3dad3aeded
Karissa <k@karissademi.com>
2018-05-18 11:43:29 -0400

    Add fields

--- wp/wp-content/plugins/phila.gov-customization/admin/departments/class-phila-gov-department-register-templates.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/departments/class-phila-gov-department-register-templates.php
@@ -90,6 +90,48 @@ class Phila_Gov_Deparment_Templates {
         'type' => 'custom_html',
         'std' => 'Visit <a href="/wp-admin/edit.php?post_type=staff_directory">staff members</a> section to add/edit staff.',
         'visible' => array('phila_template_select', 'in', ['staff_directory_v2','staff_directory'])
+      ),
+      array(
+        'id' => 'phila_commission_members',
+        'name'  => 'Board or commission members',
+        'type'  => 'group',
+        'clone' => true,
+        'visible' => array('phila_template_select', 'in', ['staff_directory_v2','staff_directory']),
+        'fields'  => array(
+          array(
+            'id'  => 'full_name',
+            'type'  => 'text',
+            'desc'  => 'Enter the full name, with honorific e.g.: Dr. Herbert West, Ph.D.'
+          ),
+          array(
+            'id'  => 'title',
+            'name'  => 'Title',
+            'type'  => 'text',
+            'desc'  => 'E.g.: Chief of Staff/Reanimator'
+          ),
+          array(
+            'id'  => 'email',
+            'name'  => 'Email address',
+            'type'  => 'email',
+          ),
+          array(
+            'id'  => 'phone',
+            'name'  => 'Phone',
+            'type'  => 'phone'
+          ),
+          array(
+            'id'  => 'headshot',
+            'name'  => 'Headshot',
+            'type'  => 'image',
+            'desc'  => 'Image size: 400px by 400px.'
+          ),
+          array(
+            'name' => 'Bio',
+            'id'   => 'bio',
+            'type' => 'wysiwyg',
+            'options' => Phila_Gov_Standard_Metaboxes::phila_wysiwyg_options_basic()
+          ),
+        )
       )
     ),
   );
