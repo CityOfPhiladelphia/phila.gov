0e0a3a4269f7eb1722d33963c126287c134ab41e
Karissa <k@karissademi.com>
2018-05-24 10:39:47 -0400

    Remove hanging ;

--- wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/meta-boxes.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/meta-boxes.php
@@ -34,7 +34,6 @@ function phila_return_week_array(){
 function phila_setup_tiny_mce_basic( array $options ){
 
   $output['block_formats'] = 'Paragraph=p; Heading 1=h1; Heading 2=h2; Heading 3=h3; Heading 4=h4; Heading 5=h5; Heading 6=h6;';
-;
 
   $defaults = array(
     'format_select' => false,
