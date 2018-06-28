272e85d61b609e59918afebbab489c9ef843cc67
Karissa <k@karissademi.com>
2018-06-28 11:17:05 -0400

    Make tags hierarchical + relabel them

--- wp/wp-content/plugins/phila.gov-customization/admin/class-phila-gov-custom-taxonomies.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/class-phila-gov-custom-taxonomies.php
@@ -21,6 +21,8 @@ class Phila_Gov_Custom_Taxonomies {
     add_action( 'init', array($this, 'media_type') );
     add_action( 'init', array($this, 'media_author') );
     add_action( 'init', array($this, 'spotlight_tags') );
+    add_action( 'init', array($this, 'hierarchical_tags') );
+
 
   }
 
@@ -163,5 +165,41 @@ class Phila_Gov_Custom_Taxonomies {
         'with_front' => false,
       ),
     ));
+  }
+
+  /*
+   * Relabel tags
+  */
+  function hierarchical_tags() {
+
+    $labels = array(
+    'name'                       => _x( 'Tags', 'Tags', 'hierarchical_tags' ),
+    'singular_name'              => _x( 'Tag', 'Tag', 'hierarchical_tags' ),
+    'menu_name'                  => __( 'Tags', 'hierarchical_tags' ),
+    'all_items'                  => __( 'All Tags', 'hierarchical_tags' ),
+    'parent_item'                => __( 'Parent Tag', 'hierarchical_tags' ),
+    'parent_item_colon'          => __( 'Parent Tag:', 'hierarchical_tags' ),
+    'new_item_name'              => __( 'New Tag Name', 'hierarchical_tags' ),
+    'add_new_item'               => __( 'Add New Tag', 'hierarchical_tags' ),
+    'edit_item'                  => __( 'Edit Tag', 'hierarchical_tags' ),
+    'update_item'                => __( 'Update Tag', 'hierarchical_tags' ),
+    'view_item'                  => __( 'View Tag', 'hierarchical_tags' ),
+    'separate_items_with_commas' => __( 'Separate tags with commas', 'hierarchical_tags' ),
+    'add_or_remove_items'        => __( 'Add or remove tags', 'hierarchical_tags' ),
+    'choose_from_most_used'      => __( 'Choose from the most used', 'hierarchical_tags' ),
+    'popular_items'              => __( 'Popular Tags', 'hierarchical_tags' ),
+    'search_items'               => __( 'Search Tags', 'hierarchical_tags' ),
+    'not_found'                  => __( 'Not Found', 'hierarchical_tags' ),
+  );
+
+    register_taxonomy( 'post_tag', 'post', array(
+      'hierarchical'              => true,
+      'query_var'                 => 'tag',
+      'labels'                    => $labels,
+      'public'                    => true,
+      'show_ui'                   => true,
+      'show_admin_column'         => true,
+      '_builtin'                  => true,
+    ) );
   }
 }
