0ca5f0e57d9183b2ab3bde2b791f092582958c80
Karissa <k@karissademi.com>
2018-07-06 16:18:13 -0400

    Add new functionality to city directory

--- wp/wp-content/themes/phila.gov-theme/archive-department_page.php
+++ wp/wp-content/themes/phila.gov-theme/archive-department_page.php
@@ -16,27 +16,50 @@ get_header(); ?>
     </div>
     <div class="row">
       <div class="small-24 columns results mbm">
-        <div id="filter-list">
+        <div id="filter-list" class="city-government-directory">
           <form>
-            <input class="search" type="text" placeholder="Filter results...">
+            <input class="search" type="text" placeholder="Begin typing to filter results by title, keyword or acronym...">
           </form>
             <?php
             $args = phila_get_department_homepage_list();
             $department_list = new WP_Query( $args );
 
             if ( $department_list->have_posts() ) : ?>
-              <ul class="list no-bullet">
+            <div class="row list-heading">
+              <div class="columns medium-6">
+                Name
+              </div>
+              <div class="columns medium-10 phl">
+                Description
+              </div>
+              <div class="columns medium-8">
+                Connect
+              </div>
+            </div>
+            <hr class="strong" />
+            <div class="list">
               <?php while ( $department_list->have_posts() ) : $department_list->the_post(); ?>
-                <li>
+                <div class="row pvm">
+                  <div class="columns medium-6">
                   <?php
                     //NOTE: The content-department class is used for Google Analytics and should not be removed.
-                  ?><a href="<?php echo get_permalink(); ?>" class="content-department item"><?php echo the_title(); ?></a>
-                  <p class="item-desc"><?php echo phila_get_item_meta_desc( ); ?> </p>
-                </li>
+                  ?><a href="<?php echo get_permalink(); ?>" class="content-department item"><?php echo the_title(); ?>
+                    <?php $acronym = rwmb_meta( 'phila_department_acronym' );?> <?php echo !empty($acronym) ? '(' . $acronym . ')' : ''; ?>
+                    </a>
+                  </div>
+
+                  <div class="columns medium-10 phl">
+                    <p class="item-desc"><?php echo phila_get_item_meta_desc( ); ?>
+                    <span class="hidden"><?php echo  rwmb_meta('phila_department_keywords')?></span>               </p>
+                  </div>
+
+                  <?php get_template_part( 'partials/departments/v2/content', 'connect' ); ?>
+              </div>
+              <hr />
 
               <?php endwhile; ?>
 
-              </ul>
+            </div>
             <?php endif; ?>
 
             <?php wp_reset_query(); ?>
