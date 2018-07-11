445f0709c33b147a9f4599224acd13b6d925c67a
Karissa <k@karissademi.com>
2018-07-11 15:26:23 -0400

    Add useragent + clean up)

--- wp/wp-content/plugins/phila.gov-customization/public/hostname-redirect.php
+++ wp/wp-content/plugins/phila.gov-customization/public/hostname-redirect.php
@@ -3,17 +3,22 @@
 *  Non-logged in users get kicked back to the public site.
 */
 
-add_action('template_redirect', 'my_non_logged_redirect');
+add_action('template_redirect', 'admin_phila_redirect');
 
-function my_non_logged_redirect(){
-  global $SERVER;
-  $domain = parse_url($SERVER['REQUEST_URI'], PHP_URL_HOST);
+function admin_phila_redirect(){
+  $user_agent = $_SERVER['HTTP_USER_AGENT'];
+  if (strpos($user_agent, 'beta-static-generator') === true){
+    return;
+  }
+
+  $domain = parse_url($_SERVER['HTTP_HOST']);
   $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-  if ( !is_user_logged_in() && $domain === 'admin.phila.gov' ) {
+  if ( !is_user_logged_in() && $domain['path'] === 'admin.phila.gov' ) {
     wp_redirect( 'https://'.'www.phila.gov' . $path );
     die();
-  }else if(!is_user_logged_in() && $domain === 'admin.phila.website'){
+  }else if( !is_user_logged_in() && $domain['path'] === 'admin.phila.website' ){
     wp_redirect( 'https://' . 'www.phila.website' . $path );
+    die();
   }
 }
