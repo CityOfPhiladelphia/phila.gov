fe9a0d449a2e8f1cab3c1b01b25e92a9e133e107
Karissa <k@karissademi.com>
2018-08-08 09:32:16 -0400

    Add staff units to staff template

--- wp/wp-content/plugins/phila.gov-customization/admin/departments/class-phila-gov-department-register-templates.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/departments/class-phila-gov-department-register-templates.php
@@ -95,6 +95,15 @@ class Phila_Gov_Deparment_Templates {
           'phila_template_select', 'in', ['staff_directory_v2','staff_directory']
         )
       ),
+      array(
+        'id'  => 'units',
+        'name'  => 'Display staff grouped by the following units',
+        'type' => 'unit',
+        'visible' => array(
+          'phila_template_select', 'in', ['staff_directory_v2','staff_directory']
+        )
+        //'callback' => $this->phila_get_term_meta( $meta_boxes ),
+      ),
     ),
   );
 
