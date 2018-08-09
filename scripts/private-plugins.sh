d5c00e238accdc170532ca7450df711ca3674bf4
Karissa <k@karissademi.com>
2018-08-09 16:39:38 -0400

    Metabox tabs 1.1.0

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -11,7 +11,7 @@ meta-box-columns-1.2.3.zip
 meta-box-conditional-logic-1.5.7.zip
 meta-box-group-1.2.16.zip
 meta-box-include-exclude-1.0.10.zip
-meta-box-tabs-1.0.3.zip
+meta-box-tabs-1.1.0.zip
 meta-box-tooltip-1.1.1.zip
 meta-box-updater-1.3.0.zip
 wpfront-user-role-editor-personal-pro-2.14.1.zip"
