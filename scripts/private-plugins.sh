c05f3e80bc212a910c29ae04989ab54f7dfc307f
Karissa <k@karissademi.com>
2018-08-09 16:17:08 -0400

    meta-box-include-exclude-1.0.10

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -10,7 +10,7 @@ mb-revision-1.1.1.zip
 meta-box-columns-1.2.3.zip
 meta-box-conditional-logic-1.5.7.zip
 meta-box-group-1.2.16.zip
-meta-box-include-exclude-1.0.9.zip
+meta-box-include-exclude-1.0.10.zip
 meta-box-tabs-1.0.3.zip
 meta-box-tooltip-1.1.1.zip
 wpfront-user-role-editor-personal-pro-2.14.1.zip"
