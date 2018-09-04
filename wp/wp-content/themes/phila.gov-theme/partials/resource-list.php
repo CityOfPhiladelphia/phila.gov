023837b1f06eddd5508aa29c90a9e4415894a752
Karissa <k@karissademi.com>
2018-09-04 17:06:57 -0400

    Update resource layout, begin adding expand/collapse option

--- wp/wp-content/themes/phila.gov-theme/partials/resource-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/resource-list.php
@@ -3,14 +3,12 @@
 /* displays a grouped list of on-site links */
 
 ?>
-<?php if (function_exists('rwmb_meta')): ?>
-
+  <?php if (function_exists('rwmb_meta')): ?>
   <?php //set the vars
   $user_selected_template = phila_get_selected_template();
   $resource_list_groups = rwmb_meta( 'phila_resource_list' );
 
-    //we require at least three
-    if ( ! empty( $resource_list_groups ) && count($resource_list_groups) >= 3 ) :
+    if ( ! empty( $resource_list_groups ) ) :
       $i=0;
       $total = count($resource_list_groups);
       $count = 0;
@@ -20,23 +18,24 @@
       //group
       foreach ( $resource_list_groups as $resource_list_group ) :
         $i++;
-        if ( $count == 0 ) :
-        $list_item_output .= '<div class="row column">';
-        endif;
 
         //assign vars
-        $count++;
         $resource_list_title = isset( $resource_list_group['phila_resource_list_title'] ) ? $resource_list_group['phila_resource_list_title'] : '';
         $resource_list_slug = sanitize_title_with_dashes($resource_list_title);
 
-        $list_item_output .= '<section class="medium-8 small-24 column resource-list end">';
-        $list_item_output .= '<header><h2 id="' . $resource_list_slug . '" class="h4 phm pvs">' . $resource_list_title . '</h2></header>';
+        $list_item_output .= '<div class="row one-quarter-row mvl">
+        <section>';
+
+        $list_item_output .= '<div class="columns medium-6"><header><h2 id="' . $resource_list_slug . '" class="h4 phm pvs">' . $resource_list_title . '</h2></header></div>';
+        $list_item_output .= '<div class="column medium-16 pbxl">';
+        $list_item_output .= '<div class="resource-list">';
         $list_item_output .= '<ul>';
 
         //items
         $list_items_group = $resource_list_group['phila_resource_list_items'];
 
         foreach ( $list_items_group as $list_items ) :
+          $count++;
 
           $item_title = isset( $list_items['phila_list_item_title'] ) ? $list_items['phila_list_item_title'] : '';
           $item_url = isset( $list_items['phila_list_item_url'] ) ? $list_items['phila_list_item_url'] : '';
@@ -69,92 +68,85 @@
             $featured_resources[$featured_display_order] = array('title' => $item_title , 'alt-title' => $item_alt_title ,  'url' => $item_url , 'type' => $item_resource_type, 'icon' => $icon , 'summary' => $featured_summary );
           endif;
 
-          $list_item_output .= '<li class="phm pvs clickable-row" data-href="' . $item_url . '"><a href="' . $item_url . '"><div><i class="fa ' . $icon . ' fa-lg" aria-hidden="true"></i></div> <div>' . $item_title . '</div></a></li>';
+          if ( $count === 4 ):
+            $expand = true;
+            $list_item_output .= '<div class="staff-bio expandable">';
+          endif;
+
+            $list_item_output .= '<li class="phm pvs clickable-row" data-href="' . $item_url . '"><a href="' . $item_url . '"><i class="fa ' . $icon . ' fa-lg" aria-hidden="true"></i> ' . $item_title . '</a></li>';
+
 
         endforeach;
 
-        $list_item_output .=  '</ul></section>'; ?>
-
-      <?php if ( $count == 3 || $i == $total ) :
-      $list_item_output .= '</div>';
-      $count = 0;
-      endif; ?>
+        $list_item_output .=  '</ul></div></div></section></div>'; ?>
 
       <?php endforeach; ?>
 
       <!-- Loop featured resources -->
       <?php if ( !empty( $featured_resources ) ):?>
 
+          <?php
+          ksort($featured_resources);
+          $item_count = count( $featured_resources );
+          $columns = phila_grid_column_counter( $item_count );
 
-              <?php
-              ksort($featured_resources);
-              $item_count = count( $featured_resources );
-              $columns = phila_grid_column_counter( $item_count );
+          // TODO: Validate featured resource order
+          //Current Featured Resource limit
+          $limit = 4;
+          $current_position = 0;
+          ?>
+          <section class="row mbl <?php if( $item_count > 1 ) echo 'equal-height';?>">
 
-              // TODO: Validate featured resource order
-              //Current Featured Resource limit
-              $limit = 4;
-              $current_position = 0;
-              ?>
-              <section class="row mbl <?php if( $item_count > 1 ) echo 'equal-height';?>">
+          <div class="columns">
+              <h2 class="h3">Featured <?php echo strtolower(the_title()) ?></h2>
+          </div>
 
-                <?php if ($user_selected_template != 'resource_list') : ?>
-                  <div class="columns">
-                      <h2 class="contrast">Featured Resources</h2>
-                  </div>
+          <?php
+          foreach ($featured_resources as $key => $value): ?>
+          <div class="large-<?php echo $columns ?> columns">
+            <a href="<?php echo $featured_resources[$key]['url']; ?>"  class="card action-panel">
+              <div class="panel <?php if( $item_count > 1 ) echo 'equal';?>">
+              <header class="<?php echo $columns == '24' ? 'text-align-left' : ''; ?>">
+                <div class="<?php echo $columns == '24' ? 'float-left mrm' : ''; ?>">
+                  <span class="fa-stack <?php echo $columns == '24' ? 'fa-3x' : 'fa-4x'; ?> center" aria-hidden="true">
+                    <i class="fa fa-circle fa-stack-2x"></i>
+                    <i class="fa <?php echo $featured_resources[$key]['icon']; ?> fa-stack-1x fa-inverse"></i>
+                  </span>
+                </div>
+                <?php if (!$featured_resources[$key]['alt-title'] == ''): ?>
+                  <span class=""><?php echo $featured_resources[$key]['alt-title']; ?></span>
+                <?php elseif (!$featured_resources[$key]['title'] == ''): ?>
+                  <span class=""><?php echo $featured_resources[$key]['title']; ?></span>
                 <?php endif; ?>
-
-              <?php
-              foreach ($featured_resources as $key => $value): ?>
-              <div class="large-<?php echo $columns ?> columns">
-                <a href="<?php echo $featured_resources[$key]['url']; ?>"  class="action-panel">
-                  <div class="panel <?php if( $item_count > 1 ) echo 'equal';?>">
-                  <header class="<?php echo $columns == '24' ? 'text-align-left' : ''; ?>">
-                    <div class="<?php echo $columns == '24' ? 'float-left mrm' : ''; ?>">
-                      <span class="fa-stack <?php echo $columns == '24' ? 'fa-3x' : 'fa-4x'; ?> center" aria-hidden="true">
-                        <i class="fa fa-circle fa-stack-2x"></i>
-                        <i class="fa <?php echo $featured_resources[$key]['icon']; ?> fa-stack-1x fa-inverse"></i>
-                      </span>
-                    </div>
-                    <?php if (!$featured_resources[$key]['alt-title'] == ''): ?>
-                      <span class=""><?php echo $featured_resources[$key]['alt-title']; ?></span>
-                    <?php elseif (!$featured_resources[$key]['title'] == ''): ?>
-                      <span class=""><?php echo $featured_resources[$key]['title']; ?></span>
-                    <?php endif; ?>
-                  </header>
-                  <?php echo $columns == '24' ? '' : '<hr class="mll mrl">'; ?>
-                    <span class="details"><?php echo $featured_resources[$key]['summary']; ?></span>
-                  </div>
-                </a>
+              </header>
+              <?php echo $columns == '24' ? '' : '<hr class="mll mrl">'; ?>
+                <span class="details"><?php echo $featured_resources[$key]['summary']; ?></span>
               </div>
-              <?php
-                ++$current_position;
-                if ($current_position >= 4):
-                  break;
-                endif;
-              ?>
-              <?php endforeach; ?>
+            </a>
+          </div>
+          <?php
+            ++$current_position;
+            if ($current_position >= 4):
+              break;
+            endif;
+          ?>
+          <?php endforeach; ?>
         </section>
       <?php endif; ?>
 
+    <?php if (! empty( $featured_resources ) && count($featured_resources) > 0) : ?>
       <section class="row">
-        <?php if (! empty( $featured_resources ) && count($featured_resources) > 0) : ?>
-            <div class="column">
-                <?php //TODO: Allow custom resource list title ?>
-                <h2 class="contrast">Collections</h2>
-            </div>
-        <?php endif; ?>
-        <?php echo $list_item_output; ?>
-      </section>
-
-    <?php else : ?>
-      <div class="row mvl">
-        <div class="columns">
-          <div class="placeholder">
-            <?php echo 'Please enter at least three groups of links.'; ?>
-          </div>
+        <div class="column">
+          <h2 class="h3 mbn">All <?php echo strtolower(the_title()) ?></h2>
+          <hr />
         </div>
-      </div>
+      </section>
+    <?php endif; ?>
+
+    <div class="one-quarter-layout bdr-dark-gray">
+      <?php echo $list_item_output; ?>
+    </div>
+
     <?php endif; ?>
 
   <?php endif; ?>
