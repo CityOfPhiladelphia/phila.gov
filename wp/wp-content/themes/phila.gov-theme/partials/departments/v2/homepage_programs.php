0f08501a7f60afbfd7cc466548867385f84acd0d
Karissa <k@karissademi.com>
2018-09-20 11:50:56 -0400

    Scraper displays blank page when 302 is scraped, prevent this by using the off-site url instead of WordPress one

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/homepage_programs.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/homepage_programs.php
@@ -7,7 +7,7 @@
   if ( !isset( $cards) ):
     $cards = rwmb_meta('phila_select_programs');
   endif;
-  ?>
+?>
 <?php if ( !empty($cards) ) :?>
   <div class="row">
     <div class="columns">
@@ -18,8 +18,9 @@
     <div class="columns">
       <div class="row fat-gutter">
         <?php foreach( $cards as $card ) : ?>
+          <?php $template = phila_get_selected_template( $card ); ?>
           <div class="medium-8 columns end mbl">
-            <a class="card program-card" href="<?php echo get_the_permalink($card);?>">
+            <a class="card program-card" href="<?php echo ($template == 'prog_off_site') ? rwmb_meta('prog_off_site_link', '', $card) : get_the_permalink($card); ?>">
               <?php
               $img = rwmb_meta( 'prog_header_img', $args = array( 'size' => 'medium', 'limit' => 1 ), $card );
               $img = reset( $img );?>
