bd35b45f5036d80c09340f619821c08fc5603dc5
Karissa <k@karissademi.com>
2018-10-23 11:17:40 -0400

    Render resource list for program templates

--- wp/wp-content/themes/phila.gov-theme/single-programs.php
+++ wp/wp-content/themes/phila.gov-theme/single-programs.php
@@ -35,12 +35,25 @@ get_header();
       <?php get_template_part( 'partials/content', 'custom-markup-after-wysiwyg' ); ?>
 
       <?php get_template_part( 'partials/departments/v2/our', 'services' );
-      if ($user_selected_template == 'phila_one_quarter'):
-        get_template_part( 'partials/departments/v2/content', 'one-quarter' );
-      endif;
-      get_template_part( 'partials/departments/content', 'programs-initiatives' );
-    endwhile;
 
-  ?>
+      if ($user_selected_template == 'phila_one_quarter'): ?>
+      <!--1/4 Content-->
+        <?php get_template_part( 'partials/departments/v2/content', 'one-quarter' ); ?>
+      <!--/ 1/4 Content-->
+
+      <?php endif; ?>
+      <?php if ($user_selected_template == 'resource_list_v2'): ?>
+
+        <!-- Resource list -->
+        <section class="mtl">
+          <?php include(locate_template('partials/resource-list.php')); ?>
+        </section>
+
+        <!-- /Resource list -->
+      <?php endif; ?>
+      <!-- Program and initiatives -->
+      <?php get_template_part( 'partials/departments/content', 'programs-initiatives' ); ?>
+      <!-- /Program and initiatives -->
+    <?php endwhile; ?>
 </div><!-- #post-## -->
 <?php get_footer(); ?>
