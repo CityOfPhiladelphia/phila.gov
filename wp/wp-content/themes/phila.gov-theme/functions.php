3496f78f67af972be8d4ac738bcb3935d93cd44b
Karissa <k@karissademi.com>
2018-10-24 10:34:14 -0400

    Clean up

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -1279,7 +1279,6 @@ function phila_additional_content( $input ){
 
       $output['related_picker'] = isset( $input['phila_related']['phila_related_content_picker'] ) ? $input['phila_related']['phila_related_content_picker'] : '';
 
-
       $output['related'] = isset( $input['phila_related']['phila_related_content'] ) ? $input['phila_related']['phila_related_content'] : '';
 
       $output['aside']['did_you_know'] = isset( $input['phila_did_you_know']['phila_did_you_know_content'] ) ? $input['phila_did_you_know']['phila_did_you_know_content'] : '';
