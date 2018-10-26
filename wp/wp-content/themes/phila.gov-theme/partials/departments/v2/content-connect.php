4776bd4bfdc666519fdfe15118fee79f1e08ed5d
Karissa <k@karissademi.com>
2018-10-26 13:18:38 -0400

    Add ability for email helper text

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-connect.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/content-connect.php
@@ -54,6 +54,7 @@
           <i class="far fa-envelope fa-2x" aria-hidden="true"></i>
       </th>
       <td class="<?php echo (!$archive) ? 'pvl': 'percent-90' ?>">
+        <?php echo $connect_vars['email_exp']; ?><br />
         <a href="mailto:<?php echo $connect_vars['email']; ?>" class="u-email"><?php echo phila_util_return_parsed_email($connect_vars['email']); ?></a>
       </td>
     </tr>
