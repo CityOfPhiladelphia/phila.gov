2ec04d866553adc5e08398b769bd8a4c8810ec19
Karissa <k@karissademi.com>
2018-11-01 14:56:00 -0400

    Metabox revision 1.3.2

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -6,7 +6,7 @@ source /home/ubuntu/.ssh/environment
 
 _dir="$(dirname "$0")"
 plugins="mb-admin-columns-1.3.0.zip
-mb-revision-1.1.1.zip
+mb-revision-1.3.2.zip
 meta-box-columns-1.2.3.zip
 meta-box-conditional-logic-1.5.7.zip
 meta-box-group-1.2.16.zip
