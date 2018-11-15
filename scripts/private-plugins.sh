827070ecd2cfb4cad2fc05c21b2b969cdfad472a
Karissa <k@karissademi.com>
2018-11-15 13:12:33 -0500

    Metabox group 1.2.17

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -9,7 +9,7 @@ plugins="mb-admin-columns-1.3.0.zip
 mb-revision-1.3.2.zip
 meta-box-columns-1.2.3.zip
 meta-box-conditional-logic-1.6.3.zip
-meta-box-group-1.2.16.zip
+meta-box-group-1.2.17.zip
 meta-box-include-exclude-1.0.10.zip
 meta-box-tabs-1.1.1.zip
 meta-box-tooltip-1.1.1.zip
