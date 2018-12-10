214f2880556b415d57ed7cd001a54fe3ae4dade5
Karissa <k@karissademi.com>
2018-12-10 11:33:23 -0500

    Fix namespaces, correct function names

--- wp/wp-content/plugins/phila.gov-customization/public/class-phila-staff-member-controller.php
+++ wp/wp-content/plugins/phila.gov-customization/public/class-phila-staff-member-controller.php
@@ -5,31 +5,30 @@ class Phila_Staff_Member_Controller {
   // Initialize the namespace and resource name.
   public function __construct() {
     $this->namespace     = 'staff_directory/v1';
-    $this->all_resources = 'all';
-    $this->add            = 'add';
+    $this->resource_name = 'all';
   }
 
   // Register our routes.
   public function register_routes() {
   // Register the endpoint for collections.
-    register_rest_route( $this->namespace, '/' . $this->all_resources, array(
+    register_rest_route( $this->namespace, '/' . $this->resource_name, array(
       array(
-        'methods'   => WP_REST_Server::READONLY,
+        'methods'   => WP_REST_Server::READABLE,
         'callback'  => array( $this, 'get_items' ),
       ),
       'schema' => array( $this, 'get_item_schema' ),
     ) );
 
-    register_rest_route( $this->namespace, '/' . $this->add, array(
+    register_rest_route( $this->namespace, '/' . $this->resource_name, array(
       array(
-        'methods'   => WP_REST_Server::ALLMETHODS,
-        'callback'  => array( $this, 'add_items' ),
+        'methods'   => WP_REST_Server::CREATABLE,
+        'callback'  => array( $this, 'create_item' ),
       ),
       'schema' => array( $this, 'get_item_schema' ),
     ) );
 
     //Register individual items
-    register_rest_route( $this->namespace, '/' . $this->all_resources . '/(?P<id>[\d]+)', array(
+    register_rest_route( $this->namespace, '/' . $this->resource_name . '/(?P<id>[\d]+)', array(
       array(
         'methods'   => WP_REST_Server::ALLMETHODS,
         'callback'  => array( $this, 'get_item' ),
@@ -69,29 +68,33 @@ class Phila_Staff_Member_Controller {
   }
 
 
-  public function add_items( $request ) {
+  public function create_item( $request ) {
 
     $response = new WP_REST_Response();
     $params = $request->get_params();
 
-    if ( isArray($params['posts'] ) ) {
+    if ( isset( $params['posts'] ) && is_array( $params['posts'] ) ) {
+
       foreach ( $posts as $post ) {
-        if( isArray( $post ) ){
 
-          if( $results != is_wp_error() ) {
+        if( is_array( $post ) ){
+          $results = wp_insert_post( $post );
 
-            $results = wp_insert_post($post);
-            $response->set_status(200);
+          if( $results != is_wp_error( $results ) ) {
+            $response->set_status( 201 );
 
           }else{
-            $response->set_status(400);
+            $response->set_status( 400 );
             $response->set_data(
               ['message' => 'data is bad',
-              'data' => ['request' => $params]]
+              'data' => ['WP_ERROR' =>  $results ] ]
             );
+            return $response;
+
           }
         }
       }
+      return $response;
     }
   }
 
