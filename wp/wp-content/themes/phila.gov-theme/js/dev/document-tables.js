f428caa2254a1a7bf8aff3d62420a6e3317cc34c
Karissa <k@karissademi.com>
2018-12-12 15:58:15 -0500

    Cleaning up pagination

--- wp/wp-content/themes/phila.gov-theme/js/dev/document-tables.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/document-tables.js
@@ -16,31 +16,50 @@ module.exports = $(function(){
 
   $('.search-sort-table').each(function( j ) {
     var table = new List('sortable-table-' + j, options)
+
     table.on('updated', function (list) {
-      console.log(table)
 
-    if (list.matchingItems.length > 0) {
-      $('.no-results').hide()
-    } else {
-      $('.no-results').show()
-    }
-    if ( table.i + pageNum > table.visibleItems.length) {
-      $('.next').addClass('disabled')
-    }else{
-      $('.next').removeClass('disabled')
-    }
-  })
+      if (list.matchingItems.length > 0) {
+        $('.no-results').hide()
+      } else {
+        $('.no-results').show()
+      }
+      checkNavButtons()
+      })
 
-  $('.next-' + j).on('click', function(e){
-      table.show( table.i + pageNum , pageNum);
+      function checkNavButtons() {
+
+        if ( table.i === 1 && table.page === pageNum && table.visibleItems.length === pageNum ) {
+          $('.prev-' + j).addClass('disabled')
+        }else{
+          if( table.visibleItems.length < pageNum ){
+            $('.prev-' + j).addClass('disabled')
+          }else {
+            $('.prev-' + j).removeClass('disabled')
+          }
+        }
+        console.log(table)
+        console.log(table.visibleItems.length)
+        console.log(table.i)
+
+
+        if ( table.visibleItems.length < pageNum ) {
+          $('.next-' + j).addClass('disabled')
+        }else{
+          $('.next-' + j).removeClass('disabled')
+        }
+      }
+
+    $('.next-' + j).on('click', function(e){
+      table.show( table.i + pageNum, pageNum);
       e.preventDefault();
-  })
+    })
 
-  $('.prev-' + j).on('click', function(e){
-      // i, page
+    $('.prev-' + j).on('click', function(e){
       table.show( table.i - pageNum, pageNum);
       e.preventDefault();
+    })
+    checkNavButtons()
   })
-});
 
-});
+})
--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
@@ -20,88 +20,86 @@ $c = -1;
             <input type="text" class="table-search search-field" placeholder="Filter documents by title, category, or author" />
             <input type="submit" class="search-submit" />
           </div>
-          <table class="responsive mbxl js-hide-empty">
-            <?php echo !empty( $table['phila_custom_wysiwyg']['phila_wysiwyg_content'] ) ? '<caption class="ptn accessible">' . $table['phila_custom_wysiwyg']['phila_wysiwyg_content'] . '</caption>' : ''; ?>
-            <thead>
-              <tr>
-                <th class="table-sort" data-sort="title"><span>Title</span></th>
-                <th class="table-sort" data-sort="category"><span>Category</span></th>
-                <th class="table-sort" data-sort="author"><span>Author</span></th>
-                <th class="table-sort" data-sort="date" width="150"><span>Date</span></th>
-                <th width="100"><span>Format</span></th>
-              </tr>
-            </thead>
-            <tbody class="search-sortable">
-              <?php
+          <div class="table-wrapper">
 
-              foreach ( $table['phila_files'] as $id ) :
-                $file = wp_prepare_attachment_for_js($id);
-                $file_type = $file['subtype'];
-
-                $document_published = rwmb_meta( 'phila_document_page_release_date', $args = array(), $post_id = $id );
-
-                if ( empty($document_published) ){
-                  $document_published = get_the_date( $d = '', $id );
-                }
-                $url = get_post_meta($id, 'amazonS3_info');
-                $full_url = get_site_url() . '/' . $url[0]['key'];
-                $type = get_the_terms($id, 'media_type');
-                $author = get_the_terms($id, 'media_author');
-                $types = array();
-                $authors = array();
-                ?>
-                <tr class="clickable-row" data-href="<?php echo $full_url; ?>" id="<?php echo phila_format_uri( $file['title'] ); ?>">
-                  <td>
-                    <a href="<?php echo $full_url ?>"><span class="title"><?php echo $file['title'] ?></span> <span class="show-for-sr"><?php phila_format_document_type( $file_type ); ?></span></a>
-                  </td>
-                  <td class="category"><?php
-                    if( !empty( $type ) ) :
-                      foreach ( $type as $t ) :
-                        array_push( $types, $t->name );
-                      endforeach;
-                      echo implode( ', ', $types );
-                    endif;?></td>
-                  <td class="author"><?php
-                    if( !empty( $author ) ) :
-                      foreach ( $author as $a ) :
-                        array_push( $authors, $a->name );
-                      endforeach;
-                      echo implode( ', ', $authors );
-                    endif;
-                    ?></td>
-                  <td class="date">
-                    <?php echo $document_published ?>
-                  </td>
-                  <td class="format">
-                    <?php if ( $file_type ): ?>
-                      <div aria-hidden="true">
-                        <span class="file-type prs"><?php phila_format_document_type( $file_type ); ?></span>
-                        <a href="<?php echo $full_url ?>" data-file-name="<?php echo $file['title'] ?>" aria-hidden="true"><i class="fas fa-download fa-2x"></i>
-                        </a>
-                      </div>
-                    <?php endif; ?>
-                  </td>
+            <table class="responsive mbxl js-hide-empty">
+              <?php echo !empty( $table['phila_custom_wysiwyg']['phila_wysiwyg_content'] ) ? '<caption class="ptn accessible">' . $table['phila_custom_wysiwyg']['phila_wysiwyg_content'] . '</caption>' : ''; ?>
+              <thead>
+                <tr>
+                  <th class="table-sort" data-sort="title"><span>Title</span></th>
+                  <th class="table-sort" data-sort="category"><span>Category</span></th>
+                  <th class="table-sort" data-sort="author"><span>Author</span></th>
+                  <th class="table-sort" data-sort="date" width="150"><span>Date</span></th>
+                  <th width="100"><span>Format</span></th>
                 </tr>
-              <?php endforeach; ?>
-          </tbody>
-        </table>
+              </thead>
+              <tbody class="search-sortable">
+                <?php
+
+                foreach ( $table['phila_files'] as $id ) :
+                  $file = wp_prepare_attachment_for_js($id);
+                  $file_type = $file['subtype'];
+
+                  $document_published = rwmb_meta( 'phila_document_page_release_date', $args = array(), $post_id = $id );
+
+                  if ( empty($document_published) ){
+                    $document_published = get_the_date( $d = '', $id );
+                  }
+                  $url = get_post_meta($id, 'amazonS3_info');
+                  $full_url = get_site_url() . '/' . $url[0]['key'];
+                  $type = get_the_terms($id, 'media_type');
+                  $author = get_the_terms($id, 'media_author');
+                  $types = array();
+                  $authors = array();
+                  ?>
+                  <tr class="clickable-row" data-href="<?php echo $full_url; ?>" id="<?php echo phila_format_uri( $file['title'] ); ?>">
+                    <td>
+                      <a href="<?php echo $full_url ?>"><span class="title"><?php echo $file['title'] ?></span> <span class="show-for-sr"><?php phila_format_document_type( $file_type ); ?></span></a>
+                    </td>
+                    <td class="category"><?php
+                      if( !empty( $type ) ) :
+                        foreach ( $type as $t ) :
+                          array_push( $types, $t->name );
+                        endforeach;
+                        echo implode( ', ', $types );
+                      endif;?></td>
+                    <td class="author"><?php
+                      if( !empty( $author ) ) :
+                        foreach ( $author as $a ) :
+                          array_push( $authors, $a->name );
+                        endforeach;
+                        echo implode( ', ', $authors );
+                      endif;
+                      ?></td>
+                    <td class="date">
+                      <?php echo $document_published ?>
+                    </td>
+                    <td class="format">
+                      <?php if ( $file_type ): ?>
+                        <div aria-hidden="true">
+                          <span class="file-type prs"><?php phila_format_document_type( $file_type ); ?></span>
+                          <a href="<?php echo $full_url ?>" data-file-name="<?php echo $file['title'] ?>" aria-hidden="true"><i class="fas fa-download fa-2x"></i>
+                          </a>
+                        </div>
+                      <?php endif; ?>
+                    </td>
+                  </tr>
+                <?php endforeach; ?>
+            </tbody>
+          </table>
+        </div>
+        <div class="no-results">Sorry, there are no results for that search.</div>
         <ul class="pagination-wrapper no-js">
+          <li class="prev">
+            <a class="prev-<?php echo $c?>" href="#">Previous</a>
+          </li>
+          <ul class="pagination"></ul>
+          <li class="next">
+            <a class="next-<?php echo $c?>" href="#">Next</a>
+          </li>
+        </ul>
 
-        <li class="next">
-          <a class="next-<?php echo $c?>" href="#">Next</a>
-        </li>
-        <ul class="pagination"></ul>
-        <li class="prev">
-          <a class="prev-<?php echo $c?>" href="#">Previous</a>
-        </li>
-      </ul>
-
-      </div>
-      <div class="no-results">
-        No results
-      </div>
       </div>
       <?php endif;?>
     <?php endforeach; ?>
-  </div>
 </div>
