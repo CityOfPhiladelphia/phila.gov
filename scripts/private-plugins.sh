43b4a6128b52f12471e1d49a73d52c5ec1031b01
Karissa <k@karissademi.com>
2019-01-07 10:27:48 -0500

    Metabox condtional logic 1.6.4

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -8,7 +8,7 @@ _dir="$(dirname "$0")"
 plugins="mb-admin-columns-1.4.0.zip
 mb-revision-1.3.2.zip
 meta-box-columns-1.2.3.zip
-meta-box-conditional-logic-1.6.3.zip
+meta-box-conditional-logic-1.6.4.zip
 meta-box-group-1.2.17.zip
 meta-box-include-exclude-1.0.10.zip
 meta-box-tabs-1.1.1.zip
