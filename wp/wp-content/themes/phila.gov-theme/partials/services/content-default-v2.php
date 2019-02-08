88e67a7e7abedf38efaedaddc31f4d8d4e917c33
Karissa <k@karissademi.com>
2019-02-08 15:50:26 -0500

    Begin new default template for services

---
+++ wp/wp-content/themes/phila.gov-theme/partials/services/content-default-v2.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ *
+ * Default Page or Service Template
+ *
+ */
+?>
+
+<?php get_template_part('partials/services/content', 'start-process'); ?>
+
+<?php get_template_part( 'partials/content', 'custom-markup-before-wysiwyg' ); ?>
+
+<div class="row">
+  <div class="columns">
+    <?php the_content(); ?>
+  </div>
+</div>
+
+<?php get_template_part( 'partials/content', 'custom-markup-after-wysiwyg' ); ?>
+
+<?php $heading_groups = rwmb_meta( 'phila_heading_groups' ); ?>
+<?php include(locate_template('partials/content-heading-groups.php')); ?>
+
+<?php get_template_part( 'partials/content', 'additional' ); ?>
