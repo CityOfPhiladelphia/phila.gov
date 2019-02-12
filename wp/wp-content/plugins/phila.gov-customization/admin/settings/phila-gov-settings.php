9bc0af4498d27bb58dfbff0845291c8eab40ff93
Karissa <k@karissademi.com>
2019-02-12 11:26:46 -0500

    Begin interface for featured job

--- wp/wp-content/plugins/phila.gov-customization/admin/settings/phila-gov-settings.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/settings/phila-gov-settings.php
@@ -3,69 +3,77 @@
 add_filter( 'mb_settings_pages', 'phila_options_page' );
 
 function phila_options_page( $settings_pages ) {
-    $settings_pages[] = array(
-        'id'          => 'phila-gov',
-        'option_name' => 'phila_featured_jobs',
-        'menu_title'  => 'Featured Jobs',
-        'parent'      => 'options-general.php',
-    );
-    return $settings_pages;
+  $settings_pages[] = array(
+    'id'          => 'phila_gov',
+    'option_name' => 'phila_settings',
+    'menu_title'  => 'Phila.gov',
+    'parent'      => 'options-general.php',
+    'tabs'        => array(
+      'general' => 'General Settings',
+      'jobs'  => 'Featured jobs',
+    ),
+  );
+  return $settings_pages;
 }
 
 add_filter( 'rwmb_meta_boxes', 'prefix_options_meta_boxes' );
 
 function prefix_options_meta_boxes( $meta_boxes ) {
-    $meta_boxes[] = array(
-        'id'             => 'general',
-        'title'          => 'General',
-        'settings_pages' => 'phila-gov',
-        'fields'         => array(
-            array(
-                'name' => 'Logo',
-                'id'   => 'logo',
-                'type' => 'file_input',
-            ),
-            array(
-                'name'    => 'Layout',
-                'id'      => 'layout',
-                'type'    => 'image_select',
-                'options' => array(
-                    'sidebar-left'  => 'https://i.imgur.com/Y2sxQ2R.png',
-                    'sidebar-right' => 'https://i.imgur.com/h7ONxhz.png',
-                    'no-sidebar'    => 'https://i.imgur.com/m7oQKvk.png',
-                ),
-            ),
-        ),
-    );
-    $meta_boxes[] = array(
-        'id'             => 'colors',
-        'title'          => 'Colors',
-        'settings_pages' => 'phila-gov',
-        'fields'         => array(
-            array(
-                'name' => 'Heading Color',
-                'id'   => 'heading-color',
-                'type' => 'color',
-            ),
-            array(
-                'name' => 'Text Color',
-                'id'   => 'text-color',
-                'type' => 'color',
-            ),
-        ),
-    );
+  $meta_boxes[] = array(
+    'id'             => 'homepage_image',
+    'title'          => 'Homepage image',
+    'settings_pages' => 'phila_gov',
+    'tab'            => 'general',
+    'fields'         => array(
+      array(
+        'name' => 'Mobile image',
+        'id'   => 'homepage_mobile',
+        'type' => 'file_input',
+      ),
+      array(
+        'name' => 'Full size',
+        'id'   => 'homepage_desktop',
+        'type' => 'file_input',
+      ),
+    ),
+  );
 
-    $meta_boxes[] = array(
-        'id'             => 'info',
-        'title'          => 'Theme Info',
-        'context'        => 'side',
-        'settings_pages' => 'phila-gov',
-        'fields'         => array(
-            array(
-                'type' => 'custom_html',
-                'std'  => 'A responsive theme for businesses and agencies.',
-            )
+  $meta_boxes[] = array(
+    'id'             => 'featured_jobs',
+    'title'          => 'Featured jobs',
+    'settings_pages' => 'phila_gov',
+    'tab'            => 'jobs',
+    'fields'  => array(
+      array(
+        'id'  => 'phila_featured_jobs',
+        'type'   => 'group',
+        'clone' => true,
+        'max_clone'  => 2,
+        'add_button' => '+ Add a second featured job',
+
+        'fields'  => array(
+          array(
+            'id'  => 'job_title',
+            'name'  => 'Job title',
+            'type'  => 'text',
+            'required'  => true
+          ),
+          array(
+            'id'  => 'job_link',
+            'name'  => 'URL to listing',
+            'type'  => 'url',
+            'required'  => true
+          ),
+          array(
+            'id'  => 'job_description',
+            'name'  => 'Description',
+            'type'  => 'wysiwyg',
+            'options' => Phila_Gov_Standard_Metaboxes::phila_wysiwyg_options_basic(),
+            'required'  => true
+          ),
         ),
-    );
-    return $meta_boxes;
+      ),
+    ),
+  );
+  return $meta_boxes;
 }
