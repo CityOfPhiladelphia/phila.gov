93a7d69536f19359b862f2d3c11dd510bc1d3683
Karissa <k@karissademi.com>
2019-03-06 17:31:37 -0500

    Correct param

--- wp/wp-content/themes/phila.gov-theme/partials/global/registration.php
+++ wp/wp-content/themes/phila.gov-theme/partials/global/registration.php
@@ -45,7 +45,7 @@ endif;
                 <?php echo ($is_closed == true ? apply_filters( 'the_content', $registration['closed']['phila_custom_wysiwyg']['phila_wysiwyg_content']) : apply_filters( 'the_content',$registration['open']['phila_custom_wysiwyg']['phila_wysiwyg_content'] ) )?>
                 <?php if ($is_closed == false) : ?>
                   <?php foreach ($registration['open']['links'] as $link) : ?>
-                    <a class="button <?php echo isset( $link[is_external] ) ? 'external' : '' ?>" href="<?php echo $link['link_url']?>"><?php echo $link['link_text']?></a>
+                    <a class="button <?php echo isset( $link['is_external'] ) ? 'external' : '' ?>" href="<?php echo $link['link_url']?>"><?php echo $link['link_text']?></a>
                   <?php endforeach; ?>
                 <?php endif; ?>
               </div>
