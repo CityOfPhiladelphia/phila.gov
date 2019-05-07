8c00b5d1025762a7cd7d58f292ef3b8ab0a79d14
Karissa <k@karissademi.com>
2019-05-07 16:18:12 -0400

    Add dataLayer

--- wp/wp-content/themes/phila.gov-theme/js/gtm/document-page.js
+++ wp/wp-content/themes/phila.gov-theme/js/gtm/document-page.js
@@ -1,14 +1,13 @@
 $(function() {
   window.dataLayer = window.dataLayer || [];
-  console.log(params)
 
-  $('.clickable-row').click( function(){
+  $('.clickable-row').click( function(e){
+    var name = $(this).children().children()[1];
     window.dataLayer.push({
       'event' : 'GAEvent',
       'eventCategory' : 'Content Download',
       'eventAction' : params.postTitle,
-      'eventLabel' : $(this).text(),
+      'eventLabel' : $(name)[0].innerText,
     });
-
   });
 });
