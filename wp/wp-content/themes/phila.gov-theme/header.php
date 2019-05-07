b60a02b1ee409db4c86c5441f518948bc0e49512
Karissa <k@karissademi.com>
2019-05-07 15:16:21 -0400

    Close if statement

--- wp/wp-content/themes/phila.gov-theme/header.php
+++ wp/wp-content/themes/phila.gov-theme/header.php
@@ -10,19 +10,20 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-<?php if ( !is_archive() && !is_tax() && !is_home() && !is_404() ) : ?>
-  <!-- Google Tag Manager DataLayer -->
-  <?php $category = get_the_category();
-    $departments = phila_get_current_department_name( $category, $byline = false, $break_tags = false, $name_list = true );
-  ?>
-  <script>
-    dataLayer = [{
-      "contentModifiedDepartment": "<?php echo $departments ?>",
-      "lastUpdated": "<?php the_modified_time('Y-m-d H:i:s'); ?>",
-      "templateType": "<?php echo phila_get_selected_template() ?>"
-    }];
-  </script>
-  <!-- End Google Tag Manager DataLayer -->
+  <?php if ( !is_archive() && !is_tax() && !is_home() && !is_404() ) : ?>
+    <!-- Google Tag Manager DataLayer -->
+    <?php $category = get_the_category();
+      $departments = phila_get_current_department_name( $category, $byline = false, $break_tags = false, $name_list = true );
+    ?>
+    <script>
+      dataLayer = [{
+        "contentModifiedDepartment": "<?php echo $departments ?>",
+        "lastUpdated": "<?php the_modified_time('Y-m-d H:i:s'); ?>",
+        "templateType": "<?php echo phila_get_selected_template() ?>"
+      }];
+    </script>
+    <!-- End Google Tag Manager DataLayer -->
+  <?php endif; ?>
   <!-- Google Tag Manager --> 
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-MC6CR2');</script> 
   <!-- End Google Tag Manager -->
@@ -68,8 +69,6 @@
 </head>
 
 <body <?php body_class(); ?> lang="en">
-<?php endif; ?>
-
 <!-- Google Tag Manager (noscript) -->
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MC6CR2" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
