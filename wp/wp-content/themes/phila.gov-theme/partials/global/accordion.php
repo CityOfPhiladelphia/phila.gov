f467a38898d0f02ea9b59dae9f445ba10463b8be
Karissa <k@karissademi.com>
2019-06-15 13:33:25 -0400

    Begin styling

--- wp/wp-content/themes/phila.gov-theme/partials/global/accordion.php
+++ wp/wp-content/themes/phila.gov-theme/partials/global/accordion.php
@@ -2,8 +2,11 @@
   /* Required vars:
     $accordion_title - string
     $accordion_group - array
+    Optional:
+    $is_icon_template - Boolean
   */
 ?>
+<?php if (!isset($is_icon_template)) : ?>
 <section class="mvl">
   <div class="grid-container">
     <h2 id="#<?php echo phila_format_uri($accordion_title)?>"><?php echo isset($accordion_title) ? $accordion_title : '' ?></h2>
@@ -19,3 +22,18 @@
     </div>
   </div>
 </section>
+<?php else: ?>
+<section class="mvl">
+  <div class="grid-container">
+    <?php foreach ($accordion_group as $accordion ) : ?>
+      <?php echo $accordion['phila_custom_wysiwyg']['phila_wysiwyg_title'] ?>
+
+      <div class="expandable" aria-controls="<?php echo sanitize_title_with_dashes($accordion['phila_custom_wysiwyg']['phila_wysiwyg_title']) . '-control' ?>" aria-expanded="false">
+        <?php echo apply_filters( 'the_content', $accordion['phila_custom_wysiwyg']['phila_wysiwyg_content']); ?>
+      </div>
+      <a href="#" data-toggle="expandable" class="float-right" id="<?php echo sanitize_title_with_dashes($accordion['phila_custom_wysiwyg']['phila_wysiwyg_title']) . '-control' ?>"> More + </a>
+      
+    <?php endforeach;?>
+  </div>
+</section>
+<?php endif; ?>
