2d985bc61cb8fbe3fcfb70ce192e3e425a7bc8cb
Karissa <k@karissademi.com>
2019-06-27 15:14:08 -0400

    Simplify implementation and add to phila.gov main js

--- wp/wp-content/themes/phila.gov-theme/js/gtm/service-page.js
+++
@@ -1,9 +0,0 @@
-function serviceMenuClick(){
-  window.dataLayer = window.dataLayer || [];
-  window.dataLayer.push({
-    'event' : 'GAEvent', 
-    'eventCategory' : 'Service Page Conversion', 
-    'eventAction' : phila_js_vars.postTitle, 
-    'eventLabel' : document.referrer, 
-  })
-}
