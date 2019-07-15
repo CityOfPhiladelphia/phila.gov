6b86edbb1a44b79b4719beb8f2e12bfe53aabd8a
Karissa <k@karissademi.com>
2019-07-15 11:06:49 -0400

    Meta box 4.18.4

--- wp/wp-content/plugins/meta-box/inc/fields/taxonomy.php
+++ wp/wp-content/plugins/meta-box/inc/fields/taxonomy.php
@@ -31,8 +31,9 @@ class RWMB_Taxonomy_Field extends RWMB_Object_Choice_Field {
 		$field = wp_parse_args(
 			$field,
 			array(
-				'taxonomy'   => 'category',
-				'query_args' => array(),
+				'taxonomy'       => 'category',
+				'query_args'     => array(),
+				'remove_default' => false,
 			)
 		);
 
@@ -44,14 +45,11 @@ class RWMB_Taxonomy_Field extends RWMB_Object_Choice_Field {
 		 * - If multiple taxonomies: show 'Select a term'.
 		 * - If single taxonomy: show 'Select a %taxonomy_name%'.
 		 */
-		$placeholder = __( 'Select a term', 'meta-box' );
-		if ( 1 === count( $field['taxonomy'] ) ) {
-			$taxonomy        = reset( $field['taxonomy'] );
-			$taxonomy_object = get_taxonomy( $taxonomy );
-			if ( false !== $taxonomy_object ) {
-				// Translators: %s is the taxonomy singular label.
-				$placeholder = sprintf( __( 'Select a %s', 'meta-box' ), strtolower( $taxonomy_object->labels->singular_name ) );
-			}
+		$placeholder   = __( 'Select a term', 'meta-box' );
+		$taxonomy_name = self::get_taxonomy_singular_name( $field );
+		if ( $taxonomy_name ) {
+			// Translators: %s is the taxonomy singular label.
+			$placeholder = sprintf( __( 'Select a %s', 'meta-box' ), strtolower( $taxonomy_name ) );
 		}
 		$field = wp_parse_args(
 			$field,
@@ -69,7 +67,7 @@ class RWMB_Taxonomy_Field extends RWMB_Object_Choice_Field {
 		);
 
 		// Prevent cloning for taxonomy field, not for child fields (taxonomy_advanced).
-		if ( 'taxonomy' == $field['type'] ) {
+		if ( 'taxonomy' === $field['type'] ) {
 			$field['clone'] = false;
 		}
 
@@ -113,6 +111,24 @@ class RWMB_Taxonomy_Field extends RWMB_Object_Choice_Field {
 	}
 
 	/**
+	 * Get meta values to save.
+	 *
+	 * @param mixed $new     The submitted meta value.
+	 * @param mixed $old     The existing meta value.
+	 * @param int   $post_id The post ID.
+	 * @param array $field   The field parameters.
+	 *
+	 * @return array
+	 */
+	public static function value( $new, $old, $post_id, $field ) {
+		$new   = (array) $new;
+		$new[] = self::add_term( $field );
+		$new   = array_unique( array_map( 'intval', array_filter( $new ) ) );
+
+		return $new;
+	}
+
+	/**
 	 * Save meta value.
 	 *
 	 * @param mixed $new     The submitted meta value.
@@ -124,12 +140,28 @@ class RWMB_Taxonomy_Field extends RWMB_Object_Choice_Field {
 		if ( empty( $field['id'] ) || ! $field['save_field'] ) {
 			return;
 		}
-		$new = array_unique( array_map( 'intval', (array) $new ) );
-		$new = empty( $new ) ? null : $new;
 
 		foreach ( $field['taxonomy'] as $taxonomy ) {
 			wp_set_object_terms( $post_id, $new, $taxonomy );
 		}
+	}
+
+	/**
+	 * Add new terms if users created some.
+	 *
+	 * @param array $field Field settings.
+	 * @return int|null Term ID if added successfully, null otherwise.
+	 */
+	protected static function add_term( $field ) {
+		$term = filter_input( INPUT_POST, $field['id'] . '_new' );
+		if ( ! $field['add_new'] || ! $term || 1 !== count( $field['taxonomy'] ) ) {
+			return null;
+		}
+
+		$taxonomy = reset( $field['taxonomy'] );
+		$term     = wp_insert_term( $term, $taxonomy );
+
+		return isset( $term['term_id'] ) ? $term['term_id'] : null;
 	}
 
 	/**
@@ -208,5 +240,86 @@ class RWMB_Taxonomy_Field extends RWMB_Object_Choice_Field {
 			esc_attr( $value->name ),
 			esc_html( $value->name )
 		);
+	}
+
+	/**
+	 * Render "Add New" form
+	 *
+	 * @param array $field Field settings.
+	 * @return string
+	 */
+	public static function add_new_form( $field ) {
+		// Only add new term if field has only one taxonomy.
+		if ( 1 !== count( $field['taxonomy'] ) ) {
+			return '';
+		}
+
+		$taxonomy        = reset( $field['taxonomy'] );
+		$taxonomy_object = get_taxonomy( $taxonomy );
+		if ( false === $taxonomy_object ) {
+			return '';
+		}
+
+		$html = '
+		<div class="rwmb-taxonomy-add">
+			<button class="rwmb-taxonomy-add-button">%s</button>
+			<div class="rwmb-taxonomy-add-form rwmb-hidden">
+				<input type="text" name="%s_new" size="30" placeholder="%s">
+			</div>
+		</div>';
+
+		$html = sprintf(
+			$html,
+			esc_html( $taxonomy_object->labels->add_new_item ),
+			esc_attr( $field['id'] ),
+			esc_attr( $taxonomy_object->labels->new_item_name )
+		);
+
+		return $html;
+	}
+
+	/**
+	 * Enqueue scripts and styles.
+	 */
+	public static function admin_enqueue_scripts() {
+		parent::admin_enqueue_scripts();
+		wp_enqueue_style( 'rwmb-taxonomy', RWMB_CSS_URL . 'taxonomy.css', array(), RWMB_VER );
+		wp_enqueue_script( 'rwmb-taxonomy', RWMB_JS_URL . 'taxonomy.js', array( 'jquery' ), RWMB_VER, true );
+
+		// Field is the 1st param.
+		$args  = func_get_args();
+		$field = $args[0];
+		self::remove_default_meta_box( $field );
+	}
+
+	/**
+	 * Remove default WordPress taxonomy meta box.
+	 *
+	 * @param array $field Field settings.
+	 */
+	protected static function remove_default_meta_box( $field ) {
+		if ( empty( $field['remove_default'] ) || ! function_exists( 'remove_meta_box' ) ) {
+			return;
+		}
+		foreach ( $field['taxonomy'] as $taxonomy ) {
+			$id = is_taxonomy_hierarchical( $taxonomy ) ? "{$taxonomy}div" : "tagsdiv-{$taxonomy}";
+			remove_meta_box( $id, null, 'side' );
+		}
+	}
+
+	/**
+	 * Get taxonomy singular name.
+	 *
+	 * @param array $field Field settings.
+	 * @return string
+	 */
+	protected static function get_taxonomy_singular_name( $field ) {
+		if ( 1 !== count( $field['taxonomy'] ) ) {
+			return '';
+		}
+		$taxonomy        = reset( $field['taxonomy'] );
+		$taxonomy_object = get_taxonomy( $taxonomy );
+
+		return false === $taxonomy_object ? '' : $taxonomy_object->labels->singular_name;
 	}
 }
