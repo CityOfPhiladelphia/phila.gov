25b68eae34f472527dc57f928b7bcedaf56228b8
Karissa <k@karissademi.com>
2019-07-15 11:32:04 -0400

    Update plugins, lock file

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -10,8 +10,8 @@ mb-revision-1.3.2.zip
 mb-settings-page-1.3.4.zip
 mb-term-meta-1.2.5.zip
 meta-box-columns-1.2.5.zip
-meta-box-conditional-logic-1.6.4.zip
-meta-box-group-1.2.17.zip
+meta-box-conditional-logic-1.6.6.zip
+meta-box-group-1.3.4.zip
 meta-box-include-exclude-1.0.10.zip
 meta-box-tabs-1.1.4.zip
 meta-box-tooltip-1.1.1.zip
