4a8b408f205e477e26cd86b5c020730fc3ffecc9
Karissa <k@karissademi.com>
2019-07-31 13:36:21 -0400

    Refactor programs template for consistency clarity

--- wp/wp-content/themes/phila.gov-theme/single-programs.php
+++ wp/wp-content/themes/phila.gov-theme/single-programs.php
@@ -12,6 +12,7 @@ get_header();
 ?>
 
 <?php if ( $user_selected_template == 'prog_off_site' ) : ?>
+
   <?php include(locate_template('templates/single-off-site.php')); ?>
 
   <?php get_footer(); ?>
@@ -34,51 +35,26 @@ get_header();
 
       <?php get_template_part( 'partials/content', 'custom-markup-after-wysiwyg' ); ?>
 
-      <?php get_template_part( 'partials/departments/v2/our', 'services' );
+      <?php get_template_part( 'partials/departments/v2/our', 'services' );?>
 
-      if ($user_selected_template == 'phila_one_quarter'): ?>
-      <!--1/4 Content-->
-        <?php get_template_part( 'partials/departments/v2/content', 'one-quarter' ); ?>
-      <!--/ 1/4 Content-->
-      <?php endif; ?>
-
-      <?php if ($user_selected_template == 'resource_list_v2'): ?>
-        <!-- Resource list -->
-        <section class="mtl">
-          <?php include(locate_template('partials/resource-list.php')); ?>
-        </section>
-        <!-- /Resource list -->
-        <?php wp_reset_postdata(); ?>
-      <?php endif; ?>
-
-      <?php if ($user_selected_template == 'collection_page_v2'): ?>
-        <!-- Collection page -->
-        <section class="mtl">
-          <?php include(locate_template('partials/departments/v2/collection-page.php')); ?>
-        </section>
-        <?php wp_reset_postdata(); ?>
-        <!-- Collection page -->
-      <?php endif; ?>
-
-      <?php if ($user_selected_template == 'document_finder_v2'): ?>
-        <!-- Document finder -->
-        <section class="mtl">
-          <?php include(locate_template('partials/departments/v2/document-finder.php')); ?>
-        </section>
-        <?php wp_reset_postdata(); ?>
-        <!-- Document finder -->
-      <?php endif; ?>
-
-      <!-- Program and initiatives -->
+      <?php 
+      switch ($user_selected_template){
+        case ('phila_one_quarter'):
+          get_template_part( 'partials/departments/v2/content', 'one-quarter' );
+          break;
+        case ('resource_list_v2'):
+          include(locate_template('partials/resource-list.php'));
+          break;
+        case('collection_page_v2') :
+          include(locate_template('partials/departments/v2/collection-page.php')); 
+          break;
+        case('document_finder_v2'):
+          include(locate_template('partials/departments/v2/document-finder.php'));
+          break;
+      } ?>
       <?php get_template_part( 'partials/departments/content', 'programs-initiatives' ); ?>
-      <!-- /Program and initiatives -->
 
-      <!-- Additional Content-->
-      <div class="mtxl">
-        <?php get_template_part( 'partials/content', 'additional' ); ?>
-      </div>
-
-      <!-- /Additional Content-->
+      <?php get_template_part( 'partials/content', 'additional' ); ?>
 
     <?php endwhile; ?>
 </div><!-- #post-## -->
