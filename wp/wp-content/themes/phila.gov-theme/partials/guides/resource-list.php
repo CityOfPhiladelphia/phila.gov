26480b348b0e9893bda3353731685e036e6a1035
Karissa <k@karissademi.com>
2019-09-24 16:37:24 -0400

    Add ability to print entire guide from resources page

--- wp/wp-content/themes/phila.gov-theme/partials/guides/resource-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/guides/resource-list.php
@@ -3,8 +3,38 @@
   * Resource list template
   * 
 */
-$resource_lists = rwmb_meta('phila_resource_list_v2');
 ?>
+
+<div class="guides-print-all-content"> 
+  <?php 
+    wp_reset_query(); 
+    global $post;
+    $direct_parent = $post->post_parent;
+
+    $args = array(
+      'post_parent' => $direct_parent,
+      'post_type'   => 'guides',
+      'numberposts' => -1,
+      'post_status' => 'any',
+      'orderby' => 'menu_order',
+      'order'   => 'asc',
+    );
+    $children = new WP_Query( $args );
+
+    if ( $children->have_posts() ) : ?>
+      <?php while ( $children->have_posts() ) : $children->the_post(); ?>
+        <?php $heading_groups = rwmb_meta( 'phila_heading_groups' );
+          $heading_content = phila_extract_clonable_wysiwyg( $heading_groups, $array_key = 'phila_wywiwyg_alt_heading' ); ?>
+          <h1><?php echo get_the_title(); ?></h1>
+          <?php include(locate_template('partials/content-heading-groups.php')); ?>
+      <?php endwhile; ?>
+    <?php endif ;?>
+  <?php wp_reset_query(); ?>
+</div>
+<!-- End Guides-print-all-content -->
+
+
+<?php $resource_lists = rwmb_meta('phila_resource_list_v2'); ?>
 <?php foreach($resource_lists as $resource_list) : ?>
   <h2 class="h5 bg-ghost-gray pas">
     <?php echo $resource_list['phila_resource_list_title'] ?>
@@ -36,4 +66,6 @@ $resource_lists = rwmb_meta('phila_resource_list_v2');
   
   <?php endforeach; ?>
 
-
+<div class="mtxl pas">
+  <a href="javascript:window.print()" class="print-entire-guide"><i class="fal fa-print fa-fw fa-lg mrm"></i> Print this entire guide</a>
+</div>
