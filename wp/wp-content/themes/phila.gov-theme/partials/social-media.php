824f4e81d36b4e2ed8140210503dd855cad19859
matia-gov <mohamed.atia@phila.gov>
2020-01-09 09:44:55 -0500

    added target blank to sharing links

--- wp/wp-content/themes/phila.gov-theme/partials/social-media.php
+++ wp/wp-content/themes/phila.gov-theme/partials/social-media.php
@@ -11,8 +11,8 @@ $email_title = urlencode(html_entity_decode($the_title));
 ?>
 
 <div class="social-media">
-  <a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()) ?>" id="fb-share" data-analytics="social"><i class="fab fa-facebook fa-lg" aria-hidden="true"></i></a>
-  <a href="https://twitter.com/intent/tweet?text=<?php echo ( $tweet_intent != '' ) ? phila_encode_title(rwmb_meta('phila_social_intent') ) :  phila_encode_title( $the_title );?>&url=<?php echo get_permalink()?>"><i class="fab fa-twitter fa-lg" aria-hidden="true"></i></a>
+  <a href="http://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode(get_permalink()) ?>" id="fb-share" data-analytics="social"><i class="fab fa-facebook fa-lg" aria-hidden="true" target="_blank"></i></a>
+  <a href="https://twitter.com/intent/tweet?text=<?php echo ( $tweet_intent != '' ) ? phila_encode_title(rwmb_meta('phila_social_intent') ) :  phila_encode_title( $the_title );?>&url=<?php echo get_permalink()?>" target="_blank"><i class="fab fa-twitter fa-lg" aria-hidden="true"></i></a>
   <a href="mailto:?subject=<?php echo str_replace('+', '%20', $email_title) ?>&body=<?php echo get_permalink()?>" data-analytics="social"><i class="far fa-envelope fa-lg" aria-hidden="true"></i></a>
   <a href="javascript:window.print()" data-analytics="social"><i class="fal fa-print fa-lg" aria-hidden="true"></i></a>
 </div>
