21972d8dd6ce0f1ab1e9579f960084c244514f7b
Karissa <k@karissademi.com>
2020-03-19 16:55:49 -0400

    Downgrade plugin

--- wp/wp-content/plugins/restrict-categories/uninstall.php
+++ wp/wp-content/plugins/restrict-categories/uninstall.php
@@ -3,4 +3,4 @@ if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
 	exit();
 
 delete_option( 'RestrictCats_options' );
-delete_option( 'RestrictCats_user_options' );
+delete_option( 'RestrictCats_user_options' ); 
