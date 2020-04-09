769ed187fa37115dcd4373da757112524cc3e8f6
Derrick Dieso <derrick@imfromthefuture.com>
2020-04-09 16:51:10 -0400

    desktop w/o image

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/photo_callout.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/photo_callout.php
@@ -15,20 +15,18 @@
     <div class="mvl grid-x large-padding-collapse medium-padding-collapse small-padding-collapse small-margin-collapse align-center photo-callout-block ">
         <?php if (empty( $photo_callout['phila_v2_photo_callout_block__image_toggle'] ) || !isset( $photo_callout['phila_v2_photo_callout_block__photo'] )) : ?>
 
-            <div class="photo-callout-block__txt large-10 medium-12 small-20 cell">
+            <div class="photo-callout-block__txt large-20 medium-12 small-20 cell callout-photo-toggle-false">
                     <div class="grid align-center-middle grid-x grid-padding-x">
                         <div class="cell small-22 large-18 ">
-                            <h4 class="h5 photo-callout-block__txt-sub-header"><?php echo $subHeader ?></h4>
-                            <h2 class="h2 photo-callout-block__txt-header"><?php echo $header ?></h2>
-                            <p class="photo-callout-block__desc"><?php echo $description ?></p>
-                            <?php if($btnLink): ?>
-                            <a href="<?php echo $btnLink ?>" class="photo-callout-block__txt-btn button icon">
-                                <div class="valign">
-                                    <i class="far fa-map valign-cell"></i>
-                                    <div class="button-label valign-cell"><?php echo $btnTxt ?></div>
-                                </div>
-                            </a>
-                            <?php endif; ?>
+                            <div class="valign">        
+                                <i class="far fa-map valign-cell fa-3x"></i>
+                                <h2 class="h2 photo-callout-block__txt-header callout-photo-toggle-false"><?php echo $header ?></h2>
+                                <?php if($btnLink): ?>
+                                    <a href="<?php echo $btnLink ?>" class="photo-callout-block__txt-btn button icon callout-photo-toggle-false">
+                                        <div class="button-label valign-cell"><?php echo $btnTxt ?></div>
+                                    </a>
+                                <?php endif; ?>
+                            </div>
                         </div>
                     </div>
             </div>
