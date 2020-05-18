83d29d4159529a4d6c2a0c1f511b999bef43859d
Derrick Dieso <derrick@imfromthefuture.com>
2020-05-18 10:56:05 -0400

    styled elements

--- wp/wp-content/themes/phila.gov-theme/partials/posts/custom-content-card.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/custom-content-card.php
@@ -28,10 +28,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( $article_classes ); ?>>
   <a <?php echo ($label_arr['label'] !== 'announcement') ? 'href=' . get_permalink() : '' ?> class="custom grid-x hover-fade custom cell medium-24 card card--<?php echo $label_arr['label'] ?>" <?php echo ($label_arr['label'] == 'announcement') ? 'data-open="announcement-' . get_the_ID() .'"' : ''?>>
-    <div class="cell medium-2 pam card mtm">
-      <i class="<?php echo isset($label_arr['icon']) ? $label_arr['icon'] : '' ?> fa-lg fa-2x strong" aria-hidden="true"></i>
+    <div class="cell medium-4 small-6 pam card mtm">
+      <i class="<?php echo isset($label_arr['icon']) ? $label_arr['icon'] : '' ?> fa-lg fa-3x strong" aria-hidden="true"></i>
     </div>
-    <div class="cell medium-22 grid-x card pam">
+    <div class="cell medium-20 small-18 grid-x card pam">
       <div class="cell align-self-top">
         <div>
           <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
