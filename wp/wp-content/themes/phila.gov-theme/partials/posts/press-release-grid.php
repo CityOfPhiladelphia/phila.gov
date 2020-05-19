2e72de44a41e4052a544fde6ef5bf71e55d86388
Derrick Dieso <derrick@imfromthefuture.com>
2020-05-19 15:22:32 -0400

    code cleanup

--- wp/wp-content/themes/phila.gov-theme/partials/posts/press-release-grid.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/press-release-grid.php
@@ -113,68 +113,40 @@ if( !empty($tag) ) {
         <?php $post_type = get_post_type(); ?>
         <?php $post_obj = get_post_type_object( $post_type ); ?>
         <?php $count++; ?>
-        <?php if ($count == 1) :?>
-          <div class="grid-x grid-margin-x grid-full-height">
-        <?php elseif( $count == 3 ) : ?>
-          <div class="grid-x grid-margin-x grid-full-height mtm">
-        <?php endif;?>
-          <?php
-          if ($count <= 4) : ?>
-          <?php if ($user_selected_template == 'custom_content'): ?>
+        <?php if( $user_selected_template == 'custom_content' ): ?>
+          <?php if ($count <= 4) : ?>
             <div class="cell align-self-stretch">
               <?php include( locate_template( 'partials/posts/custom-content-card.php' ) ); ?>
-          <?php else: ?>
-            <div class="cell medium-12 align-self-stretch">
-              <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
+            </div>
           <?php endif; ?>
-              <?php if ($count == 4) : ?>
-                <?php if ($user_selected_template == 'custom_content'): ?>
-                  <?php $see_all = array(
-                  'URL' => '/the-latest/archives/#/?templates=press_release&department=' . $slang_name,
-                  'content_type' => 'press_release',
-                  'nice_name' => 'Press releases',
-                  'is_full' => false
-                ); ?>
-                <?php else: ?>
-                  <?php $see_all = array(
-                  'URL' => '/the-latest/archives/#/?templates=press_release&department=' . $slang_name,
-                  'content_type' => 'press_release',
-                  'nice_name' => 'Press releases',
-                  'is_full' => true
-                ); ?>
-                <?php endif; ?>
-                <?php if( !empty( $tag ) ) :
-                  if (gettype($tag) === 'string' ) {
-                    $term = get_term($tag, 'post_tag');
-                  }else{
-                    $term = get_term($tag[0], 'post_tag');
-                  }
-                  $see_all_URL = array(
-                    'URL' => '/the-latest/archives/#/?tag=' . $term->name,
-                  );
-                  $see_all = array_replace($see_all, $see_all_URL );
-                  endif;?>
-              <?php if (!empty($override_url)) : ?>
-              <?php $see_all_URL = array(
-                  'URL' => $override_url
-                ); ?>
-              <?php endif; ?>
-                <?php if ($user_selected_template == 'custom_content'): ?>
-                <div class="custom">
-                  <?php include( locate_template( 'partials/custom-content-see-all.php' ) ); ?>
-                </div>
-                <?php else: ?>
-                  <?php include( locate_template( 'partials/content-see-all.php' ) ); ?>
-                <?php endif; ?>
-              <?php endif;?>
-              <?php if ($count == 2 || $count == 4) :?>
+          <?php if( $count == 4 ) : ?>
+            <div class="cell align-self-stretch">
+              <?php include( locate_template( 'partials/posts/press-release-grid-view-all.php' ) ); ?>
+            </div>
+          <?php endif; ?>
+        <?php else: ?>
+          <?php if ($count == 1) :?>
+            <div class="grid-x grid-margin-x grid-full-height">
+          <?php elseif( $count == 3 ) : ?>
+            <div class="grid-x grid-margin-x grid-full-height mtm">
+          <?php endif;?>
+          <?php if ($count <= 4) : ?>
+            <?php if ($user_selected_template == 'custom_content'): ?>
+              <div class="cell align-self-stretch">
+                <?php include( locate_template( 'partials/posts/custom-content-card.php' ) ); ?>
+            <?php else: ?>
+              <div class="cell medium-12 align-self-stretch">
+                <?php include( locate_template( 'partials/posts/content-card.php' ) ); ?>
+            <?php endif; ?>
+            <?php include( locate_template( 'partials/posts/press-release-grid-view-all.php' ) ); ?>
+            <?php if ($count == 2 || $count == 4) :?>
               </div>
             <?php endif;?>
-          </div>
+            </div>
+          <?php endif;?>
         <?php endif;?>
-
-        <?php endwhile; ?>
-      <?php endif; ?>
-    <?php wp_reset_postdata(); ?>
+      <?php endwhile; ?>
+  <?php endif; ?>
+  <?php wp_reset_postdata(); ?>
   </div>
 </div>
