ef634f806920c791ce2035450797964d05179c6d
Derrick Dieso <derrick@imfromthefuture.com>
2020-05-26 11:59:34 -0400

    returning list of closures

---
+++ wp/wp-content/plugins/phila.gov-customization/public/controllers/class-phila-closures.php
@@ -0,0 +1,182 @@
+<?php
+
+class Phila_Closures_Controller {
+
+  // Initialize the namespace and resource name.
+  public function __construct() {
+    $this->namespace     = 'closures/v1';
+    $this->resource_name = 'closure';
+  }
+
+  // Register our routes.
+  public function register_routes() {
+  // Register the endpoint for collections.
+    register_rest_route( $this->namespace, '/' . $this->resource_name, array(
+      array(
+        'methods'   => WP_REST_Server::READABLE,
+        'callback'  => array( $this, 'get_items' ),
+      ),
+      'schema' => array( $this, 'get_item_schema' ),
+    ) );
+
+    //Register individual items
+    register_rest_route( $this->namespace, '/' . $this->resource_name . '/(?P<id>[\d]+)', array(
+      array(
+        'methods'   => WP_REST_Server::READABLE,
+        'callback'  => array( $this, 'get_item' ),
+      ),
+      'schema' => array( $this, 'get_item_schema' ),
+    ) );
+  }
+
+  /**
+   * Return all services, excluding stubs
+   *
+   * @param WP_REST_Request $request Current request.
+  */
+  public function get_items( $request ) {
+
+    $closures = rwmb_meta( 'phila_closures', array( 'object_type' => 'setting' ), 'phila_settings' );
+
+    $data = array();
+
+    if ( empty( $closures ) ) {
+      return rest_ensure_response( $data );
+    }
+
+    foreach ( $closures as $closure ) {
+      $response = $this->prepare_item_for_response( $closure, $request );
+
+      $data[] = $this->prepare_response_for_collection( $response );
+    }
+
+    // Return all response data.
+    return rest_ensure_response( $data );
+  }
+
+
+  /**
+   * Outputs an individual item's data
+   *
+   * @param WP_REST_Request $request Current request.
+   */
+  public function get_item( $request ) {
+    $id = (int) $request['id'];
+    $post = get_post( $id );
+
+    if ( empty( $post ) ) {
+      return rest_ensure_response( array() );
+    }
+
+    $response = $this->prepare_item_for_response( $post, $request );
+
+    return $response;
+  }
+
+  /**
+   * Matches the post data to the schema. Also, rename the fields to nicer names.
+   *
+   * @param WP_Post $post The comment object whose response is being prepared.
+   */
+
+  public function prepare_item_for_response( $post, $request ) {
+    $post_data = array();
+
+    $schema = $this->get_item_schema( $request );
+
+    $post_data['closure_label'] = (string) $post['closure_label'] ?? '';
+
+    $post_data['start_date']  = (string) $post['start_date'] ?? '';
+
+    $post_data['end_date'] = (string) $post['end_date'] ?? '';
+
+    $post_data['is_recycling_biweekly'] = array_key_exists('is_recycling_biweekly', $post) ? (boolean) $post['is_recycling_biweekly'] : false;
+
+    $post_data['is_active'] = array_key_exists('is_active', $post) ? (boolean) $post['is_active'] : false;
+
+    return rest_ensure_response( $post_data );
+}
+
+  /**
+   * Prepare a response for inserting into a collection of responses.
+   *
+   * This is copied from WP_REST_Controller class in the WP REST API v2 plugin.
+   *
+   * @param WP_REST_Response $response Response object.
+   * @return array Response data, ready for insertion into collection data.
+   */
+  public function prepare_response_for_collection( $response ) {
+    if ( ! ( $response instanceof WP_REST_Response ) ) {
+      return $response;
+    }
+
+    $data = (array) $response->get_data();
+    $server = rest_get_server();
+
+    if ( method_exists( $server, 'get_compact_response_links' ) ) {
+      $links = call_user_func( array( $server, 'get_compact_response_links' ), $response );
+    } else {
+      $links = call_user_func( array( $server, 'get_response_links' ), $response );
+    }
+
+    if ( ! empty( $links ) ) {
+      $data['_links'] = $links;
+    }
+
+    return $data;
+  }
+
+  /**
+   * Get sample schema for a collection.
+   *
+   * @param WP_REST_Request $request Current request.
+   */
+  public function get_item_schema( $request ) {
+    $schema = array(
+      // This tells the spec of JSON Schema we are using which is draft 4.
+      '$schema'              => 'http://json-schema.org/draft-04/schema#',
+      // The title property marks the identity of the resource.
+      'title'                => 'post',
+      'type'                 => 'object',
+      // Specify object properties in the properties attribute.
+      'properties'           => array(
+        'closure_label'=> array(
+          'description'  => esc_html__( 'Label of the object.', 'phila-gov' ),
+          'type'         => 'string',
+          'readonly'     => true,
+        ),
+        'start_date'  => array(
+          'description' => esc_html__('The start date for this object.', 'phila-gov'),
+          'type'  => 'date',
+          'readonly'     => true,
+        ),
+        'end_date'  => array(
+          'description' => esc_html__('The end date for this object.', 'phila-gov'),
+          'type'  => 'date',
+          'readonly'     => true,
+        ),
+        'is_recycling_biweekly'  => array(
+          'description' => esc_html__('Is recycling bikweekly in this duration?', 'phila-gov'),
+          'type'  => 'boolean',
+          'readonly'     => true,
+        ),
+        'is_active'  => array(
+          'description' => esc_html__('Is this closure active?', 'phila-gov'),
+          'type'  => 'boolean',
+          'readonly'     => true,
+        ),
+      ),
+    );
+
+    return $schema;
+  }
+
+}
+
+// Function to register our new routes from the controller.
+function phila_register_closures_rest_routes() {
+  $controller = new Phila_Closures_Controller();
+  $controller->register_routes();
+}
+
+add_action( 'rest_api_init', 'phila_register_closures_rest_routes' );
