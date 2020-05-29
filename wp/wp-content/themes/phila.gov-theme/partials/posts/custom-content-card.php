6df5102a31eb60c28a0c52638227c3c3a5169bb0
Karissa <k@karissademi.com>
2020-05-29 09:44:09 -0400

    Adjust templates + add new classes

--- wp/wp-content/themes/phila.gov-theme/partials/posts/custom-content-card.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/custom-content-card.php
@@ -28,10 +28,10 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class( $article_classes ); ?>>
   <div class="custom grid-x custom cell medium-24 card <?php echo ($count == $total) ? 'card--last' : '' ?>  card--<?php echo $label_arr['label'] ?>">
-    <div class="cell medium-4 small-6 pam card mtm">
+    <div class="cell medium-2 small-6 pam card mtm">
       <i class="<?php echo isset($label_arr['icon']) ? $label_arr['icon'] : '' ?> fa-lg fa-3x strong" aria-hidden="true"></i>
     </div>
-    <div class="cell medium-20 small-18 grid-x card pam">
+    <div class="cell medium-22 small-18 grid-x card pam">
       <div class="cell align-self-top">
         <div>
           <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
