c42aa1e409ce713ec42cc8bba12d45a72cccf7b1
Karissa <k@karissademi.com>
2020-06-05 17:07:52 -0400

    Continue loop fix

--- wp/wp-content/themes/phila.gov-theme/templates/the-latest-events-archive.php
+++ wp/wp-content/themes/phila.gov-theme/templates/the-latest-events-archive.php
@@ -16,11 +16,21 @@ get_header();
 </div>
 
 <?php
+
 $cal_a = array(
   'post_type' => 'calendar',
   'posts_per_page'  => -1,
-  'post_status' => 'any'
+  'post_status' => 'any',
+  'tax_query' => array(
+    'relation' => 'OR',
+    array(
+        'taxonomy' => 'calendar_category',
+        'field'    => 'slug',
+        'terms'    => array( 'all-events' ),
+    ),
+  )
 );
+
 $calendar_q = new WP_Query( $cal_a );
 
 if ( $calendar_q->have_posts() ) {
@@ -34,66 +44,101 @@ if ( $calendar_q->have_posts() ) {
   $i = 0;
 
   while ( $calendar_q->have_posts() ) : $calendar_q->the_post();
-  $ids = get_the_id();
-  //var_dump( $ids);
+    $ids = get_the_id();
     $categories = get_the_category( get_the_id() );
-    //var_dump( get_post_meta( get_the_id() ));
 
     if ($categories != null) {
       $i++;
       array_push($post_ids, get_the_id() );
-      array_push($cal_cat_ids, $categories[0]->cat_ID);
+
+      // $cat = phila_get_department_homepage_typography( null, $return_stripped = true, $page_title = $categories[0]->name);
+      // $trimmed_name = preg_replace('/( & )/', ' and ', $cat);
+
+      array_push($cal_cat_ids,  $categories[0]->slug);
       array_push($cal_nice_name, $categories);
 
-      $names[$i]['id'] = $categories[0]->cat_ID;
-      $names[$i]['name'] = phila_get_department_homepage_typography( null, $return_stripped = true, $page_title = $categories[0]->name );
+      // $names[$i]['id'] = $categories[0]->cat_ID;
+      // $names[$i]['name'] = phila_get_department_homepage_typography( null, $return_stripped = true, $page_title = $categories[0]->name );
+      
     }
-  endwhile;
 
+    endwhile;
+  
     wp_reset_postdata();
   }
+
+  $grouped_cals = array();
   foreach ($post_ids as $post_id) {
     $categories = get_the_category( $post_id );
     $category = $categories[0];
-    // var_dump($category);
-    $grouped_cals[$category->term_id] = get_post_meta( $post_id, '_grouped_calendars_ids', true );
-    //  $test = get_post_meta($post_id, '_grouped_calendars_ids');
-    array_push($grouped_cals, get_post_meta( $post_id, '_grouped_calendars_ids', true ) );
+
+    // $cat = phila_get_department_homepage_typography( null, $return_stripped = true, $page_title = $category->name);
+    // $trimmed_name = preg_replace('/( & )/', ' and ', $cat);
+    // $final =  html_entity_decode(trim($trimmed_name));
+    $current_cal = get_post_meta( $post_id, '_grouped_calendars_ids', true );
+   // var_dump($current_cal );
+    $grouped_cals[$category->slug] = get_post_meta( $post_id, '_grouped_calendars_ids', true );;
+
+  
+    //$grouped_cals[$category->slug] = get_post_meta( $post_id, '_grouped_calendars_ids', true );
+    
+    //array_push($grouped_cals, get_post_meta( $post_id, '_grouped_calendars_ids', true ) );
     array_push($cal_ids, base64_decode(get_post_meta( $post_id, '_google_calendar_id', true ) ) );
   }
   //remove duplicates
   $clean_grouped_cals = array_filter($grouped_cals);
   $just_ids = array();
+  $names = array();
+
+  //var_dump($clean_grouped_cals);
 
   foreach ($clean_grouped_cals as $key => $value){
+    $j = 0;
     if ( is_array($value) ) {
-      foreach ($value as $id) {
-        $categories = get_the_category( $id );
+      $k=0;
+      foreach ($value as $cal_id) {
+        $j++;
+        $k++;
+        $categories = get_the_category( $cal_id );
         $category = $categories[0];
 
-        //var_dump($categories);
-        $single_cal_id[$category->term_id] = base64_decode(get_post_meta($id, '_google_calendar_id', true));
+        $single_cal_id[$category->slug] = base64_decode(
+          get_post_meta($cal_id, '_google_calendar_id', true)
+        );
+        d($single_cal_id[$category->slug]);
+        var_dump($j);
+        var_dump($key);
+        //TODO: add if statement to catch the loop and stop it from duplicating 
+        // if () {
+        array_push($names, $just_ids[$key]);
+        // }
+
       }
-      $just_ids[$key] = $single_cal_id;
+      $names = $single_cal_id;
+
     }
-  }
+  }    
 
   $i=0;
   foreach ($cal_nice_name as $nice){
     $i++;
     $links[$nice[0]->cat_ID] = phila_get_current_department_name($nice);
   }
-
+  // this is good. 
   $final_array_single = array_combine($cal_cat_ids, $cal_ids);
-  //var_dump($final_array_single);
+  //var_dump($just_ids);
+ //somewhere in here is the issue.
   $final_array = array_replace($final_array_single, $just_ids);
 
+  //var_dump($final_array);
   //remove duplicates
-  $names = array_map("unserialize", array_unique(array_map("serialize", $names)));
+  $names = array_map('unserialize', array_unique(array_map('serialize', $names)));
 
   $links = array_filter($links);
 
   $calendar_ids = json_encode($final_array);
+
+  d($calendar_ids);
 
   function sort_by_name($a, $b){
     return strcmp($a['name'], $b['name']);
