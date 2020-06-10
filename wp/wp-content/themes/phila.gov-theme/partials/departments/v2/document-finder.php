f5595d5b6bc0229aa577de1b6f5b96a8bd68ad93
Derrick Dieso <derrickjdieso@gmail.com>
2020-06-10 15:53:38 -0400

    working vue app structure

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
@@ -5,7 +5,14 @@
 <?php
 $tables = rwmb_meta('phila_document_table');
 $no_pagination = rwmb_meta('phila_doc_no_paginate') ;
+$app_title = 'Document Finder';
 //ensure 0 index for js initialization
 $c = -1;
+
+$app_id = 'vue-app';
+
+$vuejs_js_ids = ['https://philagov-vue-apps.s3.amazonaws.com/document-finder/v2/js/chunk-vendors.js', 'https://philagov-vue-apps.s3.amazonaws.com/document-finder/v2/js/app.js'];
+$vuejs_css_ids = ['https://philagov-vue-apps.s3.amazonaws.com/document-finder/v2/css/chunk-vendors.css', 'https://philagov-vue-apps.s3.amazonaws.com/document-finder/v2/css/app.css'];
+
 ?>
-<?php include(locate_template( 'partials/departments/content-programs-initiatives.php' ) );?>
+<?php include(locate_template( 'partials/vue-apps/vue-register.php' ) );?>
