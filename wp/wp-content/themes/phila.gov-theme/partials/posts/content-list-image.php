86bfb3e70b44295499a01d5bc88f3ba393e91447
Karissa <k@karissademi.com>
2020-06-10 12:37:29 -0400

    Correct post display

--- wp/wp-content/themes/phila.gov-theme/partials/posts/content-list-image.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/content-list-image.php
@@ -6,27 +6,25 @@
  */
 ?>
 
-<?php $label_arr = phila_get_post_label($label); ?>
-
-<article id="post-<?php the_ID(); ?>" <?php post_class('full-height'); ?>>
-  <a href="<?php echo the_permalink(); ?>" class="card card--<?php echo $label_arr['label'] ?> flex-container flex-dir-row full-height">
-    <div class="grid-x flex-dir-column">
-      <div class="flex-child-shrink">
-        <?php if ( has_post_thumbnail() ) : ?>
-            <?php echo phila_get_thumbnails(); ?>
-        <?php endif; ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class('mbm'); ?>>
+  <a href="<?php echo the_permalink(); ?>" class="grid-x hover-fade faux-card cell medium-24">
+    <?php if ( has_post_thumbnail() ) : ?>
+      <div class="cell medium-8 prm">
+        <?php echo phila_get_thumbnails(); ?>
       </div>
-      <div class="card--content pam flex-child-auto">
-        <div class="cell align-self-top post-label post-label--<?php echo $label_arr['label']?>">
-          <i class="<?php echo $label_arr['icon'] ?> fa-lg" aria-hidden="true"></i> <span><?php echo $label_arr['nice']; ?></span>
-          <header class="cell mvm">
-            <h1><?php echo get_the_title(); ?></h1>
-          </header>
-        </div>
-        <div class="cell align-self-bottom">
-          <div class="post-meta">
-            <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
-          </div>
+    <?php endif; ?>
+    <div class="cell medium-<?php echo (has_post_thumbnail() ) ? '16' : '24' ?> grid-x bg-ghost-gray card <?php echo isset($label_arr['label']) ? 'card--' . $label_arr['label'] : '' ?> pam">
+      <div class="cell align-self-top post-label <?php echo isset($label_arr['label']) ? 'post-label--' . $label_arr['label'] : '' ?>">
+        <?php if ( isset( $label_arr['nice'] ) ) : ?>
+          <i class="<?php echo isset($label_arr['icon']) ? $label_arr['icon'] : '' ?> fa-lg" aria-hidden="true"></i> <span><?php echo isset($label_arr['nice']) ? $label_arr['nice'] : '' ?></span>
+        <?php endif; ?>
+        <header class="mvm">
+          <h1><?php echo get_the_title(); ?></h1>
+        </header>
+      </div>
+      <div class="cell align-self-bottom">
+        <div class="post-meta">
+          <span class="date-published"><time datetime="<?php echo get_post_time('Y-m-d'); ?>"><?php echo get_the_date();?></time></span>
         </div>
       </div>
     </div>
