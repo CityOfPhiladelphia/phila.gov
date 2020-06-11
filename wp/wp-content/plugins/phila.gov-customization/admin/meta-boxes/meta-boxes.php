b5e10083d659d0c7ebe461fc8b3a0e41226a9f35
Derrick Dieso <derrickjdieso@gmail.com>
2020-06-11 12:59:01 -0400

    datatable toggle

--- wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/meta-boxes.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/meta-boxes.php
@@ -978,10 +978,34 @@ $meta_boxes[] = array(
     ),
   ),
   'fields' => array(
+    array (
+      'id'  => 'phila_vue_toggle',
+      'type'  => 'switch',
+      'name'  => 'Use datables?',
+      'on_label'  => 'Yes',	
+      'off_label' => 'No'
+    ),
     array(
       'id'  => 'phila_vue_app_title',
       'type'  => 'text',
       'name'  => 'Optional title for document tables section',
+      'visible' => array(
+        'when'  => array(
+          array('phila_vue_toggle', '=', true),
+        ),
+      ),
+    ),
+    array(
+      'id'  => 'phila_doc_no_paginate',
+      'type'  => 'switch',
+      'name'  => 'Turn off pagination for all tables on this page?',
+      'on_label'  => 'Yes',
+      'off_label' => 'No',
+      'visible' => array(
+        'when'  => array(
+          array('phila_vue_toggle', '=', false),
+        ),
+      ),
     ),
     array(
       'id' => 'phila_document_table',
@@ -989,6 +1013,46 @@ $meta_boxes[] = array(
       'clone' => true,
       'sort_clone' => true,
       'add_button'  => 'Add another table',
+      'visible' => array(
+        'when'  => array(
+          array('phila_vue_toggle', '=', false),
+        ),
+      ),
+      'fields' =>
+      array(
+        Phila_Gov_Standard_Metaboxes::phila_metabox_v2_wysiwyg( $section_title = 'Table title', $wysiwyg_desc = 'Enter a description to describe the contents of this table for users with screenreaders. '),
+        array(
+          'id'  => 'phila_search_bar_text',
+          'type'  => 'text',
+          'name'  => 'Text for the document search',
+          'desc'  => 'Defaults to: Begin typing to filter documents',
+        ),
+        array(
+          'name'  => 'Add files to table',
+          'id'    => 'phila_files',
+          'type'  => 'file_advanced',
+          'class' =>  'add-files',
+          'mime_type' => 'application/pdf, application/msword, application/vnd.openxmlformats-officedocument.wordprocessingml.document,
+          application/vnd.ms-powerpointtd, application/vnd.openxmlformats-officedocument.presentationml.presentation,
+          application/vnd.ms-excel,
+          application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,
+          text/csv,
+          text/plain,
+          application/zip'
+        ),
+      )
+    ),
+    array(
+      'id' => 'phila_document_table',
+      'type'  => 'group',
+      'clone' => true,
+      'sort_clone' => true,
+      'add_button'  => 'Add another table',
+      'visible' => array(
+        'when'  => array(
+          array('phila_vue_toggle', '=', true),
+        ),
+      ),
 
       'fields' =>
       array(
