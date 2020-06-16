7ff42e94cb55032dc3670f0bb82be6782f160cc3
Derrick Dieso <derrickjdieso@gmail.com>
2020-06-16 12:37:58 -0400

    working endpoint

--- wp/wp-content/plugins/phila.gov-customization/public/controllers/class-phila-pages.php
+++ wp/wp-content/plugins/phila.gov-customization/public/controllers/class-phila-pages.php
@@ -28,58 +28,51 @@ class Phila_Pages_Controller {
   public function get_items( $request ) {
 
     $posts = new WP_Query( array(
-      // 'post_type' => array('department_page', 'programs'),
-      'posts_per_page'         => 10,
-      // 'posts_per_page'  => -1,
+      'posts_per_page'  => -1,
       'order' => 'asc',
       'orderby' => 'title',
       'post_status' => 'any',
     ) );
 
     if ( $posts->have_posts() ) {
-      $pages = '{"pages": [';
+      $pages = array();
       while ( $posts->have_posts() ) {
         $posts->the_post();
-
         $terms = get_the_terms( get_the_id(), 'category' ); 
         if ($terms) {
-          $numTerms = count($terms);
-          $i = 0;
           foreach($terms as $term) {
-            $pages .= '{ "id": '. the_id() .',';
-            $pages .= '"title": "'. get_the_title() .'",';
-            $pages .= '"url": "'. get_the_permalink() .'",';
-            $pages .= '"owner": "'. $term->name .'",';
-            $pages .= '"status": "'. get_post_status() .'",';
-            $pages .= '"last_modified": "'. the_modified_time('F jS, Y') . ', ' . the_modified_time() . '",';
+            $page = new stdClass();
+            $page->id = get_the_id();
+            $page->last_modified = get_the_modified_time('F jS, Y') . ', ' . get_the_modified_time();
             if (get_userdata( rwmb_meta( '_edit_last', $args = array(), $post_id = get_the_id() ) )) {
-              $pages .= '"last_modified_user": "'. get_userdata( rwmb_meta( '_edit_last', $args = array(), $post_id = get_the_id() ) )->user_email .'",';
+              $page->last_modified_user = get_userdata( rwmb_meta( '_edit_last', $args = array(), $post_id = get_the_id() ) )->user_email;
             }
             else {
-              $pages .= '"last_modified_user": "",';
+              $page->last_modified_user = '';
             }
-            $pages .= '"post_type": "'. get_post_type_object(get_post_type())->labels->singular_name .'",';
-            $pages .= '"short_description": "'. rwmb_meta( 'phila_meta_desc', $args = array(), $post_id = get_the_id() ) .'",';
-            $pages .= '"template": "'. rwmb_meta( 'phila_template_select', $args = array(), $post_id = get_the_id() ) .'",';
-            if ($posts->current_post +1 == $posts->post_count && ++$i === $numTerms) {
-              $pages .= '}';
-            }
-            else {
-              $pages .= '},';
-            }
+            $page->owner = $term->name;
+            $page->post_type = get_post_type_object(get_post_type())->labels->singular_name;
+            $page->short_description = rwmb_meta( 'phila_meta_desc', $args = array(), $post_id = get_the_id() );
+            $page->status = get_post_status();
+            $page->template = rwmb_meta( 'phila_template_select', $args = array(), $post_id = get_the_id() );
+            $page->title = get_the_title();
+            $page->url = get_the_permalink();
+            array_push($pages, $page);
           }
         }
       }
-      $pages .= ']}';
       wp_reset_postdata();
     }
 
-    if ( $pages != '' ) {
-      return $pages;
+    foreach ( $pages as $page ) {
+      $response = $this->prepare_item_for_response( $page, $request );
+
+      $data[] = $this->prepare_response_for_collection( $response );
     }
-    else {
-      return '';
-    }
+
+    // Return all response data.
+    return rest_ensure_response( $data );
+
   }
 
   /**
@@ -92,16 +85,17 @@ class Phila_Pages_Controller {
     $post_data = array();
 
     $schema = $this->get_item_schema( $request );
+    $post_data['id'] = (integer) $post->id ?? '';
+    $post_data['last_modified'] = (string) $post->last_modified ?? '';
+    $post_data['last_modified_user'] = (string) $post->last_modified_user ?? '';
+    $post_data['owner'] = (string) $post->owner ?? '';
+    $post_data['post_type'] = (string) $post->post_type ?? '';
+    $post_data['short_description'] = (string) $post->short_description ?? '';
+    $post_data['status'] = (string) $post->status ?? '';
+    $post_data['template'] = (string) $post->template ?? '';
+    $post_data['title'] = (string) $post->title ?? '';
+    $post_data['url'] = (string) $post->url ?? '';
 
-    $post_data['page_label'] = (string) $post['page_label'] ?? '';
-
-    $post_data['start_date']  = (string) $post['start_date'] ?? '';
-
-    $post_data['end_date'] = (string) $post['end_date'] ?? '';
-
-    $post_data['is_recycling_biweekly'] = array_key_exists('is_recycling_biweekly', $post) ? (boolean) $post['is_recycling_biweekly'] : false;
-
-    $post_data['is_active'] = array_key_exists('is_active', $post) ? (boolean) $post['is_active'] : false;
 
     return rest_ensure_response( $post_data );
 }
@@ -149,31 +143,56 @@ class Phila_Pages_Controller {
       'type'                 => 'object',
       // Specify object properties in the properties attribute.
       'properties'           => array(
-        'page_label'=> array(
-          'description'  => esc_html__( 'Label of the object.', 'phila-gov' ),
-          'type'         => 'string',
-          'readonly'     => true,
+        'id' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'integer',
+          'readonly' => true,
         ),
-        'start_date'  => array(
-          'description' => esc_html__('The start date for this object.', 'phila-gov'),
-          'type'  => 'date',
-          'readonly'     => true,
+        'last_modified' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
         ),
-        'end_date'  => array(
-          'description' => esc_html__('The end date for this object.', 'phila-gov'),
-          'type'  => 'date',
-          'readonly'     => true,
+        'last_modified_user' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
         ),
-        'is_recycling_biweekly'  => array(
-          'description' => esc_html__('Is recycling bikweekly in this duration?', 'phila-gov'),
-          'type'  => 'boolean',
-          'readonly'     => true,
+        'owner' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
         ),
-        'is_active'  => array(
-          'description' => esc_html__('Is this page active?', 'phila-gov'),
-          'type'  => 'boolean',
-          'readonly'     => true,
+        'post_type' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
         ),
+        'short_description' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
+        ),
+        'status' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
+        ),
+        'template' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
+        ),
+        'title' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
+        ),
+        'url' => array(
+          'description' => esc_html__('item', 'phila-gov'),
+          'type' => 'string',
+          'readonly' => true,
+        )
       ),
     );
 
