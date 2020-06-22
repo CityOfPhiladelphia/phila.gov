0a19c3bcfb1208b6b187f9185f5821f5b17dcf1d
Derrick Dieso <derrickjdieso@gmail.com>
2020-06-22 14:47:16 -0400

    one sort

--- wp/wp-content/themes/phila.gov-theme/partials/content-timeline.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-timeline.php
@@ -7,24 +7,24 @@
 
 <?php $timeline_title = rwmb_meta( 'timeline-title' ) !== null ? rwmb_meta( 'timeline-title' ) : ''; ?>
 <?php $timeline_items = rwmb_meta( 'timeline-items' ) !== null ? rwmb_meta( 'timeline-items' ) : []; ?>
-<?php $month_list = array(); ?>
+<?php 
+  $month_list = array(); 
+  usort($timeline_items, function($a, $b) {
+    return strtotime(date($b['phila_timeline_item_timestamp']['timestamp'])) - strtotime(date($a['phila_timeline_item_timestamp']['timestamp']));
+  });
+?>
 
-
+<!-- Timeline Section -->
 <section>
   <div class="grid-container">
     <div class="grid-x">
       <div class="cell">
         <h2 class="contrast"><?php echo $timeline_title; ?></h2>
-
         <?php 
           foreach($timeline_items as $item) {
             array_push($month_list, date('F Y', $item['phila_timeline_item_timestamp']['timestamp']));
           }
-
           $month_list = array_unique ($month_list);
-          usort($month_list, function($a, $b) {
-            return strtotime($b) - strtotime($a);
-          }); 
         ?>
 
         <!-- Menu -->
@@ -41,10 +41,11 @@
         
         <!-- Timeline -->
         <?php foreach($timeline_items as $item) { ?>
-          <div><?php echo $item['phila_timeline_item_timestamp']['formatted'];?></div>
+          <div><?php echo date('F d', $item['phila_timeline_item_timestamp']['timestamp']);?></div>
           <div><?php echo $item['phila_timeline_item'];?></div>
         <?php } ?>
       </div>
     </div>
   </div>
 </section>
+<!-- Timeline Section/ -->
