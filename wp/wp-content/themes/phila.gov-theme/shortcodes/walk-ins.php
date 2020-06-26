5d209cf2712a5a01d0df55b1d3a0eb045efd2dae
Karissa <k@karissademi.com>
2020-06-26 13:31:47 -0400

    Adjust walk in for service pages

--- wp/wp-content/themes/phila.gov-theme/shortcodes/walk-ins.php
+++ wp/wp-content/themes/phila.gov-theme/shortcodes/walk-ins.php
@@ -7,6 +7,7 @@
 * @package phila-gov_customization
 */
 function walk_ins_shortcode($atts, $content=null){
+  $post_type = get_post_type();
 
   $allowed_html = [
     'a'      => [
@@ -24,11 +25,23 @@ function walk_ins_shortcode($atts, $content=null){
   $output = '';
 
   if ( $content != '' ){
-    $output .= '<div class="walk-ins grid-x mtxl">
-    <div class="cell small-3 medium-2 walk-ins-icon"><i class="fas fa-street-view fa-3x"></i></div>
-    <div class="cell small-21 medium-22 walk-ins-copy">';
+    if ($post_type === 'service_page'){
+      $output .= '<div class="walk-ins grid-x mvl">';
+      $output .= '<div class="cell small-3 medium-3 walk-ins-icon">';
+    }else{
+      $output .= '<div class="walk-ins grid-x mtxl">';
+      $output .= '<div class="cell small-3 medium-2 walk-ins-icon">';
+    }
+    
+    $output .= '<i class="fas fa-street-view fa-3x"></i></div>';
+    if ($post_type === 'service_page'){
+      $output .= '<div class="cell small-21 medium-21 walk-ins-copy">';
+    }else{
+    $output .= '<div class="cell small-21 medium-22 walk-ins-copy">';
+    }
+
     $output .=  wp_kses($content, $allowed_html) ;
-    $output .= '</div></div>';
+    $output .= '</div></div></div>';
 
     return $output;
   } else {
