0a1acb9e1591b7f57e1067ba5e6f12099cd8f542
Derrick Dieso <derrickjdieso@gmail.com>
2020-07-22 09:21:42 -0400

    point to prod

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
@@ -8,8 +8,8 @@ $vue_or_not = rwmb_meta('phila_vue_toggle');
 if ($vue_or_not) {
   $app_id = 'vue-app';
 
-  $vuejs_js_ids = ['https://philagov-vue-apps.s3.amazonaws.com/document-finder/dev/js/chunk-vendors.js', 'https://philagov-vue-apps.s3.amazonaws.com/document-finder/dev/js/app.js'];
-  $vuejs_css_ids = ['https://philagov-vue-apps.s3.amazonaws.com/document-finder/dev/css/chunk-vendors.css', 'https://philagov-vue-apps.s3.amazonaws.com/document-finder/dev/css/app.css'];
+  $vuejs_js_ids = ['https://philagov-vue-apps.s3.amazonaws.com/document-finder/production/js/chunk-vendors.js', 'https://philagov-vue-apps.s3.amazonaws.com/document-finder/production/js/app.js'];
+  $vuejs_css_ids = ['https://philagov-vue-apps.s3.amazonaws.com/document-finder/production/css/chunk-vendors.css', 'https://philagov-vue-apps.s3.amazonaws.com/document-finder/production/css/app.css'];
   
   include(locate_template( 'partials/vue-apps/vue-register.php' ) );
 }
