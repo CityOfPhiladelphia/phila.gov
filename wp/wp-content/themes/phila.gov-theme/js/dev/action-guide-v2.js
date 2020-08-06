055e9d6f7c9df416ab66177d6474e202db1162fd
Derrick Dieso <derrickjdieso@gmail.com>
2020-08-06 15:58:46 -0400

    tab query string nav

--- wp/wp-content/themes/phila.gov-theme/js/dev/action-guide-v2.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/action-guide-v2.js
@@ -1,5 +1,59 @@
 module.exports = $(function () {
 
+  var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1),
+      sURLVariables = sPageURL.split('&'),
+      sParameterName,
+      i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+      sParameterName = sURLVariables[i].split('=');
+
+      if (sParameterName[0] === sParam) {
+        return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+      }
+    }
+  };
+
+  $( document ).ready(function() {
+    switch(getUrlParameter('tab')) {
+      case '1':
+        if (!$('#step-1-label').hasClass('active')) {
+          $('#tab-1-content').addClass('active');
+          $('#step-1-label').addClass('active');
+      
+          $('#step-2-label').removeClass('active');
+          $('#step-3-label').removeClass('active');
+          $('#tab-2-content').removeClass('active');
+          $('#tab-3-content').removeClass('active');
+        }
+        break;
+      case '2':
+        if (!$('#step-2-label').hasClass('active')) {
+          $('#tab-2-content').addClass('active');
+          $('#step-2-label').addClass('active');
+    
+          $('#step-1-label').removeClass('active');
+          $('#step-3-label').removeClass('active');
+          $('#tab-1-content').removeClass('active');
+          $('#tab-3-content').removeClass('active');
+        }
+        break;
+      case '3':
+        if (!$('#step-3-label').hasClass('active')) {
+          $('#tab-3-content').addClass('active');
+          $('#step-3-label').addClass('active');
+    
+          $('#step-2-label').removeClass('active');
+          $('#step-1-label').removeClass('active');
+          $('#tab-2-content').removeClass('active');
+          $('#tab-1-content').removeClass('active');
+        }
+        break;
+      default:
+    }
+  });
+
   $('#step-1-label, #step-2-to-1-nav span, #step-2-to-1-nav i').click(function () {
     if (!$('#step-1-label').hasClass('active')) {
       $('#tab-1-content').addClass('active');
@@ -9,6 +63,7 @@ module.exports = $(function () {
       $('#step-3-label').removeClass('active');
       $('#tab-2-content').removeClass('active');
       $('#tab-3-content').removeClass('active');
+      history.pushState({}, null, window.location.origin+window.location.pathname+'?tab=1');
     }
   });
 
@@ -21,6 +76,7 @@ module.exports = $(function () {
       $('#step-3-label').removeClass('active');
       $('#tab-1-content').removeClass('active');
       $('#tab-3-content').removeClass('active');
+      history.pushState({}, null, window.location.origin+window.location.pathname+'?tab=2');
     }
   });
 
@@ -33,6 +89,7 @@ module.exports = $(function () {
       $('#step-1-label').removeClass('active');
       $('#tab-2-content').removeClass('active');
       $('#tab-1-content').removeClass('active');
+      history.pushState({}, null, window.location.origin+window.location.pathname+'?tab=3');
     }
   });
 
