8a440a4fdf916bcc44c9dccaa5f10851d020d6e5
Derrick Dieso <derrickjdieso@gmail.com>
2020-08-06 14:24:27 -0400

    desktop translations

--- wp/wp-content/themes/phila.gov-theme/partials/posts/post-translated-content.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/post-translated-content.php
@@ -1,35 +1,37 @@
 <!-- Translated content -->
 <?php 
-if ( count( $language_list ) >= 8 ) {
-  $language_list_over_7 = array_slice($language_list, 7);
-  $language_list = array_slice($language_list, 0, 7, true);
+if ( count( $language_list ) >= 9 ) {
+  $language_list_overflow = array_slice($language_list, 8);
+  $language_list = array_slice($language_list, 0, 8, true);
 }
 ?>
 
 <div class="grid-container translations-container">
-    <div class="grid-x medium-24 bg-ghost-gray mvl pas translations">
+    <div class="grid-x medium-24 bg-ghost-gray mvl translations">
       <span class="border-right phl-mu hide-for-small-only"><i class="fas fa-globe fa-2x"></i></span>
       <ul class="inline-list no-bullet mbn pln">
         <?php foreach ($language_list as $key => $value): ?>
-          <li class="phl-mu phs">
-            <?php echo ( $value === get_the_permalink() ) ? '' : '<a href="' .  $value . '">' ?><?php echo phila_language_output($key)?><?php echo ($value === get_the_permalink()) ? '' : '</a>' ?>
-          </li>
+          <?php echo ( $value === get_the_permalink() ) 
+            ? '<li class="phl-mu phs active">' . phila_language_output($key) .'</li>' 
+            : '<li class="phl-mu phs"><a href="' .  $value . '">' . phila_language_output($key) . '</a></li>' ?>
         <?php endforeach; ?>
-      </ul>
-      <?php if ( isset($language_list_over_7) ) { ?>
-        <ul class="dropdown menu" data-dropdown-menu>
-          <li>
-            <a href="#"></a>
-            <ul class="menu">
-              <?php foreach ($language_list_over_7 as $key => $value): ?>
-                <li class="phl-mu phs">
-                  <?php echo ( $value === get_the_permalink() ) ? '' : '<a href="' .  $value . '">' ?><?php echo phila_language_output($key)?><?php echo ($value === get_the_permalink()) ? '' : '</a>' ?>
-                </li>
-              <?php endforeach; ?>
+        <?php if ( isset($language_list_overflow) ) { ?>
+          <li class="phl-mu phs">
+            <ul class="dropdown menu" data-dropdown-menu>
+              <li>
+                <a href="#" class="dropdown-selector"></a>
+                <ul class="menu">
+                  <?php foreach ($language_list_overflow as $key => $value): ?>
+                    <?php echo ( $value === get_the_permalink() ) 
+                      ? '<li class="phs active">' . phila_language_output($key) .'</li>' 
+                      : '<li class="phs"><a href="' .  $value . '">' . phila_language_output($key) . '</a></li>' ?>
+                  <?php endforeach; ?>
+                </ul>
+              </li>
             </ul>
           </li>
-        </ul>
-      <?php } ?>
+        <?php } ?>
+      </ul>
 
     </div>
   </div>
