9c14a71359cb442bab1073f557acf81203bb2c4c
Derrick Dieso <derrickjdieso@gmail.com>
2020-08-11 11:42:23 -0400

    conditional nav style

--- wp/wp-content/themes/phila.gov-theme/partials/posts/action_guide_v2/action_guide_v2_sections/action-guide-content-tab.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/action_guide_v2/action_guide_v2_sections/action-guide-content-tab.php
@@ -115,7 +115,7 @@
           <?php if( $tab_id != 1 ) { ?>
             <?php $prev_tab = $tabs[$tab_key-1]; ?>
             <?php if( isset($prev_tab['tab_label']) ): ?>
-              <div class="medium-12 cell pbxl">
+              <div class="<?php echo ($tab_id == $tab_count) ? 'medium-24' : 'medium-12' ?> cell pbxl">
                 <div class="prev-tab">
                   <i class="fas fa-caret-left"></i>
                   <span><?php echo $prev_tab['tab_label'];?></span>
@@ -126,7 +126,7 @@
           <?php if( $tab_id != $tab_count ) { ?>
             <?php $next_tab = $tabs[$tab_key+1]; ?>
             <?php if( isset($next_tab['tab_label']) ): ?>
-              <div class="medium-12 cell pbxl">
+              <div class="<?php echo ($tab_id == 1) ? 'medium-24' : 'medium-12' ?> cell pbxl">
                 <div class="next-tab">
                   <span><?php echo $next_tab['tab_label'];?></span>
                   <i class="fas fa-caret-right"></i>
