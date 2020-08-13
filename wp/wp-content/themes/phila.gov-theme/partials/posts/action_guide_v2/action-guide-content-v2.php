80b1b9a21cefa77eeeae3867e9aa563b4f675084
Derrick Dieso <derrickjdieso@gmail.com>
2020-08-13 11:02:18 -0400

    removed vestigial code

--- wp/wp-content/themes/phila.gov-theme/partials/posts/action_guide_v2/action-guide-content-v2.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/action_guide_v2/action-guide-content-v2.php
@@ -3,30 +3,6 @@
  * Action guide V2 display
  */
 
-$get_step_1_content = rwmb_meta( 'step_1_content' );
-$step_1_content = phila_loop_clonable_metabox( $get_step_1_content );
-$step_1_wysiwyg = rwmb_meta('step_1_wysiwyg');
-
-$step_2_wysiwyg = rwmb_meta('step_2_wysiwyg');
-$get_step_2_content_before_steps = rwmb_meta( 'step_2_content_before_steps' );
-$step_2_content_before_steps = phila_loop_clonable_metabox( $get_step_2_content_before_steps );
-
-if( isset($step_2_content_before_steps['phila_custom_wysiwyg']['phila_wysiwyg_title'])) {
-  $step_2_content_before_steps['url'] = strtolower(str_replace(' ', '-', $step_2_content_before_steps['phila_custom_wysiwyg']['phila_wysiwyg_title']));
-}
-
-$get_phila_stepped_content_step_2 = rwmb_meta( 'phila_stepped_content_step_2' );
-$phila_stepped_content_step_2 = phila_extract_stepped_content( $get_phila_stepped_content_step_2 );
-
-$get_step_2_content_after_steps = rwmb_meta( 'step_2_content_after_steps' );
-$step_2_content_after_steps = phila_loop_clonable_metabox( $get_step_2_content_after_steps );
-
-$step_3_wysiwyg = rwmb_meta('step_3_wysiwyg');
-$step_3_stepped_content_title = rwmb_meta('phila_stepped_content_step_3_title');
-$get_phila_stepped_content_step_3 = rwmb_meta( 'phila_stepped_content_step_3' );
-$phila_stepped_content_step_3 = phila_extract_stepped_content( $get_phila_stepped_content_step_3 );
-
-
 // MetaBox variables
 $tabs = rwmb_meta('phila_tabbed_content');
 if (!phila_util_is_array_empty($tabs)):
