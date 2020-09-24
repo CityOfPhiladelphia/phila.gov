503038d56b51d115b09ebe9d0f296952e79631cc
Derrick Dieso <derrickjdieso@gmail.com>
2020-09-24 11:09:11 -0400

    cache + resolve

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/document-finder.php
@@ -8,8 +8,8 @@ $vue_or_not = rwmb_meta('phila_vue_toggle');
 if ($vue_or_not) {
   $app_id = 'vue-app';
 
-  $vuejs_js_ids = ['https://www.phila.gov/embedded/document-finder/dev/js/chunk-vendors.js?tester', 'https://www.phila.gov/embedded/document-finder/dev/js/app.js?tester'];
-  $vuejs_css_ids = ['https://www.phila.gov/embedded/document-finder/dev/css/chunk-vendors.css?tester', 'https://www.phila.gov/embedded/document-finder/dev/css/app.css?tester'];
+  $vuejs_js_ids = ['https://www.phila.gov/embedded/document-finder/dev/js/chunk-vendors.js?tester123', 'https://www.phila.gov/embedded/document-finder/dev/js/app.js?tester123'];
+  $vuejs_css_ids = ['https://www.phila.gov/embedded/document-finder/dev/css/chunk-vendors.css?tester123', 'https://www.phila.gov/embedded/document-finder/dev/css/app.css?tester123'];
   
   include(locate_template( 'partials/vue-apps/vue-register.php' ) );
 }
