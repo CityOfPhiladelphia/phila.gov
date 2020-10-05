927e19b3da171f5f56179935e001d405e361d6bb
Derrick Dieso <derrickjdieso@gmail.com>
2020-10-05 14:47:22 -0400

    style for longform app

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
@@ -3,7 +3,7 @@
 
 $app_id = 'vue-app';
 
-$vuejs_js_ids = ['https://www.phila.gov/embedded/longform-content/dev-check/js/chunk-vendors.js?moh1', 'https://www.phila.gov/embedded/longform-content/dev-check/js/app.js?moh1'];
-$vuejs_css_ids = ['https://www.phila.gov/embedded/longform-content/dev-check/css/chunk-vendors.css?moh1', 'https://www.phila.gov/embedded/longform-content/dev-check/css/app.css?moh1'];
+$vuejs_js_ids = ['https://www.phila.gov/embedded/longform-content/dev-2/js/chunk-vendors.js?tester345', 'https://www.phila.gov/embedded/longform-content/dev-2/js/app.js?tester345'];
+$vuejs_css_ids = ['https://www.phila.gov/embedded/longform-content/dev-2/css/chunk-vendors.css?tester345', 'https://www.phila.gov/embedded/longform-content/dev-2/css/app.css?tester345'];
 
 include(locate_template( 'partials/vue-apps/vue-register.php' ) );
