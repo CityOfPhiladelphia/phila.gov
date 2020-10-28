91213bf7a820a3afe1c4918e34b735ba988565c7
Derrick Dieso <derrickjdieso@gmail.com>
2020-10-28 15:52:31 -0400

    cache 5: return of the cache

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
@@ -3,7 +3,7 @@
 
 $app_id = 'vue-app';
 
-$vuejs_js_ids = ['https://www.phila.gov/embedded/longform-content/staging/js/chunk-vendors.js?test987', 'https://www.phila.gov/embedded/longform-content/staging/js/app.js?test987'];
-$vuejs_css_ids = ['https://www.phila.gov/embedded/longform-content/staging/css/chunk-vendors.css?test987', 'https://www.phila.gov/embedded/longform-content/staging/css/app.css?test987'];
+$vuejs_js_ids = ['https://www.phila.gov/embedded/longform-content/staging/js/chunk-vendors.js?check1010', 'https://www.phila.gov/embedded/longform-content/staging/js/app.js?check1010'];
+$vuejs_css_ids = ['https://www.phila.gov/embedded/longform-content/staging/css/chunk-vendors.css?check1010', 'https://www.phila.gov/embedded/longform-content/staging/css/app.css?check1010'];
 
 include(locate_template( 'partials/vue-apps/vue-register.php' ) );
