da834fc29da7596b179c73e110a65c5968357f57
Derrick Dieso <derrickjdieso@gmail.com>
2020-10-28 14:58:15 -0400

    cache bust 3

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
@@ -3,7 +3,7 @@
 
 $app_id = 'vue-app';
 
-$vuejs_js_ids = ['https://www.phila.gov/embedded/longform-content/staging/js/chunk-vendors.js?test765', 'https://www.phila.gov/embedded/longform-content/staging/js/app.js?test765'];
-$vuejs_css_ids = ['https://www.phila.gov/embedded/longform-content/staging/css/chunk-vendors.css?test765', 'https://www.phila.gov/embedded/longform-content/staging/css/app.css?test765'];
+$vuejs_js_ids = ['https://www.phila.gov/embedded/longform-content/staging/js/chunk-vendors.js?check876', 'https://www.phila.gov/embedded/longform-content/staging/js/app.js?check876'];
+$vuejs_css_ids = ['https://www.phila.gov/embedded/longform-content/staging/css/chunk-vendors.css?check876', 'https://www.phila.gov/embedded/longform-content/staging/css/app.css?check876'];
 
 include(locate_template( 'partials/vue-apps/vue-register.php' ) );
