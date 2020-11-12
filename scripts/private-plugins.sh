82fc8d2b7a91fb93933e9cd366f2830710ad2200
Derrick Dieso <derrickjdieso@gmail.com>
2020-11-12 09:40:13 -0500

    remove login plugin

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -5,8 +5,7 @@
 source /home/ubuntu/.ssh/environment
 
 _dir="$(dirname "$0")"
-plugins="wps-hide-login.1.5.7.zip
-two-factor-authentication-premium.1.8.6.zip
+plugins="two-factor-authentication-premium.1.8.6.zip
 mb-admin-columns-1.5.0.zip
 mb-revision-1.3.3.zip
 mb-settings-page-2.1.3.zip
