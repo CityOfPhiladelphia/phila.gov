8a0c3dcca1ab1575c540841b899801b2d7a7cc1d
Karissa <k@karissademi.com>
2020-12-22 10:47:03 -0500

    Add Clarity code

--- wp/wp-content/themes/phila.gov-theme/partials/global/analytics.php
+++ wp/wp-content/themes/phila.gov-theme/partials/global/analytics.php
@@ -59,3 +59,13 @@
   <!-- Google Tag Manager --> 
   <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0], j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src= 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f); })(window,document,'script','dataLayer','GTM-MC6CR2');</script> 
   <!-- End Google Tag Manager -->
+  
+  <!-- Begin Microsoft Clarity -->
+  <script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "4l8dhsl6kn");
+  </script>
+  <!-- End Microsoft Clarity -->
