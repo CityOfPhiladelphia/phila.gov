cc6c78cdfebc1ab0cb59c9b848b7900957c55760
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-04 11:12:17 -0500

    more cache

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/longform-content.php
@@ -3,7 +3,7 @@
 
 $app_id = 'vue-app';
 $longform_file_path = 'https://www.phila.gov/embedded/longform-content/'.$phila_environment;
-$vuejs_js_ids = [$longform_file_path.'/js/chunk-vendors.js', $longform_file_path.'/js/app.js?newcache'];
-$vuejs_css_ids = [$longform_file_path.'/css/chunk-vendors.css', $longform_file_path.'/css/app.css?newcache'];
+$vuejs_js_ids = [$longform_file_path.'/js/chunk-vendors.js?newcache', $longform_file_path.'/js/app.js?newcache'];
+$vuejs_css_ids = [$longform_file_path.'/css/chunk-vendors.css?newcache', $longform_file_path.'/css/app.css?newcache'];
 
 include(locate_template( 'partials/vue-apps/vue-register.php' ) );
