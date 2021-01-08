1fa28dc4f3ce556efa3db934a0b8f58a2b292a76
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-08 15:32:58 -0500

    db testing

---
+++ scripts/db-config.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+echo '<?php
+/**
+ * Register the primary server to HyperDB
+ */
+$wpdb->add_database( array(
+	"host"     => $DB_HOST,
+	"user"     => $DB_USER,
+	"password" => $DB_PASS,
+	"name"     => "wp",
+	"write"    => 1,
+	"read"     => 0,
+) );
+/**
+ * Register replica database server if it"s available in this environment
+ */
+if ( ! empty( $REPLICA_DB_HOST ) ) {
+	$wpdb->add_database(array(
+					"host"     => $REPLICA_DB_HOST,
+					"user"     => $DB_USER,
+					"password" => $DB_PASS,
+					"name"     => "wp",
+					"write"    => 0,
+					"read"     => 1,
+	));
+}
+' > "../wp/db-config.php"
