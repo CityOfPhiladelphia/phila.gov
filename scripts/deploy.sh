ef3ac704790e461849d16f0ca0ff7830f91d49c4
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-11 15:24:44 -0500

    chmod 4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ source "$_dir/lib/mo"
 echo 'Running wp-config.sh'
 "$_dir/wp-config.sh"
 
-sudo chmod 755 /home/ubuntu/phila.gov/scripts/db-config.sh
+sudo chmod 755 phila.gov/scripts/db-config.sh
 
 echo 'Running db-config.sh'
 "$_dir/db-config.sh"
