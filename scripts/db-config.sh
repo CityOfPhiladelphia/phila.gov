13e1294729e69ce517213ce6fb05fa07792a299d
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-12 09:14:34 -0500

    back to multiple echos

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -6,25 +6,28 @@ echo '<?php
 /**
  * Register the primary server to HyperDB
  */
-$wpdb->add_database( array(
-	"host"     => $DB_HOST,
-	"user"     => $DB_USER,
-	"password" => $DB_PASS,
-	"name"     => "wp",
-	"write"    => 1,
-	"read"     => 0,
+$wpdb->add_database( array('  > '/home/ubuntu/app/wp/db-config.php'
+
+echo "'host'     => $DB_HOST,
+	'user'     => $DB_USER,
+	'password' => $DB_PASS,
+	'name'     => 'wp',
+	'write'    => 1,
+	'read'     => 0,
 ) );
 /**
- * Register replica database server if it"s available in this environment
+ * Register replica database server if it's available in this environment
  */
-if ( ! empty( $REPLICA_DB_HOST ) ) {
-	$wpdb->add_database(array(
-		"host"     => $REPLICA_DB_HOST,
-		"user"     => $DB_USER,
-		"password" => $DB_PASS,
-		"name"     => "wp",
-		"write"    => 0,
-		"read"     => 1,
+if ( ! empty( $REPLICA_DB_HOST ) ) {"  >> '/home/ubuntu/app/wp/db-config.php'
+
+echo '$wpdb->add_database(array(' >> '/home/ubuntu/app/wp/db-config.php'
+
+echo "'host'     => $REPLICA_DB_HOST,
+	'user'     => $DB_USER,
+	'password' => $DB_PASS,
+	'name'     => 'wp',
+	'write'    => 0,
+	'read'     => 1,
 	));
 }
-' > "/home/ubuntu/app/wp/db-config.php"
+" >> '/home/ubuntu/app/wp/db-config.php'
