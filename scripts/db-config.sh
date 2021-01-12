46a60fa9e27558b9815fd25351508e0e0202090e
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-12 09:10:42 -0500

    blank test

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -2,28 +2,4 @@
 
 source /home/ubuntu/.ssh/environment
 
-echo '$REPLICA_DB_HOST' > '/home/ubuntu/app/wp/test.txt'
-
-# echo "'host'     => $DB_HOST,
-# 	'user'     => $DB_USER,
-# 	'password' => $DB_PASS,
-# 	'name'     => 'wp',
-# 	'write'    => 1,
-# 	'read'     => 0,
-# ) );
-# /**
-#  * Register replica database server if it's available in this environment
-#  */
-# if ( ! empty( $REPLICA_DB_HOST ) ) {"  >> '/home/ubuntu/app/wp/db-config.php'
-
-# echo '$wpdb->add_database(array(' >> '/home/ubuntu/app/wp/db-config.php'
-
-# echo "'host'     => $REPLICA_DB_HOST,
-# 	'user'     => $DB_USER,
-# 	'password' => $DB_PASS,
-# 	'name'     => 'wp',
-# 	'write'    => 0,
-# 	'read'     => 1,
-# 	));
-# }
-# " >> '/home/ubuntu/app/wp/db-config.php'
+echo 'test' > '/home/ubuntu/app/wp/test.txt'
