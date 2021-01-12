31ce1562a5ac7cf90039881fe7b493cd275ee807
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-12 08:28:58 -0500

    change ' to "

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -2,29 +2,29 @@
 
 source /home/ubuntu/.ssh/environment
 
-echo '<?php
+echo "<?php
 /**
  * Register the primary server to HyperDB
  */
 $wpdb->add_database( array(
-	"host"     => $DB_HOST,
-	"user"     => $DB_USER,
-	"password" => $DB_PASS,
-	"name"     => "wp",
-	"write"    => 1,
-	"read"     => 0,
+	'host'     => $DB_HOST,
+	'user'     => $DB_USER,
+	'password' => $DB_PASS,
+	'name'     => 'wp',
+	'write'    => 1,
+	'read'     => 0,
 ) );
 /**
- * Register replica database server if it"s available in this environment
+ * Register replica database server if it's available in this environment
  */
 if ( ! empty( $REPLICA_DB_HOST ) ) {
 	$wpdb->add_database(array(
-					"host"     => $REPLICA_DB_HOST,
-					"user"     => $DB_USER,
-					"password" => $DB_PASS,
-					"name"     => "wp",
-					"write"    => 0,
-					"read"     => 1,
+					'host'     => $REPLICA_DB_HOST,
+					'user'     => $DB_USER,
+					'password' => $DB_PASS,
+					'name'     => 'wp',
+					'write'    => 0,
+					'read'     => 1,
 	));
 }
-' > "/home/ubuntu/app/wp/db-config.php"
+" > '/home/ubuntu/app/wp/db-config.php'
