f95f561bd2d8dd6cba147c7324899db720f7a197
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-12 15:47:21 -0500

    test if can find file

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -1,30 +1,33 @@
 #!/bin/bash
 
-source /home/ubuntu/.ssh/environment
+FILE=/home/ubuntu/.ssh/environment
+if test -f "$FILE"; then
+    echo "$FILE exists." > "/home/ubuntu/app/wp/db-config.php"
+fi
 
-echo '<?php
-/**
- * Register the primary server to HyperDB
- */
-\$wpdb->add_database( array(
-	"host"     => $DB_HOST,
-	"user"     => $DB_USER,
-	"password" => $DB_PASS,
-	"name"     => "wp",
-	"write"    => 1,
-	"read"     => 0,
-) );
-/**
- * Register replica database server if it"s available in this environment
- */
-if ( ! empty( $REPLICA_DB_HOST ) ) {
-	\$wpdb->add_database(array(
-		"host"     => $REPLICA_DB_HOST,
-		"user"     => $DB_USER,
-		"password" => $DB_PASS,
-		"name"     => "wp",
-		"write"    => 0,
-		"read"     => 1,
-	));
-}
-' > "/home/ubuntu/app/wp/db-config.php"
+# echo '<?php
+# /**
+#  * Register the primary server to HyperDB
+#  */
+# \$wpdb->add_database( array(
+# 	"host"     => $DB_HOST,
+# 	"user"     => $DB_USER,
+# 	"password" => $DB_PASS,
+# 	"name"     => "wp",
+# 	"write"    => 1,
+# 	"read"     => 0,
+# ) );
+# /**
+#  * Register replica database server if it"s available in this environment
+#  */
+# if ( ! empty( $REPLICA_DB_HOST ) ) {
+# 	\$wpdb->add_database(array(
+# 		"host"     => $REPLICA_DB_HOST,
+# 		"user"     => $DB_USER,
+# 		"password" => $DB_PASS,
+# 		"name"     => "wp",
+# 		"write"    => 0,
+# 		"read"     => 1,
+# 	));
+# }
+# ' > "/home/ubuntu/app/wp/db-config.php"
