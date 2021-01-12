65bf69ebbda1d3624e5c947ad0ccc6d0d8eb5b61
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-12 09:12:00 -0500

    revert

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -2,4 +2,29 @@
 
 source /home/ubuntu/.ssh/environment
 
-echo 'test' > '/home/ubuntu/app/wp/test.txt'
+echo '<?php
+/**
+ * Register the primary server to HyperDB
+ */
+$wpdb->add_database( array(
+	"host"     => $DB_HOST,
+	"user"     => $DB_USER,
+	"password" => $DB_PASS,
+	"name"     => "wp",
+	"write"    => 1,
+	"read"     => 0,
+) );
+/**
+ * Register replica database server if it"s available in this environment
+ */
+if ( ! empty( $REPLICA_DB_HOST ) ) {
+	$wpdb->add_database(array(
+		"host"     => $REPLICA_DB_HOST,
+		"user"     => $DB_USER,
+		"password" => $DB_PASS,
+		"name"     => "wp",
+		"write"    => 0,
+		"read"     => 1,
+	));
+}
+' > "/home/ubuntu/app/wp/db-config.php"
