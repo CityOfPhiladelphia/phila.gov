dc0d680bf29296c0af938943b5d23c78e0a399e8
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-13 10:46:42 -0500

    removed extra quotes

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -8,10 +8,10 @@ if test -f "$FILE"; then
 		* Register the primary server to HyperDB
 		*/
 		\$wpdb->add_database( array(
-			'host'     => '$DB_HOST',
-			'user'     => '$DB_USER',
-			'password' => '$DB_PASS',
-			'name'     => 'wp',
+			'host'     => $DB_HOST,
+			'user'     => $DB_USER,
+			'password' => $DB_PASS,
+			'name'     => wp,
 			'write'    => 1,
 			'read'     => 0,
 		) );
@@ -20,10 +20,10 @@ if test -f "$FILE"; then
 		*/
 		if ( ! empty( $REPLICA_DB_HOST ) ) {
 			\$wpdb->add_database(array(
-				'host'     => '$REPLICA_DB_HOST',
-				'user'     => '$DB_USER',
-				'password' => '$DB_PASS',
-				'name'     => 'wp',
+				'host'     => $REPLICA_DB_HOST,
+				'user'     => $DB_USER,
+				'password' => $DB_PASS,
+				'name'     => wp,
 				'write'    => 0,
 				'read'     => 1,
 			));
