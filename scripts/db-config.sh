8d341f8300b86c4198e8d6daa13a4f7c861e90cd
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-15 10:19:45 -0500

    remove if statement

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -18,16 +18,14 @@ if test -f "$FILE"; then
 		/**
 		* Register replica database server if it's available in this environment
 		*/
-		if ( ! empty( $REPLICA_DB_HOST ) ) {
-			\$wpdb->add_database(array(
-				'host'     => '$REPLICA_DB_HOST',
-				'user'     => '$DB_USER',
-				'password' => '$DB_PASS',
-				'name'     => wp,
-				'write'    => 0,
-				'read'     => 1,
-			));
-		}
+		\$wpdb->add_database(array(
+			'host'     => '$REPLICA_DB_HOST',
+			'user'     => '$DB_USER',
+			'password' => '$DB_PASS',
+			'name'     => wp,
+			'write'    => 0,
+			'read'     => 1,
+		));
 		?>
 		" > '/home/ubuntu/app/wp/db-config.php'
 fi
