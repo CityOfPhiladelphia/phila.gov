236082d89b44e0ae3a78165f4aa4b7738c92eb1a
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-15 11:15:43 -0500

    isset????

--- scripts/db-config.sh
+++ scripts/db-config.sh
@@ -18,14 +18,16 @@ if test -f "$FILE"; then
 		/**
 		* Register replica database server if it's available in this environment
 		*/
-		\$wpdb->add_database(array(
-			'host'     => '$REPLICA_DB_HOST',
-			'user'     => '$DB_USER',
-			'password' => '$DB_PASS',
-			'name'     => wp,
-			'write'    => 0,
-			'read'     => 1,
-		));
+		if ( ! isset( $REPLICA_DB_HOST ) ) {
+			\$wpdb->add_database(array(
+				'host'     => '$REPLICA_DB_HOST',
+				'user'     => '$DB_USER',
+				'password' => '$DB_PASS',
+				'name'     => wp,
+				'write'    => 0,
+				'read'     => 1,
+			));
+		}
 		?>
 		" > '/home/ubuntu/app/wp/db-config.php'
 fi
