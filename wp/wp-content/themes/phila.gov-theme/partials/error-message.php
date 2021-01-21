9803cd984ced80230ace6bcc574a16d57a4c6bb7
Derrick Dieso <derrickjdieso@gmail.com>
2021-01-21 13:59:24 -0500

    temp error messaging

---
+++ wp/wp-content/themes/phila.gov-theme/partials/error-message.php
@@ -0,0 +1,29 @@
+<div class="phila-error-background columns">
+  <div class="phila-error-card row">
+    <div class="medium-2">
+      <i class="fas fa-exclamation-circle"></i>
+    </div>
+    <div class="medium-22 phila-error">
+      <div class="phila-error-title">
+      This is the title
+      </div>
+      <ul class="phila-error-list">
+        <li class="phila-error-item">
+          <a href="#">
+            item
+          </a>
+        </li>
+        <li class="phila-error-item">
+          <a href="#">
+            item
+          </a>
+        </li>
+        <li class="phila-error-item">
+          <a href="#">
+            item
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>
