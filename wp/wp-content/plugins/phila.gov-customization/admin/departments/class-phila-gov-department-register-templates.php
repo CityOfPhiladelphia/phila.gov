138a091811401c62ce8635db38756592d6553424
Derrick Dieso <derrickjdieso@gmail.com>
2021-02-19 11:19:24 -0500

    organize template options

--- wp/wp-content/plugins/phila.gov-customization/admin/departments/class-phila-gov-department-register-templates.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/departments/class-phila-gov-department-register-templates.php
@@ -35,7 +35,6 @@ function register_template_selection_metabox_departments( $meta_boxes ){
         'clone' => false,
         'placeholder'  => 'Select a template',
         'options' => array(
-          'default'                 => 'Default',
           'homepage_v2'             => 'Homepage',
           'repeating_rows'          => 'Repeating rows',
           'one_quarter_headings_v2' => '1/4 headings',
