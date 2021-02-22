2aeb1cb7fa28024f7e58384e0452a2249376d89f
Karissa <k@karissademi.com>
2021-02-22 12:18:45 -0500

    Remove code

--- wp/wp-content/themes/phila.gov-theme/partials/content-feedback.php
+++ wp/wp-content/themes/phila.gov-theme/partials/content-feedback.php
@@ -13,7 +13,7 @@
       <div class="row" data-toggle="feedback">
         <div class="small-24 columns">
           <div class="call-to-action phs pvm center">
-            <a href="#" class="no-link"></i><span class="break-before-sm">We’re always working to improve phila.gov.</span>
+            <a href="#" class="no-link"><span class="break-before-sm">We’re always working to improve phila.gov.</span>
             <span class="break-before-sm"> How can we make it this page better?</span></a>
           </div>
         </div>
