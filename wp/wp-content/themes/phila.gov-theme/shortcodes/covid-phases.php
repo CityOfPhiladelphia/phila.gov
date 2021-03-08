bf53d87aa4c1843e0a850db4849e3d5f0908aa72
Derrick Dieso <derrickjdieso@gmail.com>
2021-03-08 14:27:30 -0500

    covid phases

---
+++ wp/wp-content/themes/phila.gov-theme/shortcodes/covid-phases.php
@@ -0,0 +1,141 @@
+<?php
+/**
+*
+* Shortcode for easy display of covid phases
+* [display_category]
+*
+* @package phila-gov_customization
+*/
+function covid_phases( $atts ) {
+  $a = shortcode_atts( array(
+    'phase' => 0,
+  ), $atts );
+  ?>
+
+  <?php 
+    if ($a['phase'] == '1a') {
+      $phase_1a = 'active';
+      $phase_1b = 'upcoming';
+      $phase_1c = 'upcoming';
+      $phase_2 = 'upcoming';
+    } else if ($a['phase'] == '1b') {
+      $phase_1a = 'completed';
+      $phase_1b = 'active';
+      $phase_1c = 'upcoming';
+      $phase_2 = 'upcoming';
+    } else if ($a['phase'] == '1c') {
+      $phase_1a = 'completed';
+      $phase_1b = 'completed';
+      $phase_1c = 'active';
+      $phase_2 = 'upcoming';
+    } else if ($a['phase'] == '2') {
+      $phase_1a = 'completed';
+      $phase_1b = 'completed';
+      $phase_1c = 'completed';
+      $phase_2 = 'active';
+    }
+  
+  ?>
+
+  <section class="phila-redesign covid-phases mvl phl">
+    <div class="process">
+      <div class="row collapse process-bar">
+        <div class="medium-6 columns">
+          <section class="chevron <?php echo $phase_1a; ?>">
+            <div class="row collapse active-marker">
+              <div class="medium-6 columns center small-centered marker">
+                <i class="fas fa-map-marker-alt fa-3x" aria-hidden="true"></i>
+              </div>
+            </div>
+            <header class="bg-dark-gray">
+              <div class="valign process-label left-arrow-indent right-arrow">
+                <div class="valign-cell">
+                  <h4 class="mbn h5 white"><i class="fas fa-check-circle"></i>Phase 1a</h4>
+                </div>
+              </div>
+            </header>
+            <div class="description">
+              <ul>
+                <li>Patient-facing healthcare workers</li>
+                <li>Long-term care facility residents and staff</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+        <div class="medium-6 columns small-text">
+          <section class="chevron <?php echo $phase_1b; ?>">
+            <div class="row collapse active-marker">
+              <div class="medium-6 columns center small-centered marker">
+                <i class="fas fa-map-marker-alt fa-3x" aria-hidden="true"></i>
+              </div>
+            </div>
+            <header class="bg-dark-gray">
+              <div class="valign process-label left-arrow-indent right-arrow">
+                <div class="valign-cell">
+                  <h4 class="mbn h5 white"><i class="fas fa-check-circle"></i>Phase 1b</h4>
+                </div>
+              </div>
+            </header>
+            <div class="description">
+              <ul>
+                <li>Frontline essential workers at high risk for exposure</li>
+                <li>Individuals 75+ with highest risk medical conditions</li>
+                <li>Those working or residing in congregate settings</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+        <div class="medium-6 columns small-text">
+          <section class="chevron <?php echo $phase_1c; ?>">
+            <div class="row collapse active-marker">
+              <div class="medium-6 columns center small-centered marker">
+                <i class="fas fa-map-marker-alt fa-3x" aria-hidden="true"></i>
+              </div>
+            </div>
+            <header class="bg-dark-gray">
+              <div class="valign process-label left-arrow-indent right-arrow-ben-franklin-blue right-arrow">
+                <div class="valign-cell">
+                  <h4 class="mbn h5 white"><i class="fas fa-check-circle"></i>Phase 1c</h4>
+                </div>
+              </div>
+            </header>
+            <div class="description">
+              <ul>
+                <li>Essential workers at lower risk of exposure</li>
+                <li>Individuals 65–74 with highest risk medical conditions</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+        <div class="medium-6 columns end">
+          <section class="chevron <?php echo $phase_2; ?>">
+            <div class="row collapse active-marker">
+              <div class="medium-6 columns center small-centered marker">
+                <i class="fas fa-map-marker-alt fa-3x" aria-hidden="true"></i>
+              </div>
+            </div>
+            <header class="bg-dark-gray">
+              <div class="valign process-label left-arrow-indent right-arrow">
+                <div class="valign-cell">
+                  <h4 class="mbn h5 white"><i class="fas fa-check-circle"></i>Phase 2</h4>
+                </div>
+              </div>
+            </header>
+            <div class="description">
+              <ul>
+                <li>Anyone 16+ not yet immunized</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
+  </section>
+  <?php 
+}
+
+add_action( 'init', 'register_covid_phases' );
+
+function register_covid_phases(){
+  add_shortcode( 'covid_phases', 'covid_phases' );
+}
