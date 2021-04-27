6318e7d629fdb01899ac4f055c021638039215f7
Derrick Dieso <derrickjdieso@gmail.com>
2021-04-27 16:30:03 -0400

    v2 endpoint with owners

--- wp/wp-content/plugins/phila.gov-customization/public/controllers/class-phila-programs.php
+++ wp/wp-content/plugins/phila.gov-customization/public/controllers/class-phila-programs.php
@@ -5,6 +5,7 @@ class Phila_Programs_Controller {
   // Initialize the namespace and resource name.
   public function __construct() {
     $this->namespace     = 'programs/v1';
+    $this->namespace_v2     = 'programs/v2';
     $this->resource_name = 'archives';
     $this->service_resource = 'related_service';
 
@@ -40,6 +41,16 @@ class Phila_Programs_Controller {
         'permission_callback' => '__return_true',
       ),
       'schema' => array( $this, 'get_services_schema' ),
+    ) );
+
+    //Register individual items v2
+    register_rest_route( $this->namespace_v2, '/' . $this->resource_name, array(
+      array(
+        'methods'   => WP_REST_Server::READABLE,
+        'callback'  => array( $this, 'get_item_v2' ),
+        'permission_callback' => '__return_true',
+      ),
+      'schema' => array( $this, 'get_item_schema_v2' ),
     ) );
   }
 
@@ -231,6 +242,24 @@ class Phila_Programs_Controller {
   }
 
   /**
+   * Outputs an individual item's data
+   *
+   * @param WP_REST_Request $request Current request.
+   */
+  public function get_item_v2( $request ) {
+
+    $id = (int) $request['id'];
+    $post = get_post( $id );
+    if ( empty( $post ) ) {
+      return rest_ensure_response( array() );
+    }
+
+    $response = $this->prepare_item_for_response_v2( $post, $request );
+
+    return $response;
+  }
+
+  /**
    * Matches the post data to the schema. Also, rename the fields to nicer names.
    *
    * @param WP_Post $post The comment object whose response is being prepared.
@@ -292,6 +321,96 @@ class Phila_Programs_Controller {
       $audiences = get_the_terms($post->ID, 'audience');
 
       $post_data['audiences']  = (array) $audiences;
+    }
+
+    if (isset( $schema['properties']['services'] )) {
+      $services = get_the_terms($post->ID, 'service_type');
+
+      $post_data['services']  = (array) $services;
+    }
+
+    if (isset( $schema['properties']['image'] )) {
+      $img = rwmb_meta( 'prog_header_img', array( 'limit' => 1 ), $post->ID );
+      $img = reset($img);
+      $medium_image = str_replace('.jpg', '-700x400.jpg', $img['full_url']);
+
+      if( !isset( $img['sizes']['medium'] ) ){
+        $medium_image = $img['full_url'];
+      }
+
+      $post_data['image']  = (string) $medium_image;
+    }
+
+    return rest_ensure_response( $post_data );
+}
+
+  /**
+   * Matches the post data to the schema. Also, rename the fields to nicer names.
+   *
+   * @param WP_Post $post The comment object whose response is being prepared.
+   */
+
+  public function prepare_item_for_response_v2( $post, $request ) {
+    $post_data = array();
+
+    $schema = $this->get_item_schema_v2( $request );
+
+    if ( isset( $schema['properties']['id'] ) ) {
+        $post_data['id'] = (int) $post->ID;
+    }
+
+    if (isset( $schema['properties']['title'] )) {
+      $post_data['title']  =  (string) html_entity_decode($post->post_title);
+    }
+
+    if (isset( $schema['properties']['short_description'] )) {
+      $short_desc = rwmb_meta( 'phila_meta_desc', array(), $post->ID );
+
+      $post_data['short_description']  =  (string) html_entity_decode($short_desc);
+    }
+
+    if (isset( $schema['properties']['template'] )) {
+      $post_data['template']  = (string) phila_get_selected_template($post->ID);
+    }
+
+    if (isset( $schema['properties']['external_link'] )) {
+      $link = rwmb_meta( 'prog_off_site_link', array(), $post->ID );
+
+      $post_data['external_link']  = (string) $link;
+    }
+
+    if (isset( $schema['properties']['link'] )) {
+      $external_link = rwmb_meta( 'prog_off_site_link', array(), $post->ID );
+      if( empty($external_link) ) {
+        $link = get_permalink($post->ID);
+        $parsed_link = parse_url($link);
+        $post_data['link']  = (string) $parsed_link['path'];
+      }else{
+        $post_data['link']  = (string) $external_link;
+      }
+    }
+
+    if (isset( $schema['properties']['categories'] )) {
+      $categories = get_the_category($post->ID);
+
+      foreach ($categories as $category){
+          $trimmed_name = phila_get_department_homepage_typography( null, $return_stripped = true, $page_title = $category->name );
+
+          $category->slang_name = html_entity_decode(trim($trimmed_name));
+      }
+
+      $post_data['categories']  = (array) $categories;
+    }
+
+    if (isset( $schema['properties']['audiences'] )) {
+      $audiences = get_the_terms($post->ID, 'audience');
+
+      $post_data['audiences']  = (array) $audiences;
+    }
+
+    if (isset( $schema['properties']['owners'] )) {
+
+      $post_data['owners']  = ( array ) get_the_terms( $post->ID, 'category' );
     }
 
     if (isset( $schema['properties']['services'] )) {
@@ -388,6 +507,71 @@ class Phila_Programs_Controller {
         ),
         'audiences'  => array(
           'description' => esc_html__('The audience taxonomy assigned to this object.', 'phila-gov'),
+          'type'  => 'array',
+        ),
+        'services'  => array(
+          'description' => esc_html__('The service category assigned to this object.', 'phila-gov'),
+          'type'  => 'array',
+        ),
+        'image'  => array(
+          'description' => esc_html__('The medium size image associated with this program.', 'phila-gov'),
+          'type'  => 'string',
+        ),
+      ),
+    );
+
+    return $schema;
+  }
+
+
+  /**
+   * Get sample schema for a collection.
+   *
+   * @param WP_REST_Request $request Current request.
+   */
+  public function get_item_schema_v2( $request ) {
+    $schema = array(
+      // This tells the spec of JSON Schema we are using which is draft 4.
+      '$schema'              => 'http://json-schema.org/draft-04/schema#',
+      // The title property marks the identity of the resource.
+      'title'                => 'post',
+      'type'                 => 'object',
+      // Specify object properties in the properties attribute.
+      'properties'           => array(
+        'id' => array(
+          'description'  => esc_html__( 'Unique identifier for the object.', 'phila-gov' ),
+          'type'         => 'integer',
+          'context'      => array( 'view', 'edit', 'embed' ),
+          'readonly'     => true,
+        ),
+        'title'=> array(
+          'description'  => esc_html__( 'Title of the object.', 'phila-gov' ),
+          'type'         => 'string',
+          'readonly'     => true,
+        ),
+        'short_description'=> array(
+          'description'  => esc_html__( 'Short description.', 'phila-gov' ),
+          'type'         => 'string',
+          'readonly'     => true,
+        ),
+        'template'  => array(
+          'description' => esc_html__('The template this object is using.', 'phila-gov'),
+          'type'  => 'string',
+        ),
+        'link'  => array(
+          'description' => esc_html__('The permalink for this object.', 'phila-gov'),
+          'type'  => 'string',
+        ),
+        'categories'  => array(
+          'description' => esc_html__('The categories assigned to this object.', 'phila-gov'),
+          'type'  => 'array',
+        ),
+        'audiences'  => array(
+          'description' => esc_html__('The audience taxonomy assigned to this object.', 'phila-gov'),
+          'type'  => 'array',
+        ),
+        'owners'  => array(
+          'description' => esc_html__('The owner categories of this object.', 'phila-gov'),
           'type'  => 'array',
         ),
         'services'  => array(
