a48cf3b251b769be12c1a01e4b5578a578ce5388
Derrick Dieso <derrickjdieso@gmail.com>
2021-05-03 11:49:07 -0400

    undo

--- wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-row-metaboxes.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/meta-boxes/class-phila-gov-row-metaboxes.php
@@ -13,7 +13,6 @@ class Phila_Gov_Row_Metaboxes {
     'class' => 'phila_row',
     'type'  => 'group',
     'clone' => true,
-    'max_clone' => 8,
     'sort_clone' => true,
     'add_button'  => '+ Add row',
     'fields' => array(
