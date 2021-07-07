1ddfdab8e0dc0e4d5d2d522416936eb438573752
derrickdso <derrickjdieso@gmail.com>
2021-07-07 16:23:03 -0400

    media endpoint date

--- wp/wp-includes/js/media-views.js
+++ wp/wp-includes/js/media-views.js
@@ -6617,6 +6617,7 @@ Attachment = View.extend(/** @lends wp.media.view.Attachment.prototype */{
 				caption:       '',
 				title:         '',
 				dateFormatted: '',
+				overrideDate:  '',
 				width:         '',
 				height:        '',
 				compat:        false,
