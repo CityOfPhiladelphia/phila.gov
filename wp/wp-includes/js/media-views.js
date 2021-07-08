e688310d54b8e6782f8f68543eb9601592759613
derrickdso <derrickjdieso@gmail.com>
2021-07-08 09:08:28 -0400

    no wp core

--- wp/wp-includes/js/media-views.js
+++ wp/wp-includes/js/media-views.js
@@ -6617,7 +6617,6 @@ Attachment = View.extend(/** @lends wp.media.view.Attachment.prototype */{
 				caption:       '',
 				title:         '',
 				dateFormatted: '',
-				overrideDate:  '',
 				width:         '',
 				height:        '',
 				compat:        false,
