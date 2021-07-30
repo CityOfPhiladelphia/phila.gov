49456867c617b225610cbdb529a148051f8a54fa
derrickdso <derrickjdieso@gmail.com>
2021-07-30 11:10:00 -0400

    testing 9

--- wp/wp-content/themes/phila.gov-theme/shortcodes/longform-content-footnote.php
+++ wp/wp-content/themes/phila.gov-theme/shortcodes/longform-content-footnote.php
@@ -12,13 +12,12 @@ function longform_content_footnote_shortcode($atts) {
   $a = shortcode_atts( array(
     'id' => 0,
   ), $atts );
-  $object = [];
-  $object['content'] = 'link copied';
-  $object['trigger'] = 'click';
 
-  return '<i v-tooltip='.$object.' class="pls fas fa-space-station-moon" />';
-  // $object['content'] = 'link copied';
-  // $object['trigger'] = 'click';
+  // $content = 'link copied';
+  // $trigger = 'click';
+
+  // return '<i v-tooltip='.$object.' class="pls fas fa-space-station-moon" />';
+  return '<i v-tooltip="{ "content": "link copied!", "trigger": "click" }" class="pls fas fa-space-station-moon" />';
 
   wp_reset_postdata();
 
