494999d507e5b0b23d43da13d44bfa6a3016da4c
m-atia <mohamed.atia@phila.gov>
2021-11-22 12:46:09 -0500

    update vendored plugins and embedded plugins except nested pages

--- wp/wp-content/plugins/jwt-auth/class-setup.php
+++ wp/wp-content/plugins/jwt-auth/class-setup.php
@@ -17,7 +17,8 @@ class Setup {
 	public function __construct() {
 		add_action( 'init', array( $this, 'setup_text_domain' ) );
 
-		$auth = new Auth();
+		$auth    = new Auth();
+		$devices = new Devices();
 
 		add_action( 'rest_api_init', array( $auth, 'register_rest_routes' ) );
 		add_filter( 'rest_api_init', array( $auth, 'add_cors_support' ) );
