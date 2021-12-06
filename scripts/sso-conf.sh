c2b1cb98181f40ec12844865abd84bdd5a99cc18
derrickdso <derrickjdieso@gmail.com>
2021-12-06 09:50:15 -0500

    ready for deployment

---
+++ scripts/sso-conf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE=/home/ubuntu/.ssh/environment
+if test -f "$FILE"; then
+		source $FILE
+		echo "location ^~ /wp-login.php {
+  return 301 $SSO_LOGIN;
+}
+
+location /wp-login {
+  return 301 $SSO_LOGIN;
+}
+" > '/home/ubuntu/app/nginx/server.d/sso.conf'
+fi
