078d32a5d449b1cff551c977bd7557c87022c276
derrickdso <derrickjdieso@gmail.com>
2021-12-09 16:43:30 -0500

    sso: remove conf 8

--- scripts/sso-conf.sh
+++ scripts/sso-conf.sh
@@ -1,14 +1,14 @@
-# #!/bin/bash
+#!/bin/bash
 
-# FILE=/home/ubuntu/.ssh/environment
-# if test -f "$FILE"; then
-# 		source $FILE
-# 		echo "location ^~ /wp-login.php {
-#   return 301 $SSO_LOGIN;
-# }
+FILE=/home/ubuntu/.ssh/environment
+if test -f "$FILE"; then
+		source $FILE
+		echo "location ^~ /wp-login.php {
+  return 301 $SSO_LOGIN;
+}
 
-# location /wp-login {
-#   return 301 $SSO_LOGIN;
-# }
-# " > '/home/ubuntu/app/nginx/server.d/sso.conf'
-# fi
+location /wp-login {
+  return 301 $SSO_LOGIN;
+}
+" > '/home/ubuntu/app/nginx/server.d/sso.conf'
+fi
