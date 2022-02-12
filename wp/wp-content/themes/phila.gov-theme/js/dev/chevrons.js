da8eb71989f66e3b6f5dc25673549a610665b656
Karissa <karissa.demi@phila.gov>
2022-02-12 09:54:59 -0500

    New phase tracker

--- wp/wp-content/themes/phila.gov-theme/js/dev/chevrons.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/chevrons.js
@@ -1,18 +1,18 @@
 module.exports = $(function(){
-  $('.covid-phases .chevron .bg-dark-gray').click(function(e){
+  $('.stage-tracker .chevron .bg-dark-gray').click(function(e){
     e.preventDefault();
     e.stopPropagation();
-    $('.covid-phases .chevron').each(function() {
+    $('.stage-tracker .chevron').each(function() {
       $(this).removeClass('open');
     });
     $(this).closest(".chevron").toggleClass('open');
   });
-  $('.covid-phases .chevron .description').click(function(e){
+  $('.stage-tracker .chevron .description').click(function(e){
     e.preventDefault();
     e.stopPropagation();
   });
   $(document).on('click', function (e) {
-    $('.covid-phases .chevron').each(function() {
+    $('.stage-tracker .chevron').each(function() {
       $(this).removeClass('open');
     });
   });
