7be390ffca2bc08b286c8a2598b2682bb62359ae
Karissa <karissa.demi@phila.gov>
2022-02-17 17:04:21 -0500

    Clean up for mobile

--- wp/wp-content/themes/phila.gov-theme/js/dev/chevrons.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/chevrons.js
@@ -1,19 +1,30 @@
 module.exports = $(function(){
-  $('.stage-tracker .chevron .bg-dark-gray').hover(function(e){
-    e.preventDefault();
-    e.stopPropagation();
+
+  $('.stage-container').hover(function(e){
+
     $('.stage-tracker .chevron').each(function() {
       $(this).removeClass('open');
     });
     $(this).closest(".chevron").toggleClass('open');
+
   });
   $('.stage-tracker .chevron .description').hover(function(e){
-    e.preventDefault();
     e.stopPropagation();
   });
 
-  $( '.stage-tracker .chevron .bg-dark-gray' ).on( "vmouseover", "p", function() {
+  var size = Foundation.MediaQuery.is('medium down');
+  if (size) {
+  $('.stage-container').click(function(e){
+
+    $('.stage-tracker .chevron').each(function() {
+      $(this).removeClass('open');
+    });
     $(this).closest(".chevron").toggleClass('open');
+    e.stopPropagation();
   });
+    $('.stage-tracker .chevron .description').click(function(e){
+      e.stopPropagation();
+    });
+  }
 
 });
