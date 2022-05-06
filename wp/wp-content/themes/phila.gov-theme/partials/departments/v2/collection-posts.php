b6ca1abfeac6a745fb0f03899fa31b8be9b53370
derrickdso <derrickjdieso@gmail.com>
2022-05-06 10:54:54 -0400

    collection page posts 4

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/collection-posts.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/collection-posts.php
@@ -17,13 +17,13 @@
       ?>
       <?php $collection_post_count++; ?>
       <?php if ($collection_post_count < 4) {  ?>
-        <?php if ( has_post_thumbnail() ) : ?>
-        <div class="columns medium-8 flex-child-shrink">
-          <?php echo phila_get_thumbnails(); ?>
-        </div>
-        <?php endif; ?>
-        <article id="post-<?php the_ID(); ?>" class="mbm columns medium-16">
-          <a href="<?php echo the_permalink(); ?>" class="card flex-container flex-dir-row full-height">
+        <article id="post-<?php the_ID(); ?>" class="mbm flex-container">
+          <?php if ( has_post_thumbnail() ) : ?>
+          <div class="columns medium-8">
+            <?php echo phila_get_thumbnails(); ?>
+          </div>
+          <?php endif; ?>
+          <a href="<?php echo the_permalink(); ?>" class="card flex-dir-row full-height columns medium-16">
             <div class="grid-x flex-dir-column">
               <div class=" card--content pam flex-child-auto">
                 <div class="cell align-self-top post-label">
