e8bcda94d839887649193e7ef26ba0cfce274f5a
IshKeyes <ishmael.williams-keyes@phila.gov>
2022-08-30 16:16:28 -0400

    small-24 change

--- wp/wp-content/plugins/phila.gov-customization/public/class-phila-gov-site-wide-alert-rendering.php
+++ wp/wp-content/plugins/phila.gov-customization/public/class-phila-gov-site-wide-alert-rendering.php
@@ -80,14 +80,14 @@ class Phila_Gov_Site_Wide_Alert_Rendering {
             <div class="row">
               <div class="medium centered">
                 <div class="grid-x grid-padding-x align-top pvs align-justify">
-                  <div class="cell medium-auto medium-shrink small-12 align-self-middle">
+                  <div class="cell medium-auto medium-shrink small-24 align-self-middle">
                     <i class="fas fa-check-to-slot fa-fw fa-2x icon hide-for-small-only" aria-hidden="true"></i>
                     <div class="voting-text">
                       <p class="mbn"><b>Election day is Nov. 8, 2022</b></p>
                       <p class="vote-deadline mbn">The deadline to register to vote is Oct. 24, 2022.</p>
                       </div>
                     </div>
-                    <div class="cell medium-auto medium-shrink small-12 align-self-right">
+                    <div class="cell medium-auto medium-shrink small-24 align-self-right">
                     <div class="vote-button">
                     <a class="button" href="https://vote.phila.gov/my-vote-my-way/"><b>MAKE A PLAN TO VOTE</b></a>
                   </div>
