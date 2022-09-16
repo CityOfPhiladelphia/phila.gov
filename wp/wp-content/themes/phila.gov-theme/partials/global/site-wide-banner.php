0769ea747a6b0d1014c45c6e5153f25a41822ede
IshKeyes <ishmael.williams-keyes@phila.gov>
2022-09-16 10:53:15 -0400

    site-wide-banner update

--- wp/wp-content/themes/phila.gov-theme/partials/global/site-wide-banner.php
+++ wp/wp-content/themes/phila.gov-theme/partials/global/site-wide-banner.php
@@ -1,7 +1,7 @@
 <?php $voting_banner_feature_flag = rwmb_meta( 'display_site_wide_banner', array( 'object_type' => 'setting' ), 'phila_settings' );
   if(isset($voting_banner_feature_flag) && $voting_banner_feature_flag != 0) {
 ?>
-<div class="voting-banner">
+<div class="site-wide-banner">
   <div class="row">
     <div class="medium centered">
       <div class="grid-x grid-padding-x align-top pvs align-justify">
@@ -9,13 +9,13 @@
           <i class="fas fa-check-to-slot fa-fw fa-2x icon hide-for-small-only" aria-hidden="true"></i>
         </div>
         <div class="cell auto message align-self-middle">
-          <div class="voting-text">
+          <div class="banner-text">
             <p class="mbn"><b>Election day is Nov. 8, 2022</b></p>
-            <p class="vote-deadline mbn">The deadline to register to vote is Oct. 24, 2022.</p>
+            <p class="site-wide-deadline mbn">The deadline to register to vote is Oct. 24, 2022.</p>
           </div>
         </div>
         <div class="cell medium-auto medium-shrink small-24 align-self-right">
-          <a class="vote-button button" href="https://vote.phila.gov/voting/my-vote-my-way/"><b>Make a plan to vote</b></a>
+          <a class="banner-button button" href="https://vote.phila.gov/voting/my-vote-my-way/"><b>Make a plan to vote</b></a>
         </div>
       </div>
     </div>
