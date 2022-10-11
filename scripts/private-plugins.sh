8c77b3efbf28cdc785f379f0f6f3c80b278bffbf
derrickdso <derrick.dieso@phila.gov>
2022-10-11 14:18:55 -0400

    composer test 4

--- scripts/private-plugins.sh
+++ scripts/private-plugins.sh
@@ -6,18 +6,7 @@ source /home/ubuntu/.ssh/environment
 # wps-hide-login.1.5.7.zip <- hide login plugin
 
 _dir="$(dirname "$0")"
-plugins="mb-admin-columns-1.6.2.zip
-mb-rest-api-1.5.0.zip
-mb-revision-1.3.3.zip
-mb-settings-page-2.1.7.zip
-mb-term-meta-1.2.10.zip
-meta-box-columns-1.2.15.zip
-meta-box-conditional-logic-1.6.17.zip
-meta-box-group-1.3.14.zip
-meta-box-include-exclude-1.0.11.zip
-meta-box-tabs-1.1.13.zip
-wp-nested-pages-phila-3.1.14.zip
-meta-box-tooltip-1.1.4.zip
+plugins="wp-nested-pages-phila-3.1.14.zip
 wpfront-user-role-editor-personal-pro-2.14.5.zip"
 
 cd /home/ubuntu/app
