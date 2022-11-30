4a7ba84be06e17802d1b22be5a11bbec595abe0b
derrickdso <derrick.dieso@phila.gov>
2022-11-30 15:47:21 -0500

    delete composer-managed plugins

--- wp/wp-content/plugins/amazon-s3-and-cloudfront/vendor/Aws3/Aws/S3/Exception/PermanentRedirectException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace DeliciousBrains\WP_Offload_Media\Aws3\Aws\S3\Exception;
-
-class PermanentRedirectException extends \DeliciousBrains\WP_Offload_Media\Aws3\Aws\S3\Exception\S3Exception
-{
-}
--- wp/wp-content/plugins/amazon-s3-and-cloudfront/vendor/Gcp/google/cloud-core/system-bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-use DeliciousBrains\WP_Offload_Media\Gcp\Google\Cloud\Core\Testing\TestHelpers;
-\DeliciousBrains\WP_Offload_Media\Gcp\Google\Cloud\Core\Testing\TestHelpers::systemBootstrap();
