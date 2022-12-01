b0c86ad41967eb069196ba90e35963070168d11c
derrickdso <derrick.dieso@phila.gov>
2022-12-01 12:15:44 -0500

    composer phl-aqi

--- scripts/composer.sh
+++ scripts/composer.sh
@@ -16,10 +16,15 @@ echo '
     {
       "type": "vcs",
       "url": "https://github.com/CityOfPhiladelphia/phila.gov-customization.git"
+    },
+    {
+      "type": "vcs",
+      "url": "https://github.com/CityOfPhiladelphia/phl-aqi-plugin.git"
     }
   ],
   "require": {
     "cityofphiladelphia/phila.gov-customization": "dev-main",
+    "cityofphiladelphia/phl-aqi": "dev-main",
     "meta-box/mb-admin-columns":"dev-master",
     "meta-box/mb-revision":"dev-master",
     "meta-box/mb-settings-page":"dev-master",
@@ -33,17 +38,17 @@ echo '
     "wpackagist-plugin/better-search-replace":"1.4.1",
     "wpackagist-plugin/disable-gutenberg":"2.8.1",
     "wpackagist-plugin/jwt-auth":"2.1.3",
-    "wpackagist-plugin/admin-email-as-from-address":"^1.2",
     "wpackagist-plugin/gathercontent-import":"3.2.12",
+    "wpackagist-plugin/easy-wp-smtp":"^1.5.2",
+    "wpackagist-plugin/admin-email-as-from-address":"^1.2",
     "wpackagist-plugin/jwt-authentication-for-wp-rest-api":"1.3.2",
     "wpackagist-plugin/meta-box":"^5.6.5",
-    "wpackagist-plugin/meta-box-text-limiter":"^1.1.3",
-    "wpackagist-plugin/mb-relationships":"^1.10.11",
-    "wpackagist-plugin/easy-wp-smtp":"^1.5.2",
     "wpackagist-plugin/mb-rest-api":"^1.4.1",
+    "wpackagist-plugin/mb-relationships":"^1.10.11",
+    "wpackagist-plugin/meta-box-text-limiter":"^1.1.3",
     "wpackagist-plugin/miniorange-saml-20-single-sign-on":"^4.9.29",
-    "wpackagist-plugin/amazon-s3-and-cloudfront":"^2.6.2",
     "wpackagist-plugin/reusable-text-blocks":"^1.5.3",
+    "wpackagist-plugin/amazon-s3-and-cloudfront":"^2.6.2",
     "wpackagist-plugin/classic-editor":"^1.6.2",
     "wpackagist-plugin/wp-rest-api-v2-menus":"^0.10",
     "wpackagist-plugin/wordpress-importer":"^0.8",
