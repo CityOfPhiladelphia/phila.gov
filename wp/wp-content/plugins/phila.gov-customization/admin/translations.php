4bfc657bc6e77e42bf3df3fdd505850dcc18168c
Khristian Marcial <khristianmarcial@gmail.com>
2023-01-05 13:51:28 -0500

    adds dept billing code and send to translation boolean to publish

--- wp/wp-content/plugins/phila.gov-customization/admin/translations.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/translations.php
@@ -69,7 +69,7 @@ function publish_translated_post($new_status, $old_status, $post) {
   //TODO: make billing from settings page a fallback - pull data from individual page settings
   //example payload: { "page_slug": "services/culture-recreation", "department_code":"1 - ABC" }
   $webhook = $endpoint;
-  if( 'publish' === $new_status  && $send_to_translation == true) {
+  if( 'publish' === $new_status && $send_to_translation == true) {
     if(isset( $post->post_type )) {
       $post_type = $post->post_type;
     }
