2248243243401d31c13709a778d2b65fb51be9f0
derrickdso <derrick.dieso@phila.gov>
2023-03-21 11:00:22 -0400

    set lang on lang selector 2

--- wp/wp-content/themes/phila.gov-theme/partials/global/translations-modal.php
+++ wp/wp-content/themes/phila.gov-theme/partials/global/translations-modal.php
@@ -1,7 +1,11 @@
 <div class="reveal center phila-modal" id="translations-modal" data-reveal data-deep-link="true" data-options="closeOnClick:false; closeOnEsc:false;">
+
+  <h3 class="ptl"><i class="dark-ben-franklin fas fa-earth-americas prs"></i>Translate</h3>
   <div class="content">
     <div>It looks like your device language is set to <span id="translations-modal-lang"></span>. Would you like to translate this page?</div>
-    <button class="button-text" aria-label="Close modal" type="button" data-close>Cancel</button>
-    <button id="translate-page" class="button-text" aria-label="Close modal" type="button" data-close>Translate</button>
+  </div>
+  <div class="button-container pam">
+    <button class="translate-close button-text mrl" aria-label="Close modal" type="button" data-close>Cancel</button>
+    <button id="translate-page" class="translate-button button-text" aria-label="Translate page" type="button">Translate</button>
   </div>
 </div>
