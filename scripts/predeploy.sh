a140713c3c8cee743aa3865f43766037b68d4aba
derrickdso <derrick.dieso@phila.gov>
2023-04-03 15:36:31 -0400

    cleaning scripts

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -4,4 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
 sudo apt-get -yq upgrade
+sudo apt-get autoremove
+sudo du -sh /var/cache/apt
+sudo apt-get autoclean
+sudo rm -r /var/log/*
 source /home/ubuntu/.bashrc
