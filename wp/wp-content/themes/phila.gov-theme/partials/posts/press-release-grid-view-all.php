9d7133d154e314eeb589f44594f0f5f907c67648
Moh Atia <48361663+m-atia@users.noreply.github.com>
2023-05-04 14:51:03 -0400

    update links to the latest archives to no longer have the # in them

--- wp/wp-content/themes/phila.gov-theme/partials/posts/press-release-grid-view-all.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/press-release-grid-view-all.php
@@ -1,14 +1,14 @@
 <?php if ($count == 4) : ?>
   <?php if ($user_selected_template == 'custom_content' || $post_type_parent == 'guides'): ?>
     <?php $see_all = array(
-    'URL' => '/the-latest/archives/#/?templates=press_release&department=' . $slang_name,
+    'URL' => '/the-latest/archives/?templates=press_release&department=' . $slang_name,
     'content_type' => 'press_release',
     'nice_name' => 'Press releases',
     'is_full' => false
   ); ?>
   <?php else: ?>
     <?php $see_all = array(
-    'URL' => '/the-latest/archives/#/?templates=press_release&department=' . $slang_name,
+    'URL' => '/the-latest/archives/?templates=press_release&department=' . $slang_name,
     'content_type' => 'press_release',
     'nice_name' => 'Press releases',
     'is_full' => true
@@ -21,7 +21,7 @@
       $term = get_term($tag[0], 'post_tag');
     }
     $see_all_URL = array(
-      'URL' => '/the-latest/archives/#/?tag=' . $term->name,
+      'URL' => '/the-latest/archives/?tag=' . $term->name,
     );
     $see_all = array_replace($see_all, $see_all_URL );
     endif;?>
