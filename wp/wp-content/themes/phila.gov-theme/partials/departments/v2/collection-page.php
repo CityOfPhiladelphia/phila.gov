18a008e5cfeda5aae4c5cfac02cdf2e27dea1141
Ritika Desai <ritikahdesai186@gmail.com>
2023-08-24 14:06:19 -0400

    fixed the program cards to show the external link icon near the title

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/collection-page.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/collection-page.php
@@ -95,7 +95,7 @@ $row_content = rwmb_meta('collection_row');?>
                     $img = reset( $img );?>
                     <img src="<?php echo $img['url'] ?>" alt="<?php echo $img['alt']?>">
                     <div class="content-block">
-                        <h3 class="h3"><?php echo get_the_title($program_page); ?></h3>
+                        <h4 class="h3 <?php echo !empty($off_site) ? 'external' : ''; ?>"><?php echo get_the_title($program_page); ?></h4>
                       <?php echo rwmb_meta( 'phila_meta_desc', $args = '', $program_page ); ?></h4>
                     </div>
                   </a>
