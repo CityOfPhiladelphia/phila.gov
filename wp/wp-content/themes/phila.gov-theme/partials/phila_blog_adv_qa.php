1ffd038db59e1a69987e86f1f1701df6738cc1c8
Ritika Desai <ritikahdesai186@gmail.com>
2023-10-31 14:23:48 -0400

    code clean up

--- wp/wp-content/themes/phila.gov-theme/partials/phila_blog_adv_qa.php
+++ wp/wp-content/themes/phila.gov-theme/partials/phila_blog_adv_qa.php
@@ -2,28 +2,28 @@
 /*
 Partial for Advanced Blog Posts Q&A Component
 */
-
+?>
+<div class="mvl">
+<?php
 if ($qna['phila_qna_title'] != "") {
 ?>
-<div class = "mvl">
     <h2><?php echo $qna['phila_qna_title'] ?></h2>
     <?php }
-if ($qna['phila_qna_style'] == 'name') {
-    foreach ($qna['phila_qna_person_repeater'] as $qa) { ?>
-        <strong>
-            <p><?php echo $qa['phila_qna_question_person'] ?> (Name): <?php echo $qa['phila_qna_question'] ?></p>
-        </strong>
-        <p><strong><?php echo $qa['phila_qna_answer_person'] ?> (Name): </strong><?php echo $qa['phila_qna_answer'] ?></p>
-    </div>
-    <?php }
-} elseif ($qna['phila_qna_style'] == 'qa') {
-    foreach ($qna['phila_qna_repeater'] as $qa) { ?>
-        <strong>
-            <p>Q: <?php echo $qa['phila_qna_question'] ?></p>
-        </strong>
-        <p><strong>A: </strong><?php echo $qa['phila_qna_answer'] ?></p>
-    </div>
-<?php
-    }
-}
-?>
+    if ($qna['phila_qna_style'] == 'name') {
+        foreach ($qna['phila_qna_person_repeater'] as $qa) { ?>
+            <strong>
+                <p><?php echo $qa['phila_qna_question_person'] ?> (Name): <?php echo $qa['phila_qna_question'] ?></p>
+            </strong>
+            <p><strong><?php echo $qa['phila_qna_answer_person'] ?> (Name): </strong><?php echo $qa['phila_qna_answer'] ?></p>
+        <?php }
+    } elseif ($qna['phila_qna_style'] == 'qa') {
+        foreach ($qna['phila_qna_repeater'] as $qa) { ?>
+            <strong>
+                <p>Q: <?php echo $qa['phila_qna_question'] ?></p>
+            </strong>
+            <p><strong>A: </strong><?php echo $qa['phila_qna_answer'] ?></p>
+        <?php
+        }
+    } 
+    ?>
+</div>    
