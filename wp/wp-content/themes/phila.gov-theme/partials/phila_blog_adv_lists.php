a032c5d1fe7910bd470e7308be0546718336e738
Ritika Desai <ritikahdesai186@gmail.com>
2023-10-31 14:23:48 -0400

    code clean up

--- wp/wp-content/themes/phila.gov-theme/partials/phila_blog_adv_lists.php
+++ wp/wp-content/themes/phila.gov-theme/partials/phila_blog_adv_lists.php
@@ -4,26 +4,22 @@ Partial for Advanced Blog Posts Lists Component
 */
 ?>
 
+<div class = "mvl">
 <?php if ($list['phila_list_type'] == "unordered") { ?>
-    <div class = "mvl">
     <<?php echo $list['phila_list_title_style'] ?>><?php echo $list['phila_list_builder_title']; ?></<?php echo $list['phila_list_title_style'] ?>>
     <ul style="padding-left: 0">
         <?php foreach ($list['phila_unordered_list_fields'] as $item) { ?>
             <li><?php echo $item["phila_unordered_list_item"] ?></li>
         <?php } ?>
     </ul>
-    </div>
 <?php } elseif ($list['phila_list_type'] == "ordered") { ?>
-    <div class = "mvl">
     <<?php echo $list['phila_list_title_style'] ?>><?php echo $list['phila_list_builder_title']; ?></<?php echo $list['phila_list_title_style'] ?>>
     <ol style="padding-left: 0">
         <?php foreach ($list['phila_ordered_list_fields'] as $item) { ?>
             <li><?php echo $item["phila_ordered_list_item"] ?></li>
         <?php } ?>
     </ol>
-    </div>
 <?php } elseif ($list['phila_list_type'] == "unordered_with_paragraph") { ?>
-    <div class = "mvl">
     <<?php echo $list['phila_list_title_style'] ?>><?php echo $list['phila_list_builder_title']; ?></<?php echo $list['phila_list_title_style'] ?>>
     <ul style="padding-left: 0">
         <?php foreach ($list['phila_unordered_with_paragraph_fields'] as $item) { ?>
@@ -33,9 +29,7 @@ Partial for Advanced Blog Posts Lists Component
             <p><?php echo $item["phila_paragraph"] ?></p>
         <?php } ?>
     </ul>
-    </div>
 <?php } elseif ($list['phila_list_type'] == "ordered_with_paragraph") { ?>
-    <div class = "mvl">
     <<?php echo $list['phila_list_title_style'] ?>><?php echo $list['phila_list_builder_title']; ?></<?php echo $list['phila_list_title_style'] ?>>
     <ol style="padding-left: 0">
         <?php foreach ($list['phila_ordered_with_paragraph_fields'] as $item) { ?>
@@ -43,14 +37,12 @@ Partial for Advanced Blog Posts Lists Component
             <p><?php echo $item["phila_paragraph"] ?></p>
         <?php } ?>
     </ol>
-    <div class = "mvl">
 <?php } elseif ($list['phila_list_type'] == "check_list") { ?>
-    <div class = "mvl">
     <<?php echo $list['phila_list_title_style'] ?>><?php echo $list['phila_list_builder_title']; ?></<?php echo $list['phila_list_title_style'] ?>>
     <ul style="list-style: none; padding-left: 0">
         <?php foreach ($list['phila_check_list_fields'] as $item) { ?>
             <li><i style="padding-right: 8px;" class="<?php echo $list['phila_icon_fields']['phila_check_list_icon'] ?? "fas fa-check" ?>"></i><?php echo $item['phila_check_list_item'] ?></li>
         <?php } ?>
     </ul>
+    <?php } ?>
 </div>
-<?php } ?>
