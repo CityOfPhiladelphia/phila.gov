852e3951c8c201dc6fe0885e0828ebbc8c0e08c0
IshKeyes <ishmael.williams-keyes@phila.gov>
2023-11-10 08:49:43 -0500

    resource list switch function fixed2

--- wp/wp-content/themes/phila.gov-theme/inc/resource-list-switch.php
+++ wp/wp-content/themes/phila.gov-theme/inc/resource-list-switch.php
@@ -1,12 +1,12 @@
 <?php
 function phila_resource_list_switch($item_resource_type) {
-    if ($item_resource_type === 'document') {
+    if ($item_resource_type === 'document' || $item_resource_type === 'phila_resource_document') {
         return 'fas fa-file-alt';
-    } elseif ($item_resource_type === 'map') {
+    } elseif ($item_resource_type === 'map' || $item_resource_type === 'phila_resource_map') {
         return 'fas fa-map-marker-alt';
-    } elseif ($item_resource_type === 'link') {
+    } elseif ($item_resource_type === 'link' || $item_resource_type === 'phila_resource_link') {
         return 'far fa-link';
-    } elseif ($item_resource_type === 'video') {
+    } elseif ($item_resource_type === 'video' || $item_resource_type === 'phila_resource_video') {
         return 'fas fa-video';
     } else {
         return 'fas fa-file-alt';
