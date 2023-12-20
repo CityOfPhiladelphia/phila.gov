b37e286c923f53e7f11e776020105784754054b7
IshKeyes <ishmael.williams-keyes@phila.gov>
2023-12-20 13:59:09 -0500

    no translate attribute added

--- wp/wp-content/themes/phila.gov-theme/partials/departments/phila_staff_directory_loop.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/phila_staff_directory_loop.php
@@ -77,7 +77,7 @@ if ( $staff_member_loop->have_posts() ):
 
       $staff_summary = isset($staff_options['phila_summary']) ? wpautop($staff_options['phila_summary']) : '';
 
-      $staff_leadership_output .= '<div translate="no" class="row staff-highlight '.$staff_first_name.'-'.$staff_last_name.'-card">';
+      $staff_leadership_output .= '<div class="row staff-highlight '.$staff_first_name.'-'.$staff_last_name.'-card">';
       // Leadership Thumbnail
       if ( has_post_thumbnail() ):
         $staff_photo = get_the_post_thumbnail( $post->ID, 'staff-thumb', 'class= staff-thumbnail' );
@@ -87,7 +87,7 @@ if ( $staff_member_loop->have_posts() ):
       // Leadership Contact Info
       $staff_leadership_output .= '<div class="small-24 medium-6 columns staff-contact">';
 
-      $staff_leadership_output .= '<div translate="no" class="name">';
+      $staff_leadership_output .= '<div class="name">';
       $staff_leadership_output .= $staff_member_name_output;
       $staff_leadership_output .= '</div>';
 
@@ -127,8 +127,8 @@ if ( $staff_member_loop->have_posts() ):
       $staff_leadership_array[$staff_display_order] = $staff_leadership_output;
 
     else:
-      $staff_table_output .= '<tr class="'.$staff_first_name.'-'.$staff_last_name.'-row">
-        <td class="name"><span class="list-name">' . $staff_member_name_output . '</span></td>
+      $staff_table_output .= '<tr translate="no" class="'.$staff_first_name.'-'.$staff_last_name.'-row">
+        <td translate="no" class="name"><span class="list-name">' . $staff_member_name_output . '</span></td>
         <td class="title">' . $staff_title . '<br><span class="staff-unit">' . urldecode( $staff_unit ). '</span></td>';
         if (!empty($staff_email)) :
         $staff_table_output .= '<td translate="no" class="email"><a href="mailto:' . $staff_email . '">' . $staff_email . '</a></td>';
