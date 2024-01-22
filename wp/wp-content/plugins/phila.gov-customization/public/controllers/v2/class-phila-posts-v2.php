66486c40b7514776c3cd21d12976d81b3a084b71
Moh Atia <mohamed.atia@phila.gov>
2024-01-22 09:42:49 -0500

    testing archive

--- wp/wp-content/plugins/phila.gov-customization/public/controllers/v2/class-phila-posts-v2.php
+++ wp/wp-content/plugins/phila.gov-customization/public/controllers/v2/class-phila-posts-v2.php
@@ -17,6 +17,7 @@ function get_phila_template( $post ) {
 }
 
 function get_archive_status( $post ) {
+  var_dump(phila_get_archive_status($post['id']));
   return phila_get_archive_status($post['id']);
 }
 
@@ -38,25 +39,80 @@ function get_phila_featured_media ( $post ) {
 
 }
 
-function filter_post_by_archived( $args, $request ) {
-  $archived = $request->get_param( 'archived' );
 
-  if ( empty( $archived )) {
-      return $args;
+
+
+function filter_post_by_archived($args, $request) {
+
+  $archived = $request->get_param('archived');
+
+  $two_years_ago = date('Y-m-d\TH:i:s', strtotime('-2 years'));
+  // $now = current_time('timestamp');
+  // $archived = filter_var($archived, FILTER_VALIDATE_BOOLEAN);
+
+  // if ($archived == 'true') {
+  //   $args['meta_query'][] = array(
+  //     // 'relation' => 'OR',
+  //     // array(
+  //     //     'key'     => 'phila_archive_post',
+  //     //     'value'   => 'archive_now',
+  //     //     'compare' => '=',
+  //     // ),
+  //     // array(
+  //     //   'relation' => 'AND',
+  //     //   array(
+  //     //       'key'     => 'phila_archive_post',
+  //     //       'value'   => 'default',
+  //     //       'compare' => '=',
+  //     //   ),
+  //       // array(
+  //       //   'before' => $two_years_ago,
+  //       //   'inclusive' => true,
+  //       // ),
+  //       // array(
+  //       //   'key'     => 'modified',
+  //       //   'value'   =>  $two_years_ago,
+  //       //   'compare' => '<',
+  //       //   'type'    => 'DATETIME',
+  //       // )
+  //     // ),
+  //     // array(
+  //     //   'key'     => 'phila_archive_post',
+  //     //   'value'   => '',
+  //     //   'compare' => '=',
+  //     // ),
+  //   );
+  // } else if ($archived == 'false') {
+  //    $args['meta_query'][] = array(
+  //     'relation' => 'OR',
+  //     array(
+  //         'key'     => 'phila_archive_post',
+  //         'value'   => 'do_not_archive',
+  //         'compare' => '=',
+  //     ),
+  //     array(
+  //       'relation' => 'AND',
+  //       array(
+  //           'key'     => 'phila_archive_post',
+  //           'value'   => 'default',
+  //           'compare' => '=',
+  //       ),
+  //       array(
+  //         'after' => $two_years_ago,
+  //         'inclusive' => true,
+  //       ),
+  //     ),
+  //   );
+  // }
+
+  if ($archived == 'true') {
+    $args['date_query'][] = array(
+      'before' => $two_years_ago,
+      'inclusive' => true,
+    );
   }
 
-  if ( $archived === 'true' ) {
-    $archived = 'archive_now';
-  } else if ( $archived === 'false' ){
-    $archived = 'default';
-  }
-
-  $args['meta_query'][] = array(
-    'key'     => 'phila_archive_post',
-    'value'   => $archived,
-    'compare' => '=',
-  );
-
+  // var_dump($args);
   return $args;
 }
 
@@ -114,3 +170,22 @@ function filter_post_by_template($args, $request) {
 
   return $args;
 }
+
+// function filter_post_by_archived($args, $request){
+//   $archived = $request->get_param('archived');
+
+//   if ( empty( $archived )) {
+//     return $args;
+// }
+
+// $archived = filter_var($archived, FILTER_VALIDATE_BOOLEAN);
+
+// $args['meta_query'][] = array(
+//   'key'     => 'is_archived',
+//   'value'   => $archived,
+//   'compare' => '=',
+//   'type'    => 'BOOLEAN',
+// );
+
+// return $args;
+// }
