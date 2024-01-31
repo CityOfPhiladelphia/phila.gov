689f1342e9e7f58bb303ea315a873f2226b045b7
IshKeyes <ishmael.williams-keyes@phila.gov>
2024-01-31 09:45:25 -0500

    update conditional

--- wp/wp-content/themes/phila.gov-theme/partials/resource-list.php
+++ wp/wp-content/themes/phila.gov-theme/partials/resource-list.php
@@ -128,7 +128,7 @@
                 <?php if (isset($resource_list_title)) { ?>
                   <div class="small-24 medium-24">
                     <header>
-                    <h2 id="<?php echo $resource_list_slug ?>" class="h4"><?php echo $resource_list_title ?></h2>
+                      <h2 id="<?php echo $resource_list_slug ?>" class="<?php if (!empty($featured_resources) || $user_selected_template == 'collection_page_v2') { echo 'h4'; } else { echo 'h3'; } ?>"><?php echo $resource_list_title ?></h2>
                     </header>
                   </div>
                 <?php } ?>
