4b2a1d131edfc65a1c121db5b772a1745449c044
Moh Atia <mohamed.atia@phila.gov>
2024-01-31 10:51:56 -0500

    fixing anchor links on obile

--- wp/wp-content/themes/phila.gov-theme/js/dev/hashchange-check.js
+++ wp/wp-content/themes/phila.gov-theme/js/dev/hashchange-check.js
@@ -1,11 +1,20 @@
 function anchorOffset() {
-  /* Account for sticky header when anchor links are present */
   var $anchor = $(':target');
   var fixedElementHeight = $('.sticky').outerHeight();
+  console.log("anchoring offset");
+  console.log('$anchor.length:', $anchor.length);
+  console.log('$anchor.offset().top:', $anchor.offset().top);
+  console.log('fixedElementHeight:', fixedElementHeight);
+
   if ($anchor.length > 0)
     window.scrollTo(0, $anchor.offset().top - fixedElementHeight);
 }
-$(window).on('hashchange load', anchorOffset);
+
+// $(window).on('hashchange load', anchorOffset());
+$(window).on('hashchange load', setTimeout(() => {
+  anchorOffset();
+}, 250));
+
 $('body').on('click', "a[href^='#']", function (ev) {
   if (window.location.hash === $(this).attr('href')) {
     ev.preventDefault();
