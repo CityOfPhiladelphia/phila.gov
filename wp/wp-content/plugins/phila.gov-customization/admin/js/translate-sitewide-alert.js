6f0b98ecd872b61bdb2e4be6afc24317672b6250
Ritika Desai <ritikahdesai186@gmail.com>
2024-06-27 16:20:02 -0400

    typo fix

--- wp/wp-content/plugins/phila.gov-customization/admin/js/translate-sitewide-alert.js
+++ wp/wp-content/plugins/phila.gov-customization/admin/js/translate-sitewide-alert.js
@@ -1,6 +1,6 @@
 jQuery( function( $ ) {
 
-    $( '.translate-sitewide-alerts' ).on( 'click', function() {
+    $( '.translate-sitewide-alert' ).on( 'click', function() {
     $.post(window.phila_sitewide_alert_js_vars.update_translations_webhook, JSON.stringify({page_slug: 'site_wide_alert' ,
 department_code: phila_sitewide_alert_js_vars.update_translations_dept_billing_code})
         );
