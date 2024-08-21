cd8151d7b9abfdd64bf637c8fd3058887a6470fd
Ritika Desai <ritikahdesai186@gmail.com>
2024-08-21 11:55:26 -0400

    added creative philadelphia to list of headings

--- wp/wp-content/themes/phila.gov-theme/functions.php
+++ wp/wp-content/themes/phila.gov-theme/functions.php
@@ -1693,6 +1693,7 @@ function get_target_phrases() {
     "Mayor's Commission on",
     "Mayor's Office of",
     "Mural Arts Philadelphia",
+    "Creative Philadelphia",
     "Philadelphia",
     "Commission on",
     "Zoning Board of",
