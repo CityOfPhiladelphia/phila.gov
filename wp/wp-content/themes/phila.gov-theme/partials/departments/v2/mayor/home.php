6a70a506727aec1d2f251c47af31f1549437d6da
Karissa Demi <karissa.demi@phila.gov>
2024-11-08 20:01:15 -0500

    add header

--- wp/wp-content/themes/phila.gov-theme/partials/departments/v2/mayor/home.php
+++ wp/wp-content/themes/phila.gov-theme/partials/departments/v2/mayor/home.php
@@ -1,24 +1,32 @@
 <?php
   /* breaking all the rules */
+
+  $images = rwmb_meta( 'phila_v2_homepage_hero', ['size' => 'full'] );
+    function extract_full_url($array) {
+      foreach ($array as $key => $value) {
+        if (is_array($value) && array_key_exists("full_url", $value)) {
+          return $value["full_url"];
+        }
+      }
+    }
+
+    $full_url = extract_full_url($images);
 ?>
 <div class="mayor-page">
-
 <header>
-    <div class="hero-content">
-        <div class="hero-subpage">
-            <div class="row expanded ">
-              <div class="medium-18 small-centered columns text-overlay">
-                <h1 class="page-title">Office of the <span class="MAYOR">Mayor</span></h1>
-
-                    <div class="row">
-                      <div class="medium-16 small-centered columns text-overlay">
-                        <p class="sub-title mbn-mu"><strong><?php echo phila_get_item_meta_desc( ); ?></strong></p>
-                      </div>
-                    </div>
-
+  <div class="hero-content">
+    <div class="hero-subpage">
+      <div class="grid-x expanded">
+        <div class="cell medium-8">
+          <div class="image-offset">
+            <img src="<?php echo $full_url ?>" alt="Mayor Cherelle L. Parker" class="one-philly-mayor" />
+          </div>
+        </div>
+          <div class="cell medium-10 align-self-middle mr-meeseeks">
+            <h1>Office of the <span class="MAYOR">Mayor</span></h1>
+                <div class="tagline"><?php echo phila_get_item_meta_desc( ); ?></div>
               </div>
               </div>
-
             </div><!-- END .row.expanded   -->
             <?php
 
@@ -34,5 +42,3 @@
 
     </div> <!-- END .hero-wrap  -->
 </header>
-
-<div class="test">great</div>
