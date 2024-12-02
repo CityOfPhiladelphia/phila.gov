caefa20eb635b06cec75bfb323468a903345d894
Ritika Desai <ritikahdesai186@gmail.com>
2024-12-02 21:14:41 +0530

    added status check

--- wp/wp-content/themes/phila.gov-theme/partials/posts/trashday-alerts.php
+++ wp/wp-content/themes/phila.gov-theme/partials/posts/trashday-alerts.php
@@ -51,7 +51,7 @@ foreach ( $holidays as $holiday ) {
         <th class="phl-mu <?php if ( !phila_is_department_homepage( $post ) && !is_home() && $a['icon_padding'] == 0 ) echo 'icon-only';?>">
           <i class="fa-solid fa-xl service-icon service-icon <?php if( $a['icon_padding'] && $a['icon_padding'] == 1) echo 'plm-mu' ?> 
           <?php 
-            if ( $status == 1 || $status == 2 || $is_holiday == true)
+            if ( $status == 1 || $status == 2 || $status == 3 || $is_holiday == true )
             { echo "fa-circle-exclamation"; } 
           else if ( $status == 0) 
             { echo "fa-circle-check"; } 
