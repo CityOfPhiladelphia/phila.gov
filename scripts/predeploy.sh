ea0bc23f84d9dacf2b5b00b9583e16b51009922f
Jake Rosenberg <Jake.Rosenberg@phila.gov>
2025-03-26 11:17:43 -0400

    added removal commands for duplicative files living on the AMI, added more memory to WP for speed

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -4,4 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
 sudo apt-get -yq upgrade
+
+sudo rm -rf home/ubuntu/app/wp/wp-content/themes/phila.gov-theme/
+sudo rm -rf home/ubuntu/app/wp/wp-content/plugins/
+sudo rm -f home/ubuntu/app/wp/wp-config.php
+
 source /home/ubuntu/.bashrc
