b00d7769eb3827e5b43cdd9072c7bd69d0061eff
Jake Rosenberg <Jake.Rosenberg@phila.gov>
2025-03-26 12:48:08 -0400

    removed command to delete themes

--- scripts/predeploy.sh
+++ scripts/predeploy.sh
@@ -5,7 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get -yq upgrade
 
-sudo rm -rf /home/ubuntu/app/wp/wp-content/themes/phila.gov-theme/
 sudo rm -rf /home/ubuntu/app/wp/wp-content/plugins/
 
 source /home/ubuntu/.bashrc
