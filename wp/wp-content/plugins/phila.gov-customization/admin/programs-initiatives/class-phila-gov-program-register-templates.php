90a36a27d430e62ee21b3a7b07f3e1582d482e84
Courtney Christian <courtneychristian@copadmins-MacBook-Pro.local>
2026-09-04 13:57:16 -0400

    added meta boxes to new page template and renamed to remove confusion

--- wp/wp-content/plugins/phila.gov-customization/admin/programs-initiatives/class-phila-gov-program-register-templates.php
+++ wp/wp-content/plugins/phila.gov-customization/admin/programs-initiatives/class-phila-gov-program-register-templates.php
@@ -35,7 +35,7 @@ class Phila_Gov_Register_Program_Templates {
 
           'options' => array(
             'prog_landing_page'  => 'Homepage',
-            'homepage_v2' => 'Homepage v2 (full hero)',
+            'prog_homepage_v2' => 'Homepage v2 (full hero)',
             'phila_one_quarter'    => '1/4 Headings (subpage)',
             'collection_page_v2'   => 'Collection page',
             'covid_guidance'   =>  'Covid guidance',
@@ -212,7 +212,8 @@ class Phila_Gov_Register_Program_Templates {
       'visible' => array(
         'when'  => array(
           array( 'phila_template_select', '=', 'prog_association' ),
-          array( 'service_template_selection', '=', 'custom_content' )
+          array( 'service_template_selection', '=', 'custom_content' ),
+          array('phila_template_select', '=', 'prog_homepage_v2')
         ),
         'relation' => 'or',
       ),
